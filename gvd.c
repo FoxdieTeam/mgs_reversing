36090b953d96bc3518f7960cde4e602c4d7454e4
Paul <paul@paulsapps.com>
2019-11-10 18:44:18 +0000

    missed file from last commit

--- gvd.c
+++ gvd.c
@@ -1,10 +1,39 @@
 #include <libsn.h>
 #include "actor.h"
 
-struct Actor;
+int SECTION(".sbss") dword_800AB918;
+int SECTION(".sbss") dword_800AB91C;
+int SECTION(".sbss") dword_800AB920;
+int SECTION(".sbss") dword_800AB924;
+
+extern const char aBislpm86247[];
+
+int SECTION(".sdata") dword_800AB2E4 = 0x200000; // ram size
+int SECTION(".sdata") dword_800AB2E8 = 0x8000; // stack size
+const char* SECTION(".sdata") off_800AB2EC = aBislpm86247;
+const char SECTION(".sdata") aMem[] = "mem:";
+const char SECTION(".sdata") aPad[] = "pad:";
+//                db 0, 0, 0
+const char SECTION(".sdata") aGv[] = "gv:";
+const char SECTION(".sdata") aFs[] = "fs:";
+const char SECTION(".sdata") aDg[] = "dg:";
+const char SECTION(".sdata") aGcl[] = "gcl:";
+//  db 0, 0, 0
+const char SECTION(".sdata") aHzd[] = "hzd:";
+//                 db 0, 0, 0
+const char SECTION(".sdata") aSound[] = "sound:";
+//  db 0
+const char SECTION(".sdata") aGm[] = "gm:";
+const char SECTION(".sdata") aStart[] = "start\n";
+//                 db 0
+int SECTION(".sdata") dword_800AB330 = 0;
+
+extern const char aGvdC[];
+
+//   db 0, 0
+//const char SECTION(".sdata") asc_800AB340[] = "#";
 
 extern void GV_Act_80014b60(struct Actor*);
-extern const char aGvdC[];
 
 extern void sub_800163b0(void);
 extern void sub_80015434(void);
@@ -16,7 +45,6 @@ extern void sub_80014b34(void);
 
 struct Actor SECTION(".gGVActor_800acbf8") gGVActor_800acbf8;
 
-/*
 void SECTION(".0x80014d18") GV_StartDaemon_80014d18(void)
 {
 	GV_ActorList_Init_80014d98();
@@ -26,8 +54,8 @@ void SECTION(".0x80014d18") GV_StartDaemon_80014d18(void)
 	sub_80014cc8();
 	GV_ActorPushBack_800150a8(0, &gGVActor_800acbf8, 0);
 	GV_ActorInit_8001514c(&gGVActor_800acbf8, GV_Act_80014b60, 0, aGvdC);
-	dword_800AB928 = 0;
-	dword_800AB9B0 = 0;
+	dword_800AB920 = 0;
+	dword_800AB330 = 0;
 	sub_800892a8(sub_80014b34);
 }
-*/
+
