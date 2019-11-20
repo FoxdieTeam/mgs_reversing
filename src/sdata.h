46712790b8b9ef5e8357123d837a1a0ec845a59f
V <varrok@gmail.com>
2019-11-21 00:12:06 +0100

    First batch.

---
+++ src/sdata.h
@@ -0,0 +1,21 @@
+extern const char* aCigs[];
+extern const char* aSocom[];
+extern const char* aLife[];
+extern const char* aO2[];
+extern const char* aError[];
+extern const char* aComplete[];
+
+
+extern const char* dword_800AB728[];
+extern const char* dword_800AB724[];
+extern const char* dword_800AB720[];
+extern const char* aEx[];
+extern const char* aMemoryCard1[];
+extern const char* aMemoryCard2[];
+extern const char* dword_800AB754[];
+extern const char* aNo[];
+extern const char* dword_800122D8[];
+extern const char* dword_800122CC[];
+extern const char* dword_800122C0[];
+extern const char* dword_800122B4[];
+extern const char* dword_800122A8[];
