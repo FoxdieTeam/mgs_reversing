5918a9500256cfce1888921f8dc872464e57b493
Paul <github@paulsapps.com>
2019-11-24 23:43:22 +0000

    it appears mts was probably built with another compiler

--- src/mts_new.c
+++ src/mts_new.c
@@ -3,7 +3,26 @@
 
 extern int dword_800A3D94;
 
-extern void mts_start_8008AAEC(int taskNum, void (*pTaskFn)(void), void* pStack);
+void mts_start_8008AAEC(int taskNum, void (*pTaskFn)(void), void* pStack);
+
+/*
+extern unsigned int gMtsVSyncCount_800A3D78;
+
+void sub_800893E8(void);
+
+int VSync_80098108(int);
+void VSyncCallback_800983A8(void (*func)());
+
+
+void mts_init_vsync_800895AC(void)
+{
+    if (gMtsVSyncCount_800A3D78 == 0xffffffff)
+    {
+        gMtsVSyncCount_800A3D78 =  VSync_80098108(0xffffffff);
+        VSyncCallback_800983A8(sub_800893E8);
+    }
+}
+*/
 
 /*
 void mts_boot_task_8008AAC4(int taskNum, void (*pTaskFn)(void), void* pStack, long stackSize)
