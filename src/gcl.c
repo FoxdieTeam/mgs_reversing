05321c3651085f75fecd4b3d395ba8e68e5004a9
Paul <github@paulsapps.com>
2019-11-25 21:44:11 +0000

    GCL_InitCommandTable_8001FD2C

--- src/gcl.c
+++ src/gcl.c
@@ -1,6 +1,8 @@
 #include "linker.h"
 #include "gcl.h"
 
+GCL_CommandChain*  SECTION(".sdata") dword_800AB3B8 = 0;
+
 void sub_80020B68(void);
 void sub_80021264(void);
 void sub_8002040C(void);
@@ -16,4 +18,20 @@ void GCL_StartDaemon_8001FCDC(void)
     sub_8002040C();
     GD_SetFileHandler_80015418('g', sub_8001FC88);
     sub_8001FCB0(0);
+}
+
+void GLC_Null_8001FD24(void)
+{
+
+}
+
+int GCL_InitCommandTable_8001FD2C(GCL_CommandChain* pChain)
+{
+    // Set the new chains next to the existing chain
+    pChain->pNext = dword_800AB3B8;
+
+    // Update the existing chain to be the new chain
+    dword_800AB3B8 = pChain;
+    
+    return 0;
 }
