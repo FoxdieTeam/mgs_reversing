c7ab2cb72b0f8ea48145a3e7a35d91185df46df2
Paul <github@paulsapps.com>
2019-11-30 22:20:01 +0000

    Script_tbl_delay_8002C074

---
+++ src/delay.h
@@ -0,0 +1,9 @@
+#ifndef _DELAY_H
+#define _DELAY_H
+
+#include "actor.h"
+#include "gcl.h"
+
+Actor *delay_init_80033230(int script_pVar, GCLArgsPtr *pGCLArgs, int script_tVar);
+
+#endif // _DELAY_H
