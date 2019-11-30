680bd2ac9f4936c2f6f4f2a9a7653736f69743f0
Paul <github@paulsapps.com>
2019-11-30 22:20:01 +0000

    Script_tbl_delay_8002C074

--- src/gcl.h
+++ src/gcl.h
@@ -1,6 +1,12 @@
 #ifndef GCL_H
 #define GCL_H
 
+typedef struct GCLArgsPtr
+{
+    short count;
+    int* pArgs;
+} GCLArgsPtr;
+
 typedef int(*TGCL_CommandFn)(unsigned char* pScript);
 
 // A hashed name of a GCL command and pointer to function that implements the command
@@ -19,5 +25,12 @@ typedef struct GCL_CommandChain
 }  GCL_CommandChain;
 
 void GCL_StartDaemon_8001FCDC(void);
+
+int GCL_Run_80020118(unsigned char *pScript, GCLArgsPtr *pArgs);
+int GCL_RunOrCancelProc_8001FF2C(int procId, GCLArgsPtr *pArgs);
+int GCL_GetParam_80020968(int paramName);
+int sub_80020AD4(void);
+unsigned char* sub_80020AA4(void);
+unsigned char* GCL_Execute_8002069C(unsigned char* pScript, int* ppScript, int* pRet);
 
 #endif // GCL_H
