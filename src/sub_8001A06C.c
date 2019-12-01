394e3b6c71cca21b131773b62e15149d09a611ea
V <varrok@gmail.com>
2019-12-01 22:47:00 +0100

    sub_8001A06C

---
+++ src/sub_8001A06C.c
@@ -0,0 +1,18 @@
+#include "linker.h"
+static int SECTION(".bss_800B1E08") strangeTable_800B1E08[16];
+int sub_8001A06C(void)
+{
+    int *tbl_p;
+    int it;
+
+    tbl_p = (int*) &strangeTable_800B1E08;
+    it = 8;
+    for (it = 8; 0 < it;)
+    {
+        it -= 1;
+        *tbl_p = 0;
+        tbl_p[1] = 0;
+        tbl_p += 2;
+    }
+    return (int) tbl_p;
+}
