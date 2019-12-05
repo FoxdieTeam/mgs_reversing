6d69d9ec6e0b1448a8af0b6a0111bd047c024fa5
Paul <github@paulsapps.com>
2019-12-05 00:13:14 +0000

    move in some sdata

--- src/libgv/gvd.c
+++ src/libgv/gvd.c
@@ -10,36 +10,20 @@ int SECTION(".sbss") dword_800AB920;
 int SECTION(".sbss") dword_800AB924;
 
 
+
 // sdata ================================================
-
-
-extern const char aBislpm86247[];
-
-int SECTION(".sdata") dword_800AB2E4 = 0x200000; // ram size
-int SECTION(".sdata") dword_800AB2E8 = 0x8000; // stack size
-const char* SECTION(".sdata") off_800AB2EC = aBislpm86247;
-
-const char SECTION(".sdata") aMem[] = "mem:";
-const char SECTION(".sdata") aPad[] = "pad:";
-const char SECTION(".sdata") aGv[] = "gv:";
-const char SECTION(".sdata") aFs[] = "fs:";
-const char SECTION(".sdata") aDg[] = "dg:";
-const char SECTION(".sdata") aGcl[] = "gcl:";
-const char SECTION(".sdata") aHzd[] = "hzd:";
-const char SECTION(".sdata") aSound[] = "sound:";
-const char SECTION(".sdata") aGm[] = "gm:";
-const char SECTION(".sdata") aStart[] = "start\n";
-
-
 int SECTION(".sdata") dword_800AB330 = 0;
-int SECTION(".sdata") dword_800AB334 = 0; // pad ??
+int SECTION(".sdata") dword_800AB334 = 0;
 
+
+
+extern const char aGvdC[];
 
 // [other_c_sdata]
 
 // ========================================================
 
-extern const char aGvdC[];
+
 extern int dword_8009D460;
 extern int dword_800AB928;
 
@@ -131,7 +115,7 @@ void GV_StartDaemon_80014d18(void)
 	sub_80015458();
 	sub_80014cc8();
 	GV_ActorPushBack_800150a8(0, &gGVActor_800acbf8, 0);
-	GV_ActorInit_8001514c(&gGVActor_800acbf8, GV_Act_80014b60, 0, aGvdC);
+	GV_ActorInit_8001514c(&gGVActor_800acbf8, GV_Act_80014b60, 0, "gvd.c"); // sdata
 	dword_800AB920 = 0;
 	dword_800AB330 = 0;
 	sub_800892A8(sub_80014B34);
