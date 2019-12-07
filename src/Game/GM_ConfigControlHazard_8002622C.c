3e843f92dffaeebfee2411f7e79dfdb3e9369c0d
Paul <github@paulsapps.com>
2019-12-07 13:50:33 +0000

    GM_Control_RotateByMatrix_80026154

--- src/Game/GM_ConfigControlHazard_8002622C.c
+++ src/Game/GM_ConfigControlHazard_8002622C.c
@@ -1,5 +1,34 @@
 #include "linker.h"
 #include "GM_Control.h"
+#include "gcl.h"
+
+void DG_MatrixRotYXZ_8001E734(MATRIX *pMatrix, SVECTOR *pVector);
+
+void GM_Control_RotateByMatrix_80026154(Res_Control *pControl, MATRIX* pMatrix)
+{
+    pControl->field_0_vec.vx = pMatrix->t[0];
+    pControl->field_0_vec.vy = pMatrix->t[1];
+    pControl->field_0_vec.vz = pMatrix->t[2];
+    
+    DG_MatrixRotYXZ_8001E734(pMatrix, &pControl->field_8_vec);
+
+    pControl->field_4C_turn_vec = pControl->field_8_vec;
+}
+
+void GM_ConfigControl_SetVectorsGCL_800261C0(Res_Control *pControl, char *bReadVec1, char *bReadVec2)
+{
+    if (bReadVec1)
+    {
+        GCL_ReadVector_80020A14(bReadVec1, &pControl->field_0_vec.vx);
+    }
+
+    if (bReadVec2)
+    {
+        GCL_ReadVector_80020A14(bReadVec2, &pControl->field_8_vec.vx);
+    }
+
+    pControl->field_4C_turn_vec = pControl->field_8_vec;
+}
 
 void GM_ConfigControlHazard_8002622C(Res_Control *pControl, short height, short f36, short f38)
 {
