f8a5af5c08d08df1fe298fde277c4dcd583f9be1
Paul <github@paulsapps.com>
2019-12-07 13:30:14 +0000

    GM_ConfigControlAttribute_8002623C

--- src/Game/GM_ConfigControlHazard_8002622C.c
+++ src/Game/GM_ConfigControlHazard_8002622C.c
@@ -1,9 +1,15 @@
 #include "linker.h"
 #include "GM_Control.h"
 
-void GM_ConfigControlHazard_8002622C(Res_Control *pControl, int height, int f36, int f38)
+void GM_ConfigControlHazard_8002622C(Res_Control *pControl, short height, short f36, short f38)
 {
     pControl->field_32_height = height;
     pControl->field_36 = f36;
     pControl->field_38 = f38;
+}
+
+void GM_ConfigControlAttribute_8002623C(Res_Control *pControl, short f3a)
+{
+    pControl->field_3A = f3a;
+    return;
 }
