2e00769ba7e84faa26c22c9979095a9a5d2253ec
Paul <github@paulsapps.com>
2019-12-15 11:56:05 +0000

    GM_Stage_ClearStageHistory_8002A704 GM_SaveAreaHistory_8002A730

---
+++ src/Game/GM_Stage_ClearStageHistory_8002A704.c
@@ -0,0 +1,21 @@
+#include "linker.h"
+#include <sys/types.h>
+
+void *memset_8008E688(void *pSrc, int value, int len);
+
+typedef struct AreaHistory
+{
+    short history[8];
+} AreaHistory;
+
+AreaHistory SECTION(".gAreaHistory_800B5850") gAreaHistory_800B5850;
+
+void GM_Stage_ClearStageHistory_8002A704(void)
+{
+    memset_8008E688(&gAreaHistory_800B5850, 0, sizeof(gAreaHistory_800B5850));
+}
+
+void GM_SaveAreaHistory_8002A730(AreaHistory *pSaveBuffer)
+{
+    *pSaveBuffer = gAreaHistory_800B5850;
+}
