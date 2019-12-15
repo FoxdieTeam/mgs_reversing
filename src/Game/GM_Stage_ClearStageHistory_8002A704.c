eb23f7f2d74286e017e3ae9ee7009a25004b4f7e
Paul <github@paulsapps.com>
2019-12-15 12:21:15 +0000

    GM_RestoreAreaHistory_8002A784

--- src/Game/GM_Stage_ClearStageHistory_8002A704.c
+++ src/Game/GM_Stage_ClearStageHistory_8002A704.c
@@ -15,7 +15,12 @@ void GM_Stage_ClearStageHistory_8002A704(void)
     memset_8008E688(&gAreaHistory_800B5850, 0, sizeof(gAreaHistory_800B5850));
 }
 
-void GM_SaveAreaHistory_8002A730(AreaHistory *pSaveBuffer)
+void GM_SaveAreaHistory_8002A730(AreaHistory *pHistoryCopy)
 {
-    *pSaveBuffer = gAreaHistory_800B5850;
+    *pHistoryCopy = gAreaHistory_800B5850;
+}
+
+void GM_RestoreAreaHistory_8002A784(AreaHistory *pNewHistory)
+{
+    gAreaHistory_800B5850 = *pNewHistory;
 }
