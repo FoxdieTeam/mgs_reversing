a368b05f47826be33433eed2aed43ee75eb211c8
Paul <github@paulsapps.com>
2019-12-15 12:28:54 +0000

    rename

--- asm/GM_SetBinds_80029A5C.s
+++ asm/GM_SetBinds_80029A5C.s
@@ -1,8 +1,8 @@
 	opt	c+, at+, e+, n-
 	section .text
 
-	xdef sub_80029A5C
-sub_80029A5C:
+	xdef GM_SetBinds_80029A5C
+GM_SetBinds_80029A5C:
 	dw 0xAF8606D4 ; 0x80029A5C
 	dw 0xAF8506D8 ; 0x80029A60
 	dw 0x03E00008 ; 0x80029A64
