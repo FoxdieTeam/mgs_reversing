047c5964f4d6587e1e129d0e580e7883cc3aad5a
Paul <github@paulsapps.com>
2019-12-16 00:43:52 +0000

    renames

---
+++ asm/kogaku2_act_helper_800615F4.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section .text
+
+	xdef kogaku2_act_helper_800615F4
+kogaku2_act_helper_800615F4:
+	dw 0x03E00008 ; 0x800615F4
+	dw 0x00000000 ; 0x800615F8
--- asm/sub_800615F4.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section .text
-
-	xdef sub_800615F4
-sub_800615F4:
-	dw 0x03E00008 ; 0x800615F4
-	dw 0x00000000 ; 0x800615F8
