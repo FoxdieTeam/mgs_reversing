98b861ea2d3b7e2064357e61ff540b5c5821867a
Paul <github@paulsapps.com>
2019-12-18 22:09:22 +0000

    mts_vsync_counter_8008BBB0

--- asm/sub_8008BBB0.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section .text
-
-	xdef sub_8008BBB0
-sub_8008BBB0:
-	dw 0x3C02800A ; 0x8008BBB0
-	dw 0x8C423D78 ; 0x8008BBB4
-	dw 0x03E00008 ; 0x8008BBB8
-	dw 0x00000000 ; 0x8008BBBC
