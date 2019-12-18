e139e0f8b3581bf5f6aa86f47df55496bddc55d9
Paul <github@paulsapps.com>
2019-12-18 22:02:13 +0000

    mts_set_callback_controller_800893D8

--- asm/mts_set_callback_controller_800893D8.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section .text
-
-	xdef mts_set_callback_controller_800893D8
-mts_set_callback_controller_800893D8:
-	dw 0x3C01800A ; 0x800893D8
-	dw 0xAC243D74 ; 0x800893DC
-	dw 0x03E00008 ; 0x800893E0
-	dw 0x00000000 ; 0x800893E4
