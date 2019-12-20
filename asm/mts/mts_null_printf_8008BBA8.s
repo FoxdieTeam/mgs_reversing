7036a50e8742c55e6669bb10f8c6c84793dd6217
Paul <github@paulsapps.com>
2019-12-20 00:34:59 +0000

    move mts to own dir

--- asm/mts/mts_null_printf_8008BBA8.s
+++ asm/mts/mts_null_printf_8008BBA8.s
@@ -1,7 +1,7 @@
 	opt	c+, at+, e+, n-
 	section .text
 
-	xdef null_printf_8008BBA8
-null_printf_8008BBA8:
+	xdef mts_null_printf_8008BBA8
+mts_null_printf_8008BBA8:
 	dw 0x03E00008 ; 0x8008BBA8
 	dw 0x00000000 ; 0x8008BBAC
