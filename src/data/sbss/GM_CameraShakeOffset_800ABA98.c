84280360f1ea2243bdc3c00efa5b051fd925f7c3
V <varrok@gmail.com>
2019-12-21 10:37:13 +0100

    Remaining sbss splitting.

---
+++ src/data/sbss/GM_CameraShakeOffset_800ABA98.c
@@ -0,0 +1,4 @@
+#include "linker.h"
+
+int SECTION(".sbss") GM_CameraShakeOffset_800ABA98;
+
--- src/data/sbss/sbss_800ABBB8.c
+++ src/data/sbss/sbss_800ABBB8.c
@@ -1,11 +1,16 @@
 #include "linker.h"
 
-
-int SECTION(".sbss") dword_800ABBB8[7];
-
-short SECTION(".sbss") word_800ABBD4[2];
+int SECTION(".sbss") dword_800ABBB8;
+int SECTION(".sbss") dword_800ABBBC;
+int SECTION(".sbss") dword_800ABBC0;
+int SECTION(".sbss") dword_800ABBC4;
+int SECTION(".sbss") dword_800ABBC8;
+int SECTION(".sbss") dword_800ABBCC;
+int SECTION(".sbss") dword_800ABBD0;
+int SECTION(".sbss") dword_800ABBD4;
 int SECTION(".sbss") GM_BombSeg_800ABBD8;
-short SECTION(".sbss") word_800ABBDC[2];
-int SECTION(".sbss") dword_800ABBE0[2];
+int SECTION(".sbss") dword_800ABBDC;
+int SECTION(".sbss") dword_800ABBE0;
+int SECTION(".sbss") dword_800ABBE4;
 int SECTION(".sbss") dword_800ABBE8;
 
