8feb655466e27c50779e981332c762cc84baaac8
Paul <github@paulsapps.com>
2019-12-23 10:42:28 +0000

    sub_80047838

--- src/Menu/menu_gcl_set_radio_var_80047768.c
+++ src/Menu/menu_gcl_set_radio_var_80047768.c
@@ -5,6 +5,8 @@
 
 extern const char aSetDS[];
 
+unsigned char* sub_800217F0(unsigned char*);
+
 void menu_gcl_set_radio_var_80047768(menu_chara_struct *unknown, unsigned char *pScript)
 {
     int varId;
@@ -29,4 +31,12 @@ void sub_8004780C(menu_chara_struct *unknown, unsigned char *pScript)
     unknown->field_C_pScript = pScript;
     unknown->field_1A = 0;
     mts_8008A400();
+}
+
+void sub_80047838(menu_chara_struct *unknown, unsigned char *pScript)
+{
+    while (*pScript)
+    {
+        pScript = sub_800217F0(pScript);
+    }
 }
