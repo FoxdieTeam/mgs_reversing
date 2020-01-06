b5b5638d97aa513f19a4eb02913a7b79844fc8ed
V <varrok@gmail.com>
2020-01-06 21:18:15 +0100

    GV_HashFileName_800152FC().

--- src/libgv/GV_FileNameHashed_800152DC.c
+++ src/libgv/GV_FileNameHashed_800152DC.c
@@ -1,8 +1,8 @@
-int GV_FileNameHashed_800152DC(int param_1,int param_2)
+int GV_FileNameHashed_800152DC(int hashedFileName,int param_2)
 {
   param_2 -= 0x61;
   if (param_2 > 0x19) {
     param_2 = param_2 + 0x20;
   }
-  return param_1 + param_2 * 0x10000;
+  return hashedFileName + param_2 * 0x10000;
 }
