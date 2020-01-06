aba337347ad4b2a1c1fa7c6c9f0601a9dcc5f1cc
V <varrok@gmail.com>
2020-01-06 22:22:44 +0100

    GV_DirVec3_80016FA0().

---
+++ src/libgv/GV_DirVec3_80016FA0.c
@@ -0,0 +1,34 @@
+#include <util/idaTypes.h>
+#include <sys/types.h>
+#include <libgte.h>
+
+extern MATRIX *RotMatrixYXZ_80093798(SVECTOR *r, MATRIX *m);
+
+void GV_DirVec3_80016FA0(SVECTOR *pSrcVec, int param_2, SVECTOR *pDstVec)
+{
+    MATRIX matrix;
+    int iVar1;
+
+    RotMatrixYXZ_80093798(pSrcVec, &matrix);
+
+    iVar1 = param_2 * matrix.m[0][2];
+    if (iVar1 < 0)
+    {
+        iVar1 += 0xfff;
+    }
+    pDstVec->vx = (short)(iVar1 >> 0xc);
+
+    iVar1 = param_2 * matrix.m[1][2];
+    if (iVar1 < 0)
+    {
+        iVar1 += 0xfff;
+    }
+    pDstVec->vy = (short)(iVar1 >> 0xc);
+
+    iVar1 = param_2 * matrix.m[2][2];
+    if (iVar1 < 0)
+    {
+        iVar1 += 0xfff;
+    }
+    pDstVec->vz = (short)(iVar1 >> 0xc);
+}
