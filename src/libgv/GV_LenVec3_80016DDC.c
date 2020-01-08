8b02f8bc0bc7db1cd9bba3f6b8a0000a9318e9a3
V <varrok@gmail.com>
2020-01-09 00:35:00 +0100

    Simplified funcs.

--- src/libgv/GV_LenVec3_80016DDC.c
+++ src/libgv/GV_LenVec3_80016DDC.c
@@ -3,32 +3,14 @@
 
 void GV_LenVec3_80016DDC(SVECTOR *pSrcVec, SVECTOR *pDstVec, int param_3, int param_4)
 {
-    int iVar1;
-    param_4 = param_4 << 0xc;
+    param_4 = param_4 * ONE;
     if (param_3 == 0)
     {
         param_3 = 1;
     }
     param_4 /= param_3;
 
-    iVar1 = pSrcVec->vx * param_4;
-    if (iVar1 < 0)
-    {
-        iVar1 += ONE - 1;
-    }
-    pDstVec->vx = (short)(iVar1 >> 0xc);
-
-    iVar1 = pSrcVec->vy * param_4;
-    if (iVar1 < 0)
-    {
-        iVar1 += ONE - 1;
-    }
-    pDstVec->vy = (short)(iVar1 >> 0xc);
-
-    iVar1 = pSrcVec->vz * param_4;
-    if (iVar1 < 0)
-    {
-        iVar1 += ONE - 1;
-    }
-    pDstVec->vz = (short)(iVar1 >> 0xc);
+    pDstVec->vx = pSrcVec->vx * param_4 / ONE;
+    pDstVec->vy = pSrcVec->vy * param_4 / ONE;
+    pDstVec->vz = pSrcVec->vz * param_4 / ONE;
 }
