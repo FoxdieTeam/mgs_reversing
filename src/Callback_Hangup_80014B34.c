f1039d935c35bd423a998c035156de732cbc4771
V <varrok@gmail.com>
2020-01-09 00:42:13 +0100

    Small stuff.

--- src/Callback_Hangup_80014B34.c
+++ src/Callback_Hangup_80014B34.c
@@ -2,7 +2,7 @@ extern const char* aNone2_800AB34C;
 extern const char aHangupS[];
 extern void mts_printf_8008BBA0(const char*, ...);
 
-void Callback_Hangup_80014B34()
+void Callback_Hangup_80014B34(void)
 {
   mts_printf_8008BBA0(aHangupS, aNone2_800AB34C);
 }
