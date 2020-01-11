ac3dd8b8ddefc49f56196a33d7bd4b6be7c32089
Paul <github@paulsapps.com>
2020-01-11 00:40:40 +0000

    refactor

--- src/libdg/DG_ResetFixedLight_8001A06C.c
+++ src/libdg/DG_ResetFixedLight_8001A06C.c
@@ -1,24 +1,36 @@
 #include "linker.h"
-typedef struct strangeStruct
-{
-    int a;
-    int b;
-} strangeStruct;
+#include <sys/types.h>
+#include <libgte.h>
 
-static strangeStruct SECTION(".bss_800B1E08") strangeTable_800B1E08[8];
-strangeStruct* sub_8001A06C(void)
+typedef struct
 {
-    strangeStruct *tableIt;
-    int i;
+    short int field_0_x;
+    short int field_2_y;
+    short int field_4_z;
+    short int field_6_w; // padding?
+    short int field_8_brightness;
+    short int field_A_radius;
+    CVECTOR field_C_colour;
+} Light;
+typedef struct
+{
+    int field_0_lightCount;
+    Light* field_4_pLights;
+} DG_FixedLight;
 
-    tableIt = &strangeTable_800B1E08[0];
-    i = 8;
-    while(i > 0)
+
+static DG_FixedLight SECTION(".bss_800B1E08") gFixedLights_800B1E08[8];
+
+DG_FixedLight* DG_ResetFixedLight_8001A06C(void)
+{
+    DG_FixedLight *pLightIter = &gFixedLights_800B1E08[0];
+    int i = COUNTOF(gFixedLights_800B1E08);
+    while (i > 0)
     {
         i--;
-        tableIt->a = 0;
-        tableIt->b = 0;
-        tableIt++;
+        pLightIter->field_0_lightCount = 0;
+        pLightIter->field_4_pLights = 0;
+        pLightIter++;
     }
-    return tableIt;
+    return pLightIter;
 }
