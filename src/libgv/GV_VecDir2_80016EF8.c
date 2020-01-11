09d5a118def5c9209f7ce98f1ea3eda09e95b164
Paul <github@paulsapps.com>
2020-01-11 00:35:32 +0000

    refactor

--- src/libgv/GV_VecDir2_80016EF8.c
+++ src/libgv/GV_VecDir2_80016EF8.c
@@ -2,10 +2,9 @@
 #include <sys/types.h>
 #include <libgte.h>
 
-extern ratan2_80094308(DWORD,DWORD);
+extern int ratan2_80094308(DWORD,DWORD);
 
 DWORD GV_VecDir2_80016EF8(SVECTOR* vec)
 {
-   DWORD ratan = ratan2_80094308(vec->vx,vec->vz);
-   return ratan & 0xfff;
+   return ratan2_80094308(vec->vx,vec->vz) & (ONE-1);
 }
