209e8482d27fe8f7e0168c8a564c4c459d424668
Paul <github@paulsapps.com>
2020-01-25 14:03:30 +0000

    set types in GV_FindCachedFile_8001538C

--- src/libgv/GV_FindCachedFile_8001538C.c
+++ src/libgv/GV_FindCachedFile_8001538C.c
@@ -1,13 +1,19 @@
 #include <util/idaTypes.h>
 
-extern DWORD sub_80015240(DWORD);
-
-DWORD GV_FindCachedFile_8001538C(DWORD param_1)
+typedef struct
 {
-    int iVar1 = sub_80015240(param_1);
-    if (iVar1)
+    DWORD mId;
+    void* mFileBuffer;
+} LibGV_FileRecord;
+
+extern LibGV_FileRecord* sub_80015240(DWORD);
+
+void* GV_FindCachedFile_8001538C(DWORD fileNameHashed)
+{
+    LibGV_FileRecord* pRecord = sub_80015240(fileNameHashed);
+    if (pRecord)
     {
-        return *(DWORD *)(iVar1 + 4);
+        return pRecord->mFileBuffer;
     }
 
     return 0;
