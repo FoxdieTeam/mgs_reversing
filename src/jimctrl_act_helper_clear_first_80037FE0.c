d3bedf187054a6f08a36650d2a0e84de7f0f505b
V <varrok@gmail.com>
2020-01-29 14:49:33 +0100

    Casting.

--- src/jimctrl_act_helper_clear_first_80037FE0.c
+++ src/jimctrl_act_helper_clear_first_80037FE0.c
@@ -7,7 +7,7 @@ dword_800B9358_struct SECTION(".gStuff_800B933C") array_800B9358[2]; //todo figu
   
 void sub_80037FE0(int index, WORD value)
 {
-    array_800B933C_child *pIter = (array_800B933C_child*) ((DWORD*) &array_800B933C + 1);
+    array_800B933C_child *pIter = (array_800B933C_child*) &array_800B933C + 1;
     index--;
     pIter[index].field_0 = value;
 }
