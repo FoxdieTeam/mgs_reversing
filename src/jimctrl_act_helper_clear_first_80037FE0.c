176a8dc6259a7270bd96090749f28b27e0d1cd91
V <varrok@gmail.com>
2020-01-29 14:03:33 +0100

    Array madness.

---
+++ src/jimctrl_act_helper_clear_first_80037FE0.c
@@ -0,0 +1,13 @@
+#include <util/idaTypes.h>
+#include "linker.h"
+#include "jimctrl.h"
+
+array_800B933C_child SECTION(".gStuff_800B933C") array_800B933C[array_800B933C_SIZE]; //todo figure out
+dword_800B9358_struct SECTION(".gStuff_800B933C") array_800B9358[2]; //todo figure out real size
+  
+void sub_80037FE0(int index, WORD value)
+{
+    array_800B933C_child *pIter = (array_800B933C_child*) ((DWORD*) &array_800B933C + 1);
+    index--;
+    pIter[index].field_0 = value;
+}
--- src/jimctrl_kill_helper_clear_80038004.c
+++ src/jimctrl_kill_helper_clear_80038004.c
@@ -4,11 +4,11 @@
 
 extern void MENU_JimakuClear_80049518(void);
 
-extern array_800B9340_child array_800B9340[array_800B9340_SIZE];
+extern array_800B933C_child array_800B933C[array_800B933C_SIZE];
 
 void jimctrl_kill_helper_clear_80038004(Actor_JimCtrl *pJimCtrl)
 {
-    array_800B9340_child *pIter;
+    array_800B933C_child *pIter;
     int i;
 
     if (pJimCtrl->field_44 != 0)
@@ -19,11 +19,12 @@ void jimctrl_kill_helper_clear_80038004(Actor_JimCtrl *pJimCtrl)
     if (pJimCtrl->field_38 != 0)
     {
         i = 0;
-        pIter = &array_800B9340[0];
-        for (; i < array_800B9340_SIZE; pIter++, i++)
+        pIter = &array_800B933C[i] + 1;
+        for (; i < array_800B933C_SIZE - 1; i++)
         {
             pIter->field_2 = 0;
             pIter->field_3 = 0;
+            pIter++;
         }
     }
 }
