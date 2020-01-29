cca2fc491f0e3c7eda99f1f2d8bc098e57b2e67f
d3nwah <Varrok@gmail.com>
2020-01-29 15:37:41 +0100

    Array madness (#113)
    * Minor cleanup.
    
    * WIP refactor.
    
    * WIP function.
    
    * how did those get there? Purged!
    
    * jimctrl_init_80038568.
    
    * Match fix.
    
    * Array madness.
    
    * Endl.
    
    * Bad group removed. Why is that file there??
    
    * Casting.
    
    * Casting.
    
    * Fix.

---
+++ src/jimctrl_act_helper_clear_first_80037FE0.c
@@ -0,0 +1,12 @@
+#include <util/idaTypes.h>
+#include "linker.h"
+#include "jimctrl.h"
+
+array_800B933C_child SECTION(".gStuff_800B933C") array_800B933C[array_800B933C_SIZE]; //todo figure out
+dword_800B9358_struct SECTION(".gStuff_800B933C") array_800B9358[2]; //todo figure out real size
+  
+void sub_80037FE0(int index, WORD value)
+{
+    array_800B933C_child *pIter = &array_800B933C[1];
+    pIter[--index].field_0 = value;
+}
--- src/jimctrl_init_helper_clear_80037FB8.c
+++ src/jimctrl_init_helper_clear_80037FB8.c
@@ -2,14 +2,15 @@
 #include "linker.h"
 #include "jimctrl.h"
 
-extern array_800B9340_child array_800B9340[array_800B9340_SIZE];
+extern array_800B933C_child array_800B933C[array_800B933C_SIZE];
 
 void jimctrl_init_helper_clear_80037FB8(void)
 {
-    int i = array_800B9340_SIZE - 1;
-    array_800B9340_child *pIter = &array_800B9340[i];
-    for (; i >= 0; i--, pIter--)
+    int i = array_800B933C_SIZE - 2;
+    array_800B933C_child *pIter = &array_800B933C[i] + 1;
+    for (; i >= 0; i--)
     {
         pIter->field_0 = 0;
+        pIter--;
     }
 }
