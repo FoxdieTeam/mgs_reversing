802a026f6a6f64ef0e42dd86c853a25a32a5bbe4
Wanted <4wantedthing@gmail.com>
2021-01-23 06:36:06 +0100

    removed duplicated struct

--- src/Script_tbl_rand_8002CD94.c
+++ src/Script_tbl_rand_8002CD94.c
@@ -1,18 +1,19 @@
 #include "linker.h"
 #include "gcl.h"
 #include "idaTypes.h"
+#include "Script_tbl_map_8002BB44.h"
 
 unsigned int rand_8008E6B8(void);
 
-extern unknown_800B4D98 gUnknown_struct_800B4D98;
+extern GameState gGameState_800B4D98;
 
-int Script_tbl_rand_8002CD94(unsigned char *pScript)
+int Script_tbl_rand_8002CD94(void)
 {
     int param;
     int rand;
 
     param = GCL_Get_Param_80020AD4();
     rand = rand_8008E6B8();
-    gUnknown_struct_800B4D98.unknown_6 = (short)(rand % param);
+    gGameState_800B4D98.unk.LastRand = (short)(rand % param);
     return 0;
 }
