d205b9da66cd3b22c9cc87be9188d013121577c8
Paul <github@paulsapps.com>
2021-12-21 12:56:32 +0000

    match memcard_delete_800253C4

--- asm/psyq/erase_800995FC.s
+++ asm/psyq/erase_800995FC.s
@@ -1,8 +1,8 @@
 	opt	c+, at+, e+, n-
 	section .text
 
-	xdef sub_800995FC
-sub_800995FC:
+	xdef erase_800995FC
+erase_800995FC:
 	dw 0x240A00B0 ; 0x800995FC
 	dw 0x01400008 ; 0x80099600
 	dw 0x24090045 ; 0x80099604
