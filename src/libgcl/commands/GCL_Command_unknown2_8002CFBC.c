cdb3e453b821145c2fea0d2c6e82e5a02e92cb9d
Wanted <4wantedthing@gmail.com>
2021-12-27 16:28:42 +0100

    more gcl funcs

---
+++ src/libgcl/commands/GCL_Command_unknown2_8002CFBC.c
@@ -0,0 +1,66 @@
+#include "gcl.h"
+#include "mts_new.h"
+#include "linker.h"
+
+extern int          gBinds_800ABA60;
+int SECTION(".sdata") gBinds_800ABA60;
+extern int          dword_800AB414;
+int SECTION(".sdata") dword_800AB414;
+
+extern const char   aErrorDemoThrea[];
+
+extern int          dword_800AB9B0;
+
+extern char*        GCL_Read_String_80020A70(char *pScript);
+extern int          demothrd_init_80079460(int param_1, int param_2);
+extern int          demothrd_init_800794E4(int param_1, int param_2);
+
+int GCL_Command_unknown2_8002CFBC(void)
+{
+    int tmp, demo, flags, ivar;
+    char *str;
+
+    ivar = 0;
+    str = (char*)0;
+    flags = GCL_GetParam_80020968('e') != 0;
+    if (GCL_GetParam_80020968('a'))
+    {
+        flags |= 4;
+    }
+    if (GCL_GetParam_80020968('v'))
+    {
+        flags |= 2;
+    }
+    if (GCL_GetParam_80020968('s'))
+    {
+        ivar = GCL_GetNextInt_800209E8(GCL_Get_Param_Result_80020AA4());
+    }
+    if (GCL_GetParam_80020968('f'))
+    {
+        str = GCL_Read_String_80020A70(GCL_Get_Param_Result_80020AA4());
+    }
+    if (GCL_GetParam_80020968('p'))
+    {
+        dword_800AB414 = GCL_GetNextParamValue_80020AD4();
+    }
+    else
+    {
+        dword_800AB414 = -1;
+    }
+    tmp = dword_800AB9B0;
+    dword_800AB9B0 = gBinds_800ABA60;
+    if (str)
+    {
+        demo = demothrd_init_800794E4(flags, (int)str);
+    }
+    else 
+    {
+        demo = demothrd_init_80079460(flags, ivar);
+    }
+    dword_800AB9B0 = tmp;
+    if (!demo)
+    {
+        mts_printf_8008BBA0(aErrorDemoThrea);
+    }
+    return 0;
+}
