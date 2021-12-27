d3c9405eb78b58cd7892a3068f78873be07a4b48
Wanted <4wantedthing@gmail.com>
2021-12-27 16:49:44 +0100

    rename fix

--- src/libgcl/commands/GCL_Command_sound_8002CA28.c
+++ src/libgcl/commands/GCL_Command_sound_8002CA28.c
@@ -1,9 +1,9 @@
 
 
-extern void GCL_CommandHelper_sound_impl_8002E688(void);
+extern void GCL_Command_sound_impl_8002E688(void);
 
 int GCL_Command_sound_8002CA28(void)
 {
-    GCL_CommandHelper_sound_impl_8002E688();
+    GCL_Command_sound_impl_8002E688();
     return 0;
 }
