f7c21feca90e64238ab0bcf0dc9a1e9856af451b
Paul <github@paulsapps.com>
2022-01-19 13:27:40 +0000

    match DG_WriteObjPacketRGB_8001A9B8

--- src/Equip/kogaku2_kill_helper_80061384.c
+++ src/Equip/kogaku2_kill_helper_80061384.c
@@ -1,6 +1,6 @@
 
 
-extern void DG_WriteObjPacketRGB_8001A774(int param_1, int param_2);
+extern void DG_8001A774(int param_1, int param_2);
 
 void kogaku2_kill_helper_80061384(int param_1)
 
@@ -15,8 +15,8 @@ void kogaku2_kill_helper_80061384(int param_1)
     *(unsigned int *)(iVar1 + 0x28) = *(unsigned int *)(iVar1 + 0x28) & 0x80 | *(unsigned int *)(param_1 + 0x28) & 0xffffff7f;
     while (0 < iVar3)
     {
-        DG_WriteObjPacketRGB_8001A774(iVar2, 0);
-        DG_WriteObjPacketRGB_8001A774(iVar2, 1);
+        DG_8001A774(iVar2, 0);
+        DG_8001A774(iVar2, 1);
         iVar3 = iVar3 + -1;
         iVar2 = iVar2 + 0x5c;
     }
