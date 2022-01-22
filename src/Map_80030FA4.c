812d51b0b9360649984083e591f2ca609516ec2f
Paul <github@paulsapps.com>
2022-01-22 14:46:50 +0000

    match Map_80030FA4

---
+++ src/Map_80030FA4.c
@@ -0,0 +1,15 @@
+#include "linker.h"
+
+extern int gMapsChanged_800ABAAC;
+int SECTION(".sbss") gMapsChanged_800ABAAC;
+
+int Map_light_80030C6C(int a1);
+
+void Map_80030FA4()
+{
+    if (gMapsChanged_800ABAAC)
+    {
+        Map_light_80030C6C(gMapsChanged_800ABAAC - 1);
+        gMapsChanged_800ABAAC = 0;
+    }
+}
