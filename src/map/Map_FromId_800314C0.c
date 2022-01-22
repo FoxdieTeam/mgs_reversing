6fa3b270d40b2050ed93f86090306ff980284904
Paul <github@paulsapps.com>
2022-01-22 15:30:02 +0000

    match Map_FromId_800314C0

---
+++ src/map/Map_FromId_800314C0.c
@@ -0,0 +1,37 @@
+#include "linker.h"
+#include "map.h"
+#include "gcl.h"
+
+// re-declare to force GP usage
+extern int gMapCount_800ABAA8;
+int SECTION(".sbss") gMapCount_800ABAA8;
+
+extern struct map_record gMapRecs_800B7910[16];
+
+struct map_record *Map_FromId_800314C0(int id)
+{
+    struct map_record *pRecIter; // $a1
+    int counter;                 // $v1
+    struct map_record *pFound;   // $a2
+
+    pRecIter = gMapRecs_800B7910;
+    counter = gMapCount_800ABAA8;
+    pFound = 0;
+    while ( counter > 0)
+    {
+        if (!id)
+        {
+            break;
+        }
+        
+        if ((id & 1) != 0)
+        {
+            pFound = pRecIter;
+        }
+
+        id >>= 1;
+        pRecIter++;
+        counter--;
+    }
+    return pFound;
+}
