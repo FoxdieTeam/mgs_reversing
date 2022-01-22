3da75f0bdbc53ff4ea68e7fb4f808b253eb8d50d
Wanted <4wantedthing@gmail.com>
2022-01-22 07:28:29 +0100

    more gcl

--- build/progress_script/symProgress.c
+++ build/progress_script/symProgress.c
@@ -475,7 +475,7 @@ int main(int argc, const char** argv)
 
 	fclose(f);
 	
-	size_t totalFuncsToReverse = 1848;
+	size_t totalFuncsToReverse = 2204;
 	size_t textSectionLength = 0x00088A24;
 	float progress = ((float)totalFuncsSize / (float)textSectionLength) * 100.0f;
 	float funcsProgress = ((float)totalFuncs / (float)totalFuncsToReverse) * 100.0f;
