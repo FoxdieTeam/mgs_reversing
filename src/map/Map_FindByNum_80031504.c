148fdfc599f3bf64a0b39cb21e1617374801ed96
Paul <github@paulsapps.com>
2022-01-22 01:58:10 +0000

    match sub_8003153C

--- src/map/Map_FindByNum_80031504.c
+++ src/map/Map_FindByNum_80031504.c
@@ -9,7 +9,7 @@ struct map_record SECTION(".gMapRecs_800B7910") gMapRecs_800B7910[16];
 
 struct map_record *Map_FindByNum_80031504(int toFind)
 {
-    int counter; 
+    int counter;
     struct map_record *pIter;
 
     pIter = gMapRecs_800B7910;
@@ -22,6 +22,26 @@ struct map_record *Map_FindByNum_80031504(int toFind)
         }
 
         ++pIter;
+    }
+    return 0;
+}
+
+int sub_8003153C(struct map_record *a1)
+{
+    int v1;
+    struct map_record *i;
+
+    i = gMapRecs_800B7910;
+    for (v1 = gMapCount_800ABAA8; v1 > 0; --v1)
+    {
+        if (i == a1)
+        {
+            if (i->field_6_bUsed)
+            {
+                return 1;
+            }
+        }
+        ++i;
     }
     return 0;
 }
