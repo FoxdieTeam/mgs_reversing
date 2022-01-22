c84fc0f9249b8b680b105e1238d85dd0a68bda5b
Paul <github@paulsapps.com>
2022-01-22 03:00:52 +0000

    match Map_FreeKmds_80031028

---
+++ src/Map_FreeKmds_80031028.c
@@ -0,0 +1,33 @@
+#include "linker.h"
+#include "libdg.h"
+#include "dgd.h"
+
+// re-declare to force GP usage
+extern int N_StageObjs_800ABAA4;
+int SECTION(".sbss") N_StageObjs_800ABAA4;
+
+extern DG_OBJS *StageObjs_800B7890[32];
+
+extern void DG_DequeueObjs_800181E4(DG_OBJS *objs);
+void Prim_free_colour_buffer_80032110(DG_OBJS *pPrim);
+extern void DG_FreeObjs_800318D0(DG_OBJS *objs);
+
+void Map_FreeKmds_80031028()
+{
+    int counter;         // $s1
+    DG_OBJS **pObjIter; // $s0
+
+    counter = N_StageObjs_800ABAA4;
+    for (pObjIter = StageObjs_800B7890; counter > 0; ++pObjIter)
+    {
+        if (*pObjIter)
+        {
+            DG_DequeueObjs_800181E4(*pObjIter);
+            Prim_free_colour_buffer_80032110(*pObjIter);
+            DG_FreeObjs_800318D0(*pObjIter);
+        }
+        *pObjIter = 0;
+        --counter;
+    }
+    N_StageObjs_800ABAA4 = 0;
+}
