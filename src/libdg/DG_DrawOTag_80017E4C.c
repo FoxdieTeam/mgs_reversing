f961dd37829a99b2cef712906159f417c529e028
Paul <github@paulsapps.com>
2022-01-23 23:21:27 +0000

    renames

--- src/libdg/DG_DrawOTag_80017E4C.c
+++ src/libdg/DG_DrawOTag_80017E4C.c
@@ -1,7 +1,7 @@
 #include "linker.h"
 #include "dgd.h"
 
-extern struct_dg gOts_800B1800[3];
+extern DG_CHNL DG_Chanls_800B1800[3];
 
 // Force gp usage
 extern unsigned short gCurrentRootCnt_800AB984;
@@ -16,5 +16,5 @@ void DG_DrawOTag_80017E4C(int activeBuffer)
 {
     gOldRootCnt_800B1DC8[0] = gCurrentRootCnt_800AB984;
     gCurrentRootCnt_800AB984 = GetRCnt_800996E8(0xF2000001);
-    DrawOTag_8008FE58(&gOts_800B1800[0].dword_6BC3D8_dst[activeBuffer][0]);
+    DrawOTag_8008FE58(&DG_Chanls_800B1800[0].dword_6BC3D8_dst[activeBuffer][0]);
 }
