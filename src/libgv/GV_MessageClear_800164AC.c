12ba2b99d61c144694a5348ee1b6c21a6d020a2d
Paul <github@paulsapps.com>
2022-01-23 14:42:30 +0000

    match GV_MessageClear_800164AC

---
+++ src/libgv/GV_MessageClear_800164AC.c
@@ -0,0 +1,15 @@
+#include "linker.h"
+#include "libgv.h"
+
+extern struct GV_Messages gMessageQueue_800B0320[2];
+
+// force gp usage
+extern int active_msg_queue_800AB948;
+int SECTION(".sbss") active_msg_queue_800AB948;
+
+void GV_MessageClear_800164AC()
+{
+    gMessageQueue_800B0320[0].field_0_count = 0;
+    gMessageQueue_800B0320[1].field_0_count = 0;
+    active_msg_queue_800AB948 = 0;
+}
