8680c8f3bb09de689e443794ef930bce1211b029
Paul <github@paulsapps.com>
2022-01-26 12:10:57 +0000

    wip tabako_loader_800620B4

--- src/Equip/tabako.h
+++ src/Equip/tabako.h
@@ -10,14 +10,16 @@
 typedef struct Actor_tabako
 {
     Actor field_0;
-    OBJECT field_20_pObj;
+    OBJECT_NO_ROTS field_20_pObj;
     Res_Control *field_44_pCtrl;
     OBJECT *field_48_pObj;
     int field_4C_unknown;
     DG_PRIM *field_50_prims;
     SVECTOR field_54_vec;
-    int field_5C_unknown;
-    int field_60_unknown;
+    RECT field_5C_unknown;
+    //short field_5E;
+    //short field_60_unknown;
+    //short field_62;
 } Actor_tabako;
 
 #endif // _TABAKO_H_
