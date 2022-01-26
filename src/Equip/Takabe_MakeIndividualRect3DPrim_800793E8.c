d8dbe787a2b7180d1441a1166ac9f00da1557045
Paul <github@paulsapps.com>
2022-01-26 12:47:59 +0000

    refactor

--- src/Equip/Takabe_MakeIndividualRect3DPrim_800793E8.c
+++ src/Equip/Takabe_MakeIndividualRect3DPrim_800793E8.c
@@ -21,20 +21,13 @@ int group_id;
 
 DG_PRIM* Takabe_MakeIndividualRect3DPrim_800793E8(int prim_count, SVECTOR *pVec)
 {
-    short _prim_count; // $s1
-    DG_PRIM *pNewPrim;   // $v0
-    DG_PRIM *pPrim;      // $s0
-
-    _prim_count = prim_count;
-    pNewPrim = DG_Prim_Alloc_8001BABC(0x2012, prim_count, 0, pVec, 0);
-    pPrim = pNewPrim;
+    DG_PRIM *pNewPrim = DG_Prim_Alloc_8001BABC(0x2012, prim_count, 0, pVec, 0);
     if (pNewPrim)
     {
         DG_QueuePrim_80018274((DG_OBJS *)pNewPrim);
-        DG_GroupPrim(pPrim, GM_CurrentMap_800AB9B0);
-        pNewPrim = pPrim;
+        DG_GroupPrim(pNewPrim, GM_CurrentMap_800AB9B0);
     }
     pNewPrim->field_50_pFn = (int)sub_80079284;
-    pNewPrim->field_48_prim_count = _prim_count;
+    pNewPrim->field_48_prim_count = prim_count;
     return pNewPrim;
 }
