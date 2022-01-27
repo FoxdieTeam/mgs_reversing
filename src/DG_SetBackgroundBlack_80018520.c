b8145ead04c7237a03f3e827470b35ba259b91f8
Paul <github@paulsapps.com>
2022-01-27 10:37:42 +0000

    match DG_BackGroundNormal_80018548 and sub_80018574

--- src/DG_SetBackgroundBlack_80018520.c
+++ src/DG_SetBackgroundBlack_80018520.c
@@ -1,5 +1,29 @@
-extern void DG_SetBackgroundRGB_80018464(int, int, int);
+#include "linker.h"
+
+extern void DG_SetBackgroundRGB_80018464(int r, int g, int b);
+
+// Force gp
+unsigned char SECTION(".sbss") DG_r_800AB97C;
+unsigned char SECTION(".sbss") DG_b_800AB97D;
+unsigned char SECTION(".sbss") DG_g_800AB97E;
+
+extern unsigned char SECTION(".sbss") DG_r_800AB97C;
+extern unsigned char SECTION(".sbss") DG_b_800AB97D;
+extern unsigned char SECTION(".sbss") DG_g_800AB97E;
+
 void DG_SetBackgroundBlack_80018520(void)
 {
   DG_SetBackgroundRGB_80018464(0, 0, 0);
+}
+void DG_BackGroundNormal_80018548()
+{
+  DG_SetBackgroundRGB_80018464(DG_r_800AB97C, DG_b_800AB97D, DG_g_800AB97E);
+}
+
+void sub_80018574(unsigned char *a1)
+{
+  // TODO: Structure
+  a1[4] = DG_r_800AB97C;
+  a1[5] = DG_b_800AB97D;
+  a1[6] = DG_g_800AB97E;
 }
