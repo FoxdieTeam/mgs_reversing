07f13ba5b230791b31d4e7566fb904602f0fe34f
Paul <github@paulsapps.com>
2022-01-27 10:26:16 +0000

    match DG_8001844C

--- src/libdg/DG_FreeObjectQueue_800183D4.c
+++ src/libdg/DG_FreeObjectQueue_800183D4.c
@@ -10,11 +10,13 @@ void DG_void_all_prim_buffers_8001ABA8(DG_OBJS *pObjs, int idx);
 extern int gObjectQueueVoided_800AB388;
 int SECTION(".sdata") gObjectQueueVoided_800AB388;
 
+extern int counter_800AB380;
+
 void DG_FreeObjectQueue_800183D4()
 {
     int remainder;
-    DG_CHNL* pChan = &DG_Chanls_800B1800[1];
-    DG_OBJS** mQueue = pChan->mQueue;
+    DG_CHNL *pChan = &DG_Chanls_800B1800[1];
+    DG_OBJS **mQueue = pChan->mQueue;
 
     gObjectQueueVoided_800AB388 = 1;
 
@@ -24,4 +26,10 @@ void DG_FreeObjectQueue_800183D4()
         DG_void_all_prim_buffers_8001ABA8(pItem, 0);
         DG_void_all_prim_buffers_8001ABA8(pItem, 1);
     }
+}
+
+void DG_8001844C()
+{
+    gObjectQueueVoided_800AB388 = 0;
+    counter_800AB380 = 1;
 }
