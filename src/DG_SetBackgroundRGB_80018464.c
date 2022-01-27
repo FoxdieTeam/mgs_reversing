af49cf7273850d39d255c7f600b2a4c0db61e40d
Paul <github@paulsapps.com>
2022-01-27 10:34:38 +0000

    match DG_Set_RGB_800184F4

--- src/DG_SetBackgroundRGB_80018464.c
+++ src/DG_SetBackgroundRGB_80018464.c
@@ -5,7 +5,7 @@ extern DG_CHNL DG_Chanls_800B1800[3];
 void DG_Init_DrawEnv_80018384(DRAWENV *pDrawEnv, short clipX1, short clipY1, short clipX2, short clipY2);
 void DG_Init_800179F4(DG_CHNL *pDg, DRAWENV *pDrawEnv, int bUnknown);
 
-void DG_SetBackgroundRGB_80018464(unsigned char r, unsigned char g, unsigned char b)
+void DG_SetBackgroundRGB_80018464(int r, int g, int b)
 {
     DRAWENV drawEnv;
     DG_CHNL *pStruct = &DG_Chanls_800B1800[0];
