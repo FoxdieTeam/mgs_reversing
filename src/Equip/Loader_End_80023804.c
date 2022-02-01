2b76403712b8071e6a560a1f16e865e7fa3116bc
Paul <github@paulsapps.com>
2022-02-01 10:32:34 +0000

    match Loader_Act_helper_800237C0

--- src/Equip/Loader_End_80023804.c
+++ src/Equip/Loader_End_80023804.c
@@ -21,14 +21,27 @@ struct Loader_Record // TODO: Share in a header
 extern const char aLoadCompleteTi[]; // "load complete time %d\n";
 
 int VSync_80098108(int);
-extern void mts_printf_8008BBA0(const char*, ...);
+extern void mts_printf_8008BBA0(const char *, ...);
 extern void GV_Free_80016230(void *ptr);
 void FS_Null_80014AAC(void);
 
 extern int DG_FrameRate_8009D45C;
 
-struct Loader_Record* SECTION(".gLoaderRec_800B5288") gLoaderRec_800B5288;
+struct Loader_Record *SECTION(".gLoaderRec_800B5288") gLoaderRec_800B5288;
 int SECTION(".gLoaderRec_800B5288") gLoaderStartTime_800B528C;
+
+int Loader_80023624(struct Loader_Record *pRec);
+int CDFS_SectorsLeft_80022854(void);
+
+int Loader_Act_helper_800237C0(struct Loader_Record *pRec)
+{
+    int ret = 0;
+    if (Loader_80023624(pRec) != 0 || CDFS_SectorsLeft_80022854() > 0)
+    {
+        ret = 1;
+    }
+    return ret;
+}
 
 void Loader_End_80023804(struct Loader_Record *pFileName)
 {
