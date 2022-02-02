856d694b2fdc1109ba433eec7c7c3dc3dffdaec5
Paul <github@paulsapps.com>
2022-02-02 04:13:34 +0000

    add code data for sound.bin

--- src/overlays/sound_bin.c
+++ src/overlays/sound_bin.c
@@ -9,20 +9,9 @@ GCL_ActorTableEntry SECTION(".gProgramBottom_800C3208") OverlayCharas[] =
 		{0, 0}};
 
 
-// Test code
-void Map_FreeKmds_80031028();
-
-void SECTION(".olay") OverlayTesting2()
-{
-
-}
-
-void SECTION(".olay")  OverlayTesting1()
-{
-	Map_FreeKmds_80031028();
-}
-
-void SECTION(".olay")  OverlayTesting3()
-{
-
-}
+const char SECTION(".olay_r") aPlaying[] = "PLAYING";
+const char SECTION(".olay_r") aO[4] = "-=O";
+const char SECTION(".olay_r") aCCD[] = "%c  %c  :%d";
+const char SECTION(".olay_r") asc_800C3654[5] = "-0=-";
+const char SECTION(".olay_r") aNoMenu[] = "NO MENU\n";
+const char SECTION(".olay_r") aSndtstC[] = "sndtst.c";
