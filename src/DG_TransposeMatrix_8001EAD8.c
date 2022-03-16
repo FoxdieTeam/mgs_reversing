70e2f98b41653c2c090ca6be39dbd679b7304e66
Kieron Josephs <kieron.josephs00@gmail.com>
2022-03-16 20:17:30 +0000

    Rename DG_8001EAD8

---
+++ src/DG_TransposeMatrix_8001EAD8.c
@@ -0,0 +1,28 @@
+#include "linker.h"
+#include <sys\types.h>
+#include <libgte.h>
+
+void DG_TransposeMatrix_8001EAD8(MATRIX *in, MATRIX *out)
+{
+    short temp;
+
+    temp = in->m[1][0];
+    out->m[1][0] = in->m[0][1];
+    out->m[0][1] = temp;
+
+    temp = in->m[2][0];
+    out->m[2][0] = in->m[0][2];
+    out->m[0][2] = temp;
+
+    temp = in->m[2][1];
+    out->m[2][1] = in->m[1][2];
+    out->m[1][2] = temp;
+
+
+    if ( in != out )
+    {
+        out->m[0][0] = in->m[0][0];
+        out->m[1][1] = in->m[1][1];
+        out->m[2][2] = in->m[2][2];
+    }
+}
