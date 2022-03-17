7f2d35faa67029b220b1a2b528d02aeed6a34041
Paul <github@paulsapps.com>
2022-03-17 20:58:43 +0000

    match DG_8001B5FC

---
+++ src/sub_8001B5FC.c
@@ -0,0 +1,15 @@
+#include "linker.h"
+#include "libdg.h"
+#include "dgd.h"
+
+extern int GV_Clock_800AB920;
+
+void sub_8001AD28(SVECTOR* pVec, int numPrims);
+
+void DG_8001B5FC(DG_PRIM *pPrim)
+{
+    int n_prims = pPrim->n_prims;
+    union Prim_Union* prim_buffer = pPrim->field_40_pBuffers[GV_Clock_800AB920];
+    sub_8001AD28(pPrim->field_38_pUnknown, pPrim->field_48_prim_count);
+    pPrim->field_50_pFn(pPrim, prim_buffer, n_prims);
+}
