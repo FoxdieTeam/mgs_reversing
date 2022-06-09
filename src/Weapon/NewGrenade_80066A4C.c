bbf927ec548d0643ca100cf3caabb85d93b4a7a8
Voxel <darrensweeneydev@gmail.com>
2022-06-09 23:27:20 +0200

    Added famas and the grenade actor create functions.

--- src/Weapon/NewGrenade_80066A4C.c
+++ src/Weapon/NewGrenade_80066A4C.c
@@ -1,6 +1,6 @@
 #include "grenade.h"
 
-Actor_Grenade *grenade_init_80066944(Unknown_Grenade_Member *a1, OBJECT *parent_obj, int num_parent, int a4, int a5, int index);
+extern Actor_Grenade *grenade_init_80066944(Unknown_Grenade_Member *a1, OBJECT *parent_obj, int num_parent, int a4, int a5, int index);
 
 Actor_Grenade *grenade_create_80066A4C(Unknown_Grenade_Member *a1, OBJECT *a2, int num_parent, int a4, int a5)
 {
