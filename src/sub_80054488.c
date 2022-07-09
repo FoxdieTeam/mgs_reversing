17428aaaaf54018d7aec4c24bc2a8e8807aa06c7
fatalis <fatalis@fatalis.pw>
2022-07-09 04:15:41 -0500

    match 8 more undocumented funcs lazily

---
+++ src/sub_80054488.c
@@ -0,0 +1,17 @@
+extern void sub_8004E2B4(unsigned int param_1);
+extern void sub_80050440(int param_1);
+extern void sub_80056928(void);
+extern void sub_80052468(void);
+
+void sub_80054488(int param_1, int param_2)
+{
+    if (param_2 == 0)
+    {
+        sub_8004E2B4(0x10);
+        *(void **)(param_1 + 0x9c8) = (void*)&sub_80056928;
+        *(void **)(param_1 + 0x9cc) = (void*)&sub_80052468;
+    }
+    *(short *)(param_1 + 0xa62) = *(short *)(param_1 + 0x98) + 0xfa;
+    sub_80050440(param_1);
+    return;
+}
