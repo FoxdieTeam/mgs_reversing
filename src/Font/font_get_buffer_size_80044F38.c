1cadc6e0f2f9ea4762ad32451f8c513a11bfc72f
twevs <77587819+twevs@users.noreply.github.com>
2022-07-10 20:15:28 +0200

    More KCB and font work.

--- src/Font/font_get_buffer_size_80044F38.c
+++ src/Font/font_get_buffer_size_80044F38.c
@@ -4,16 +4,16 @@ int font_get_buffer_size_80044F38(KCB* kcb)
 {
     short val_1 = (kcb->char_arr[2] + 0xc) * kcb->char_arr[0] - kcb->char_arr[2];
     int val_2 = (int)val_1 + 7;
-    kcb->short_arr[1] = val_1;
+    kcb->short1 = val_1;
     if (val_2 < 0)
     {
-        val_2 = kcb->short_arr[1] + 14;
+        val_2 = kcb->short1 + 14;
     }
 
-    kcb->short_arr[0] = ((val_2>>3)<<2);
-    kcb->short_arr[2] = (kcb->char_arr[3] + 12) * kcb->char_arr[1] + 2;
-    kcb->font_rect.w = kcb->short_arr[0] / 2;
-    kcb->font_rect.h = kcb->short_arr[2];
+    kcb->width_info = ((val_2>>3)<<2);
+    kcb->height_info = (kcb->char_arr[3] + 12) * kcb->char_arr[1] + 2;
+    kcb->font_rect.w = kcb->width_info / 2;
+    kcb->font_rect.h = kcb->height_info;
 
-    return kcb->short_arr[0] * kcb->short_arr[2] + 32;
+    return kcb->width_info * kcb->height_info + 32;
 }
