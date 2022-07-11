3dfb7e10fe38051918c193ebe6a25e085ea3a53f
fatalis <fatalis@fatalis.pw>
2022-07-11 11:55:19 -0500

    fix consistency of rename names in my previous commits

---
+++ asm/Menu/menu_radio_codec_helper_8004158C/menu_radio_codec_helper_helper17_80038678.s
@@ -0,0 +1,16 @@
+	opt	c+, at+, e+, n-
+	section .text
+
+	xdef menu_radio_codec_helper_helper17_80038678
+menu_radio_codec_helper_helper17_80038678:
+	dw 0x3C02800A ; 0x80038678
+	dw 0x8C42E28C ; 0x8003867C
+	dw 0x03E00008 ; 0x80038680
+	dw 0x00000000 ; 0x80038684
+	dw 0x3C02800C ; 0x80038688
+	dw 0x8C429358 ; 0x8003868C
+	dw 0x03E00008 ; 0x80038690
+	dw 0x00000000 ; 0x80038694
+	dw 0x3C02800C ; 0x80038698
+	dw 0x03E00008 ; 0x8003869C
+	dw 0x24429340 ; 0x800386A0
