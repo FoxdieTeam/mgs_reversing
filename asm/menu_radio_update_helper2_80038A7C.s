0f02a471dfde707da9e9c77cfdcbfe9cbc7a2e09
fatalis <fatalis@fatalis.pw>
2022-07-11 13:52:36 -0500

    name 96 unnamed funcs as helpers of caller

---
+++ asm/menu_radio_update_helper2_80038A7C.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section .text
+
+	xdef menu_radio_update_helper2_80038A7C
+menu_radio_update_helper2_80038A7C:
+	dw 0x3C02800C ; 0x80038A7C
+	dw 0x03E00008 ; 0x80038A80
+	dw 0xA040D538 ; 0x80038A84
