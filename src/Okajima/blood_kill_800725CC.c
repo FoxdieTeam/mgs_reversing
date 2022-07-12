e705165e21cf569f1c3cc8a8f930df05c8157235
twevs <77587819+twevs@users.noreply.github.com>
2022-07-12 19:22:33 +0200

    More matching.

--- src/Okajima/blood_kill_800725CC.c
+++ src/Okajima/blood_kill_800725CC.c
@@ -1,13 +1,13 @@
-
+#include "blood.h"
 
 extern void DG_DequeuePrim_800182E0(int param_1);
 extern void DG_FreePrim_8001BC04(int param_1);
 
-void blood_kill_800725CC(int param_1)
+void blood_kill_800725CC(Actor_Blood* actor_blood)
 {
     int prim;
 
-    prim = *(int *)(param_1 + 0x24);
+    prim = actor_blood->f24;
     if (prim)
     {
         DG_DequeuePrim_800182E0(prim);
