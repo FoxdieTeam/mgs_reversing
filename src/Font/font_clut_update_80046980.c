5ff284f35e24c8fed50ef54f036efc738ce020f8
twevs <77587819+twevs@users.noreply.github.com>
2022-07-17 18:45:29 +0200

    Applied INCLUDE_ASM() changes to font functions.

--- src/Font/font_clut_update_80046980.c
+++
@@ -1,8 +0,0 @@
-#include "kcb.h"
-
-extern void LoadImage_8008FB10(RECT *, unsigned char *);
-
-void font_clut_update_80046980(KCB* kcb)
-{
-    LoadImage_8008FB10(&kcb->font_clut_rect, kcb->font_clut_buffer);
-}
--- src/Font/font_get_buffer_ptr_80044FE8.c
+++
@@ -1,6 +0,0 @@
-#include "kcb.h"
-
-void* font_get_buffer_ptr_80044FE8(KCB* kcb)
-{
-    return kcb->font_clut_buffer;
-}
