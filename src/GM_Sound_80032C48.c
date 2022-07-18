69c70c01a5dad9ab834e506bf10f67cdfd9f68e6
twevs <77587819+twevs@users.noreply.github.com>
2022-07-18 06:26:13 +0200

    Matched rifle_kill(); updated signature of sd_set_cli().

--- src/GM_Sound_80032C48.c
+++ src/GM_Sound_80032C48.c
@@ -1,5 +1,5 @@
-extern void sd_set_cli_800887EC(int code);
+extern void sd_set_cli_800887EC(int code, int unused);
 void GM_Sound_80032C48(int code, int notUsed)
 {
-  sd_set_cli_800887EC(code);
+  sd_set_cli_800887EC(code, notUsed);
 }
