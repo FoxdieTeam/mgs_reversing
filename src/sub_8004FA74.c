31b7a9d1389db04a7f8a267564b9aa215280ed10
Paul <github@paulsapps.com>
2022-07-19 13:50:00 +0100

    draf sna_init struct

--- src/sub_8004FA74.c
+++ src/sub_8004FA74.c
@@ -1,12 +1,10 @@
-
+#include "sna_init.h"
 
 extern void GM_ClearPlayerStatusFlag_8004E2D4(unsigned int param_1);
-extern void sub_8004E344(int param_1, unsigned int param_2);
+extern void sna_init_clear_flags_8004E344(Actor_SnaInit* param_1, unsigned int param_2);
 
-void sub_8004FA74(int param_1)
-
+void sub_8004FA74(Actor_SnaInit* param_1)
 {
-    sub_8004E344(param_1, 0x20);
+    sna_init_clear_flags_8004E344(param_1, 0x20);
     GM_ClearPlayerStatusFlag_8004E2D4(0x8408);
-    return;
 }
