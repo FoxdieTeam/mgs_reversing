fd0d3bcae014375449cb17e8b0ed9f3da0a04193
Paul <github@paulsapps.com>
2022-07-20 13:06:13 +0100

    missed from last commit

--- src/chara/snake/sna_init.h
+++ src/chara/snake/sna_init.h
@@ -4,44 +4,10 @@
 #include "actor.h"
 #include "GM_Control.h"
 #include "game.h"
-
+#include "target.h"
 #include <sys/types.h>
 #include <LIBGTE.h>
 #include <LIBGPU.h>
-
-// TODO: Move to its own file
-typedef struct GM_Target
-{
-    short field_0_flags;
-    short field_2;
-    unsigned short field_4;
-    short field_6_flags;
-    SVECTOR field_8_vec;
-    short field_10;
-    short field_12;
-    short field_14;
-    short field_16;
-    int field_18;
-    int field_1C;
-    MATRIX *field_20;
-    short field_24;
-    short field_26_hp;
-    short field_28;
-    short field_2A;
-    short field_2C_vec;
-    short field_2E;
-    int field_30;
-    short field_34_vec;
-    short field_36;
-    short field_38;
-    short field_3A;
-    short field_3C;
-    short field_3E;
-    short field_40;
-    short field_42;
-    int field_44;
-} GM_Target;
-
 
 typedef struct Sna_E1
 {
