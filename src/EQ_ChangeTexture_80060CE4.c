1ce445104956f61f2a7a627890455e7265f73ac2
Paul <github@paulsapps.com>
2022-07-23 03:17:15 +0100

    match 2 head funcs

--- src/EQ_ChangeTexture_80060CE4.c
+++ src/EQ_ChangeTexture_80060CE4.c
@@ -1,4 +1,5 @@
 #include <util/idaTypes.h>
+#include "game.h"
 
 extern void		face_item_helper_80060CB8(WORD *in, WORD *out);
 extern int		GV_StrCode_80016CCC(const char* string );
@@ -18,3 +19,28 @@ void EQ_ChangeTexture_80060CE4(const char* itemName1, const char* itemName2)
 	face_item_helper_80060CB8(buff, pTexture2 + 1);
 }
 
+// Remove head model
+void EQ_InvisibleHead_80060D5C(OBJECT *pObj, short *pnPacks, short *pRaise)
+{
+    if ( pObj->objs->n_models >= 7 )
+    {
+        *pnPacks = pObj->objs->objs[6].n_packs;
+        *pRaise = pObj->objs->objs[6].raise;
+        DG_FreeObjPacket_8001AAD0(&pObj->objs->objs[6], 0);
+        DG_FreeObjPacket_8001AAD0(&pObj->objs->objs[6], 1);
+        pObj->objs->objs[6].n_packs = 1;
+        pObj->objs->objs[6].raise = -30000;
+    }
+}
+
+// Put head model back
+void EQ_VisibleHead_80060DF0(OBJECT *pObj, short *pnPacks, short *pRaise)
+{
+    if ( pObj->objs->n_models >= 7 )
+    {
+        pObj->objs->objs[6].n_packs = *pnPacks;
+        pObj->objs->objs[6].raise = *pRaise;
+        DG_FreeObjPacket_8001AAD0(&pObj->objs->objs[6], 0);
+        DG_FreeObjPacket_8001AAD0(&pObj->objs->objs[6], 1);
+    }
+}
