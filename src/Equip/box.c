5b66b85494b979a1311eced1be9db75e1a6b13bb
Paul <github@paulsapps.com>
2022-07-23 17:23:20 +0100

    create box.c/h

---
+++ src/Equip/box.c
@@ -0,0 +1,11 @@
+#include "box.h"
+
+#pragma INCLUDE_ASM("asm/Equip/BoxCheckMessage_8006195C.s")
+
+#pragma INCLUDE_ASM("asm/Equip/BoxAct_80061A14.s")
+
+#pragma INCLUDE_ASM("asm/Equip/BoxDie_80061B30.s")
+
+#pragma INCLUDE_ASM("asm/Equip/BoxGetResources_80061BA0.s")
+
+#pragma INCLUDE_ASM("asm/Equip/NewBox_80061C7C.s")
