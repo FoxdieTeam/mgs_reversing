f90c834f518923757480504a5763e7819daded7b
Paul <github@paulsapps.com>
2022-07-24 16:24:03 +0100

    merge vibrate.c

---
+++ src/vibrate.c
@@ -0,0 +1,7 @@
+#include "linker.h"
+
+#pragma INCLUDE_ASM("asm/vibrate_act_helper_8005D358.s")
+#pragma INCLUDE_ASM("asm/vibrate_act_helper_8005D3A4.s")
+#pragma INCLUDE_ASM("asm/vibrate_act_8005D424.s")
+#pragma INCLUDE_ASM("asm/vibrate_init_8005D508.s")
+#pragma INCLUDE_ASM("asm/vibrate_init2_8005D58C.s")
