964decb5323d558dd8c3d46d1c22f7cfa6495bc7
Paul <github@paulsapps.com>
2022-07-24 02:30:57 +0100

    clean sna_init_8004F034

--- src/chara/snake/sna_init.c
+++ src/chara/snake/sna_init.c
@@ -324,22 +324,20 @@ void sna_init_8004EC8C(Actor_SnaInit *pActor)
 
 extern int  sub_8004EFE4(int param_1, int param_2);
 
-void sub_8004F034(int param_1, unsigned int param_2)
+void sna_init_8004F034(Actor_SnaInit *pActor, unsigned int bits)
 {
-    int iVar1;
-
-    if (param_2 != *(unsigned int *)(param_1 + 0x18c))
+    if ( bits != pActor->field_180.field_0C )
     {
-        *(unsigned int *)(param_1 + 0x1a4) = ~param_2;
-        *(unsigned int *)(param_1 + 0x18c) = param_2;
-        if ((param_2 == 0xffff) && (iVar1 = sub_8004EFE4(param_1, 2),  -1 < iVar1))
+        pActor->field_180.field_24 = ~bits;
+        pActor->field_180.field_0C = bits;
+        if ( bits == 0xFFFF && sub_8004EFE4(pActor, 2) >= 0 )
         {
-            *(int *)(param_1 + 0x1c0) = 4;
-            *(short *)(param_1 + 0x1b0) = 2;
+            pActor->field_180.interp = 4;
+            pActor->field_180.field_30 = 2;
         }
     }
-    return;
 }
+
 #pragma INCLUDE_ASM("asm/chara/snake/sna_init_act_helper2_helper4_8004F090.s")
 #pragma INCLUDE_ASM("asm/sub_8004F14C.s")
 #pragma INCLUDE_ASM("asm/sub_8004F204.s")
