14efa0d80230bae7cdadb9529328b07925ef78df
Paul <github@paulsapps.com>
2022-07-24 14:45:22 +0100

    add Actor_kogaku2 struct

---
+++ src/Equip/kogaku2.h
@@ -0,0 +1,23 @@
+#ifndef _KOGAKU2_H_
+#define _KOGAKU2_H_
+
+#include "actor.h"
+#include "Game.h"
+#include "GM_Control.h"
+
+typedef struct Actor_kogaku2
+{
+  Actor field_0_actor;
+  OBJECT *field_20_pObj;
+  int field_24_unit;
+  int field_28_obj_old_flag;
+  int field_2C_ypos2;
+  int field_30_ypos1;
+  int field_34_ypos3_ninja;
+  int field_38_ninja_var;
+  int field_3C_msg_is_8650;
+  int field_40_rgb;
+} Actor_kogaku2;
+
+
+#endif // _KOGAKU2_H_
