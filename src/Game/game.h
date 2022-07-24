b9b2556f4bece5315648cbfc7b6b5cc6f2d7970b
Paul <github@paulsapps.com>
2022-07-24 01:53:24 +0100

    match sna_init_anim_box_stop_800554B4

--- src/Game/game.h
+++ src/Game/game.h
@@ -47,7 +47,7 @@ typedef struct _OBJECT
     unsigned short     field_12;    //0x12
     MOTION_CONTROL    *m_ctrl;      //0x14
     unsigned short     field_18;    //0x18
-    unsigned short     field_1A;    //0x1A
+    short     field_1A;    //0x1A must be signed for sna_init_anim_box_stop_800554B4 to match
     unsigned short     field_1C;    //0x1C
     unsigned short     field_1E;    //0x1C
     unsigned long      field_20;    //0x20
