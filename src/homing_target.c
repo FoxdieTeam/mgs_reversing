9edb6af2dd53125522d8387a1b82d2e9b4d5319f
Paul <github@paulsapps.com>
2022-07-26 19:18:34 +0100

    match HomingTarget_Alloc_80032C8C

--- src/homing_target.c
+++ src/homing_target.c
@@ -1,5 +1,8 @@
 #include "homing_target.h"
 #include "linker.h"
+#include "mts_new.h"
+
+extern const char aHomingTargetOv[];
 
 Homing_Target SECTION(".gHomingTargets_800B8230") gHomingTargets_800B8230[8];
 
@@ -16,7 +19,32 @@ void HomingTarget_Clear_All_80032C68()
     }
 }
 
-#pragma INCLUDE_ASM("asm/Game/HomingTarget_Alloc_80032C8C.s")
+Homing_Target* HomingTarget_Alloc_80032C8C(int a1, int a2)
+{
+    int pos; // $v1
+    Homing_Target* pIter = gHomingTargets_800B8230;
+    for ( pos = 8; pos > 0; --pos )
+    {
+        if ( !pIter->field_C_bUsed )
+        {
+            break;
+        }
+        ++pIter;
+    }
+
+    if ( !pos )
+    {
+        mts_printf_8008BBA0(aHomingTargetOv);
+        return 0;
+        
+    }
+  
+    pIter->field_0 = a1;
+    pIter->field_4 = a2;
+    pIter->field_8 = 0;
+    pIter->field_C_bUsed = 1;
+    return pIter;
+}
 
 void HomingTarget_Free_80032CFC(Homing_Target *pTarget)
 {
