d69175c974310d9b70e0fec4aba308661ac6abdc
Paul <github@paulsapps.com>
2022-07-26 19:04:25 +0100

    match HomingTarget_Free_80032CFC

--- src/homing_target.c
+++ src/homing_target.c
@@ -17,6 +17,14 @@ void HomingTarget_Clear_All_80032C68()
 }
 
 #pragma INCLUDE_ASM("asm/Game/HomingTarget_Alloc_80032C8C.s")
-#pragma INCLUDE_ASM("asm/HomingTarget_Free_80032CFC.s")
+
+void HomingTarget_Free_80032CFC(Homing_Target *pTarget)
+{
+    if ( pTarget )
+    {
+        pTarget->field_C_bUsed = 0;
+    }
+}
+
 #pragma INCLUDE_ASM("asm/HomingTarget_1_80032D10.s")
 #pragma INCLUDE_ASM("asm/HomingTarget_2_80032EAC.s")
