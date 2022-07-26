27635908afb11abddd4e6d9433ac931aafd93125
Paul <github@paulsapps.com>
2022-07-26 20:08:11 +0100

    merge menuman.c

--- src/Menu/MENU_GetPrimInfo_80038D68.c
+++
@@ -1,8 +0,0 @@
-#include "menuMan.h"
-
-extern MenuGlue gMenuPrimBuffer_8009E2D0;
-
-MenuGlue* MENU_GetPrimInfo_80038D68(void)
-{
-  return &gMenuPrimBuffer_8009E2D0;
-}
--- src/menu_radio_update_helper_80038A6C.c
+++
@@ -1,8 +0,0 @@
-#include "menuMan.h"
-
-extern MenuMan gMenuMan_800BD360;
-
-void menu_radio_update_helper_80038A6C(void)
-{
-    gMenuMan_800BD360.field_1D8 = 1;
-}
