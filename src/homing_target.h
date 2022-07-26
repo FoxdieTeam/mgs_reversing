2b36f600caf63fb4d3ccaa51c3272c0d73560d1c
Paul <github@paulsapps.com>
2022-07-26 19:00:09 +0100

    merge homing_target objs

---
+++ src/homing_target.h
@@ -0,0 +1,12 @@
+#ifndef _HOMING_TARGET_H_
+#define _HOMING_TARGET_H_
+
+typedef struct Homing_Target
+{
+  int field_0; // ptr
+  int field_4; // ptr
+  int field_8; // alive
+  int field_C_bUsed; // auto_aimable 
+} Homing_Target;
+
+#endif // _HOMING_TARGET_H_
