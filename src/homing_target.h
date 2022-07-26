90d4b3d4167b4498f953595de777046d47f4f464
Paul <github@paulsapps.com>
2022-07-26 19:31:30 +0100

    match HomingTarget_1_80032D10

--- src/homing_target.h
+++ src/homing_target.h
@@ -1,10 +1,15 @@
 #ifndef _HOMING_TARGET_H_
 #define _HOMING_TARGET_H_
 
+#include <sys/types.h>
+#include <LIBGTE.h>
+#include <LIBGPU.h>
+#include "GM_Control.h"
+
 typedef struct Homing_Target
 {
-  int field_0; // ptr
-  int field_4; // ptr
+  MATRIX* field_0; // ptr
+  GM_Control* field_4; // ptr
   int field_8; // alive
   int field_C_bUsed; // auto_aimable 
 } Homing_Target;
