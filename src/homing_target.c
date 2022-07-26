9f607c19827500e95722b29f861060a96da74e50
Paul <github@paulsapps.com>
2022-07-26 19:03:27 +0100

    match HomingTarget_Clear_All_80032C68

--- src/homing_target.c
+++ src/homing_target.c
@@ -1,6 +1,21 @@
 #include "homing_target.h"
+#include "linker.h"
 
-#pragma INCLUDE_ASM("asm/Game/HomingTarget_Clear_All_80032C68.s")
+Homing_Target SECTION(".gHomingTargets_800B8230") gHomingTargets_800B8230[8];
+
+void HomingTarget_Clear_All_80032C68()
+{
+    Homing_Target *pIter; // $v0
+    int i; // $v1
+
+    pIter = gHomingTargets_800B8230;
+    for ( i = 8; i > 0; --i )
+    {
+        pIter->field_C_bUsed = 0;
+        ++pIter;
+    }
+}
+
 #pragma INCLUDE_ASM("asm/Game/HomingTarget_Alloc_80032C8C.s")
 #pragma INCLUDE_ASM("asm/HomingTarget_Free_80032CFC.s")
 #pragma INCLUDE_ASM("asm/HomingTarget_1_80032D10.s")
