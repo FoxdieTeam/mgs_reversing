9e5984f3c23aec67f2ede55d26eaa25501e4f971
Paul <github@paulsapps.com>
2022-07-27 19:58:08 +0100

    use correct types

--- src/Equip/kogaku2.c
+++ src/Equip/kogaku2.c
@@ -4,7 +4,7 @@
 extern int GV_Clock_800AB920;
 
 extern void EQ_InvisibleUnit_80060E68(int param_1, int param_2, int param_3);
-extern void DG_WriteObjPacketUV_8001A774(int param_1, int param_2);
+void DG_WriteObjPacketUV_8001A774(DG_OBJ *pObj, int idx);
 
 #pragma INCLUDE_ASM("asm/Equip/kogaku2_tpage_uv_update_80060F98.s")
 POLY_GT4* kogaku2_tpage_uv_update_80060F98(POLY_GT4 *pPrims, int pack_count);
@@ -60,31 +60,30 @@ void kogaku2_update_prims2_800612BC(Actor_kogaku2 *pActor)
     }
 }
 
-void kogaku2_kill_helper_80061384(int param_1)
+void kogaku2_kill_helper_80061384(Actor_kogaku2 *pActor)
 {
-    int iVar1;
-    int iVar2;
-    int iVar3;
+    DG_OBJS *objs; // $a2
+    DG_OBJ *pIter; // $s0
+    int n_models; // $s1
 
-    iVar1 = **(int **)(param_1 + 0x20);
-    iVar2 = iVar1 + 0x48;
-    iVar3 = (int) *(short *)(iVar1 + 0x2e);
-    *(unsigned int *)(iVar1 + 0x28) = (*(unsigned int *)(iVar1 + 0x28) & 0x80) | (*(unsigned int *)(param_1 + 0x28) & 0xffffff7f);
-    while (0 < iVar3)
+    objs = pActor->field_20_pObj->objs;
+    pIter = objs->objs;
+    n_models = objs->n_models;
+    objs->flag = (objs->flag & 0x80) | (pActor->field_28_obj_old_flag & ~0x80u);
+    while ( n_models > 0 )
     {
-        DG_WriteObjPacketUV_8001A774(iVar2, 0);
-        DG_WriteObjPacketUV_8001A774(iVar2, 1);
-        iVar3 = iVar3 + -1;
-        iVar2 = iVar2 + 0x5c;
+        DG_WriteObjPacketUV_8001A774(pIter, 0);
+        DG_WriteObjPacketUV_8001A774(pIter, 1);
+        ++pIter;
+        --n_models;
     }
-    return;
 }
 
 #pragma INCLUDE_ASM("asm/Equip/kogaku2_act_800613FC.s")
 
-void kogaku2_kill_80061508(int param_1)
+void kogaku2_kill_80061508(Actor_kogaku2 *pActor)
 {
-    kogaku2_kill_helper_80061384(param_1);
+    kogaku2_kill_helper_80061384(pActor);
 }
 
 #pragma INCLUDE_ASM("asm/Equip/kogaku2_act_helper_80061528.s")
