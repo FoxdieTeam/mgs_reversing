9f6f85da60450869c698d690114af2acf79613e9
Paul <paul@paulsapps.com>
2022-07-27 16:26:01 +0100

    depend on header files properly in the build graph

---
+++ build/hash_include_msvc_formatter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def main(path, output):
+    with open(path) as f:
+        lines = f.readlines()
+
+    with open(output, 'w') as f:
+        for raw_line in lines:
+            line = raw_line.strip()
+            # change C:\blah.obj : C:\foo.h \ to just C:\foo.h \
+            pos = line.find(":", 3)
+            if pos != -1:
+                line = line[pos+1:]
+            if line.endswith(" \\"):
+                line = line[:-2]
+            line = line.replace('\\', '/')
+            line = line.strip()
+            if len(line) > 0:
+                # ignore psyq headers
+                if line.find("4.4") == -1 and line.find("4.3") == -1:
+                    f.write(line + "\n")
+                    print("Note: including file: " + line)
+
+if __name__ == '__main__':
+    src = sys.argv[1].replace('\\', '/')
+    dst = sys.argv[2].replace('\\', '/')
+    #src= "C:\\data\\mgs_reversing\\obj\\anime.c.preproc.headers"
+    #dst= "C:\\data\\mgs_reversing\\obj\\anime.c.preproc.headers_fixed" 
+    main(src, dst)
