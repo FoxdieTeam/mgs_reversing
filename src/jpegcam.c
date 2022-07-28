d988adac6154a2ac8836f01cacc685306c467686
fatalis <fatalis@users.noreply.github.com>
2022-07-28 13:15:40 -0500

    2 easy matches, fix warning (#435)

--- src/jpegcam.c
+++ src/jpegcam.c
@@ -1,6 +1,13 @@
-
 #pragma INCLUDE_ASM("asm/Equip/jpegcam_unk1_80063704.s")
-#pragma INCLUDE_ASM("asm/Equip/jpegcam_unk2_80063888.s")
+
+extern char aPHOTO[]; // "PHOTO %02d\n"
+extern int sprintf_8008E878(char *buffer, const char *format, ...);
+
+void jpegcam_unk2_80063888(char *param_1, int param_2)
+{
+    sprintf_8008E878(param_1, aPHOTO, *(char *)(param_2 + 6) - 0x40);
+}
+
 #pragma INCLUDE_ASM("asm/Equip/jpegcam_unk3_800638B4.s")
 #pragma INCLUDE_ASM("asm/Equip/jpegcam_act_helper2_helper_8006392C.s")
 #pragma INCLUDE_ASM("asm/Equip/jpegcam_act_helper3_helper_helper_helper3_80063988.s")
