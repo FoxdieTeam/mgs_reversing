e19f3e0bada6c51b6777dcea67096e02bdf6bce1
Paul <github@paulsapps.com>
2022-07-30 16:02:53 +0100

    merge amissile.c

---
+++ src/Bullet/amissile.c
@@ -0,0 +1,14 @@
+
+#pragma INCLUDE_ASM("asm/Bullet/amissile_loader_helper_8006D1F4.s")
+#pragma INCLUDE_ASM("asm/Bullet/amissile_act_helper_8006D2A0.s")
+#pragma INCLUDE_ASM("asm/Bullet/amissile_act_helper_8006D37C.s")
+
+int amissile_act_helper_8006D600(void)
+{
+  return 0;
+}
+
+#pragma INCLUDE_ASM("asm/Bullet/amissile_act_8006D608.s")
+#pragma INCLUDE_ASM("asm/Bullet/amissile_kill_8006D99C.s")
+#pragma INCLUDE_ASM("asm/Bullet/amissile_loader_8006DA0C.s")
+#pragma INCLUDE_ASM("asm/Bullet/NewAMissile_8006DC50.s")
