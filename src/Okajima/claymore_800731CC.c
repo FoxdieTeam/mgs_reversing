ce3e372ad391e4dfaf0563950f2d1f56e5834f1e
Paul <github@paulsapps.com>
2022-07-30 15:22:50 +0100

    merge claymore.c

--- src/Okajima/claymore_800731CC.c
+++
@@ -1,9 +0,0 @@
-#include "linker.h"
-#include "libdg.h"
-
-extern SVECTOR stru_8009F630[4];
-
-void claymore_800731CC(SVECTOR *param_1)
-{
-    DG_PutVector_8001BE48(stru_8009F630, param_1, 4); // 4 = sizeof?
-}
