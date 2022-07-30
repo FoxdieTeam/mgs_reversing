47fadf86c744210331bfcc4f059f70b16d32065f
Paul <github@paulsapps.com>
2022-07-31 00:20:11 +0100

    match AN_Stn_G_Sonic_80074CA4

--- src/Okajima/stngrnd.c
+++ src/Okajima/stngrnd.c
@@ -1,3 +1,8 @@
+#include "anime.h"
+
+extern Anim_Data stru_8009F670;
+extern SVECTOR DG_ZeroVector_800AB39C;
+
 extern void stngrnd_free_80074844(int param_1, int param_2);
 
 #pragma INCLUDE_ASM("asm/Okajima/stngrnd_loader2_80074644.s")
@@ -13,5 +18,18 @@ void stngrnd_kill_800748B8(int param_1)
 
 #pragma INCLUDE_ASM("asm/Okajima/stngrnd_loader_800748D8.s")
 #pragma INCLUDE_ASM("asm/Okajima/NewStanBlast_80074B5C.s")
-#pragma INCLUDE_ASM("asm/Okajima/AN_Stn_G_Sonic_80074CA4.s")
+
+
+void AN_Stn_G_Sonic_80074CA4(SVECTOR *pVec)
+{
+    anime_data_0x14 data; // [sp+10h] [-18h] BYREF
+
+    data.field_0_vec = *pVec;
+    data.field_8_vec = DG_ZeroVector_800AB39C;
+    data.field_12 = 0;
+    data.field_10_anim_idx = 0;
+    stru_8009F670.field_14 = &data;
+    anime_init_8005FBC8(0, 0, &stru_8009F670);
+}
+
 #pragma INCLUDE_ASM("asm/Okajima/AN_Stn_G_Center_80074D28.s")
