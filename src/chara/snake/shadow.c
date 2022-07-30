8c25fc85dcb2ddf137bdf08c329e721f0b1017fe
Paul <github@paulsapps.com>
2022-07-30 22:12:44 +0100

    match shadow_act_helper_80060028

--- src/chara/snake/shadow.c
+++ src/chara/snake/shadow.c
@@ -1,9 +1,30 @@
-#include "game.h"
+#include "shadow.h"
 
 extern void GM_FreeObject_80034BF8(OBJECT *obj);
 
 #pragma INCLUDE_ASM("asm/shadow_act_helper_8005FD28.s")
-#pragma INCLUDE_ASM("asm/shadow_act_helper_80060028.s")
+
+MATRIX* RotMatrixY_80093BC8(long r, MATRIX *m);
+int GV_NearExp8_800263E4(int arg0, int arg1);
+
+void shadow_act_helper_80060028(Actor_Shadow *pActor)
+{
+    DG_OBJS *objs = pActor->field_28_obj.objs;
+    RotMatrixY_80093BC8(pActor->field_20_ctrl->field_8_vec.vy, &objs->world);
+    objs->world.t[0] = pActor->field_24_pObj->objs->world.t[0];
+    objs->world.t[2] = pActor->field_24_pObj->objs->world.t[2];
+    objs->world.t[1] = pActor->field_20_ctrl->field_78;
+
+    if ( pActor->field_20_ctrl->field_0_position.vy - pActor->field_20_ctrl->field_78 < 450 )
+    {
+        objs->objs[0].raise = -500;
+    } 
+    else
+    {
+        objs->objs[0].raise = GV_NearExp8_800263E4(objs->objs[0].raise, 150);
+    }
+}
+
 #pragma INCLUDE_ASM("asm/shadow_act_800600E4.s")
 
 void shadow_kill_80060190(int param_1)
