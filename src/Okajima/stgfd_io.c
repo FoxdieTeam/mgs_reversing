3d7b31cbccffcd20501cc5ab839137953a1b852e
Paul <github@paulsapps.com>
2022-07-31 02:44:06 +0100

    corrected types in NewStnFade_800752A0

--- src/Okajima/stgfd_io.c
+++ src/Okajima/stgfd_io.c
@@ -127,20 +127,18 @@ int stgfd_io_loader_80075194(Actor_stgfd_io *pActor)
 
 struct Actor *NewStnFade_800752A0(void)
 {
-    struct Actor *actor;
-
-    actor = GV_NewActor_800150E4(3, 0x40);
+    Actor_stgfd_io *actor = GV_NewActor_800150E4(3, sizeof(Actor_stgfd_io));
     if (actor)
     {
         GV_SetNamedActor_8001514C(actor,
-                            (TActorFunction)&stgfd_io_act_80074F5C,
-                            (TActorFunction)&stgfd_io_kill_80075164,
+                            (TActorFunction)stgfd_io_act_80074F5C,
+                            (TActorFunction)stgfd_io_kill_80075164,
                             aStgfdIoC);
-        if (stgfd_io_loader_80075194(actor) < 0)
+        if (stgfd_io_loader_80075194(&actor->field_0_actor) < 0)
         {
-            GV_DestroyActor_800151C8(actor);
+            GV_DestroyActor_800151C8(&actor->field_0_actor);
             return 0;
         }
     }
-    return actor;
+    return &actor->field_0_actor;
 }
