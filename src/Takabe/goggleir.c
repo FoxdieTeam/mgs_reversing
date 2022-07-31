6bb264e3aef8452f02d86448055c84271c7e571a
Paul <github@paulsapps.com>
2022-07-31 16:38:50 +0100

    merge goggleir.c

--- src/Takabe/goggleir.c
+++ src/Takabe/goggleir.c
@@ -2,10 +2,17 @@
 
 extern char aGoggleirC[]; // = "goggleir.c"
 
-extern int goggleir_act_80078BE0(int a1);
-extern int goggleir_kill_80078CE4(int a1);
+extern void goggleir_act_80078BE0(Actor_GoggleIr* pActor);
+extern void goggleir_kill_80078CE4(Actor_GoggleIr* pActor);
 extern void GV_DestroyActor_800151C8(struct Actor *pActor);
 extern int  goggleir_loader_80078D8C(Actor_GoggleIr *a1, OBJECT *a2);
+
+#pragma INCLUDE_ASM("asm/goggleir_800789E0.s")
+#pragma INCLUDE_ASM("asm/goggleir_act_helper_80078AB8.s")
+#pragma INCLUDE_ASM("asm/goggleir_act_80078BE0.s")
+#pragma INCLUDE_ASM("asm/goggleir_kill_80078CE4.s")
+#pragma INCLUDE_ASM("asm/goggleir_loader_80078D8C.s")
+
 
 Actor_GoggleIr *NewGoggleIr_80078E6C(int a1, OBJECT *parent_obj)
 {
