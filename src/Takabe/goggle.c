69d6b39d037fdd91db2791e344ff217b1015e6dc
Paul <github@paulsapps.com>
2022-08-01 01:14:51 +0100

    match goggle_act_800775B4

--- src/Takabe/goggle.c
+++ src/Takabe/goggle.c
@@ -3,6 +3,7 @@
 #include "GM_Control.h"
 #include "object.h"
 #include "strcode.h"
+#include "map.h"
 
 extern const char aGoggleC[]; // = "goggle.c"
 extern const char aGoggles_0[];
@@ -28,17 +29,58 @@ void EQ_VisibleHead_80060DF0(OBJECT *pObj, short *pnPacks, short *pRaise);
 
 void DG_ResetExtPaletteMakeFunc_800791E4();
 
-extern int goggle_act_800775B4(int a1);
+struct Actor* new_scn_mask_8007895C(int a1);
+
 extern void GV_DestroyActor_800151C8(struct Actor *pActor);
 extern int  goggle_loader_8007773C(Actor_google *a1, OBJECT *a2);
 
 extern int GM_GameStatus_800AB3CC;
+extern int DG_CurrentGroupID_800AB968;
 
 Actor* gglmng_init_800779B8(int type);
 
-#pragma INCLUDE_ASM("asm/Takabe/goggle_8007743C.s") // 132 bytes
-#pragma INCLUDE_ASM("asm/Takabe/goggle_act_helper_800774C0.s") // 244 bytes
-#pragma INCLUDE_ASM("asm/Takabe/goggle_act_800775B4.s") // 248 bytes
+void set_pal_effect_fns_80079194(void* fn1, void* fn2);
+
+#pragma INCLUDE_ASM("asm/Takabe/goggle_pal_convert_8007743C.s") // 132 bytes
+void goggle_pal_convert_8007743C();
+
+#pragma INCLUDE_ASM("asm/Takabe/goggle_pal_cb_800774C0.s") // 244 bytes
+void goggle_pal_cb_800774C0();
+
+void goggle_act_800775B4(Actor_google *pActor)
+{
+    DG_OBJS *objs; // $a1
+    int new_map; // $a0
+
+    if ( pActor->field_4C_head_hidden )
+    {
+        objs = pActor->field_20_obj.objs;
+        new_map = pActor->field_44_pCtrl->field_2C_map->field_0_map_index_bit;
+        DG_SetObjectGroupId(pActor->field_20_obj.objs, DG_CurrentGroupID_800AB968);
+
+        GM_CurrentMap_800AB9B0 = new_map;
+        if ( pActor->field_48_pObj->objs->flag & 0x80 )
+        {
+            pActor->field_20_obj.objs->flag |= 0x80u;
+        }
+        else
+        {
+            pActor->field_20_obj.objs->flag &= ~0x80u;
+        }
+    }
+
+    if ( pActor->field_50 == 2 )
+    {
+        GM_GameStatus_800AB3CC |= 4u;
+        set_pal_effect_fns_80079194(goggle_pal_cb_800774C0, goggle_pal_convert_8007743C);
+        pActor->field_54_pScn_mask = new_scn_mask_8007895C(0);
+    }
+
+    if ( pActor->field_50 < 11 )
+    {
+        pActor->field_50++;
+    }
+}
 
 void goggle_kill_800776AC(Actor_google *pActor)
 {
