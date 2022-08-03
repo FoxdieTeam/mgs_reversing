ea0cf0be398a983a458d580f2ff49b378b4cfd4b
twevs <77587819+twevs@users.noreply.github.com>
2022-08-03 01:53:56 +0100

    updata_data.py: fixed data overwriting bug; added overlapping data warnings. Removed old definition of Snake flags. Added .clang-format file.

--- build/ghidra_scripts/update_data.py
+++ build/ghidra_scripts/update_data.py
@@ -94,6 +94,9 @@ def strip_braces(string):
             ret += i
     return ret 
 
+defined_data_ranges = []
+updated_keys = set()
+
 for root, subdirs, files in os.walk(root_dir + '/src'):
 
     for filename in files:
@@ -134,12 +137,25 @@ for root, subdirs, files in os.walk(root_dir + '/src'):
                 var_name = pieces[pieces_base_index + 2]
                 var_name = var_name.replace(';', '')
 
+                print_debug_info = False # var_name.__contains__('800B05A8') or var_name.__contains__('800B77E8')
+
+                if print_debug_info:
+                    print('line == ' + line)
+
                 # Do we have an array?
                 if var_name.__contains__(']'):
+                    
+                    if print_debug_info:
+                        print('detected array')
+
                     penultimate_char = var_name[len(var_name) - 2]
                     num_elements = 1 # For the missing comma, assuming we're not going to end with a comma.
                     
                     if penultimate_char == '[':
+                        
+                        if print_debug_info:
+                            print('number of elements not specified, counting')
+
                         # Count elements ourselves.
                         next_line_index = index
                         decl_braces_counted = False
@@ -156,37 +172,111 @@ for root, subdirs, files in os.walk(root_dir + '/src'):
                             next_line_index += 1
                             
                     else:
+
+                        if print_debug_info:
+                            print('number of elements specified, retrieving')
+
                         number_regex = re.findall(r'\[([A-Za-z0-9_]+)\]', var_name)
                         if len(number_regex) == 0:
                             continue
+
+                        if print_debug_info:
+                            print('regex found a possible number')
                             
                         possible_number = number_regex[0]
                         if possible_number.isdigit():
                             num_elements = int(possible_number, 10)
+
+                            if print_debug_info:
+                                print('found literal with value == ' + str(num_elements))
+
                         elif possible_number in defines:
                             if (defines[possible_number].isdigit()):
                                 num_elements = int(defines[possible_number], 10)
+
+                                if print_debug_info:
+                                    print('found use of #define with value == ' + str(num_elements))
+
                         else:
+
+                            if print_debug_info:
+                                print('found use of #define not yet put in dictionary')
+
                             # @todo: fix headers that contain relevant #defines possibly being read after the files that need them.
                             continue
                     
                     key = var_name.partition('[')[0]
-                    if key in symbol_address_map:
+                    if key not in updated_keys and key in symbol_address_map:
                         address = symbol_address_map[key]
+
+                        if print_debug_info:
+                            print('key found in symbol-address map with address == ' + address)
+
                         if ishex(address) and len(address) == 8 and address.startswith('80'):
+
+                            if print_debug_info:
+                                print('valid address, adding array')
+
                             data_type_array = ArrayDataType(data_type, num_elements, data_type.getLength())
+
                             address_as_int = int(address, 16)
+                            end_address_as_int = address_as_int + data_type_array.getLength() - 1
+                            for start_end_pairs in defined_data_ranges:
+                                defined_data_start = start_end_pairs[0]
+                                defined_data_end = start_end_pairs[1]
+                                if address_as_int <= defined_data_end and end_address_as_int >= defined_data_start:
+                                    print('WARNING: OVERLAPPING DATA')
+                                    print(var_name + ' in range ' + base_address + ' - ' + hex(end_address_as_int))
+                                    print('overlaps ' + getSymbolAt(toAddr(defined_data_start)).getName() + ' in range ' + hex(defined_data_start) + ' - ' + hex(defined_data_end))
+                                    print('*****************************')
+                            defined_data_ranges.append((address_as_int, end_address_as_int))
                             start_address = toAddr(address_as_int)
-                            end_address = toAddr(address_as_int + data_type_array.getLength())
+                            end_address = toAddr(end_address_as_int)
+
                             clearListing(start_address, end_address)
                             DataUtilities.createData(currentProgram, start_address, data_type_array, data_type_array.getLength(), True, DataUtilities.ClearDataMode.CLEAR_SINGLE_DATA)
+
+                            updated_keys.add(key)
+
+                            if print_debug_info:
+                                # exit()
+                                print('*****************************')
                         
                 else:
-                    if var_name in symbol_address_map:
+
+                    if print_debug_info:
+                        print('detected single element')
+
+                    if var_name not in updated_keys and var_name in symbol_address_map:
                         base_address = symbol_address_map[var_name]
+
+                        if print_debug_info:
+                            print('key found in symbol-address map with base_address == ' + base_address)
+
                         if ishex(base_address) and len(base_address) == 8 and base_address.startswith('80'):
+
+                            if print_debug_info:
+                                print('valid address, adding element')
+
                             address_as_int = int(base_address, 16)
+                            end_address_as_int = address_as_int + data_type.getLength() - 1
+                            for start_end_pairs in defined_data_ranges:
+                                defined_data_start = start_end_pairs[0]
+                                defined_data_end = start_end_pairs[1]
+                                if address_as_int <= defined_data_end and end_address_as_int >= defined_data_start:
+                                    print('WARNING: OVERLAPPING DATA')
+                                    print(var_name + ' in range ' + base_address + ' - ' + hex(end_address_as_int))
+                                    print('overlaps ' + getSymbolAt(toAddr(defined_data_start)).getName() + ' in range ' + hex(defined_data_start) + ' - ' + hex(defined_data_end))
+                                    print('*****************************')
+                            defined_data_ranges.append((address_as_int, end_address_as_int))
                             start_address = toAddr(address_as_int)
-                            end_address = toAddr(address_as_int + data_type.getLength())
+                            end_address = toAddr(end_address_as_int)
+
                             clearListing(start_address, end_address)
                             DataUtilities.createData(currentProgram, start_address, data_type, data_type.getLength(), True, DataUtilities.ClearDataMode.CLEAR_SINGLE_DATA)
+
+                            updated_keys.add(var_name)
+
+                            if print_debug_info:
+                                # exit()
+                                print('*****************************')
