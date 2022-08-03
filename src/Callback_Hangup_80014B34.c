4954556c0265ab4d3f25c07d8328167ce4c3341b
Jayveer <jayveerk@gmail.com>
2022-08-04 00:39:49 +0100

    add some file info to the linker file

--- src/Callback_Hangup_80014B34.c
+++
@@ -1,8 +0,0 @@
-extern const char* GV_DebugMes_800AB34C;
-extern const char aHangupS[];
-void mts_printf_8008BBA0(const char*, ...);
-
-void Callback_Hangup_80014B34(void)
-{
-  mts_printf_8008BBA0(aHangupS, GV_DebugMes_800AB34C);
-}
