6c4d48e2c6090ea1e632595c01aae4ad53f23f70
Paul <github@paulsapps.com>
2022-08-03 02:43:47 +0100

    match FS_StreamClose_80024098

--- src/libfs/FS_StreamIsEnd_800240D0.c
+++ src/libfs/FS_StreamIsEnd_800240D0.c
@@ -1,8 +1,19 @@
 #include "linker.h"
+#include "mts_new.h"
 
-int SECTION(".int_800B5298") int_800B5298;
+int SECTION(".int_800B5298") fs_stream_ref_count_800B5298;
+
+void mts_lock_sem_8008A6CC(int taskNr);
+void mts_unlock_sem_8008A85C(int taskNum);
+
+void FS_StreamClose_80024098()
+{
+    mts_lock_sem_8008A6CC(1);
+    --fs_stream_ref_count_800B5298;
+    mts_unlock_sem_8008A85C(1);
+}
 
 int FS_StreamIsEnd_800240D0(void)
 {
-    return int_800B5298 == 0;
+    return fs_stream_ref_count_800B5298 == 0;
 }
