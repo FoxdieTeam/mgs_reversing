dc8bb0ee6256d27fc21519194386634932fd9a7a
Paul <github@paulsapps.com>
2022-08-03 17:56:36 +0100

    match sub_8003214C

--- src/Prim_free_colour_buffer_80032110.c
+++ src/Prim_free_colour_buffer_80032110.c
@@ -1,7 +1,11 @@
 #include "linker.h"
 #include "libdg.h"
+#include "dgd.h"
+
+extern DG_CHNL  DG_Chanls_800B1800[3];
 
 extern void GV_Free_80016230(void *ptr);
+extern int ratan2_80094308(int,int);
 
 void Prim_free_colour_buffer_80032110(DG_OBJS *pPrim)
 {
@@ -11,4 +15,14 @@ void Prim_free_colour_buffer_80032110(DG_OBJS *pPrim)
         GV_Free_80016230(pBuffer);
         pPrim->objs[0].rgbs = 0;
     }
+}
+
+void sub_8003214C(SVECTOR *pVec, int *pRet)
+{
+    DG_CHNL* pChanl = &DG_Chanls_800B1800[1];
+    MATRIX* mtx = &pChanl->dword_6BC39C;
+    pVec->vx = mtx->t[0];
+    pVec->vy = mtx->t[1];
+    pVec->vz = mtx->t[2];
+    *pRet = ratan2_80094308(mtx->m[0][2], mtx->m[2][2]);
 }
