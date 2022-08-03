b53a34d3fbf02fc8fcee3881859a6a5d662f804d
Paul <github@paulsapps.com>
2022-08-03 02:34:34 +0100

    match FS_StreamTickStart_800243D8

--- src/libfs/FS_StreamIsForceStop_800243C8.c
+++ src/libfs/FS_StreamIsForceStop_800243C8.c
@@ -9,3 +9,15 @@ int FS_StreamIsForceStop_800243C8(void)
 {
     return fs_stream_is_force_stop_8009D518;
 }
+
+// TODO: the var might be part of a struct and the code
+// takes a ptr to that struct, unknown currently
+static inline int* GetTicksPtr(void)
+{
+    return &fs_stream_tick_start_8009D510;
+}
+
+void FS_StreamTickStart_800243D8()
+{
+    *GetTicksPtr() = VSync_80098108(-1);    
+}
