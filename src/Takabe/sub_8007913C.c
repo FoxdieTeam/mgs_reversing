5ae6faddd347707a3e866987dac42535e842f928
Kieron Josephs <kieron.josephs00@gmail.com>
2022-08-04 13:24:42 +0100

    Match sub_8007913C

---
+++ src/Takabe/sub_8007913C.c
@@ -0,0 +1,15 @@
+extern int dword_8009F728;
+extern void (*pfn_800BDFB0)(void);
+
+void DG_StorePaletteEffect_80078F30(void);
+
+void sub_8007913C(void)
+{
+	dword_8009F728 &= ~0x1;
+    
+    if ((dword_8009F728 & 2) != 0) {
+        (*pfn_800BDFB0)();
+    } else {
+        DG_StorePaletteEffect_80078F30();
+    }
+}
