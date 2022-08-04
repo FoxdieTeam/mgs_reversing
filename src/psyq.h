0af614fc2f4305d6ea4a905f37ab28553b68fdc5
Kieron Josephs <kieron.josephs00@gmail.com>
2022-08-04 14:56:13 +0100

    Fix includes for psyq.h

--- src/psyq.h
+++ src/psyq.h
@@ -1,4 +1,6 @@
-#include <LIBGTE.h>
+#include <SYS/TYPES.H>
+#include <LIBGTE.H>
+#include <LIBGPU.H>
 
 void ReadRotMatrix_80092DD8(MATRIX *m);
 int SquareRoot0_80092708(int);
@@ -6,3 +8,4 @@ int Square0_80093340(VECTOR*, VECTOR*);
 int ratan2_80094308(int,int);
 int rsin_80092508(int a1);
 int rcos_800925D8(int a1);
+int StoreImage_8008FB70(RECT*, u_long*);
