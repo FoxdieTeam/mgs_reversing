68af57eb98068a9b2f37351bfaa9f84ca9f7114d
Paul <github@paulsapps.com>
2022-08-05 20:48:53 +0100

    move couple of bss vars

--- src/data/bss.c
+++ src/data/bss.c
@@ -1,9 +1,13 @@
 #include "bss.h"
 #include "linker.h"
-
+#include "libgv\actor.h"
 
 unsigned char SECTION(".bss") main_task_stack_800ABBF0[2048]; // 800ABBEC
 
 unsigned int SECTION(".bss") sdStack_800AC3F0[512]; // 800AC3F0
 
-int SECTION(".gDiskNum_800ACBF0") gDiskNum_800ACBF0;
+int SECTION(".bss") gDiskNum_800ACBF0;
+int SECTION(".bss") pad_800ACBF0;
+
+struct Actor SECTION(".bss") gGVActor_800acbf8;
+
