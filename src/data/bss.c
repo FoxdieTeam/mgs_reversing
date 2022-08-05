3946d27ad5c078bd4bc4c43c844178566f3cfb71
Paul <github@paulsapps.com>
2022-08-05 20:40:01 +0100

    actually make .bss be bss

--- src/data/bss.c
+++ src/data/bss.c
@@ -2,8 +2,8 @@
 #include "linker.h"
 
 
-unsigned char SECTION(".0x800ABBF0") main_task_stack_800ABBF0[2048]; // 800ABBEC
+unsigned char SECTION(".bss") main_task_stack_800ABBF0[2048]; // 800ABBEC
 
-unsigned int SECTION(".0x800AC3F0") sdStack_800AC3F0[512]; // 800AC3F0
+unsigned int SECTION(".bss") sdStack_800AC3F0[512]; // 800AC3F0
 
 int SECTION(".gDiskNum_800ACBF0") gDiskNum_800ACBF0;
