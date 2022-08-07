d7065e02479822b8b3b75b4103abb30be7f0cb10
Kieron Josephs <kieron.josephs00@gmail.com>
2022-08-07 23:13:46 +0100

    Match NewStnSight_800693E0

--- src/Weapon/stnsight.h
+++ src/Weapon/stnsight.h
@@ -1,6 +1,21 @@
 #ifndef _STNSIGHT_H_
 #define _STNSIGHT_H_
 
-// stinger first person HUD?
+#include "libgv/libgv.h"
+
+// stinger first person HUD
+
+typedef struct Actor_stnsight
+{
+    Actor actor;
+    int   field_20_pad[30];
+} Actor_stnsight;
+
+STATIC_ASSERT_SIZE(Actor_stnsight, 0x98);
+
+void stnsight_act_80068D0C(Actor_stnsight *actor);
+void stnsight_kill_80068ED8(Actor_stnsight *actor);
+int stnsight_init_helper_800692D0(Actor_stnsight *actor, int type);
+Actor_stnsight * NewStnSight_800693E0(int type);
 
 #endif // _STNSIGHT_H_
