fea61eb2c14b09941d9e5867ab81d1e3c54228bd
Paul <github@paulsapps.com>
2022-08-08 00:21:58 +0100

    merge sd_ioset.c

---
+++ src/SD/sd_ioset.c
@@ -0,0 +1,15 @@
+#include "SD/sd.h"
+
+#pragma INCLUDE_ASM("asm/SD/SD_spuwr_80087A88.s")
+#pragma INCLUDE_ASM("asm/sound_off_80087DAC.s")
+#pragma INCLUDE_ASM("asm/SD/sng_off_80087E2C.s")
+#pragma INCLUDE_ASM("asm/SD/se_off_80087E94.s")
+#pragma INCLUDE_ASM("asm/SD/sng_pause_80087EF4.s")
+#pragma INCLUDE_ASM("asm/SD/sng_pause_off_80087F24.s")
+#pragma INCLUDE_ASM("asm/SD/keyon_80087F58.s")
+#pragma INCLUDE_ASM("asm/SD/keyoff_80087F80.s")
+#pragma INCLUDE_ASM("asm/SD/tone_set_80087FA8.s")
+#pragma INCLUDE_ASM("asm/SD/pan_set2_800882E4.s")
+#pragma INCLUDE_ASM("asm/SD/vol_set_80088320.s")
+#pragma INCLUDE_ASM("asm/SD/freq_set_800885D4.s")
+#pragma INCLUDE_ASM("asm/SD/drum_set_80088694.s")
