0a9ee163c09e7ba18ba3b451193695c458708993
Paul <github@paulsapps.com>
2022-08-08 20:10:40 +0100

    match SD_MDX_FF_block_end_80087A58

--- src/SD/sd.h
+++ src/SD/sd.h
@@ -1,6 +1,22 @@
 #ifndef _SD_H_
 #define _SD_H_
 
+typedef struct WAVE_W
+{
+  int field_0_addr;
+  char field_4_sample_note;
+  char field_5_sample_tune;
+  char field_6_a_mode;
+  char field_7_ar;
+  char field_8_dr;
+  char field_9_s_mode;
+  char field_A_sr;
+  char field_B_sl;
+  char field_C_r_mode;
+  char field_D_rr;
+  char field_E_pan;
+  char field_F_decl_vol;
+} WAVE_W;
 
 
 #endif // _SD_H_
