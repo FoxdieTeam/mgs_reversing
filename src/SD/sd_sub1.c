dc5f80c3174dbb5555deee852782dfec3bf3a3f1
Paul <github@paulsapps.com>
2022-08-09 01:22:36 +0100

    match random_80086B84

--- src/SD/sd_sub1.c
+++ src/SD/sd_sub1.c
@@ -1,5 +1,8 @@
 #include "SD/sd.h"
 
+unsigned char rdm_tbl_8009F9BC[129];
+extern SOUND_W* sptr_800C057C;
+
 #pragma INCLUDE_ASM("asm/SD/SD_80085A50.s")
 #pragma INCLUDE_ASM("asm/SD/tx_read_80085B84.s")
 #pragma INCLUDE_ASM("asm/SD/note_set_80085CD8.s")
@@ -15,6 +18,28 @@
 #pragma INCLUDE_ASM("asm/sub_80086694.s")
 #pragma INCLUDE_ASM("asm/SD/SD_80086734.s")
 #pragma INCLUDE_ASM("asm/SD/note_cntl_8008686C.s")
-#pragma INCLUDE_ASM("asm/SD/SD_80086B84.s")
+
+unsigned int random_80086B84()
+{
+    unsigned int temp = 0; // $a1
+    unsigned char temp2;
+
+    if ( sptr_800C057C->field_94_rdms )
+    {
+        sptr_800C057C->field_8C_rdmc += sptr_800C057C->field_94_rdms;
+        if ( sptr_800C057C->field_8C_rdmc > 256 )
+        {
+            sptr_800C057C->field_8C_rdmc &= 255;
+            sptr_800C057C->field_90_rdmo++;
+            sptr_800C057C->field_90_rdmo &= 0x7F;
+            temp2 = rdm_tbl_8009F9BC[sptr_800C057C->field_90_rdmo];
+            temp = rdm_tbl_8009F9BC[sptr_800C057C->field_90_rdmo+1] << 8;
+            temp += temp2;
+            temp &= sptr_800C057C->field_98_rdmd;
+        }
+    }
+    return temp;
+}
+
 #pragma INCLUDE_ASM("asm/SD/tempo_ch_80086C08.s")
 #pragma INCLUDE_ASM("asm/SD/SD_80086C98.s")
