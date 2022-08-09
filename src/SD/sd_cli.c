c15e7b7a2c226cb0c931c749487027b91c89c0ae
Paul <github@paulsapps.com>
2022-08-09 22:06:21 +0100

    match XA_Stop_800888B4

--- src/SD/sd_cli.c
+++ src/SD/sd_cli.c
@@ -5,6 +5,8 @@
 extern char byte_800C0468[];
 extern unsigned int sng_status_800C04F8;
 extern const char aXaSoundStart[];
+extern const char aXaSoundStop[];
+extern int dword_8009F7B4;
 
 int sd_set_cli_800887EC(int sdCode, int unused)
 {
@@ -44,11 +46,19 @@ void XA_Start_80088868()
     mts_printf_8008BBA0(aXaSoundStart);
 }
 
-#pragma INCLUDE_ASM("asm/sub_800888B4.s")
+void XA_Stop_800888B4()
+{
+    SpuCommonAttr attr; // [sp+10h] [-28h] BYREF
+
+    attr.mask = 704;
+    attr.cd.volume.left = 0;
+    attr.cd.volume.right = 0;
+    attr.cd.mix = 0;
+    SpuSetCommonAttr_80097038(&attr);
+    mts_printf_8008BBA0(aXaSoundStop);
+}
 
 #pragma INCLUDE_ASM("asm/SD/SePlay_800888F8.s")
-
-extern int dword_8009F7B4;
 
 int get_str_counter_80088CA0(void)
 {
