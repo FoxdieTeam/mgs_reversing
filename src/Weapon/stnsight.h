d6a556928c5de9499c6b1d10e69f156906692219
Kieron Josephs <kieron.josephs00@gmail.com>
2022-08-09 18:40:48 +0100

    Match stnsight_act_helper_80068798

--- src/Weapon/stnsight.h
+++ src/Weapon/stnsight.h
@@ -17,16 +17,14 @@ typedef struct Actor_stnsight {
     LINE_F4 *field_2C_lines;
     LINE_F4 *field_30_lines;
     LINE_F4 *field_34_lines;
-    LINE_F4 *field_38_lines;
-    LINE_F4 *field_3C_lines;
-    LINE_F4 *field_40_lines;
-    LINE_F4 *field_44_lines;
+    LINE_F4 *field_38_lines_2Array[2];
+    LINE_F4 *field_40_lines_2Array[2];
     TILE_1 *field_48_tiles;
     TILE_1 *field_4C_tiles;
     POLY_G4 *field_50_polys_2Array[2];
-    int field_58;
-    int field_5C;
-    short field_60_18Array[18];
+    int field_58_ybase;
+    int field_5C_xbase;
+    DVECTOR field_60_coords_9Array[9];
     int field_84_4Array[4];
     int field_94;
 } Actor_stnsight;
@@ -35,10 +33,10 @@ STATIC_ASSERT_SIZE(Actor_stnsight, 0x98);
 
 void stnsight_act_helper_helper_80068320(unsigned int *ot, unsigned int *prim);
 
-void stnsight_act_helper_80068420(Actor_stnsight *actor, unsigned char *);
-void stnsight_act_helper_80068798(Actor_stnsight *actor, unsigned char *);
-void stnsight_act_helper_80068A24(Actor_stnsight *actor, unsigned char *);
-void stnsight_act_helper_80068BF4(Actor_stnsight *actor, unsigned int *);
+void stnsight_act_helper_80068420(Actor_stnsight *actor, unsigned int *ot);
+void stnsight_act_helper_80068798(Actor_stnsight *actor, unsigned int *ot);
+void stnsight_act_helper_80068A24(Actor_stnsight *actor, unsigned int *ot);
+void stnsight_act_helper_80068BF4(Actor_stnsight *actor, unsigned int *ot);
 void stnsight_act_helper_8006837C(Actor_stnsight *actor);
 
 void stnsight_act_80068D0C(Actor_stnsight *actor);
