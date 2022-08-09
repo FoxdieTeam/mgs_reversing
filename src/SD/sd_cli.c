7e9d49a96d2ae234686fecfdad126cb6f02846c4
Paul <github@paulsapps.com>
2022-08-09 22:03:41 +0100

    match sub_80088838

--- src/SD/sd_cli.c
+++ src/SD/sd_cli.c
@@ -3,6 +3,7 @@
 #include "unknown.h"
 
 extern char byte_800C0468[];
+extern unsigned int sng_status_800C04F8;
 
 int sd_set_cli_800887EC(int sdCode, int unused)
 {
@@ -15,12 +16,21 @@ void sd_set_path_8008880C(const char *pName)
     strcpy_8008E768(byte_800C0468, pName);
 }
 
-#pragma INCLUDE_ASM("asm/sub_80088838.s")
+unsigned int sub_80088838()
+{
+    if ( sng_status_800C04F8 == -1 || sng_status_800C04F8 == 0  )
+    {
+        return 0;
+    }
+
+    return sng_status_800C04F8;
+}
 
 void sub_80088860(void)
 {
-    return;
+
 }
+
 #pragma INCLUDE_ASM("asm/SD/sub_80088868.s")
 #pragma INCLUDE_ASM("asm/sub_800888B4.s")
 #pragma INCLUDE_ASM("asm/SD/SePlay_800888F8.s")
