c92fee7bc805bab1afbb381a28ec055082c35396
Paul <github@paulsapps.com>
2022-08-09 01:07:00 +0100

    match sng_track_init_800859B8

--- src/SD/sd_drv.c
+++ src/SD/sd_drv.c
@@ -55,4 +55,37 @@ int SngFadeOutP_80084D60(unsigned int a1)
 #pragma INCLUDE_ASM("asm/SD/SD_8008559C.s")
 #pragma INCLUDE_ASM("asm/SD/sng_adrs_set_80085658.s")
 #pragma INCLUDE_ASM("asm/SD/SD_8008576C.s")
-#pragma INCLUDE_ASM("asm/SD/SD_800859B8.s")
+
+void sng_track_init_800859B8(SOUND_W *pSoundW)
+{
+    pSoundW->field_4_ngc = 1;
+    pSoundW->field_38_pvod = 127;
+    pSoundW->field_44_vol = 127;
+    pSoundW->field_48_pand = 2560;
+    pSoundW->field_54_panf = 10;
+    pSoundW->field_B4_tmpd = 1;
+    pSoundW->field_98_rdmd = 0;
+    pSoundW->field_5_ngo = 0;
+    pSoundW->field_6_ngs = 0;
+    pSoundW->field_7_ngg = 0;
+    pSoundW->field_8_lp1_cnt = 0;
+    pSoundW->field_9_lp2_cnt = 0;
+    pSoundW->field_C_lp1_vol = 0;
+    pSoundW->field_10_lp2_vol = 0;
+    pSoundW->field_14_lp1_freq = 0;
+    pSoundW->field_18_lp2_freq = 0;
+    pSoundW->field_34_pvoc = 0;
+    pSoundW->field_55 = 0;
+    pSoundW->field_56_panmod = 0;
+    pSoundW->field_6A_swsk = 0;
+    pSoundW->field_68_swsc = 0;
+    pSoundW->field_7C_vibd = 0;
+    pSoundW->field_80_vibdm = 0;
+    pSoundW->field_9E_tred = 0;
+    pSoundW->field_A4_snos = 0;
+    pSoundW->field_A8_ptps = 0;
+    pSoundW->field_AC_dec_vol = 0;
+    pSoundW->field_B0_tund = 0;
+    pSoundW->field_B8_tmp = -1;
+    pSoundW->field_C0_tmpc = 0;
+}
