5db043abf818e7c7650ef1858ee7e83ca8654f9b
fatalis <fatalis@fatalis.pw>
2022-08-09 12:59:40 -0500

    3 tenage matches

--- src/Bullet/tenage.c
+++ src/Bullet/tenage.c
@@ -1,6 +1,10 @@
 #include "tenage.h"
 #include "chara/snake/sna_init.h"
 
+extern char aTenageC[]; // = "tenage.c"
+
+// the projectile for all types of grenades
+
 #pragma INCLUDE_ASM("asm/tenage_act_800699A4.s") // 1048 bytes
 
 extern GM_Control *tenage_ctrls_800BDD30[16];
@@ -11,6 +15,7 @@ void tenage_kill_80069DBC(Actor_tenage *pActor)
     GM_FreeControl_800260CC(&pActor->field_20_ctrl);
     GM_ClearBulName_8004FBE4(pActor->field_20_ctrl.field_30_scriptData);
     GM_FreeObject_80034BF8(&pActor->field_9C_obj);
+
     if (pActor->field_120_ctrl_idx >= 0)
     {
         tenage_ctrls_800BDD30[pActor->field_120_ctrl_idx] = 0;
@@ -19,7 +24,69 @@ void tenage_kill_80069DBC(Actor_tenage *pActor)
 }
 
 #pragma INCLUDE_ASM("asm/tenage_init_helper_80069E28.s") // 60 bytes
-#pragma INCLUDE_ASM("asm/tenage_init_helper_80069E64.s") // 428 bytes
-#pragma INCLUDE_ASM("asm/NewTenage_8006A010.s")          // 240 bytes
-#pragma INCLUDE_ASM("asm/sub_8006A100.s")                // 40 bytes
-#pragma INCLUDE_ASM("asm/NewTenage3_8006A128.s")         // 240 bytes
+#pragma INCLUDE_ASM("asm/tenage_loader_80069E64.s")      // 428 bytes
+
+Actor_tenage *NewTenage_8006A010(SVECTOR *vec, SVECTOR *vec2, int param_3, int param_4, int param_5)
+{
+    Actor_tenage *pActor;
+
+    if (tenage_ctrls_count_800BDD70 == 16)
+    {
+        return NULL;
+    }
+
+    pActor = (Actor_tenage *)GV_NewActor_800150E4(5, sizeof(Actor_tenage));
+
+    if (pActor)
+    {
+        GV_SetNamedActor_8001514C((Actor *)pActor, (TActorFunction)tenage_act_800699A4,
+                                  (TActorFunction)tenage_kill_80069DBC, aTenageC);
+
+        if (tenage_loader_80069E64(pActor, vec, vec2, param_4, param_5, 1, 1) < 0)
+        {
+            GV_DestroyActor_800151C8((Actor *)pActor);
+            return NULL;
+        }
+
+        pActor->field_100_homing_arg2 = param_3;
+        pActor->field_104_count = 0;
+        pActor->field_110 = param_4;
+        pActor->field_118_do_sound = 1;
+        pActor->field_11C = 1;
+    }
+
+    return pActor;
+}
+
+void sub_8006A100(SVECTOR *vec, SVECTOR *vec2, int param_3)
+{
+    NewTenage_8006A010(vec, vec2, param_3, 0, 0x3b88);
+}
+
+// enemy's grenades, probably
+Actor_tenage *NewTenage3_8006A128(SVECTOR *vec, SVECTOR *vec2, int param_3, int param_4, int param_5, int param_6,
+                                  int param_7)
+{
+    Actor_tenage *pActor;
+
+    pActor = (Actor_tenage *)GV_NewActor_800150E4(6, sizeof(Actor_tenage));
+    if (pActor)
+    {
+        GV_SetNamedActor_8001514C((Actor *)pActor, (TActorFunction)tenage_act_800699A4,
+                                  (TActorFunction)tenage_kill_80069DBC, aTenageC);
+
+        if (tenage_loader_80069E64(pActor, vec, vec2, param_4, param_5, 0, 2) < 0)
+        {
+            GV_DestroyActor_800151C8((Actor *)pActor);
+            return NULL;
+        }
+
+        pActor->field_100_homing_arg2 = param_3;
+        pActor->field_104_count = 0;
+        pActor->field_110 = param_4;
+        pActor->field_118_do_sound = param_6 & 1;
+        pActor->field_11C = param_7 & 1;
+    }
+
+    return pActor;
+}
