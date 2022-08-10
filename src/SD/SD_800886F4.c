e38b8da99794abc15d070b3ef04cb7e66bda767a
Paul <github@paulsapps.com>
2022-08-10 21:57:30 +0100

    merge some files

--- src/SD/SD_800886F4.c
+++
@@ -1,6 +0,0 @@
-extern int sng_status_800BF158;
-
-int SD_800886F4(void)
-{
-    return sng_status_800BF158 > 2;
-}
--- src/SD/sd_str_play_800886DC.c
+++
@@ -1,6 +0,0 @@
-extern unsigned int gStr_FadeOut1_800BF16C;
-
-int sd_str_play_800886DC(void)
-{
-    return gStr_FadeOut1_800BF16C > 4;
-}
