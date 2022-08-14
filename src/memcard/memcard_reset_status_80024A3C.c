2a244ce7d465066fd96c638aaa9aa6436f1faa8f
Paul <github@paulsapps.com>
2022-08-14 03:31:14 +0100

    merge memcard.c

--- src/memcard/memcard_reset_status_80024A3C.c
+++
@@ -1,9 +0,0 @@
-#include "memcard.h"
-
-extern struct mem_card gMemCards_800B52F8[2];
-
-void memcard_reset_status_80024A3C(void)
-{
-    gMemCards_800B52F8[0].field_1_last_op = 2;
-    gMemCards_800B52F8[1].field_1_last_op = 2;
-}
