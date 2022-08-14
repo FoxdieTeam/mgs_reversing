56fe61e03b2438d75a34f0fb0beb9e47cbcbf4c5
Paul <github@paulsapps.com>
2022-08-14 01:52:44 +0100

    match GCL_Command_demo_helper_80037DD8

--- src/Game/strctrl.c
+++ src/Game/strctrl.c
@@ -1,9 +1,13 @@
 #include "strctrl.h"
+#include "mts/mts_new.h"
 
 extern Actor_strctrl strctrl_800B82B0;
+extern int GM_GameStatus_800AB3CC;
+extern const char aVoxstreamD[];
 
 int FS_StreamGetTop_80023F94(int is_movie);
 Actor_strctrl* strctrl_init_80037B64(int sector, int gcl_proc, int a3);
+void srand_8008E6E8(int s);
 
 #pragma INCLUDE_ASM("asm/Game/strctrl_act_helper_800377EC.s")
 #pragma INCLUDE_ASM("asm/Game/strctrl_act_80037820.s")
@@ -23,8 +27,20 @@ int GM_StreamGetLastCode_80037DC8(void)
     return strctrl_800B82B0.field_30_voxStream;
 }
 
-#pragma INCLUDE_ASM("asm/libgcl/commands/GCL_Command_demo_helper_80037DD8.s")
+Actor_strctrl* GCL_Command_demo_helper_80037DD8(int base_sector, int gcl_proc)
+{
+    int total_sector; // $s0
+
+    strctrl_800B82B0.field_30_voxStream = base_sector;
+    GM_GameStatus_800AB3CC |= 0x20u;
+    total_sector = base_sector + FS_StreamGetTop_80023F94(1);
+    do {} while (0);
+    srand_8008E6E8(1);
+    return strctrl_init_80037B64(total_sector, gcl_proc, 2);
+}
+
 #pragma INCLUDE_ASM("asm/Game/GM_VoxStream_80037E40.s")
+
 
 Actor_strctrl* sub_80037EE0(int voxStream, int gclProc)
 {
