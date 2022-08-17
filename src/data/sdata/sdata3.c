afd8feefa9dd422045f18e7a0324ab0e80518e23
fatalis <fatalis@fatalis.pw>
2022-08-17 08:36:18 -0500

    fix

--- src/data/sdata/sdata3.c
+++ src/data/sdata/sdata3.c
@@ -6,4 +6,4 @@
 short SECTION(".sdata") word_800AB3A8 = 768;
 short SECTION(".sdata") word_800AB3AA = 226;
 int SECTION(".sdata") dword_800AB3AC = 0x1E0100;
-RECT SECTION(".sdata") rect_800AB3B0 = { 0x300, 0xC4, 0x100, 0x1E };
+RECT SECTION(".sdata") rect_800AB3B0 = {768, 196, 256, 30};
