bfeff6cd468de6af8ff2d724d1b353293f83308b
fatalis <fatalis@fatalis.pw>
2022-08-17 13:01:12 -0500

    3 small matches

--- src/libdg/DG_StorePalette_8001FC28.c
+++ src/libdg/DG_StorePalette_8001FC28.c
@@ -10,7 +10,9 @@ RECT SECTION(".sdata") rect_800AB3A8;
 extern RECT rect_800AB3B0;
 RECT SECTION(".sdata") rect_800AB3B0;
 
-void DG_ReloadPalette_8001FC58(void)
+void DG_StorePalette_8001FC28(void)
 {
-    MoveImage_8008FBD0(&rect_800AB3B0, rect_800AB3A8.x, rect_800AB3A8.y);
+    MoveImage_8008FBD0(&rect_800AB3A8, rect_800AB3B0.x, rect_800AB3B0.y);
 }
+
+
