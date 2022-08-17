e3d992e4e1b1734f439028093feaf84a005fc5f6
Paul <github@paulsapps.com>
2022-08-17 02:04:00 +0100

    match scope_draw_text_80062DA8

--- src/Equip/scope.c
+++ src/Equip/scope.c
@@ -1,3 +1,11 @@
+#include "scope.h"
+#include "menu/menuman.h"
+
+extern const char aZoomLevelD[];
+extern const char aD_44[];  // = "%d"
+
+extern int GM_PlayerStatus_800ABA50;
+extern GM_Camera GM_Camera_800B77E8;
 
 #pragma INCLUDE_ASM("asm/Equip/scope_act_helper_helper_80062320.s") // 92 bytes
 #pragma INCLUDE_ASM("asm/Equip/scope_act_helper_helper_8006237C.s") // 320 bytes
@@ -8,7 +16,22 @@
 #pragma INCLUDE_ASM("asm/Equip/scope_act_helper_80062998.s")        // 580 bytes
 #pragma INCLUDE_ASM("asm/Equip/scope_act_helper_80062BDC.s")        // 160 bytes
 #pragma INCLUDE_ASM("asm/Equip/scope_act_helper_80062C7C.s")        // 300 bytes
-#pragma INCLUDE_ASM("asm/Equip/scope_draw_text_80062DA8.s")         // 228 bytes
+
+
+void scope_draw_text_80062DA8(Actor_scope *pActor)
+{
+    if ( (GM_PlayerStatus_800ABA50 & 0x4000000) == 0 )
+    {
+        menu_Text_XY_Flags_80038B34(20, 34, 0);
+        menu_Color_80038B4C(127, 166, 97);
+        menu_Text_80038C38(aZoomLevelD, 100 * (GM_Camera_800B77E8.field_20 / 320));
+        menu_Color_80038B4C(101, 133, 77);
+        menu_Text_XY_Flags_80038B34(32, 101, 1);
+        menu_Text_80038C38(aD_44, -pActor->field_6C);
+        menu_Text_Init_80038B98();
+    }
+}
+
 #pragma INCLUDE_ASM("asm/Equip/scope_act_80062E8C.s")               // 752 bytes
 #pragma INCLUDE_ASM("asm/Equip/scope_kill_8006317C.s")              // 188 bytes
 #pragma INCLUDE_ASM("asm/Equip/scope_loader_helper_80063238.s")     // 60 bytes
