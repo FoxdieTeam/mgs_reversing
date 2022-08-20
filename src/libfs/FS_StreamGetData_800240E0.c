636c89925c45827cf11542479bb086a6b9d15ded
Kieron Josephs <kieron.josephs00@gmail.com>
2022-08-20 20:59:03 +0100

    Clean up FS_StreamGetData_800240E0

--- src/libfs/FS_StreamGetData_800240E0.c
+++ src/libfs/FS_StreamGetData_800240E0.c
@@ -10,10 +10,9 @@ extern void *fs_ptr_800B52BC;
 
 void * FS_StreamGetData_800240E0(int param_1)
 {
-    int cVar2;
     char *ptr;
-    int uVar1;
-    char *stream_end;
+    int cVar1;
+    int uVar2;
     
     if (fs_stream_is_force_stop_8009D518 != 0)
     {
@@ -22,27 +21,24 @@ void * FS_StreamGetData_800240E0(int param_1)
     
     for (ptr = fs_ptr_800B52B4; ptr != fs_ptr_800B52BC;)
     {
-        cVar2 = *(int*)ptr & 0xFF;
-        uVar1 = *(int*)ptr;
-
-        uVar1 >>= 8;
+        cVar1 = *(int*)ptr & 0xFF;
+        uVar2 = *(int*)ptr >> 8;
         
-        if (cVar2 == 0xff)
+        if (cVar1 == 0xff)
         {            
             ptr = fs_stream_heap_800B52A4;
         }
         else
         {
-            if (cVar2 == param_1)
+            if (cVar1 == param_1)
             {
-                *ptr = cVar2 | 0x80;
+                *ptr = cVar1 | 0x80;
                 return ptr + 4;
             }
 
-            ptr = ptr + uVar1;
+            ptr += uVar2;
 
-            stream_end = (char *)fs_stream_heap_800B52A4 + fs_stream_heapSize_800B52AC;
-            if (ptr >= stream_end)
+            if (ptr >= (char *)fs_stream_heap_800B52A4 + fs_stream_heapSize_800B52AC)
             {
                 ptr = fs_stream_heap_800B52A4;
             }
