8f7fade55ea6a4bd9988dc605df35f01e6824324
g-e-o <34046939+g-e-o@users.noreply.github.com>
2022-08-22 02:51:35 +0200

    match GM_StreamStatus_80037CD8 (#753)
    * match GM_StreamStatus_80037CD8
    
    * removed .s file
    
    Co-authored-by: Wanted <4wantedthing@hotmail.com>

--- src/Game/strctrl.c
+++ src/Game/strctrl.c
@@ -24,18 +24,18 @@ void                    srand_8008E6E8(int s);
 
 //------------------------------------------------------------------------------
 
-void            strctrl_act_helper_800377EC(Actor_strctrl *pActor)
+void            strctrl_act_helper_800377EC( Actor_strctrl *pActor )
 {
     if ( !FS_StreamTaskState_80023E0C() )
     {
-        GV_DestroyActor_800151C8(&pActor->field_0_actor);
+        GV_DestroyActor_800151C8( &pActor->field_0_actor );
     }
 }
 
 #pragma INCLUDE_ASM("asm/Game/strctrl_act_80037820.s") // 708 bytes
-void            strctrl_act_80037820(int);
+void            strctrl_act_80037820( int );
 
-void            strctrl_kill_80037AE4(Actor_strctrl *pActor)
+void            strctrl_kill_80037AE4( Actor_strctrl *pActor )
 {
     int field_38_proc; // $a1
 
@@ -50,12 +50,12 @@ void            strctrl_kill_80037AE4(Actor_strctrl *pActor)
     }
     if ( str_sector_8009E280 )
     {
-        strctrl_init_80037B64(str_sector_8009E280, str_gcl_proc_8009E284, str_8009E288);
+        strctrl_init_80037B64( str_sector_8009E280, str_gcl_proc_8009E284, str_8009E288 );
         str_sector_8009E280 = 0;
     }
 }
 
-Actor_strctrl *strctrl_init_80037B64(int stream_code, int gcl_proc, int flags)
+Actor_strctrl   *strctrl_init_80037B64( int stream_code, int gcl_proc, int flags )
 {
     mts_printf_8008BBA0( aNewstreamD, stream_code );
     
@@ -103,77 +103,84 @@ Actor_strctrl *strctrl_init_80037B64(int stream_code, int gcl_proc, int flags)
     return &strctrl_800B82B0;
 }
 
-#pragma INCLUDE_ASM("asm/Game/GM_StreamStatus_80037CD8.s") // 68 bytes
+int GM_StreamStatus_80037CD8( void )
+{
+    int state;
+    
+    state = strctrl_800B82B0.field_20_state - 1;
+    if ( !(strctrl_800B82B0.field_20_state || !FS_StreamTaskState_80023E0C()) )
+    {
+        return 2;
+    }
+    return state;
+}
 
 void            GM_StreamPlayStart_80037D1C()
 {
     // TODO: Probably a switch
-    if ( (unsigned int)(unsigned short)strctrl_800B82B0.field_20_state - 1 < 2 )
+    if ( (u_int)(u_short)strctrl_800B82B0.field_20_state - 1 < 2 )
     {
         strctrl_800B82B0.field_22_sub_state = 0;
     }
     else
     {
-        mts_printf_8008BBA0(aStreamIsNotRea); 
+        mts_printf_8008BBA0( aStreamIsNotRea ); 
     }
 }
 
 void            GM_StreamPlayStop_80037D64()
 {
-    mts_printf_8008BBA0(aGmStreamplayst);
+    mts_printf_8008BBA0( aGmStreamplayst );
     FS_StreamStop_80024028();
 
     // TODO: Probably a switch
-    if ( (unsigned int)(unsigned short)strctrl_800B82B0.field_20_state - 1 < 2 )
+    if ( (u_int)(u_short)strctrl_800B82B0.field_20_state - 1 < 2 )
     {
-        GV_DestroyOtherActor_800151D8(&strctrl_800B82B0.field_0_actor);
+        GV_DestroyOtherActor_800151D8( &strctrl_800B82B0.field_0_actor );
     }
 }
 
-void            sub_80037DB8(void)
+void            sub_80037DB8( void )
 {
     strctrl_800B82B0.field_38_proc = -1;
 }
 
-int             GM_StreamGetLastCode_80037DC8(void)
+int             GM_StreamGetLastCode_80037DC8( void )
 {
     return strctrl_800B82B0.field_30_voxStream;
 }
 
-Actor_strctrl   *GCL_Command_demo_helper_80037DD8(int base_sector, int gcl_proc)
+Actor_strctrl   *GCL_Command_demo_helper_80037DD8( int base_sector, int gcl_proc )
 {
     int total_sector; // $s0
 
     strctrl_800B82B0.field_30_voxStream = base_sector;
     GM_GameStatus_800AB3CC |= 0x20u;
-    total_sector = base_sector + FS_StreamGetTop_80023F94(1);
+    total_sector = base_sector + FS_StreamGetTop_80023F94( 1 );
     do {} while (0);
-    srand_8008E6E8(1);
-    return strctrl_init_80037B64(total_sector, gcl_proc, 2);
+    srand_8008E6E8( 1 );
+    return strctrl_init_80037B64( total_sector, gcl_proc, 2 );
 }
 
-Actor_strctrl   *GM_VoxStream_80037E40(int voxCode, int proc)
+Actor_strctrl   *GM_VoxStream_80037E40( int vox_code, int proc )
 {
-    strctrl_800B82B0.field_30_voxStream = voxCode;
-    voxCode++; voxCode--;
-    if (GM_GameStatus_800AB3CC & 0x4000000)
+    strctrl_800B82B0.field_30_voxStream = vox_code;
+    vox_code++; vox_code--;
+    if ( GM_GameStatus_800AB3CC & 0x4000000 )
     {
         return 0;
     }
     
-    mts_printf_8008BBA0(aVoxstreamD, voxCode);
-    if (!(proc & 0x40000000))
+    mts_printf_8008BBA0( aVoxstreamD, vox_code );
+    if ( !(proc & 0x40000000) )
     {
         GM_GameStatus_800AB3CC |= 0x20;
     }
-    return strctrl_init_80037B64(voxCode + FS_StreamGetTop_80023F94(0), proc, 0);
+    return strctrl_init_80037B64( vox_code + FS_StreamGetTop_80023F94(0), proc, 0 );
 }
 
-Actor_strctrl   *sub_80037EE0(int voxStream, int gclProc)
+Actor_strctrl   *sub_80037EE0(int vox_stream, int gcl_proc)
 {
-    int pTop; // $v0
-
-    strctrl_800B82B0.field_30_voxStream = voxStream;
-    pTop = FS_StreamGetTop_80023F94(0);
-    return strctrl_init_80037B64(voxStream + pTop, gclProc, 1);
+    strctrl_800B82B0.field_30_voxStream = vox_stream;
+    return strctrl_init_80037B64( vox_stream + FS_StreamGetTop_80023F94(0), gcl_proc, 1 );
 }
--- src/Game/strctrl.h
+++ src/Game/strctrl.h
@@ -4,22 +4,22 @@
 #include "libgv/libgv.h"
 
 // TODO: might be bigger if the jimctrl actually uses this
-typedef struct Actor_strctrl
+typedef struct  Actor_strctrl
 {
-  Actor field_0_actor;
-  short field_20_state;
-  short field_22_sub_state;
-  short field_24;
-  short field_26_flags;
-  int field_28;
-  int field_2C_map;
-  int field_30_voxStream;
-  int *field_34_pStreamData;
-  int field_38_proc;
-  int field_3C;
+  Actor         field_0_actor;
+  short         field_20_state;
+  short         field_22_sub_state;
+  short         field_24;
+  short         field_26_flags;
+  int           field_28;
+  int           field_2C_map;
+  int           field_30_voxStream;
+  int           *field_34_pStreamData;
+  int           field_38_proc;
+  int           field_3C;
 } Actor_strctrl;
 
 // TODO: rename
-Actor_strctrl* GCL_Command_demo_helper_80037DD8(int base_sector, int gcl_proc);
+Actor_strctrl   *GCL_Command_demo_helper_80037DD8(int base_sector, int gcl_proc);
 
 #endif // _STRCTRL_H_
