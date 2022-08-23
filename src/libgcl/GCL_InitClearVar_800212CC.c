06f6878f5285759bdef6264d583b80d81bb9fdfd
g-e-o <34046939+g-e-o@users.noreply.github.com>
2022-08-24 00:39:57 +0200

    rename memset() to allow use of {0} (#758)

--- src/libgcl/GCL_InitClearVar_800212CC.c
+++ src/libgcl/GCL_InitClearVar_800212CC.c
@@ -7,7 +7,7 @@ extern GameState_800B4D98 gGameState_800B4D98;
 
 void GCL_InitClearVar_800212CC()
 {
-    memset_8008E688(&gGcl_vars_800B3CC8, 0, 2048);
-    memset_8008E688(&gGameState_800B4D98.field_0C_current_stage, 0, 180);
+    gGcl_vars_800B3CC8 = ( GCL_Vars ){{ 0 }};
+    memset(&gGameState_800B4D98.field_0C_current_stage, 0, 180);
     GCL_SaveVar_80021314();
 }
--- src/libgcl/GCL_InitVar_80021264.c
+++ src/libgcl/GCL_InitVar_80021264.c
@@ -10,10 +10,10 @@ void GCL_InitVar_80021264()
     int flags;
     int difficulty;
 
-    flags = (short)gGameState_800B4D98.field_04_flags;
+    flags = gGameState_800B4D98.field_04_flags;
     difficulty = gGameState_800B4D98.field_02_diffcultyLevel;
-    memset_8008E688(&gGcl_vars_800B3CC8, 0, 2048);
-    memset_8008E688(&gGameState_800B4D98, 0, 192);
+    gGcl_vars_800B3CC8 = ( GCL_Vars ){{ 0 }};
+    gGameState_800B4D98 = ( GameState_800B4D98 ){ 0 };
     gGameState_800B4D98.field_02_diffcultyLevel = difficulty;
     gGameState_800B4D98.field_04_flags = flags;
 }
