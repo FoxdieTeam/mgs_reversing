ab93c494c6c44222a634978748f20bd8744872ad
fatalis <fatalis@fatalis.pw>
2022-08-24 09:42:28 -0500

    fix

--- src/Game/sub_80032748.c
+++ src/Game/sub_80032748.c
@@ -3,7 +3,7 @@
 
 extern GM_Camera GM_Camera_800B77E8;
 
-int sub_800326D4(DVECTOR *out, SVECTOR *pos)
+int sub_80032748(DVECTOR *out, SVECTOR *pos)
 {
     if (((GM_GameStatus_800AB3CC & 0x50) == 0) && GM_Camera_800B77E8.field_22 == 0)
     {
