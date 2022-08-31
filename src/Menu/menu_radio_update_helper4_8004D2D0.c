3dc00b8beaab34a21c0272a021db6dcc02f17352
fatalis <fatalis@users.noreply.github.com>
2022-08-31 08:40:36 -0500

    1 small match (#780)

---
+++ src/Menu/menu_radio_update_helper4_8004D2D0.c
@@ -0,0 +1,13 @@
+#include "linker.h"
+#include "radio.h"
+
+extern int dword_800ABB8C;
+int SECTION(".sbss") dword_800ABB8C;
+
+extern char byte_8009EC30;
+
+void menu_radio_update_helper4_8004D2D0(int param_1)
+{
+    dword_800ABB8C = param_1;
+    init_file_mode_8004D24C((int)&byte_8009EC30, 1);
+}
