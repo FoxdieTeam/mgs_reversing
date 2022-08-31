74387ba72021baab9501f184ae2dcac3a7940955
fatalis <fatalis@fatalis.pw>
2022-08-31 08:37:59 -0500

    fix

--- src/Menu/menu_radio_update_helper4_8004D2D0.c
+++ src/Menu/menu_radio_update_helper4_8004D2D0.c
@@ -1,4 +1,5 @@
 #include "linker.h"
+#include "radio.h"
 
 extern int dword_800ABB8C;
 int SECTION(".sbss") dword_800ABB8C;
@@ -8,5 +9,5 @@ extern char byte_8009EC30;
 void menu_radio_update_helper4_8004D2D0(int param_1)
 {
     dword_800ABB8C = param_1;
-    init_file_mode_8004D24C(&byte_8009EC30, 1);
+    init_file_mode_8004D24C((int)&byte_8009EC30, 1);
 }
