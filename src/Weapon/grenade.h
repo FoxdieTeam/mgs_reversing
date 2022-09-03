8b2956424d1c219d6441a44d5a34f24b11f4bfef
Wanted <4wantedthing@hotmail.com>
2022-09-03 23:33:54 +0200

    norm grenade.c/h

--- src/Weapon/grenade.h
+++ src/Weapon/grenade.h
@@ -1,11 +1,11 @@
 #ifndef _GRENADE_H_
 #define _GRENADE_H_
 
-#include "libgv/libgv.h"
-#include "libdg/libdg.h"
+#include "Game/GM_Control.h"
 #include "Game/game.h"
 #include "Game/object.h"
-#include "Game/GM_Control.h"
+#include "libdg/libdg.h"
+#include "libgv/libgv.h"
 #include "psyq.h"
 
 // grenade/stun/chaff (in hands)
@@ -24,23 +24,31 @@ typedef struct	_Actor_Grenade
 	int			f64;
 } Actor_Grenade;
 */
-typedef struct	_Actor_Grenade
+typedef struct _Actor_Grenade
 {
-	Actor		field_0_actor;
-	GM_Control	*f20_ctrl;
-	OBJECT		*f24_parent_obj;
-	OBJECT		f28_obj;
-	int			f4c_map_id;
-	int			*f50_flags;
-	SVECTOR		f54_pos;
-	int			f5c;
-	int			f60_grenade_type;
-	int			f64;
+	Actor       field_0_actor;
+	GM_Control *f20_ctrl;
+	OBJECT     *f24_parent_obj;
+	OBJECT      f28_obj;
+	int         f4c_map_id;
+	int        *f50_flags;
+	SVECTOR     f54_pos;
+	int         f5c;
+	int         f60_grenade_type;
+	int         f64;
 } Actor_Grenade;
+
+enum GRENADE_TYPE
+{
+	GRD_GRENADE = 0,
+	GRD_STUN = 1,
+	GRD_CHAFF = 2,
+	GRD_TBOMB = 3
+};
 
 STATIC_ASSERT_SIZE( Actor_Grenade, 0x68 );
 
 // int grenade_act_8006641C(int a1);
-Actor_Grenade	*NewTimerBomb_80066ACC( GM_Control *a1, OBJECT *a2, int num_parent, int *a4, int a5 );
+Actor_Grenade *NewTimerBomb_80066ACC( GM_Control *a1, OBJECT *a2, int num_parent, int *a4, int a5 );
 
 #endif // _GRENADE_H_
