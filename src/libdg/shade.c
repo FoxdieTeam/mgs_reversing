19c07417ade1f2b783806dd699e7308b8193695a
Jayveer <jayveerk@gmail.com>
2022-09-07 14:19:30 +0100

    Finished shade.c

--- src/libdg/shade.c
+++ src/libdg/shade.c
@@ -1,6 +1,26 @@
 #include "libdg.h"
 
+/**gp*******************************************/
 extern unsigned long DG_PacketCode_800AB394[2];
+/***********************************************/
+
+/**data*******************************/
+extern SVECTOR DG_Ambient_800AB38C;
+/*************************************/
+
+//there are a few of these that are close to  gte_MulMatrix0 but with the first part changed
+#define gte_Unknown(r1, r2)                                                                                            \
+    {                                                                                                                  \
+        gte_ldclmv(r1);                                                                                                \
+        gte_rtir();                                                                                                    \
+        gte_stclmv(r2);                                                                                                \
+        gte_ldclmv((char *)r1 + 2);                                                                                    \
+        gte_rtir();                                                                                                    \
+        gte_stclmv((char *)r2 + 2);                                                                                    \
+        gte_ldclmv((char *)r1 + 4);                                                                                    \
+        gte_rtir();                                                                                                    \
+        gte_stclmv((char *)r2 + 4);                                                                                    \
+    }
 
 void DG_ShadeStart_8001CF80()
 {
@@ -193,4 +213,61 @@ void DG_Shade_Chanl_helper_8001D19C( DG_OBJ* obj, int idx )
         }
         obj = obj->extend;
     }
+}
+
+void DG_Shade_Chanl_8001D324( DG_CHNL *chnl, int idx )
+{
+    unsigned int flag;
+    int          i, j;
+    DG_OBJ      *obj; 
+    DG_OBJS     *objs;
+    DG_OBJS    **objs_queue;
+
+    i = chnl->mTotalObjectCount;
+    objs_queue = chnl->mQueue;
+
+    for ( ; i > 0 ; --i )
+    {
+        objs = *objs_queue;
+        objs_queue++;
+
+        if ( !objs->bound_mode ) continue;
+
+        flag = objs->flag;
+
+        if ( !( flag & 8 ) ) continue;
+
+        gte_SetRotMatrix( objs->light );
+        gte_SetColorMatrix( &objs->light[1] );
+
+        if ( flag & 0x100 )
+        {
+            gte_SetBackColor( objs->light->t[0], objs->light->t[1], objs->light->t[2] );
+        }
+
+        j = objs->n_models;
+        obj = objs->objs;
+
+        for ( ; j > 0 ; --j )
+        {
+            if ( obj->bound_mode )
+            {
+                
+                gte_Unknown( &obj->world, 0x1F800000 );
+                gte_SetLightMatrix( 0x1F800000 );
+                DG_Shade_Chanl_helper_8001D19C( obj, idx );
+            }
+            obj++;
+        }
+        
+        if ( objs->flag & 0x100 )
+        {
+            gte_SetBackColor( DG_Ambient_800AB38C.vx, DG_Ambient_800AB38C.vy, DG_Ambient_800AB38C.vz );
+        }
+    }
+}
+
+void DG_ShadeEnd_8001D5C0(void)
+{
+    return;
 }
