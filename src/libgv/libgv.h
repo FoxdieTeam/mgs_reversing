07c25fb2933da9b35d68549a62894f131e6bc96d
Kieron Josephs <kieron.josephs00@gmail.com>
2022-09-15 23:03:18 +0100

    Match GV_NearExp2V_8002667C

--- src/libgv/libgv.h
+++ src/libgv/libgv.h
@@ -236,7 +236,7 @@ void         CopyVectorFromScratchpad_80029360(SVECTOR *vec_1, SVECTOR *vec_2);
 int          sub_800268AC(SVECTOR *param_1, SVECTOR *param_2, int param_3, int param_4);
 int          sub_80026BC4(short *param_1, short *param_2, int param_3, int param_4);
 int          sub_800264B0(int param_1,int param_2,int param_3);
-void         GV_NearExp2V_8002667C(SVECTOR *, SVECTOR *, int);
+void         GV_NearExp2V_8002667C(short *a, short *b, int count);
 
 typedef struct GV_Vec
 {
