aee87055ac68cffa2f2672b3337177bc1dafa6ab
Kieron Josephs <kieron.josephs00@gmail.com>
2022-09-16 02:44:36 +0100

    Match amissile_loader_8006DA0C

--- src/Bullet/amissile.h
+++ src/Bullet/amissile.h
@@ -15,13 +15,15 @@ typedef struct Actor_amissile
     Actor      field_0_actor;
     GM_Control field_20_ctrl;
     OBJECT     field_9C_kmd;
-    char       field_C0[96];
+    MATRIX     field_C0_matrix;
+    char       field_E0[64];
     int        field_120;
     int        field_124;
     int        field_128;
     SVECTOR    field_12C_svector;
     DG_PRIM   *field_134_prim;
-    char       field_138[40];
+    RECT       field_138_rect;
+    SVECTOR    field_140_svector_4Array[4];
 } Actor_amissile;
 
 STATIC_ASSERT_SIZE(Actor_amissile, 0x160);
@@ -31,8 +33,9 @@ int  amissile_act_helper_8006D600(void);
 
 void amissile_kill_8006D99C(Actor_amissile *pActor);
 
-int amissile_loader_8006DA0C(Actor_amissile *pActor, int, void *);
+int  amissile_loader_8006DA0C(Actor_amissile *pActor, MATRIX *pMtx, int);
+void amissile_loader_helper_8006D1F4(void *pPrim, DG_TEX *pTex);
 
-Actor_amissile * NewAMissile_8006DC50(int arg0, void *arg1);
+Actor_amissile * NewAMissile_8006DC50(MATRIX *pMtx, int);
 
 #endif // _AMISSILE_H_
