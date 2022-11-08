d28d61d3042b33f7961fb8203c4de62d01c61d37
Kieron Josephs <kieron.josephs00@gmail.com>
2022-11-08 19:57:34 +0000

    Match rifle_act_80067D60

--- src/Weapon/rifle.h
+++ src/Weapon/rifle.h
@@ -13,10 +13,10 @@ typedef struct _Actor_Rifle
     GM_Control *field_44_pCtrl;
     OBJECT     *field_48_pParentObj;
     int         field_4c_numParent;
-    int         field_50;
+    int        *field_50;
     int         field_54;
     int         field_58;
-    int         field_5c;
+    void       *field_5c;
 } Actor_Rifle;
 
 STATIC_ASSERT_SIZE(Actor_Rifle, 0x60);
@@ -26,6 +26,6 @@ void rifle_kill_80068118(Actor_Rifle *pActor);
 
 int rifle_loader_80068184(Actor_Rifle *pActor, OBJECT *pParentObj, int numParent);
 
-Actor_Rifle * NewRifle_80068214(GM_Control *pCtrl, OBJECT *pParentObj, int numParent, int a4, int a5);
+Actor_Rifle * NewRifle_80068214(GM_Control *pCtrl, OBJECT *pParentObj, int numParent, int *a4, int a5);
 
 #endif // _RIFLE_H_
