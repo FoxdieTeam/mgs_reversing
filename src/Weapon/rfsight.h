4751aa8c7cfc5f1a1b284046fa7a9845152b77dd
Kieron Josephs <kieron.josephs00@gmail.com>
2022-11-10 00:36:11 +0000

    Match rfsight_act_helper_80069478, finish rfsight.c

--- src/Weapon/rfsight.h
+++ src/Weapon/rfsight.h
@@ -11,7 +11,7 @@ typedef Actor_Sight * (*rfsight_pfn_t)(int, int, short *, short, short *);
 typedef struct _Actor_Rfsight
 {
     Actor         field_0_actor;
-    short        *field_20;
+    DVECTOR      *field_20;
     int           field_24;
     int           field_28;
     rfsight_pfn_t field_2c_pfn;
@@ -19,10 +19,11 @@ typedef struct _Actor_Rfsight
 
 STATIC_ASSERT_SIZE(Actor_Rfsight, 0x30);
 
-int rfsight_act_helper_80069478(int, GV_PAD *, short *, int, int, int);
+void rfsight_act_helper_80069478(int a1, GV_PAD *pPad, DVECTOR *pAxis, int dir, short sens, short max);
 
 void rfsight_act_800696CC(Actor_Rfsight *pActor);
 void rfsight_kill_80069850(Actor_Rfsight *pActor);
+
 int  rfsight_init_helper_8006985C(Actor_Rfsight *pActor);
 
 Actor_Rfsight * NewRifleSight_8006989C(void);
