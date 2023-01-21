54a08c6c7136155aaff2fb7e6a823c725956c021
Kieron <kieron.josephs00@gmail.com>
2023-01-21 02:12:13 +0000

    Match over_act_helper_80037128

--- src/Game/over.h
+++ src/Game/over.h
@@ -10,14 +10,16 @@
 
 typedef struct _Actor_Over
 {
-    Actor   field_0_actor;
-    short   field_20;
-    short   field_22;
-    short   field_24;
-    short   field_26;
-    short   field_28;
-    char    field_2a_pad[0x1662];
-    DVECTOR field_168c_directions[120];
+    Actor    field_0_actor;
+    short    field_20;
+    short    field_22;
+    short    field_24;
+    short    field_26;
+    short    field_28;
+    char     field_2a_pad[0x1632];
+    TILE     field_165c_tiles[2];
+    DR_TPAGE field_167c_tpages[2];
+    DVECTOR  field_168c_directions[120];
 } Actor_Over;
 
 STATIC_ASSERT_SIZE(Actor_Over, 0x186c);
