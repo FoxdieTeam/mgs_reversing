d77e84d554697b22cee3bdc2ec02dec47120e5c3
nocato <nocato@protonmail.com>
2023-01-25 21:36:42 +0100

    Move Set/ResetSpadStack to common.h

---
+++ src/common.h
@@ -0,0 +1,22 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define DCache 0x1F8003FC
+
+// Put stack on scratchpad
+#define SetSpadStack(addr)                                                                                             \
+    {                                                                                                                  \
+        __asm__ volatile("move $8,%0" ::"r"(addr) : "$8", "memory");                                                   \
+        __asm__ volatile("sw $29,0($8)" ::: "$8", "memory");                                                           \
+        __asm__ volatile("addiu $8,$8,-4" ::: "$8", "memory");                                                         \
+        __asm__ volatile("move $29,$8" ::: "$8", "memory");                                                            \
+    }
+
+// reset scratchpad stack
+#define ResetSpadStack()                                                                                               \
+    {                                                                                                                  \
+        __asm__ volatile("addiu $29,$29,4" ::: "$29", "memory");                                                       \
+        __asm__ volatile("lw $29,0($29)" ::: "$29", "memory");                                                         \
+    }
+
+#endif // _COMMON_H_
