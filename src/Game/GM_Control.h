fc6dcdb3768e7f1fa2bb4a22fc78069043dc86de
Kieron <kieron.josephs00@gmail.com>
2023-01-31 08:29:26 +0000

    Match sna_init_80050440

--- src/Game/GM_Control.h
+++ src/Game/GM_Control.h
@@ -9,16 +9,12 @@
 
 typedef struct Res_Control_unknown
 {
-    short   field_0_scriptData_orHashedName;
-    short   field_2_name_hash;
-    short   field_4_trigger_Hash_Name_or_camera_w;
-    short   field_6_count;
-    short   field_8;
-    short   field_A;
-    short   field_C;
-    short   field_E;
-    int     field_10;
-    SVECTOR field_14_vec;
+    short          field_0_scriptData_orHashedName;
+    short          field_2_name_hash;
+    short          field_4_trigger_Hash_Name_or_camera_w;
+    short          field_6_count;
+    unsigned short field_8_array[6];
+    SVECTOR        field_14_vec;
 } Res_Control_unknown;
 
 struct map_record;
