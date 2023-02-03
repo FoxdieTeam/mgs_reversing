1e9f038184974b38d0b1be0436f2b1d2c6ce426e
Kieron <kieron.josephs00@gmail.com>
2023-02-03 21:49:32 +0000

    Match 2 chafgrnd functions

--- src/Okajima/chafgrnd.c
+++ src/Okajima/chafgrnd.c
@@ -8,7 +8,17 @@
 #include "libdg/libdg.h"
 #include "unknown.h"
 
-extern int   GM_GameStatus_800AB3CC;
+extern int     dword_800BDF98;
+extern int     dword_800BDF9C;
+extern int     dword_800BDFA0;
+
+extern int     GM_GameStatus_800AB3CC;
+extern int     GM_CurrentMap_800AB9B0;
+
+extern MATRIX  DG_ZeroMatrix_8009D430;
+extern SVECTOR DG_ZeroVector_800AB39C;
+
+extern const char aChafgrndC[]; // = "chafgrnd.c";
 
 void chafgrnd_init_tiles_800769EC(TILE *a1)
 {
@@ -39,33 +49,108 @@ void chafgrnd_act_helper_80076A6C(TILE * a0)
     }
 }
 
-#pragma INCLUDE_ASM("asm/Okajima/chafgrnd_act_helper_80076A98.s") // 144 bytes
+void chafgrnd_act_helper_80076A98(SVECTOR *va, SVECTOR *vb, SVECTOR *vout)
+{
+    SVECTOR vec;
+    int temp_s0;
+    short temp_v0;
+    int temp_v1;
+
+    GV_SubVec3_80016D40(vb, va, &vec);
+    vout->vy = ratan2_80094308(vec.vx, vec.vz) & 0xfff;
+
+    temp_s0 = vec.vy;
+    vec.vy = 0;
+
+    temp_v1 = ratan2_80094308(GV_VecLen3_80016D80(&vec), temp_s0) & 0xfff;
+    
+    temp_v0 = temp_v1 - 0x400;
+    vout->vx = temp_v0;
+
+    if (temp_v0 < 0)
+    {
+        vout->vx = temp_v1 + 0xc00;
+    }
+
+    vout->vz = 0;
+}
+
 #pragma INCLUDE_ASM("asm/Okajima/chafgrnd_act_80076B28.s")        // 1260 bytes
 
-#pragma INCLUDE_ASM("asm/Okajima/chafgrnd_loader_80077014.s")     // 520 bytes
+int chafgrnd_loader_80077014(Actor_Chafgrnd *pActor, MATRIX *pWorld)
+{
+    SVECTOR vec1;
+    SVECTOR vec2;
+    DG_PRIM *pPrim;
+    int i;
+    int j;
+
+    dword_800BDFA0 = 300;
+    dword_800BDF9C = mts_get_tick_count_8008BBB0();
+    
+    pActor->field_a38 = 64;
+    pActor->field_24 = 0;
+    pActor->field_28 = dword_800BDFA0 - 60;
+
+    pActor->field_20_map = GM_CurrentMap_800AB9B0;
+    
+    pActor->field_2c.vx = pWorld->t[0];
+    pActor->field_2c.vy = pWorld->t[1];
+    pActor->field_2c.vz = pWorld->t[2];
+
+    pActor->field_20_map = GM_CurrentMap_800AB9B0;
+    
+    vec1.vx = 0;
+    vec1.vy = 0;
+    vec2.vz = 0;
+    
+    pPrim = DG_GetPrim(1033, 64, 0, pActor->field_834, NULL);
+    pActor->field_a34 = pPrim;
+
+    if (!pPrim)
+    {
+        return -1;
+    }
+
+    chafgrnd_init_tiles_800769EC(&pPrim->field_40_pBuffers[0]->tiles);
+    chafgrnd_init_tiles_800769EC(&pPrim->field_40_pBuffers[1]->tiles);
+
+    pActor->field_a80 = DG_ZeroMatrix_8009D430;
+
+    for (i = 0; i < 64; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            vec1.vz = GV_RandU_80017090(1024) + 100;
+            vec2.vx = -GV_RandU_80017090(512) - 256;
+            vec2.vy = GV_RandU_80017090(4096);
+
+            DG_SetPos2_8001BC8C(&DG_ZeroVector_800AB39C, &vec2);
+            DG_PutVector_8001BE48(&vec1, &pActor->field_34[j][i], 1);
+
+            pActor->field_434[j][i] = pActor->field_2c;
+        }
+        
+        pActor->field_a40[i] = 0;
+    }
+
+    return 0;
+}
 
 void chafgrnd_kill_8007721C(Actor_Chafgrnd *pActor)
 {
-    DG_OBJS *pObjs = pActor->field_a34;
+    DG_PRIM *pPrim = pActor->field_a34;
 
     GM_GameStatus_800AB3CC &= ~1;
 
-    if (pObjs)
+    if (pPrim)
     {
-        DG_DequeuePrim_800182E0(pObjs);
-        DG_FreePrim_8001BC04(pObjs);
+        DG_DequeuePrim_800182E0((DG_OBJS *)pPrim);
+        DG_FreePrim_8001BC04((DG_OBJS *)pPrim);
     }
 }
 
-extern int GM_GameStatus_800AB3CC;
-extern int GM_CurrentMap_800AB9B0;
-
-extern int dword_800BDF98;
-extern int dword_800BDFA0;
-
-extern const char aChafgrndC[]; // = "chafgrnd.c";
-
-Actor_Chafgrnd * NewChaffGrd_80077264(MATRIX *pWorld)
+Actor_Chafgrnd * NewChafgrnd_80077264(MATRIX *pWorld)
 {
     SVECTOR vec;
     Actor_Chafgrnd *pActor;
