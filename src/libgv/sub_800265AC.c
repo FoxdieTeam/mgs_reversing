96d90c76de02640c1d3a804d9f6a637e8fe9e0b8
ChrizNuh <23082707+ChrizNuh@users.noreply.github.com>
2023-02-08 13:58:11 +0000

    Refactor for inline FP_Subtract

--- src/libgv/sub_800265AC.c
+++ src/libgv/sub_800265AC.c
@@ -1,18 +1,15 @@
+#include "libgv\libgv.h"
+
 int sub_800265AC(int param_1, int param_2)
 {
     short uVar1;
     int   iVar2;
 
-    uVar1 = (param_1 - param_2) & 0xfff;
-
-    if (uVar1 > 0x800)
-    {
-        uVar1 |= 0xf000;
-    }
+    uVar1 = FP_Subtract(param_1, param_2);
 
     iVar2 = param_2 - (param_2 + uVar1);
 
-    if (iVar2 + 7 < 15U)
+    if ((iVar2 + 7 < 15U))
     {
         return param_2;
     }
