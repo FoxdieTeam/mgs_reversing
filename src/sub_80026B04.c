20496af1454c379ce2c2a53e380b423fa675a6c2
ChrizNuh <23082707+ChrizNuh@users.noreply.github.com>
2023-02-08 18:52:51 +0000

    Match sub_80026B04

---
+++ src/sub_80026B04.c
@@ -0,0 +1,29 @@
+void sub_80026B04(short *arg0, short *arg1, int range, int count)
+{
+    int    temp_t1;
+    short *var_a0;
+    short *var_a1;
+    int    var_v1;
+    int    ret;
+
+    var_a0 = arg0;
+    var_a1 = arg1;
+
+    while (--count >= 0)
+    {
+        temp_t1 = *var_a0;
+
+        do {} while (0);
+
+        ret = *var_a1;
+        var_v1 = (*var_a1 - *var_a0) / 4;
+
+        if ((-range >= var_v1) || (var_v1 >= range))
+        {
+            ret = temp_t1 + var_v1;
+        }
+
+        *var_a0++ = ret;
+        var_a1++;
+    }
+}
