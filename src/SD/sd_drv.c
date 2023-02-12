c7407980f279d63ce88450d43330b4cb44cf84a1
Paul <github@paulsapps.com>
2023-02-12 22:21:43 +0000

    match IntSdMain_80084494

--- src/SD/sd_drv.c
+++ src/SD/sd_drv.c
@@ -19,11 +19,370 @@ extern int sng_fade_in_2_800BF290;
 
 extern const char aLoadsngdataFil[];
 extern const char aCompletedloads[];
+extern const char aSngStatusX[];
+extern const char aSongkaihimode[];
+extern const char aSongfadeoutSto[];
+extern const char aSongfadeoutPau[];
+extern const char aSongstop[];
+extern const char aSongpauseon[];
+extern const char aSongpauseoff[];
+extern const char aSongfadein[];
+extern const char aSamesonghasalr[];
+extern const char aSoundWorkIsBro[];
+extern const char aSongsyukanmode_0[];
+extern const char aSongsyukanmode[];
+extern const char aSongEndXX[];
+extern const char aSngcodeX[];
+extern const char aErrorSngPlayCo[];
+
+extern int sd_sng_code_buf_800BF018[16];
+extern int sng_syukan_fg_800C0510;
+extern int sng_pause_fg_800BF298;
+extern int sng_fout_fg_800BF25C;
+extern int sng_fadein_fg_800C041C;
+extern int sng_status_800BF158;
+extern int sng_fadein_fg_800C041C;
+extern int sng_fade_in_2_800BF290;
+extern int song_end_800C04E8;
+extern int se_tracks_800BF004;
+extern int keyd_800C0524;
+extern int sng_fade_in_2_800C0BC0;
+extern int dword_800C0428;
+extern int sd_sng_data_800C0420;
+extern int sd_code_read_800BF288;
+extern int stop_jouchuu_se_800BF1A0;
+extern int sng_fout_term_800C0518;
+extern int dword_800BEFF8;
+
+extern unsigned int mtrack_800BF1EC;
+extern unsigned char * mptr_800C0570;
+
+extern int spu_ch_tbl_800A2AC8[];
+extern int fade_unk_1_800C0BC8[13];
+extern int sng_fade_time_800C0430[14];
+extern int sng_fade_value_800C0538[13];
+
+extern SOUND_W * sptr_800C057C;
+extern SOUND_W sound_w_800BF2A8[21];
+extern SEPLAYTBL se_request_800BF0E0[8]; // 0x60 (96) bytes
 
 void sng_track_init_800859B8(SOUND_W *pSoundW);
 int SD_8008395C(int a1, int a2);
+void SD_80085480();
+void sng_pause_80087EF4();
+void sng_pause_off_80087F24();
+void SD_SongFadeIn_80084CCC();
+int SngFadeOutP_80084D60(unsigned int a1);
+void SD_SongFadeoutAndStop_80084E48();
+void SD_SongKaihiMode_80084F88();
+void sng_off_80087E2C();
+void sng_adrs_set_80085658();
+void SngFadeWkSet_80085020();
+void SD_80085164();
+int SD_80085A50();
+void se_off_80087E94(int a1);
+void se_adrs_set_8008576C();
+void SD_spuwr_80087A88();
 
-#pragma INCLUDE_ASM("asm/SD/IntSdMain_80084494.s") // 2104 bytes
+void IntSdMain_80084494()
+{
+    int temp; // $v0
+    int pSngData; // $a2
+    int k13Counter; // $a2
+    unsigned char *field_0_mpointer; // $v1
+    int new_sng_status; // $v0
+    unsigned char *track_ptr; // $v1
+    SOUND_W* wTmp;
+
+    if ( sd_sng_code_buf_800BF018[sd_code_read_800BF288] )
+    {
+        temp = sd_sng_code_buf_800BF018[sd_code_read_800BF288];
+        sd_sng_code_buf_800BF018[sd_code_read_800BF288] = 0;
+        sd_code_read_800BF288 = (sd_code_read_800BF288 + 1) & 0xF;
+        mts_printf_8008BBA0(aSngcodeX, temp);
+    }
+    else
+    {
+        temp = 0;
+    }
+
+    if (temp)
+    {
+    switch(temp)
+    {  
+        // Pause
+        case 0x1FFFF01:
+            sng_pause_fg_800BF298 = 1;
+            sng_pause_80087EF4();
+            mts_printf_8008BBA0(aSongpauseon);
+            break;
+
+        // Unpause
+        case 0x1FFFF02:
+            sng_pause_off_80087F24();
+            sng_pause_fg_800BF298 = 0;
+            mts_printf_8008BBA0(aSongpauseoff);
+            break;
+
+        // Fade in
+        case 0x1FFFF03:
+        case 0x1FFFF04:
+        case 0x1FFFF05:
+            if ( sng_status_800C04F8 != 0xFFFFFFFF ) 
+            {
+                sng_fout_fg_800BF25C = 0;
+                if ( sng_status_800BF158 < 3 )
+                {
+                    sng_fadein_fg_800C041C = temp;
+                }
+                else
+                {
+                    SD_SongFadeIn_80084CCC(temp);
+                }
+               sng_fade_in_2_800BF290 = 0;
+                sng_fade_in_2_800C0BC0 = 0;
+            }
+            mts_printf_8008BBA0(aSongfadein);
+            break;
+        
+        // Fade out
+        case 0x1FFFF06:
+        case 0x1FFFF07:
+        case 0x1FFFF08:
+        case 0x1FFFF09:            
+            SngFadeOutP_80084D60(temp);
+            mts_printf_8008BBA0(aSongfadeoutPau);
+            break;
+
+        // Fade out and stop
+        case 0x1FFFF0A:
+        case 0x1FFFF0B:
+        case 0x1FFFF0C:
+        case 0x1FFFF0D:
+            SD_SongFadeoutAndStop_80084E48(temp);
+            mts_printf_8008BBA0(aSongfadeoutSto);
+            break;
+
+        // Evasion Mode
+        case 0x1FFFF10:
+            SD_SongKaihiMode_80084F88();
+            mts_printf_8008BBA0(aSongkaihimode);
+            break;
+
+        // First Person Mode
+        case 0x1FFFF20:
+            sng_syukan_fg_800C0510 = 1;
+            mts_printf_8008BBA0(aSongsyukanmode);
+            break;
+
+        // Exit first person
+        case 0x1FFFF21:
+            sng_syukan_fg_800C0510 = 0;
+            mts_printf_8008BBA0(aSongsyukanmode_0);
+            break;
+        
+        case 0x1FFFFFF:
+            sng_status_800C04F8 = 0;
+            sng_off_80087E2C();
+            mts_printf_8008BBA0(aSongstop);
+            break;
+
+        case 0x1000001:
+        case 0x1000002:
+        case 0x1000003:
+        case 0x1000004:
+        case 0x1000005:
+        case 0x1000006:
+        case 0x1000007:
+        case 0x1000008:
+
+            if ( sng_status_800C04F8 != temp )
+            {
+                if ( sng_status_800BF158 >= 2 )
+                {
+                    pSngData = *(unsigned char *)sd_sng_data_800C0420;
+                    if ( pSngData < (temp & 0xF) )
+                    {
+                        mts_printf_8008BBA0(aErrorSngPlayCo, temp, pSngData);
+                    }
+                    else
+                    {
+                        sng_status_800BF158 = 2;
+                        if ( sng_status_800C04F8 == -1 )
+                        {
+                            for (k13Counter=0; k13Counter < 13; k13Counter++)
+                            {
+                                sng_fade_time_800C0430[k13Counter] = 0;
+                                sng_fade_value_800C0538[k13Counter] = 0;
+                                fade_unk_1_800C0BC8[k13Counter] = 0;
+                            }
+                            sng_fout_term_800C0518 = 0;
+                        }
+                        sng_status_800C04F8 = temp;
+                        sng_off_80087E2C();
+                        sng_pause_fg_800BF298 = 0;
+                        sng_fade_in_2_800BF290 = 0;
+                        sng_fade_in_2_800C0BC0 = 0;  
+                    }
+                }
+                else
+                {
+                  mts_printf_8008BBA0(aSngStatusX, sng_status_800BF158);
+                }
+            }
+            else
+            {
+                mts_printf_8008BBA0(aSamesonghasalr);
+            }    
+        break;
+        
+    default:
+            if ( dword_800C0428 != temp )
+            {
+                dword_800C0428 = temp;
+                sng_status_800C04F8 = 0;
+                sng_status_800BF158 = 1;
+                sng_off_80087E2C();
+                mts_wup_tsk_8008A540(5);
+            }
+
+            break;
+    }
+    }
+
+    switch(sng_status_800BF158)
+    {
+        case 2:
+            if ( sng_status_800C04F8 == 0 )
+            {
+                break;
+            }
+            
+            if ( sng_status_800C04F8 == -1 )
+            {
+                break;
+            }
+            
+            sng_adrs_set_80085658(sng_status_800C04F8);
+            SngFadeWkSet_80085020();
+            sng_status_800BF158 = 3;
+            dword_800BEFF8 = 0;
+            break;
+
+        case 3:
+            if ( dword_800BEFF8 )
+            {
+                if ( (dword_800BEFF8 & 0x1FFF) != (song_end_800C04E8 & 0x1FFF) )
+                {
+                    mts_printf_8008BBA0(aSoundWorkIsBro);
+                    mts_printf_8008BBA0(aSoundWorkIsBro);
+                    mts_printf_8008BBA0(
+                        aSongEndXX,
+                        song_end_800C04E8 & 0x1FFF,
+                        dword_800BEFF8 & 0x1FFF);
+                    mts_printf_8008BBA0(aSoundWorkIsBro);
+                    mts_printf_8008BBA0(aSoundWorkIsBro);
+                }
+            }
+        
+            SD_80085164();
+            SD_80085480();
+
+            mtrack_800BF1EC = 0;
+
+            do
+            {
+                keyd_800C0524 = spu_ch_tbl_800A2AC8[mtrack_800BF1EC];
+                if ( (song_end_800C04E8 & keyd_800C0524) == 0 )
+                {
+                    sptr_800C057C = &sound_w_800BF2A8[mtrack_800BF1EC];
+                    field_0_mpointer = sptr_800C057C->field_0_mpointer;
+                    if ( !field_0_mpointer )
+                    {
+                        song_end_800C04E8 |= keyd_800C0524;
+                    }
+                    else
+                    {
+                         mptr_800C0570 = field_0_mpointer;
+                        if ( SD_80085A50() )
+                        {
+                            song_end_800C04E8 |= keyd_800C0524;
+                            sptr_800C057C->field_0_mpointer = 0;
+                        }
+                        else
+                        {
+                            sptr_800C057C->field_0_mpointer = mptr_800C0570;
+                        }
+                    }
+                }
+                ++mtrack_800BF1EC;
+            }
+            while ( (unsigned int)mtrack_800BF1EC < 13 );
+
+            dword_800BEFF8 = song_end_800C04E8;
+            if ( (song_end_800C04E8 & 0x1FFF) == 0x1FFF )
+            {
+                new_sng_status = 4;
+                sng_status_800BF158 = new_sng_status;
+            }    
+            break;
+
+                
+        case 4:
+            sng_off_80087E2C();
+            new_sng_status = 2;
+            sng_status_800C04F8 = 0;
+            sng_status_800BF158 = new_sng_status;
+            break;
+        
+    }
+
+    for (mtrack_800BF1EC=13; mtrack_800BF1EC < 21; mtrack_800BF1EC++)
+    {
+        if ( se_tracks_800BF004 < 2 && se_request_800BF0E0[mtrack_800BF1EC - 13].field_8_code )
+        {
+            se_off_80087E94(mtrack_800BF1EC - 13);
+            se_adrs_set_8008576C(mtrack_800BF1EC - 13);
+        }
+        else
+        {
+            keyd_800C0524 = spu_ch_tbl_800A2AC8[mtrack_800BF1EC];
+            if ( (song_end_800C04E8 & keyd_800C0524) == 0 )
+            {
+                wTmp = &sound_w_800BF2A8[mtrack_800BF1EC];
+                track_ptr = wTmp->field_0_mpointer;
+                sptr_800C057C = wTmp;
+                if ( !track_ptr )
+                {
+                    song_end_800C04E8 |= keyd_800C0524;
+                }
+                else
+                {
+                    mptr_800C0570 = track_ptr;
+                    if ( SD_80085A50() )
+                    {
+                        song_end_800C04E8 |= keyd_800C0524;
+                        sptr_800C057C->field_0_mpointer = 0;
+                    }
+                    else
+                    {
+                        sptr_800C057C->field_0_mpointer = mptr_800C0570;
+                    }
+             
+                }
+            }
+        }
+    }
+
+    if ( stop_jouchuu_se_800BF1A0 >= 2 )
+    {
+        stop_jouchuu_se_800BF1A0 = 0;
+    }
+    
+    SD_spuwr_80087A88();
+}
+
+
 
 void SD_SongFadeIn_80084CCC(unsigned int mode)
 {
@@ -106,7 +465,7 @@ int SngFadeOutP_80084D60(unsigned int a1)
 
 #pragma INCLUDE_ASM("asm/SD/SD_SongFadeoutAndStop_80084E48.s") // 320 bytes
 #pragma INCLUDE_ASM("asm/SD/SD_SongKaihiMode_80084F88.s") // 152 bytes
-#pragma INCLUDE_ASM("asm/SD/SD_80085020.s") // 324 bytes
+#pragma INCLUDE_ASM("asm/SD/SngFadeWkSet_80085020.s") // 324 bytes
 #pragma INCLUDE_ASM("asm/SD/SD_80085164.s") // 796 bytes
 
 void SD_80085480()
@@ -186,7 +545,7 @@ extern int dword_800BF210;
 extern int spu_ch_tbl_800A2AC8[];
 
 // https://decomp.me/scratch/sro0f
-#pragma INCLUDE_ASM("asm/SD/SD_8008576C.s") // 588 bytes
+#pragma INCLUDE_ASM("asm/SD/se_adrs_set_8008576C.s") // 588 bytes
 
 
 void sng_track_init_800859B8(SOUND_W *pSoundW)
