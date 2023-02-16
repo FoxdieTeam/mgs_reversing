3ca6c0b0858d6a8b57d31bc4cac78d6cf228e8cf
Kieron <kieron.josephs00@gmail.com>
2023-02-16 18:34:41 +0000

    Match d_bloodr_loader_800730EC

--- src/Okajima/d_blood.h
+++ src/Okajima/d_blood.h
@@ -9,11 +9,11 @@ typedef struct _Actor_DBlood
     int    f20;
     int    f24_state;
     int    f28;
-    char   f2C_placeholder[12];
+    char   f2C_placeholder[0xC];
     int    f38_current_map;
 } Actor_DBlood;
 
-STATIC_ASSERT_SIZE(Actor_DBlood, 0x3c);
+STATIC_ASSERT_SIZE(Actor_DBlood, 0x3C);
 
 void d_blood_act_80072A0C(Actor_DBlood *pActor);
 void d_blood_kill_null_800729AC(Actor_DBlood *pActor);
