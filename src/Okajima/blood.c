7e621d6311d53c59d5007d453a71825ca1086d5d
Kieron <kieron.josephs00@gmail.com>
2023-02-16 18:00:00 +0000

    Match NewBlood_80072728

--- src/Okajima/blood.c
+++ src/Okajima/blood.c
@@ -1,6 +1,12 @@
 #include "blood.h"
+#include "psyq.h"
 #include "libdg/libdg.h"
+#include "libgv/libgv.h"
 #include "Anime/animeconv/anime.h"
+
+extern ANIMATION stru_8009F614;
+
+extern const char aBloodC[]; // = "blood.c"
 
 #pragma INCLUDE_ASM("asm/Okajima/blood_loader2_helper2_80072080.s") // 788 bytes
 #pragma INCLUDE_ASM("asm/Okajima/blood_act_helper_80072394.s")      // 228 bytes
@@ -8,11 +14,11 @@
 #pragma INCLUDE_ASM("asm/Okajima/blood_act_helper_8007250C.s")      // 44 bytes
 #pragma INCLUDE_ASM("asm/Okajima/blood_act_80072538.s")             // 148 bytes
 
-void blood_kill_800725CC(Actor_Blood *actor_blood)
+void blood_kill_800725CC(Actor_Blood *pActor)
 {
     DG_OBJS *prim;
 
-    prim = actor_blood->f24_prim;
+    prim = pActor->field_24_prim;
     if (prim)
     {
         DG_DequeuePrim_800182E0(prim);
@@ -21,9 +27,65 @@ void blood_kill_800725CC(Actor_Blood *actor_blood)
 }
 
 #pragma INCLUDE_ASM("asm/Okajima/blood_loader2_80072608.s") // 288 bytes
-#pragma INCLUDE_ASM("asm/Okajima/NewBlood_80072728.s")      // 524 bytes
 
-extern ANIMATION stru_8009F614;
+Actor_Blood * NewBlood_80072728(MATRIX *arg0, int count)
+{
+    SVECTOR input;
+    SVECTOR speed;
+    SVECTOR pos;
+    int angle;
+    short divisor;
+    int i;
+    Actor_Blood *pActor;
+
+    pActor = NULL;
+
+    if (count < 11)
+    {
+        DG_SetPos_8001BC44(arg0);
+
+        angle = GV_RandU_80017090(4096);
+        input.vx = rcos_800925D8(angle) / 4;
+        input.vz = rsin_80092508(angle) / 4;
+
+        divisor = GV_RandU_80017090(128) + 32;
+        input.vx /= divisor;
+        input.vy = divisor;
+        input.vz /= divisor;
+
+        DG_RotVector_8001BE98(&input, &speed, 1);
+        DG_PutVector_8001BE48(&input, &pos, 1);
+
+        speed.vx /= 4;
+        speed.vy /= 4;
+        speed.vz /= 4;
+
+        AN_Blood_Mist_80072934(&pos, &speed);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        pActor = (Actor_Blood *)GV_NewActor_800150E4(5, sizeof(Actor_Blood));
+
+        if (!pActor)
+        {
+            continue;
+        }
+
+        GV_SetNamedActor_8001514C(&pActor->field_0_actor,
+                                  (TActorFunction)&blood_act_80072538,
+                                  (TActorFunction)&blood_kill_800725CC,
+                                  aBloodC);
+
+        if (blood_loader2_80072608(pActor, arg0, count) < 0)
+        {
+            GV_DestroyActor_800151C8(&pActor->field_0_actor);
+            return NULL;
+        }
+    }
+
+    return pActor;
+}
 
 void AN_Blood_Mist_80072934(SVECTOR *pos, SVECTOR *speed)
 {
@@ -38,10 +100,10 @@ void AN_Blood_Mist_80072934(SVECTOR *pos, SVECTOR *speed)
     pSpeed->vx = speed->vx;
     pSpeed->vy = speed->vy;
     pSpeed->vz = speed->vz;
-    
+
     pre.scr_num = 0;
     pre.s_anim = 0;
-    
+
     anm = &stru_8009F614;
     anm->field_14_pre_script = &pre;
 
--- src/Okajima/d_bloodr.h
+++ src/Okajima/d_bloodr.h
@@ -1,11 +1,13 @@
-#ifndef _DBLOODR_H_
-#define _DBLOODR_H_
+#ifndef _D_BLOODR_H_
+#define _D_BLOODR_H_
 
 #include "libgv/libgv.h"
 
-GV_ACT *NewKetchap_r_80073148(int arg0);
-int     d_bloodr_act_80072C10(int a1);
-void    d_bloodr_kill_80072BD4(int a1);
-int     d_bloodr_loader_800730EC(GV_ACT *pActor, int a2);
+void d_bloodr_act_80072C10(int a1);
+void d_bloodr_kill_80072BD4(int a1);
 
-#endif // _DBLOODR_H_
+int d_bloodr_loader_800730EC(GV_ACT *pActor, int map);
+
+GV_ACT * NewKetchap_r_80073148(int map);
+
+#endif // _D_BLOODR_H_
