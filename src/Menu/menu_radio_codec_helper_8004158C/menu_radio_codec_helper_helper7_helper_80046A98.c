3262acf58a6345f7bf2e818e4aa0886b1ab67901
nocato <nocato@protonmail.com>
2023-02-24 01:09:43 +0100

    Match menu_radio_codec_helper_helper7_helper_80046A98

---
+++ src/Menu/menu_radio_codec_helper_8004158C/menu_radio_codec_helper_helper7_helper_80046A98.c
@@ -0,0 +1,23 @@
+#include "Menu/menuman.h"
+#include "psyq.h"
+
+extern RECT rect_800AB6C8;
+RECT        SECTION(".sdata") rect_800AB6C8;
+
+extern RECT rect_800AB6D0;
+
+void menu_radio_codec_helper_helper7_helper_80046A98(menu_chara_struct *pStru)
+{
+    char *pSaveText;
+    int   area;
+
+    area = rect_800AB6C8.w * rect_800AB6C8.h * 2;
+
+    LoadImage_8008FB10(&rect_800AB6C8, (char *)pStru->field_2C_pSaveText);
+
+    pSaveText = (char *)pStru->field_2C_pSaveText;
+    LoadImage_8008FB10(&rect_800AB6D0, pSaveText + area);
+
+    DrawSync(0);
+    GV_FreeMemory_80015FD0(0, pStru->field_2C_pSaveText);
+}
