d64f721390f2ba988c09b0b7a7b9a3091692b33d
nocato <nocato@protonmail.com>
2023-02-27 19:26:23 +0100

    Match menu_radio_codec_helper_helper2_8004DF68

--- src/Menu/menu_radio_codec_helper_8004158C/menu_radio_codec_helper_helper_8004E198.c
+++ src/Menu/menu_radio_codec_helper_8004158C/menu_radio_codec_helper_helper_8004E198.c
@@ -2,8 +2,8 @@
 
 extern RadioMemory gRadioMemory_800BDB38[RADIO_MEMORY_COUNT];
 
-extern int dword_800AB770;
-int        dword_800AB770;
+extern int gRadioCodecIdx_800AB770;
+int        gRadioCodecIdx_800AB770;
 
 void menu_radio_codec_helper_helper_8004E198(int toFind)
 {
@@ -12,6 +12,6 @@ void menu_radio_codec_helper_helper_8004E198(int toFind)
     contact = menu_radio_table_find_8004D380(toFind);
     if (contact)
     {
-        dword_800AB770 = contact - gRadioMemory_800BDB38;
+        gRadioCodecIdx_800AB770 = contact - gRadioMemory_800BDB38;
     }
 }
---
+++ src/Menu/menu_radio_codec_helper_helper2_8004DF68.c
@@ -0,0 +1,90 @@
+#include "Menu/radio.h"
+#include "Game/game.h"
+
+extern RadioCodecStru_800ABB98 *stru_800ABB98;
+RadioCodecStru_800ABB98        *stru_800ABB98;
+
+extern int gRadioCodecIdx_800AB770;
+int        gRadioCodecIdx_800AB770;
+
+extern short word_800ABB9C;
+short        word_800ABB9C;
+
+extern short gRadioCodecFrequency_800ABB9E;
+short        gRadioCodecFrequency_800ABB9E;
+
+int menu_radio_codec_helper_helper2_8004DF68(Actor_MenuMan *pActor, GV_PAD *pPad)
+{
+    RadioCodecStru_800ABB98 *pStru;
+    int                      pressed;
+
+    pStru = stru_800ABB98;
+    pressed = 0;
+    if (pPad->status & (PAD_UP | PAD_DOWN))
+    {
+        if (pPad->status & PAD_UP)
+        {
+            pressed = -2;
+        }
+        else
+        {
+            pressed = 2;
+        }
+    }
+    else if (pPad->status & (PAD_LEFT | PAD_RIGHT))
+    {
+        if (pPad->status & PAD_LEFT)
+        {
+            pressed = -1;
+        }
+        else
+        {
+            pressed = 1;
+        }
+    }
+
+    if (pressed != 0)
+    {
+        if (pStru->field_6 == pressed)
+        {
+            pStru->field_8--;
+            if (pStru->field_8 < 0)
+            {
+                sub_8004D580(pressed);
+                pStru->field_8 = 2;
+            }
+        }
+        else
+        {
+            sub_8004D580(pressed);
+            pStru->field_8 = 10;
+            pStru->field_6 = pressed;
+        }
+    }
+    else
+    {
+        pStru->field_6 = 0;
+    }
+
+    if (pPad->press & PAD_CIRCLE)
+    {
+        gRadioCodecIdx_800AB770 = pStru->field_0_idx;
+        gRadioCodecFrequency_800ABB9E = pStru->field_10_array[gRadioCodecIdx_800AB770].frequency;
+        word_800ABB9C = 1;
+        GM_Sound_80032968(0, 0x3f, 0x55);
+    }
+    if (pPad->press & PAD_CROSS)
+    {
+        gRadioCodecFrequency_800ABB9E = -1;
+        word_800ABB9C = 1;
+        GM_Sound_80032968(0, 0x3f, 0x68);
+    }
+    if (word_800ABB9C == 0 && pActor->field_212 > 4)
+    {
+        return gRadioCodecFrequency_800ABB9E;
+    }
+    else
+    {
+        return 0;
+    }
+}
