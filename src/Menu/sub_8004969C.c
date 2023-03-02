97cdc8addecb7b713cb2a4ae678aa547e43cbe72
nocato <nocato@protonmail.com>
2023-03-02 20:37:03 +0100

    Match sub_8004969C

---
+++ src/Menu/sub_8004969C.c
@@ -0,0 +1,40 @@
+#include "Menu/menuman.h"
+
+extern char aRadioCallOver[];
+
+radio_table_entry *sub_8004969C(radio_table *pRadioTable, int contactFrequency)
+{
+    radio_table_entry *entriesIter;
+    int                freq;
+    int                i;
+    int                found_idx;
+
+    found_idx = -1;
+    i = 7;
+    entriesIter = &pRadioTable->field_0_entries[7];
+
+    for (i = 7; i >= 0; i--, entriesIter--)
+    {
+        freq = entriesIter->field_0_contactFrequency;
+        if (freq == contactFrequency)
+        {
+            found_idx = i;
+            break;
+        }
+
+        if (freq == 0)
+        {
+            found_idx = i;
+        }
+    }
+
+    if (found_idx < 0)
+    {
+        mts_printf_8008BBA0(aRadioCallOver);
+        return NULL;
+    }
+    else
+    {
+        return &pRadioTable->field_0_entries[found_idx];
+    }
+}
