2ddc593be4ba17374be3bfe27bd59d40f08d0e72
nocato <nocato@protonmail.com>
2023-03-03 20:08:55 +0100

    Match sub_800469F0

---
+++ src/Menu/sub_800469F0.c
@@ -0,0 +1,32 @@
+#include "Menu/menuman.h"
+#include "psyq.h"
+
+extern RECT rect_800AB6C8;
+RECT        rect_800AB6C8;
+
+extern RECT rect_800AB6D0;
+RECT        rect_800AB6D0;
+
+extern const char aNoMemoryForSav[]; // = "NO MEMORY FOR SAVE TEX\n";
+
+void sub_800469F0(menu_chara_struct *pStru)
+{
+    int    offsetImage2;
+    int    totalSize;
+    short *pSaveText;
+    char  *pSaveTextCopy;
+
+    offsetImage2 = rect_800AB6C8.w * rect_800AB6C8.h * 2; // 16-bit per pixel
+    totalSize = offsetImage2 + rect_800AB6D0.w * rect_800AB6D0.h * 2;
+    pSaveText = (short *)GV_AllocMemory_80015EB8(0, totalSize);
+
+    pStru->field_2C_pSaveText = pSaveText;
+    if (pSaveText == NULL)
+    {
+        mts_printf_8008BBA0(aNoMemoryForSav);
+    }
+
+    pSaveTextCopy = (char *)pStru->field_2C_pSaveText;
+    StoreImage_8008FB70(&rect_800AB6C8, (u_long *)pSaveTextCopy);
+    StoreImage_8008FB70(&rect_800AB6D0, (u_long *)(pSaveTextCopy + offsetImage2));
+}
