cffaf85c517b89d4a4869471a8262fd65995d364
nocato <nocato@protonmail.com>
2023-03-07 15:07:19 +0100

    Match sub_80046BD8

---
+++ src/Menu/sub_80046BD8.c
@@ -0,0 +1,23 @@
+#include "psyq.h"
+
+extern RECT rect_800AB6C8;
+RECT        rect_800AB6C8;
+
+extern RECT rect_800AB6D0;
+RECT        rect_800AB6D0;
+
+void sub_80046BD8(int off)
+{
+    RECT rect;
+
+    rect = rect_800AB6C8;
+    rect.y += off;
+    rect.h = 1;
+    MoveImage_8008FBD0(&rect, rect.x, (rect.y * 65536) / 65536 + 2);
+
+    rect.x = off * 32 + 960;
+    rect.y = 336;
+    rect.w = 32;
+    rect.h = 96;
+    MoveImage_8008FBD0(&rect, rect_800AB6D0.x + off * 32, rect_800AB6D0.y);
+}
