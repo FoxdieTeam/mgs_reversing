eb9555036220cc18a472c996ed42da3e93e6da1b
g-e-o <34046939+g-e-o@users.noreply.github.com>
2023-03-13 20:40:19 +0100

    match Map_light_80030C6C (#1122)

--- src/map/map.h
+++ src/map/map.h
@@ -6,18 +6,18 @@
 #include <LIBGPU.H>
 #include "libhzd/libhzd.h"
 
-struct LitHeader
+typedef struct        LitHeader
 {
-    int field_0_num_lights;
-};
+    int               field_0_num_lights;
+} LitHeader;
 
-typedef struct map_record
+typedef struct        map_record
 {
     int               field_0_map_index_bit;
     unsigned short    field_4_mapNameHash;
     unsigned short    field_6_bUsed;
     HZD_MAP          *field_8_hzd;
-    struct LitHeader *field_C_lit_file;
+    LitHeader        *field_C_lit_file;
     int               field_10_zone_id;
 } map_record;
 
@@ -28,16 +28,16 @@ typedef struct AreaHistory
     short history[MAX_HISTORY];
 } AreaHistory;
 
-map_record *Map_FindByNum_80031504(int mapNameHash);
-int         Map_MarkUsed_80031324(int mapName);
-void        Map_KmdLoad_80030E74(int pLitName, struct map_record *pMap);
-HZD_MAP *Map_HZD_Load_80030F38(int resource_name_hashed, int flagsIndex, int bitIndex, int default_48, int default_24);
+map_record        *Map_FindByNum_80031504(int mapNameHash);
+int                Map_MarkUsed_80031324(int mapName);
+void               Map_KmdLoad_80030E74(int pLitName, struct map_record *pMap);
+HZD_MAP           *Map_HZD_Load_80030F38(int resource_name_hashed, int flagsIndex, int bitIndex, int default_48, int default_24);
 struct map_record *Map_GetNextFreeRecord_80030E30(int mapNameHashed);
 struct map_record *Map_FromId_800314C0(int id);
 struct map_record *Map_ScriptLoadMapBlocks_800312D0();
 int                Map_ScriptReloadMaps_80031450(int a1);
 void               Map_LoadMapData_80031244(int mapNum, int resourceNameHashed);
-int                Map_light_80030C6C(int a1);
+void               Map_light_80030C6C(int a1);
 void               Map_80030FA4();
 void               Map_ResetMapCountAndKmdsCount_800310A0();
 
