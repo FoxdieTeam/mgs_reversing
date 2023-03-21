a4e3ec19583361766b1ed43c0420dc67e98213d9
nocato <nocato@protonmail.com>
2023-03-21 02:25:53 +0100

    Collaborative match sd_set_80088CB0

--- src/SD/sd_cli.c
+++ src/SD/sd_cli.c
@@ -66,8 +66,6 @@ int get_str_counter_80088CA0(void)
     return dword_8009F7B4;
 }
 
-#pragma INCLUDE_ASM("asm/SD/sd_set_80088CB0.s") // 1528 bytes
-
 extern const char aSdcodeX[];
 extern const char aToomuchbgmsoun[];
 extern const char aSdsetSameStrea[];
@@ -107,3 +105,175 @@ extern int dword_800BF144;
 extern int se_load_code_800BF28C;
 extern int wave_load_code_800C0528;
 extern int wave_save_code_800C0578;
+
+void sd_set_80088CB0(int sdCode)
+{
+    int sdCodeTopByte, new_bgm_idx;
+
+    if (dword_800BF000 != 0)
+    {
+        mts_printf_8008BBA0(aSdcodeX, sdCode);
+    }
+
+    sdCodeTopByte = sdCode & 0xFF000000;
+    if (sdCodeTopByte == 0)
+    {
+        if (sdCode & 0xFF)
+        {
+            SePlay_800888F8(sdCode);
+        }
+    }
+    else if (sdCodeTopByte == 0x1000000)
+    {
+        if (sd_sng_code_buf_800BF018[bgm_idx_800BF1E8] == 0)
+        {
+            new_bgm_idx = (bgm_idx_800BF1E8 + 1) & 0xF;
+            sd_sng_code_buf_800BF018[bgm_idx_800BF1E8] = sdCode;
+            bgm_idx_800BF1E8 = new_bgm_idx;
+            return;
+        }
+
+        mts_printf_8008BBA0(aToomuchbgmsoun, sdCode);
+    }
+    else if (sdCodeTopByte == 0x2000000)
+    {
+        mts_printf_8008BBA0(aSdcodeX, sdCode);
+        se_load_code_800BF28C = sdCode;
+    }
+    else if (sdCodeTopByte == 0xFE000000)
+    {
+        if (wave_save_code_800C0578 != sdCode && dword_800C0410 != sdCode)
+        {
+            wave_load_code_800C0528 = sdCode;
+            dword_800BF27C = 1;
+            mts_wup_tsk_8008A540(5);
+            return;
+        }
+    }
+    else
+    {
+        if (sdCode >= 0xE0000000 && sdCode < 0xE1000000)
+        {
+            if (gStr_FadeOut1_800BF16C == 0)
+            {
+                if (gStream_800C04F0 == -1)
+                {
+                    dword_800C04F4 = 0;
+                    gStr_fadeout_2_800C0584 = 0;
+                }
+                dword_800BF160 = sdCode & 1;
+                gStream_800C04F0 = sdCode;
+                gStr_FadeOut1_800BF16C = 1;
+                mts_wup_tsk_8008A540(5);
+                return;
+            }
+            if (gStream_800C04F0 != sdCode)
+            {
+                mts_printf_8008BBA0(aSdsetLastStrea, gStr_FadeOut1_800BF16C);
+                return;
+            }
+            mts_printf_8008BBA0(aSdsetSameStrea, sdCode);
+            return;
+        }
+
+        switch (sdCode)
+        {
+        case 0xFF000005:
+            dword_800C050C = 1;
+            return;
+        case 0xFF000006:
+            dword_800C050C = 0;
+            return;
+        case 0xFF000007:
+            se_rev_on_800C0574 = 1;
+            dword_800BF144 = 0;
+            return;
+        case 0xFF000008:
+            se_rev_on_800C0574 = 0;
+            dword_800BF144 = 0;
+            return;
+        case 0xFF000009:
+            se_rev_on_800C0574 = 1;
+            dword_800BF144 = 1;
+            return;
+
+        case 0xFF0000F4:
+            StrFadeOut_80082310(0x64);
+            mts_printf_8008BBA0(aStrFoS);
+            return;
+        case 0xFF0000F5:
+            StrFadeOut_80082310(0xC8);
+            mts_printf_8008BBA0(aStrFoM);
+            return;
+        case 0xFF0000F6:
+            StrFadeOut_80082310(0x1F4);
+            mts_printf_8008BBA0(aStrFoL);
+            return;
+        case 0xFF0000F7:
+            StrFadeOut_80082310(0x3E8);
+            mts_printf_8008BBA0(aStrFoLl);
+            return;
+        case 0xFF0000F8:
+            if (gStream_800C04F0 != -1)
+            {
+                dword_800BF26C = 0;
+                if (gStr_FadeOut1_800BF16C == 0)
+                {
+                    dword_800C04EC = sdCode;
+                    mts_printf_8008BBA0(aStrFiMAtNextSt);
+                    return;
+                }
+                StrFadeIn_800822C8(0xC8);
+                mts_printf_8008BBA0(aStrFiMStart);
+                return;
+            }
+            mts_printf_8008BBA0(aErrStrFiM);
+            return;
+        case 0xFF0000F9:
+            if (gStream_800C04F0 != -1)
+            {
+                dword_800BF26C = 0;
+                if (gStr_FadeOut1_800BF16C == 0)
+                {
+                    dword_800C04EC = sdCode;
+                    mts_printf_8008BBA0(aStrFiLAtNextSt);
+                    return;
+                }
+                StrFadeIn_800822C8(0x1F4);
+                mts_printf_8008BBA0(aStrFiLStart);
+                return;
+            }
+            mts_printf_8008BBA0(aErrStrFiL);
+            return;
+        case 0xFF0000FA:
+            StrFadeOutStop_80082380(0x64);
+            mts_printf_8008BBA0(aStrFoSStop);
+            return;
+        case 0xFF0000FB:
+            StrFadeOutStop_80082380(0xC8);
+            mts_printf_8008BBA0(aStrFoMStop);
+            return;
+        case 0xFF0000FC:
+            StrFadeOutStop_80082380(0x1F4);
+            mts_printf_8008BBA0(aStrFoLStop);
+            return;
+        case 0xFF0000FD:
+            StrFadeOutStop_80082380(0x3E8);
+            mts_printf_8008BBA0(aStrFoLlStop);
+            return;
+        case 0xFF0000FE:
+            stop_jouchuu_se_800BF1A0 = 1;
+            return;
+
+        case 0xFFFFFFEC:
+            SpuSetIRQ_80096A28(0);
+            return;
+        case 0xFFFFFFED:
+            SpuSetIRQ_80096A28(1);
+            return;
+        case 0xFFFFFFFD:
+            StrFadeOutStop_80082380(0x32);
+            break;
+        }
+    }
+}
