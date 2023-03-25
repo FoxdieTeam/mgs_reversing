44542ac2fe66b394ebd206aff70c26d3f39313c4
nocato <nocato@protonmail.com>
2023-03-25 20:11:37 +0100

    Match sub_800272E0

---
+++ src/Game/sub_800272E0.c
@@ -0,0 +1,23 @@
+#include "libgv/libgv.h"
+
+// TODO: Type of arg1 is probably wrong
+// One of a callers to this function passes CONTROL.field_70[0] to arg1
+// so I guess that could would be a good place to start
+// determining the correct type.
+
+void sub_800272E0(SVECTOR *arg1, SVECTOR *arg2)
+{
+    if (arg1->pad >= 0)
+    {
+        arg2->vx = arg1[1].vy - arg1->vy;
+        arg2->vy = 0;
+        arg2->vz = arg1->vx - arg1[1].vx;
+        GV_LenVec3_80016DDC(arg2, arg2, GV_VecLen3_80016D80(arg2), 0x1000);
+    }
+    else
+    {
+        arg2->vx = arg1[2].pad * 0x10;
+        arg2->vy = arg1[4].pad * 0x10;
+        arg2->vz = arg1[3].pad * 0x10;
+    }
+}
