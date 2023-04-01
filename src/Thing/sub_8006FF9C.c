f5ccb9c3fe13f7a413e7128847e454644560eac7
Kieron <kieron.josephs00@gmail.com>
2023-04-01 05:05:50 +0100

    Match sub_8006FFE8

--- src/Thing/sub_8006FF9C.c
+++ src/Thing/sub_8006FF9C.c
@@ -1,33 +1,18 @@
-typedef struct sub_8006FF9C_unkn1
-{
-    char  field_0_pad[0x2E];
-    short field_2E;
-} sub_8006FF9C_unkn1;
+#include "libhzd/libhzd.h"
 
-typedef struct sub_8006FF9C_unkn2
+int sub_8006FF9C(HZD_MAP *pMap, Actor_Door_TParam_sub *pSub)
 {
-    char                 field_0_pad[0xC];
-    short                field_C;
-    char                 field_E_pad[0x2];
-    short                field_10;
-    char                 field_14_pad[0x8];
-    sub_8006FF9C_unkn1 **field_1C;
-} sub_8006FF9C_unkn2;
+    int count;
 
-int sub_8006FF9C(sub_8006FF9C_unkn2 *arg0, sub_8006FF9C_unkn1 *arg1)
-{
-    int temp;
-
-    if (arg0->field_C >= arg0->field_10)
+    if (pMap->f0C >= pMap->f10_24size)
     {
         return -1;
     }
 
-    temp = arg0->field_C;
-
-    arg0->field_1C[temp] = arg1;
-    arg0->field_C = temp + 1;
-    arg1->field_2E |= 0x1;
+    count = pMap->f0C;
+    pMap->f1C_pEndOfHzdMap[count] = pSub;
+    pMap->f0C = count + 1;
+    pSub[2].field_E_param_v1 |= 0x1;
 
     return 0;
 }
