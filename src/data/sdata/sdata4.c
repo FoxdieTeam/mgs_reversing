0bd4861ad849732a83164ffcdb09607198381842
nocato <nocato@protonmail.com>
2023-04-09 16:59:14 +0200

    Match GM_CheckBehindCamera_80030B3C

--- src/data/sdata/sdata4.c
+++ src/data/sdata/sdata4.c
@@ -50,7 +50,7 @@ SVECTOR SECTION(".sdata") GM_CameraRotateSave_800AB430 = { 0x0280, 0x0800, 0, 0 
 int SECTION(".sdata") GM_CameraTrackOrg_800AB438 = 0x2710;
 int SECTION(".sdata") GM_CameraTrackOrg_800AB43C = 0;
 int SECTION(".sdata") GM_CameraTrackOrg_800AB440 = 0;
-int SECTION(".sdata") GM_CameraTrackOrg_800AB444 = 0;
+HZD_TRP *SECTION(".sdata") GM_CameraTrackOrg_800AB444 = NULL;
 int SECTION(".sdata") GM_CameraTrackOrg_800AB448 = 0;
 
 const char SECTION(".sdata") aNoHzd[] = "no hzd\n";
