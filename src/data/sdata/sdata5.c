1777d360963959b705a3ec2289808721eba954c1
Kieron <kieron.josephs00@gmail.com>
2023-09-27 14:36:45 +0100

    Resolve menu string pointers

--- src/data/sdata/sdata5.c
+++ src/data/sdata/sdata5.c
@@ -59,12 +59,12 @@ int SECTION(".sdata") dword_800AB6E0 = 0;
 
 char SECTION(".sdata") aPause[] = "PAUSE";
 
-int SECTION(".sdata") dword_800AB6EC = 0;
-int SECTION(".sdata") dword_800AB6F0 = 0xFFFFFFFF;
-int SECTION(".sdata") dword_800AB6F4 = 0;
-int SECTION(".sdata") dword_800AB6F8 = 0;
-int SECTION(".sdata") dword_800AB6FC = 0xFFFFFFFF;
-int SECTION(".sdata") dword_800AB700 = 0;
+int  SECTION(".sdata") dword_800AB6EC = 0;
+int  SECTION(".sdata") dword_800AB6F0 = 0xFFFFFFFF;
+int  SECTION(".sdata") dword_800AB6F4 = 0;
+char SECTION(".sdata") byte_800AB6F8[] = "";
+int  SECTION(".sdata") dword_800AB6FC = 0xFFFFFFFF;
+int  SECTION(".sdata") dword_800AB700 = 0;
 
 char SECTION(".sdata") aError[] = "ERROR";
 char SECTION(".sdata") aDiscD[] = "DISC %d";
