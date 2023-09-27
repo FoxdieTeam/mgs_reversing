361bafe44ab1c9ed9a9483bded5fd530f5d6d407
nocato <nocato@protonmail.com>
2023-09-27 18:03:20 +0200

    Import "camera" overlay

---
+++ asm/overlays/camera/camera_800CF650.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section camera.text
+
+	xdef camera_800CF650
+camera_800CF650:
+	dw 0x8FBF0010 ; 800CF650
+	dw 0x27BD0018 ; 800CF654
+	dw 0x03E00008 ; 800CF658
+	dw 0x00000000 ; 800CF65C
---
+++ asm/overlays/camera/camera_800CF974.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section camera.text
+
+	xdef camera_800CF974
+camera_800CF974:
+	dw 0x8FBF0018 ; 800CF974
+	dw 0x27BD0020 ; 800CF978
+	dw 0x03E00008 ; 800CF97C
+	dw 0x00000000 ; 800CF980
