a57ef45c09be19ea2681d748efdb6d23298bc6df
nocato <nocato@protonmail.com>
2023-09-29 19:21:42 +0200

    Run linker separately for each overlay
    Some overlays share the same source files. An extreme example of it
    are "select2", "select3"... overlays that consist of the same 
    "Game/select.c" file.
    
    However, if you try to link 2 overlays that have the same symbols
    (global variable names) you get "symbol multiply defined" error.
    Moreover, when building an overlay source file the resulting object
    file has all sections prefixed by the overlay name (we would have to
    build those files multiple times with different prefixes).
    
    Solve the issue by running the linker multiple times. Each execution
    builds a separate overlay (and unfortunately also a separate main exe).
    
    Show that it works by building "select2", "select3", ... overlays.
    
    Another benefit of it is that we now generate a separate .sym file
    per each overlay (not a single one where all the symbols are mixed).

--- asm/overlays/camera/camera_800CDF18.s
+++ asm/overlays/camera/camera_800CDF18.s
@@ -1,5 +1,5 @@
 	opt	c+, at+, e+, n-
-	section camera.text
+	section overlay.text
 
 	xdef camera_800CDF18
 camera_800CDF18:
--- build/linker_command_file_preprocess.py
+++ build/linker_command_file_preprocess.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 from os.path import dirname, basename
 from jinja2 import Environment, FileSystemLoader
 
+def process_flag(flag):
+    # Either "KEY=VALUE":
+    kv = re.match(r'^([A-Z]+)=(.*)$', flag)
+    if kv:
+        return (kv.group(1), kv.group(2))
+    else:
+        # Just a "FLAG":
+        return (flag, True)
+
 def main(path, output, flags):
     env = Environment(loader=FileSystemLoader(dirname(path)))
-    flags = {flag: True for flag in flags}
+    flags = dict(process_flag(flag) for flag in flags)
 
     with open(output, 'w') as f:
         template = env.get_template(basename(path))
