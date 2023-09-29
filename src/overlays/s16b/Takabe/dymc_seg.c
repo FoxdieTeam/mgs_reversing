03252c790a47bdae7ea63c50b1c33f591731ba1a
Kieron <kieron.josephs00@gmail.com>
2023-09-29 12:45:03 +0100

    dymc_seg.c matches

--- src/overlays/s16b/Takabe/dymc_seg.c
+++ src/overlays/s16b/Takabe/dymc_seg.c
@@ -1,4 +1,6 @@
 #include "linker.h"
+#include "Bullet/jirai.h"
+#include "Game/target.h"
 #include "libgcl/libgcl.h"
 #include "libgv/libgv.h"
 #include "libhzd/libhzd.h"
@@ -12,7 +14,11 @@ typedef struct DymcSegWork
     HZD_SEG  seg;
 } DymcSegWork;
 
-extern int GM_CurrentMap_800AB9B0;
+extern int           bakudan_count_8009F42C;
+extern int           counter_8009F448;
+extern int           GM_CurrentMap_800AB9B0;
+extern Jirai_unknown stru_800BDD78[16];
+extern Jirai_unknown stru_800BDE78[8];
 
 extern const char     s16b_dword_800C588C[]; // = "dymc_seg.c"
 extern unsigned short s16b_dword_800C3254[]; // = {0xD182, 0x006B}
@@ -22,8 +28,90 @@ int s16b_800C44DC(unsigned short name, int hash_count, unsigned short *hashes);
 
 #define EXEC_LEVEL 5
 
-#pragma INCLUDE_ASM("asm/overlays/s16b/s16b_800C4820.s")
-#pragma INCLUDE_ASM("asm/overlays/s16b/s16b_800C4848.s")
+int s16b_800C4820(TARGET *target, int count, TARGET *iter)
+{
+    for (; count > 0; count--, iter = (TARGET *)((char *)iter + 16))
+    {
+        if (iter == target)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int s16b_800C4848(TARGET *target, int count, TARGET *iter)
+{
+    (int)target |= 0x80000000;
+
+    for (; count > 0; count--, iter = (TARGET *)((char *)iter + 48))
+    {
+        if (iter == target)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void s16b_800C4874(int arg0, TARGET *arg1, int arg2, TARGET *arg3)
+{
+    Jirai_unknown *iter;
+    int            i;
+    int            status;
+    int            tag;
+
+    if (bakudan_count_8009F42C != 0)
+    {
+        for (i = 16, tag = 0x80000000, iter = stru_800BDD78; i > 0; i--, iter++)
+        {
+            if (iter->field_4_pActor)
+            {
+                if ((int)iter->field_C_pTarget & tag)
+                {
+                    status = s16b_800C4820(iter->field_C_pTarget, arg0, arg1);
+                }
+                else
+                {
+                    status = s16b_800C4848(iter->field_C_pTarget, arg2, arg3);
+                }
+
+                if (status != 0)
+                {
+                    GV_DestroyActor_800151C8(iter->field_4_pActor);
+                }
+            }
+        }
+    }
+
+    if (counter_8009F448 != 0)
+    {
+        for (i = 8, iter = stru_800BDE78; i > 0; i--, iter++)
+        {
+            if (iter->field_4_pActor && s16b_800C4848(iter->field_C_pTarget, arg2, arg3))
+            {
+                GV_DestroyActor_800151C8(iter->field_4_pActor);
+            }
+        }
+    }
+}
+
+void s16b_800C49AC(SVECTOR *vec)
+{
+    int i;
+    int round;
+
+    for (i = 2; i > 0; i--)
+    {
+        round = (vec->vx >= 0) ? 62 : -62;
+        vec->vx = ((vec->vx + round) / 125) * 125;
+
+        round = (vec->vy >= 0) ? 62 : -62;
+        vec->vy = ((vec->vy + round) / 125) * 125;
+    }
+}
 
 void DymcSegAct_800C4A44(DymcSegWork *work)
 {
