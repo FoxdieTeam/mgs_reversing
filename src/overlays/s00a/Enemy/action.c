61d6dd2377a8177a4f5b5245f0452b4480c3f3ec
Jayveer <jayveerk@gmail.com>
2023-10-01 00:37:14 +0100

    Added some action functions

--- src/overlays/s00a/Enemy/action.c
+++ src/overlays/s00a/Enemy/action.c
@@ -3,6 +3,7 @@
 #include "libdg/libdg.h"
 #include "Game/object.h"
 #include "Game/game.h"
+#include "command.h"
 #include "watcher.h"
 
 #define COM_ST_DANBOWL 0x2000
@@ -11,7 +12,22 @@
 #define ACTINTERP   4
 
 #define STANDSTILL  0
+#define ACTION1     1
+#define ACTION2     2
+#define ACTION3     3
+#define ACTION4     4
+#define ACTION5     5
+#define ACTION6     6
 #define GRENADE     7
+#define ACTION8     8
+#define ACTION9     9
+#define ACTION10    10
+#define ACTION11    11
+#define ACTION12    12
+#define ACTION13    13
+#define ACTION14    14
+#define ACTION15    15
+#define ACTION16    16
 #define DANBOWLKERI 17
 #define DANBOWLPOSE 18
 
@@ -21,9 +37,10 @@
 
 #define SE_PINNUKI 0x2C
 
-//unsure
 #define WEAPON_TRIG  2
 #define WEAPON_TRIG2 4
+
+#define TOP_COMM_TRAVEL 0
 
 extern unsigned int COM_GameStatus_800E0F3C;
 
@@ -38,9 +55,12 @@ extern OBJECT   *GM_PlayerBody_800ABA20;
 extern short    EYE_LENGTH_800E0D8C;
 extern short    ActTable_800C3358[];
 
+extern ENEMY_COMMAND EnemyCommand_800E0D98;
+
 extern int      GM_PlayerStatus_800ABA50;
 extern SVECTOR  GM_PlayerPosition_800ABA10;
 
+//extern int     dword_800E0DB0; // EC_MODE
 
 extern void ActUnknown1_800C841C();
 extern void ActUnknown2_800C84FC();
@@ -58,15 +78,17 @@ extern void ActUnknown13_800C6164();
 extern void ActUnknown14_800C624C();
 extern void ActUnknown15_800C6320();
 extern void ActUnknown16_800C65A8();
-
 extern void ActStandStill_800C5C84();
+extern void ActUnknown_800C5E48();
 
+extern int sub_800C580C( int dir, int val );
 
+typedef	void	( *ACTION )( WatcherWork *, int ) ;
 
-static inline void SetMode( WatcherWork *work, void *func )
+static inline void SetMode( WatcherWork *work, ACTION action )
 {
-    work->field_8EC_func = func;
-    work->field_8F4 = 0;
+    work->action = action;
+    work->time = 0;
     work->control.field_4C_turn_vec.vz = 0;
     work->control.field_4C_turn_vec.vx = 0;
     GM_ConfigMotionAdjust_80035008( &( work->body ), 0 );
@@ -233,6 +255,247 @@ int CheckPad_800C5A60( WatcherWork *work )
     return 0;
 }
 
+extern int GV_VecDir2_80016EF8(SVECTOR *vec);
+extern int GV_NearExp4P_80026554(int from, int to);
+
+extern SVECTOR DG_ZeroVector_800AB39C;
+
+void ActStandStill_800C5C84(WatcherWork* work, int time ) 
+{
+    SetTargetClass( work->target, TARGET_FLAG );
+    work->act_status |= 1;
+    
+    if ( EnemyCommand_800E0D98.mode == TOP_COMM_TRAVEL )
+    {
+        work->target->class |= TARGET_C4 ;
+    }
+    
+    work->vision_length = EYE_LENGTH_800E0D8C ;
+
+    if ( time == 0 )
+    {
+        SetAction( work, STANDSTILL, ACTINTERP );
+    }
+
+    if ( CheckDamage_800C5424( work ) || CheckPad_800C5A60( work ) )
+    {
+		UnsetMode2( work ) ;
+		return ;        
+    }
+    
+
+    if ( work->pad.dir >= 0 )
+    {
+        if ( GV_DiffDirAbs_8001706C( work->control.field_8_rotator.vy, work->pad.dir ) < 0x100 )
+        {
+            if ( work->pad.field_08 & 0x1 )
+            {
+                SetAction( work, ACTION2, ACTINTERP );
+            }
+            else
+            {
+                SetAction( work, ACTION1, ACTINTERP );
+            }
+            SetMode( work, ActUnknown_800C5E48 );
+            
+        }
+        else
+        {
+            work->control.field_4C_turn_vec.vy = work->pad.dir;
+        }
+    }
+    work->vision_facedir = GV_NearExp4P_80026554( work->vision_facedir, work->control.field_8_rotator.vy );
+}
+
+void ActUnknown_800C5E48( WatcherWork* work, int time )
+{
+    int x, z;
+    int s0;
+    int interval;
+    int tmp;
+    int dist;
+    CONTROL * ctrl;
+    int dir;
+    int field_8E0;
+    SVECTOR* svec;
+
+    SetTargetClass( work->target, TARGET_FLAG );
+
+    if ( EnemyCommand_800E0D98.mode == TOP_COMM_TRAVEL )
+    {
+        work->target->class |= TARGET_C4 ;
+    }
+
+    work->vision_length = EYE_LENGTH_800E0D8C ;
+    work->act_status |= 0x100;
+    work->control.field_44_movementVector = DG_ZeroVector_800AB39C;
+
+    if ( CheckDamage_800C5424( work ) || CheckPad_800C5A60( work ) )
+    {
+		UnsetMode2( work ) ;
+		return ;        
+    }
+
+    dist = -1;
+    ctrl = &(work->control );
+    dir = work->pad.dir;
+    field_8E0 = work->field_8E0;
+    svec = work->control.field_60_vecs_ary;
+
+    if ( (work->pad.field_08 & 0x1) && ( work->field_8E0 != ACTION2 ) )
+    {
+        SetAction( work, ACTION2, ACTINTERP );
+    }
+
+    if ( dir >= 0 )
+    {
+        s0 = ctrl->field_58;
+        if ( s0 > 0 )
+        {
+            dist = GV_VecDir2_80016EF8( svec );
+    
+            if ( s0 >= 2 )
+            {
+                tmp = GV_VecDir2_80016EF8( &ctrl->field_60_vecs_ary[1] );
+                if ( GV_DiffDirAbs_8001706C( dir, tmp ) < GV_DiffDirAbs_8001706C( dir, dist ) )
+                {
+                    dist = tmp;
+                }
+            }
+        }
+    }
+    
+    if ( dir < 0 )
+    {
+        SetMode( work, ActStandStill_800C5C84);
+        UnsetMode2( work ) ;
+        return;
+    }
+
+    if ( time != 1 )
+    {
+        switch ( sub_800C580C( dir, dist ) )
+        {
+        case 1:
+            dir = (dist - 1024) & 0xFFF;
+            break;
+        case 2:
+            dir = (dist + 1024) & 0xFFF;
+            break;
+        }
+    }
+
+    if ( !work->body.objs->bound_mode )
+    {     
+        ctrl->field_8_rotator.vy   = dir;
+        ctrl->field_4C_turn_vec.vy = dir;
+        work->vision_facedir = GV_NearExp4P_80026554( work->vision_facedir, work->control.field_8_rotator.vy );
+    }
+    else
+    {  
+        ctrl->field_4C_turn_vec.vy = dir;
+        work->vision_facedir = work->control.field_8_rotator.vy;
+    }
+
+    interval = 0;
+
+    if ( field_8E0 == 1 )
+    {
+        interval = 50;
+    }
+    else if ( field_8E0 == 2 )
+    {
+        interval = 100;
+    }
+
+    x = interval * rsin( dir );
+    
+    if ( x < 0 )
+    {
+        x += 0xFFF;
+    }
+
+    ctrl->field_44_movementVector.vx = x >> 12;
+
+    z = interval * rcos( dir );
+    
+    if ( z < 0 )
+    {
+        z += 0xFFF;
+    }
+
+    ctrl->field_44_movementVector.vz = z >> 12;   
+
+    if ( !time )
+    {
+        work->field_8E6 = 0;
+    }
+}
+
+void ActUnknown12_800C615C( WatcherWork *work, int time )
+{
+}
+
+void ActUnknown13_800C6164( WatcherWork *work, int time )
+{
+    CONTROL *ctrl;
+
+    ctrl = &( work->control );
+    SetTargetClass( work->target, TARGET_FLAG );
+    work->vision_length = EYE_LENGTH_800E0D8C ;
+
+    if ( time == 0 )
+    {
+        SetAction( work, ACTION16, ACTINTERP );
+    }
+
+    if ( CheckDamage_800C5424( work ) )
+    {
+		return ;
+    }
+
+    if ( work->body.is_end || !( work->pad.press & 0x20 ) )
+    {
+        SetMode( work, ActStandStill_800C5C84 );
+        return;
+    }
+
+    ctrl->field_4C_turn_vec.vy = work->sn_dir;
+    ctrl->field_44_movementVector.vx = 0;
+    ctrl->field_44_movementVector.vz = 0;
+    work->vision_facedir = work->control.field_8_rotator.vy;
+}
+
+void ActUnknown14_800C624C( WatcherWork *work, int time )
+{
+    CONTROL *ctrl;
+
+    ctrl = &( work->control );
+    SetTargetClass( work->target, TARGET_FLAG );
+    work->vision_length = EYE_LENGTH_800E0D8C ;
+
+    if ( time == 0 )
+    {
+        SetAction( work, ACTION13, ACTINTERP );
+    }
+
+    if ( CheckDamage_800C5424( work ) )
+    {
+		return ;
+    }
+
+    if ( !( work->pad.press & 0x2000000 ) )
+    {
+        SetMode( work, ActStandStill_800C5C84 );
+        return;
+    }
+
+    ctrl->field_4C_turn_vec.vy = work->pad.dir;
+    ctrl->field_44_movementVector.vx = 0;
+    ctrl->field_44_movementVector.vz = 0;
+    work->vision_facedir = work->control.field_8_rotator.vy;
+}
+
 void ActUnknown15_800C6320( WatcherWork *work, int time )
 {
     SetTargetClass( work->target, TARGET_FLAG ) ;
@@ -350,7 +613,7 @@ void ActGrenade_800C67EC( WatcherWork *work, int time )
 
     if ( work->field_8E0 == 7 )
     {
-        if ( work->body.field_1A )
+        if ( work->body.is_end )
         {
             check = 1;
         }
