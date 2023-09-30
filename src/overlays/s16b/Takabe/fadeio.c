dcdcf47d50708017fa45a1ade31c903ee742aa8b
Kieron <kieron.josephs00@gmail.com>
2023-09-30 19:58:32 +0100

    Move misc functions from actor source files

--- src/overlays/s16b/Takabe/fadeio.c
+++ src/overlays/s16b/Takabe/fadeio.c
@@ -28,9 +28,7 @@ extern int GM_GameStatus_800AB3CC;
 
 unsigned short s16b_dword_800C3250[] = {HASH_KILL, 0x71F1};
 
-static int s16b_dword_800C58AC[2];
-
-int s16b_800C43A4(char param);
+int GetParam_800C5358(char param);
 int s16b_800C44DC(unsigned short name, int hash_count, unsigned short *hashes);
 
 #define EXEC_LEVEL 3
@@ -186,7 +184,7 @@ GV_ACT *NewFadeIo_800C42BC(int name, int where, int argc, char **argv)
     {
         GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)FadeIoAct_800C3E7C, (TActorFunction)FadeIoDie_800C40D0, "fadeio.c");
 
-        if (FadeIoGetResources_800C4100(work, s16b_800C43A4('m'), s16b_800C43A4('s')) < 0)
+        if (FadeIoGetResources_800C4100(work, GetParam_800C5358('m'), GetParam_800C5358('s')) < 0)
         {
             GV_DestroyActor_800151C8(&work->actor);
             return NULL;
@@ -196,174 +194,4 @@ GV_ACT *NewFadeIo_800C42BC(int name, int where, int argc, char **argv)
     }
 
     return &work->actor;
-}
-
-int s16b_800C4364(char param, int def)
-{
-    if (GCL_GetParam_80020968(param))
-    {
-        return GCL_GetNextInt_800209E8(GCL_Get_Param_Result_80020AA4());
-    }
-
-    return def;
-}
-
-int s16b_800C43A4(char param)
-{
-    return s16b_800C4364(param, 0);
-}
-
-unsigned short s16b_800C43C8(char param, unsigned short def)
-{
-    char *param2;
-
-    param2 = (char *)GCL_GetParam_80020968(param);
-    if (param2)
-    {
-        return GCL_GetNextInt_800209E8(param2);
-    }
-
-    return def;
-}
-
-int s16b_800C440C(char param)
-{
-    return s16b_800C43C8(param, 0);
-}
-
-void s16b_800C4430(char param, short x, short y, short z, SVECTOR *vec)
-{
-    if (GCL_GetParam_80020968(param))
-    {
-        GCL_GetSV_80020A14(GCL_Get_Param_Result_80020AA4(), vec);
-        return;
-    }
-
-    vec->vx = x;
-    vec->vy = y;
-    vec->vz = z;
-}
-
-void s16b_800C44AC(char param, SVECTOR *vec)
-{
-    s16b_800C4430(param, 0, 0, 0, vec);
-}
-
-int s16b_800C44DC(unsigned short name, int hash_count, unsigned short *hashes)
-{
-    GV_MSG *msg;
-    int     msg_count;
-    int     found;
-    int     hash;
-    int     i;
-
-    msg_count = GV_ReceiveMessage_80016620(name, &msg);
-    found = -1;
-
-    for (; msg_count > 0; msg_count--, msg++)
-    {
-        hash = msg->message[0];
-
-        for (i = 0; i < hash_count; i++)
-        {
-            if (hash == hashes[i])
-            {
-                found = i;
-                s16b_dword_800C58AC[0] = msg->message[1];
-            }
-        }
-    }
-
-    return found;
-}
-
-int s16b_800C4584(void)
-{
-    return s16b_dword_800C58AC[0];
-}
-
-void s16b_800C4594(short address, short message)
-{
-    GV_MSG msg;
-
-    msg.address = address;
-    msg._len = 1;
-    msg.message[0] = message;
-    GV_SendMessage_80016504(&msg);
-}
-
-void s16b_800C45C4(HZD_SEG *seg, MATRIX *trans, SVECTOR *arg2, SVECTOR *arg3)
-{
-    SVECTOR sp10;
-    SVECTOR sp18;
-    int     height;
-    int     y;
-
-    if (trans)
-    {
-        DG_SetPos_8001BC44(trans);
-        DG_PutVector_8001BE48(arg2, &sp10, 1);
-        DG_PutVector_8001BE48(arg3, &sp18, 1);
-    }
-    else
-    {
-        sp10 = *arg2;
-        sp18 = *arg3;
-    }
-
-    height = sp10.vy - sp18.vy;
-    y = sp18.vy;
-
-    if (height <= 0)
-    {
-        y = sp10.vy;
-        height = -height;
-    }
-
-    seg->p1.x = sp10.vx;
-    seg->p1.z = sp10.vz;
-    seg->p2.x = sp18.vx;
-    seg->p2.z = sp18.vz;
-    seg->p2.y = y;
-    seg->p1.y = y;
-    seg->p2.h = height;
-    seg->p1.h = height;
-
-    HZD_SetDynamicSegment_8006FEE4(seg, seg);
-}
-
-void s16b_800C46D8(HZD_FLR *flr, SVECTOR *arg1, SVECTOR *arg2)
-{
-    short y;
-
-    flr->p1.x = arg1->vx - arg2->vx;
-    flr->p1.z = arg1->vz - arg2->vz;
-
-    flr->p2.x = arg1->vx + arg2->vx;
-    flr->p2.z = arg1->vz - arg2->vz;
-
-    flr->p4.x = arg1->vx - arg2->vx;
-    flr->p4.z = arg1->vz + arg2->vz;
-
-    flr->p3.x = arg1->vx + arg2->vx;
-    flr->p3.z = arg1->vz + arg2->vz;
-
-    y = arg1->vy + arg2->vy;
-
-    flr->p1.h = 0;
-    flr->p2.h = 0;
-    flr->p3.h = 0xFF;
-
-    flr->p4.y = y;
-    flr->p3.y = y;
-    flr->p2.y = y;
-    flr->p1.y = y;
-
-    flr->b1.x = arg1->vx - arg2->vx;
-    flr->b1.z = arg1->vz - arg2->vz;
-    flr->b1.y = arg1->vy - arg2->vy;
-
-    flr->b2.x = arg1->vx + arg2->vx;
-    flr->b2.z = arg1->vz + arg2->vz;
-    flr->b2.y = arg1->vy + arg2->vy;
 }
