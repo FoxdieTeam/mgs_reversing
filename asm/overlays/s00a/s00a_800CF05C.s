b679286c1060a5d7589ad34b7f3e5b3ee3eef089
nocato <nocato@protonmail.com>
2023-10-03 17:41:56 +0200

    Import "s00a" overlay

---
+++ asm/overlays/s00a/s00a_800CF05C.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s00a_800CF05C
+s00a_800CF05C:
+	dw 0x2402008B ; 800CF05C
+	dw 0x03E00008 ; 800CF060
+	dw 0xAC820000 ; 800CF064
---
+++ asm/overlays/s00a/s00a_800CF068.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s00a_800CF068
+s00a_800CF068:
+	dw 0x2402008A ; 800CF068
+	dw 0x03E00008 ; 800CF06C
+	dw 0xAC820000 ; 800CF070
