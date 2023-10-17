3fa6a5076d12880365bcbb6d0f2788311b743fdf
nocato <nocato@protonmail.com>
2023-10-17 16:32:53 +0200

    Smart split "s03e" overlay

---
+++ asm/overlays/s03e/s03e_800CBC08.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s03e_800CBC08
+s03e_800CBC08:
+	dw 0x8C83003C ; 800CBC08
+	dw 0x24020001 ; 800CBC0C
+	dw 0x03E00008 ; 800CBC10
+	dw 0xAC620000 ; 800CBC14
