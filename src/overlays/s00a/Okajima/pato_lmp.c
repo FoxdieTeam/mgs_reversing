61d67045f2a1b1e56f67f9a48d571ac3c2578a8f
nocato <nocato@protonmail.com>
2023-10-28 17:47:07 +0200

    Match 2 helper functions in pato_lmp.c, fix wrong split

--- src/overlays/s00a/Okajima/pato_lmp.c
+++ src/overlays/s00a/Okajima/pato_lmp.c
@@ -1,4 +1,5 @@
 #include "libgv/libgv.h"
+#include "libgcl/libgcl.h"
 
 typedef struct _PatoLmpWork
 {
@@ -11,9 +12,51 @@ extern const char aPatoLmpC[]; // = "pato_lmp.c"
 #define EXEC_LEVEL 4
 
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D5E30.s")
-#pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D5EC8.s")
+
+int s00a_pato_lmp_800D5EC8(int unused, SVECTOR *svecs)
+{
+    int count;
+    unsigned char *param;
+
+    count = 0;
+    for (count = 0; (param = GCL_Get_Param_Result_80020AA4()); svecs++, count++)
+    {
+        GCL_StrToSV_80020A14(param, svecs);
+    }
+    return count;
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D5F1C.s")
-#pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D5F38.s")
+
+void s00a_pato_lmp_800D5F38(POLY_FT4 *polys, int count, DG_TEX *tex)
+{
+    int offx, width;
+    int offy, height;
+
+     while (--count >= 0)
+    {
+        setPolyFT4(polys);
+        setSemiTrans(polys, 1);
+
+        offx = tex->field_8_offx;
+        width = tex->field_A_width;
+        offy = tex->field_9_offy;
+        height = tex->field_B_height;
+
+        setUVWH(polys, offx, offy, width, height);
+
+        polys->tpage = tex->field_4_tPage;
+        polys->clut = tex->field_6_clut;
+        polys->tpage |= 0x60;
+
+        polys->r0 = 0;
+        polys->g0 = 0;
+        polys->b0 = 0;
+
+        polys++;
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D5FC4.s")
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D617C.s")
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D6194.s")
@@ -22,7 +65,6 @@ extern const char aPatoLmpC[]; // = "pato_lmp.c"
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D6678.s")
 int s00a_pato_lmp_800D6678(PatoLmpWork *work);
 
-#pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D6C18.s")
 #pragma INCLUDE_ASM("asm/overlays/s00a/s00a_pato_lmp_800D6C44.s")
 int s00a_pato_lmp_800D6C44(PatoLmpWork *work);
 
