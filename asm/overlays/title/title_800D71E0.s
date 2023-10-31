e7074dc2f861a65babd43bff756723f6cc7eab1c
nocato <nocato@protonmail.com>
2023-10-31 16:48:01 +0100

    Smart split .s files in title (not that effective due to many jump tables)

---
+++ asm/overlays/title/title_800D71E0.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef title_800D71E0
+title_800D71E0:
+	dw 0x3C03800A ; 800D71E0
+	dw 0x24020002 ; 800D71E4
+	dw 0x03E00008 ; 800D71E8
+	dw 0xAC62D45C ; 800D71EC
