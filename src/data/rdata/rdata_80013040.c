e2aa21c970a5a584addf571af6e72734b127259f
Kieron <kieron.josephs00@gmail.com>
2023-11-03 14:17:41 +0000

    Split memfile.c and demo.c from demothrd.c

--- src/data/rdata/rdata_80013040.c
+++
@@ -1,7 +0,0 @@
-#include "linker.h"
-
-// Inlining this string causes the jump table
-// of "demothrd_make_chara_8007AE10" to have wrong alignment
-// (extra 4 bytes).
-
-const char SECTION(".rdata") aDemothrdC[] = "demothrd.c";
