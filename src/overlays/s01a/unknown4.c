68b5e4e01a2daa9a6f5f99ddca481027dc35ca35
Kieron <kieron.josephs00@gmail.com>
2023-11-11 03:07:50 +0000

    Add s01a_800E1FCC

--- src/overlays/s01a/unknown4.c
+++ src/overlays/s01a/unknown4.c
@@ -42,7 +42,118 @@ void s01a_800E1F18(short *in, SVECTOR *out)
     out->vz = 0;
 }
 
-#pragma INCLUDE_ASM("asm/overlays/s01a/s01a_800E1FCC.s")
+int s01a_800E1FCC(SVECTOR *in0, VECTOR *in1, SVECTOR *out, int len)
+{
+    VECTOR clampv;
+    VECTOR diff;
+    int    clamped;
+    int    coord;
+    int    clamp;
+    int    mul;
+
+    clamped = 0;
+    clampv = *in1;
+
+    coord = clampv.vx;
+    if (coord < -0x7FF0)
+    {
+        clamp = -0x7FF0;
+        clampv.vx = clamp;
+
+        mul = (clampv.vx - in0->vx) >> 4;
+        coord = (coord - in0->vx) >> 4;
+
+        clampv.vy = ((clampv.vy - in0->vy) * mul) / coord + in0->vy;
+        clampv.vz = ((clampv.vz - in0->vz) * mul) / coord + in0->vz;
+
+        clamped = 1;
+    }
+    else if (coord > 0x7FEF)
+    {
+        clamp = 0x7FEF;
+        clampv.vx = clamp;
+
+        mul = (clampv.vx - in0->vx) >> 4;
+        coord = (coord - in0->vx) >> 4;
+
+        clampv.vy = ((clampv.vy - in0->vy) * mul) / coord + in0->vy;
+        clampv.vz = ((clampv.vz - in0->vz) * mul) / coord + in0->vz;
+
+        clamped = 1;
+    }
+
+    coord = clampv.vy;
+    if (coord < -0x7FF0)
+    {
+        clamp = -0x7FF0;
+        clampv.vy = clamp;
+
+        mul = (clampv.vy - in0->vy) >> 4;
+        coord = (coord - in0->vy) >> 4;
+
+        clampv.vx = ((clampv.vx - in0->vx) * mul) / coord + in0->vx;
+        clampv.vz = ((clampv.vz - in0->vz) * mul) / coord + in0->vz;
+
+        clamped = 1;
+    }
+    else if (coord > 0x7FEF)
+    {
+        clamp = 0x7FEF;
+        clampv.vy = clamp;
+
+        mul = (clampv.vy - in0->vy) >> 4;
+        coord = (coord - in0->vy) >> 4;
+
+        clampv.vx = ((clampv.vx - in0->vx) * mul) / coord + in0->vx;
+        clampv.vz = ((clampv.vz - in0->vz) * mul) / coord + in0->vz;
+
+        clamped = 1;
+    }
+
+    coord = clampv.vz;
+    if (coord < -0x7FF0)
+    {
+        clamp = -0x7FF0;
+        clampv.vz = clamp;
+
+        mul = (clampv.vz - in0->vz) >> 4;
+        coord = (coord - in0->vz) >> 4;
+
+        clampv.vy = ((clampv.vy - in0->vy) * mul) / coord + in0->vy;
+        clampv.vx = ((clampv.vx - in0->vx) * mul) / coord + in0->vx;
+
+        clamped = 1;
+    }
+    else if (coord > 0x7FEF)
+    {
+        clamp = 0x7FEF;
+        clampv.vz = clamp;
+
+        mul = (clampv.vz - in0->vz) >> 4;
+        coord = (coord - in0->vz) >> 4;
+
+        clampv.vy = ((clampv.vy - in0->vy) * mul) / coord + in0->vy;
+        clampv.vx = ((clampv.vx - in0->vx) * mul) / coord + in0->vx;
+
+        clamped = 1;
+    }
+
+    if (clamped == 1 || len == -1)
+    {
+        diff.vx = (clampv.vx - in0->vx) >> 4;
+        diff.vy = (clampv.vy - in0->vy) >> 4;
+        diff.vz = (clampv.vz - in0->vz) >> 4;
+
+        Square0(&diff, &diff);
+        len = SquareRoot0(diff.vx + diff.vy + diff.vz) * 16;
+    }
+
+    out->vx = clampv.vx;
+    out->vy = clampv.vy;
+    out->vz = clampv.vz;
+
+    return len;
+}
 
 void s01a_800E2364(MATRIX *mtx, SVECTOR *in, VECTOR *out)
 {
