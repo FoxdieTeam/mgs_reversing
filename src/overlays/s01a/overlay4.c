01d4711223d1625fd7f6e31c781b7598ee280e24
Kieron <kieron.josephs00@gmail.com>
2023-11-11 01:56:08 +0000

    Merge s01a actors

---
+++ src/overlays/s01a/overlay4.c
@@ -0,0 +1,4 @@
+#include "linker.h"
+#include "libgv/libgv.h"
+
+const char aEnvSndC[] = {'e', 'n', 'v', '_', 's', 'n', 'd', '.', 'c', 0x0, 0x9, 'F'};
---
+++ src/overlays/s16b/Takabe/dymc_seg_data.c
@@ -0,0 +1,2 @@
+unsigned short dymc_seg_hashes[] = {0xD182, 0x006B};
+const char aDymcSegC[] = "dymc_seg.c";
