dddd8840cf2d8d215872f6577a5a36f8353d2f35
nocato <nocato@protonmail.com>
2023-11-11 18:21:43 +0100

    Inline some rdata/data in overlays

--- src/overlays/s03er/overlay4.c
+++ src/overlays/s03er/overlay4.c
@@ -4,8 +4,6 @@
 const SVECTOR spark2_light_pos = {0, 0, 500, 0};
 
 const char aSpark2C[] = {'s', 'p', 'a', 'r', 'k', '2', '.', 'c', 0x0, 'a', 'd', 'I'};
-const char aFadeioC[] = {'f', 'a', 'd', 'e', 'i', 'o', '.', 'c', 0x0, '8', '0', '0'};
-const char aCinemaC[] = {'c', 'i', 'n', 'e', 'm', 'a', '.', 'c', 0x0, '_', 'L', 'o'};
 
 void SECTION("overlay.bss") *s03e_dword_800CC6B8;
 int SECTION("overlay.bss") s03e_dword_800CC6BC;
