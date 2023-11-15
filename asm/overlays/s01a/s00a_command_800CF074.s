a0fb0853c574f8a371454d78761c786015cafc23
nocato <nocato@protonmail.com>
2023-11-15 21:23:58 +0100

    Rename the rest of s01a command funcs with s00a names

---
+++ asm/overlays/s01a/s00a_command_800CF074.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s00a_command_800CF074
+s00a_command_800CF074:
+	dw 0x24020086 ; 800D1BD4
+	dw 0x03E00008 ; 800D1BD8
+	dw 0xAC820000 ; 800D1BDC
--- asm/overlays/s01a/s01a_command_800CFFD0.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s01a_command_800CFFD0
-s01a_command_800CFFD0:
-	dw 0x03E00008 ; 800CFFD0
-	dw 0x00000000 ; 800CFFD4
