0f0cc2610d217471f77700ae6ee46909ac36c7c9
nocato <nocato@protonmail.com>
2023-11-15 22:17:16 +0100

    Link with s00a's action.c in s01a

--- asm/overlays/s01a/s00a_command_800C67E4.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s00a_command_800C67E4
-s00a_command_800C67E4:
-	dw 0x03E00008 ; 800C9344
-	dw 0x00000000 ; 800C9348
--- asm/overlays/s01a/s00a_command_800C76BC.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s00a_command_800C76BC
-s00a_command_800C76BC:
-	dw 0x03E00008 ; 800CA21C
-	dw 0x00000000 ; 800CA220
