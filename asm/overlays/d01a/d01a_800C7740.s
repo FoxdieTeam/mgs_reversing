2efea57b218177ecf91468b3915612072cd76d46
nocato <nocato@protonmail.com>
2023-11-16 19:48:29 +0100

    Import d01a, smart split d01a

---
+++ asm/overlays/d01a/d01a_800C7740.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d01a_800C7740
+d01a_800C7740:
+	dw 0x3C02800D ; 800C7740
+	dw 0x03E00008 ; 800C7744
+	dw 0xAC4413BC ; 800C7748
---
+++ asm/overlays/d01a/d01a_800C7CC0.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d01a_800C7CC0
+d01a_800C7CC0:
+	dw 0x2402008A ; 800C7CC0
+	dw 0x03E00008 ; 800C7CC4
+	dw 0xAC820000 ; 800C7CC8
