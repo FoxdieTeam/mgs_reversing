003d7acd4e8566601275fd0b34b86c5f471a09cb
nocato <nocato@protonmail.com>
2023-11-18 22:39:03 +0100

    Raw import s02c, s02e

---
+++ asm/overlays/s02c/s02c_800D0FFC.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s02c_800D0FFC
+s02c_800D0FFC:
+	dw 0x24020087 ; 800D0FFC
+	dw 0x03E00008 ; 800D1000
+	dw 0xAC820000 ; 800D1004
---
+++ asm/overlays/s02c/s02c_800D334C.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s02c_800D334C
+s02c_800D334C:
+	dw 0x3C02800E ; 800D334C
+	dw 0x8C423D68 ; 800D3350
+	dw 0x03E00008 ; 800D3354
+	dw 0x00000000 ; 800D3358
