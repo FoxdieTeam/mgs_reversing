fa66bebfc31bb0849ae52f4ee7eedecacc089e45
nocato <nocato@protonmail.com>
2023-11-19 22:29:20 +0100

    Deal with some duplicates in s16c

--- asm/overlays/s16c/s16c_800D66F0.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s16c_800D66F0
-s16c_800D66F0:
-	dw 0xA4850000 ; 800D66F0
-	dw 0xA4860002 ; 800D66F4
-	dw 0x03E00008 ; 800D66F8
-	dw 0xA4870004 ; 800D66FC
