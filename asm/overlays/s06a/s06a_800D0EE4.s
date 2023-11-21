4df198acdcc1418c161e5bfc770c822a67cbc2e0
nocato <nocato@protonmail.com>
2023-11-21 17:00:16 +0100

    Raw import s06a

---
+++ asm/overlays/s06a/s06a_800D0EE4.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s06a_800D0EE4
+s06a_800D0EE4:
+	dw 0x2402008A ; 800D0EE4
+	dw 0x03E00008 ; 800D0EE8
+	dw 0xAC820000 ; 800D0EEC
---
+++ asm/overlays/s06a/s06a_800D0EF0.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s06a_800D0EF0
+s06a_800D0EF0:
+	dw 0x24020086 ; 800D0EF0
+	dw 0x03E00008 ; 800D0EF4
+	dw 0xAC820000 ; 800D0EF8
