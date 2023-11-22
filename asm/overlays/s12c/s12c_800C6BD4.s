7de620d1b1825a4df5ec7f17e25392561bec8f96
nocato <nocato@protonmail.com>
2023-11-22 22:58:13 +0100

    Raw import s12c

---
+++ asm/overlays/s12c/s12c_800C6BD4.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s12c_800C6BD4
+s12c_800C6BD4:
+	dw 0xA48503F4 ; 800C6BD4
+	dw 0xA48603F6 ; 800C6BD8
+	dw 0x03E00008 ; 800C6BDC
+	dw 0xA48703F8 ; 800C6BE0
