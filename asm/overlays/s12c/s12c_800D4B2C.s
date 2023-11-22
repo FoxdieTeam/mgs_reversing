217f5e50c5929a869a22f0b04be8d4f6da58c825
nocato <nocato@protonmail.com>
2023-11-22 23:23:53 +0100

    Smart split s12c

---
+++ asm/overlays/s12c/s12c_800D4B2C.s
@@ -0,0 +1,12 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s12c_800D4B2C
+s12c_800D4B2C:
+	dw 0x03E00008 ; 800D4B2C
+	dw 0x00000000 ; 800D4B30
+	dw 0x8C820018 ; 800D4B34
+	dw 0x00000000 ; 800D4B38
+	dw 0xE8560004 ; 800D4B3C
+	dw 0x48864000 ; 800D4B40
+	dw 0xC8A60004 ; 800D4B44
---
+++ asm/overlays/s12c/s12c_800D81E8.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s12c_800D81E8
+s12c_800D81E8:
+	dw 0xAC800028 ; 800D81E8
+	dw 0x03E00008 ; 800D81EC
+	dw 0x00001021 ; 800D81F0
