309737d17429adb32d1afccdd9c473687aecf875
Kieron <kieron.josephs00@gmail.com>
2023-11-22 16:33:26 +0000

    Merge all duplicates for s06a

--- asm/overlays/s06a/s06a_asiato_800D321C.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s06a_asiato_800D321C
-s06a_asiato_800D321C:
-	dw 0x3C02800E ; 800D321C
-	dw 0x8C4224B8 ; 800D3220
-	dw 0x03E00008 ; 800D3224
-	dw 0x00000000 ; 800D3228
