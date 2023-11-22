85bbb9351f720062cb5f0c985ad0a0145ec65ec4
nocato <nocato@protonmail.com>
2023-11-22 20:21:13 +0100

    Match s02d (without importing!!!)

---
+++ src/overlays/s02d/overlay.c
@@ -0,0 +1,31 @@
+#include "libgcl/libgcl.h"
+
+GCL_ActorTableEntry s02dOverlayCharas[] = 
+{
+    { 0xFED1, (TGCL_ActorCreateFn)0x8005D508 },
+    { 0xCBF8, (TGCL_ActorCreateFn)0x800C502C },
+    { 0x0FAD, (TGCL_ActorCreateFn)0x800C5E98 },
+    { 0xDD8B, (TGCL_ActorCreateFn)0x800C5C9C },
+    { 0x5A50, (TGCL_ActorCreateFn)0x800DC2E0 },
+    { 0xBA52, (TGCL_ActorCreateFn)0x800E0718 },
+    { 0x51C6, (TGCL_ActorCreateFn)0x800E0D78 },
+    { 0xA12E, (TGCL_ActorCreateFn)0x800DFCF0 },
+    { 0xF4C3, (TGCL_ActorCreateFn)0x800E1050 },
+    { 0xEC77, (TGCL_ActorCreateFn)0x800D72EC },
+    { 0x92BC, (TGCL_ActorCreateFn)0x800D79DC },
+    { 0xEEA7, (TGCL_ActorCreateFn)0x800DB0CC },
+    { 0xC6D7, (TGCL_ActorCreateFn)0x800D2A9C },
+    { 0x6E9A, (TGCL_ActorCreateFn)0x800C7068 },
+    { 0xDBA3, (TGCL_ActorCreateFn)0x800DCEB4 },
+    { 0x1A02, (TGCL_ActorCreateFn)0x800D87B4 },
+    { 0x81EA, (TGCL_ActorCreateFn)0x800DB3EC },
+    { 0x6E90, (TGCL_ActorCreateFn)0x800D5E2C },
+    { 0x1AD3, (TGCL_ActorCreateFn)0x800C59A8 },
+    { 0x5D64, (TGCL_ActorCreateFn)0x800E01E0 },
+    { 0x43A0, (TGCL_ActorCreateFn)0x800D9D7C },
+    { 0x5A50, (TGCL_ActorCreateFn)0x800DC2E0 },
+    { 0x8D5A, (TGCL_ActorCreateFn)0x800E041C },
+    { 0xE253, (TGCL_ActorCreateFn)0x800C4E1C },
+    { 0x03D9, (TGCL_ActorCreateFn)0x800DF254 },
+    { 0, 0 }
+};
