d436c6c8ef433515f08ab9c46e7ae0384cd4aa49
Kieron <kieron.josephs00@gmail.com>
2023-11-22 16:33:26 +0000

    Merge all duplicates for s06a

--- asm/overlays/s06a/s06a_command_800D0ED8.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s06a_command_800D0ED8
-s06a_command_800D0ED8:
-	dw 0x2402008B ; 800D0ED8
-	dw 0x03E00008 ; 800D0EDC
-	dw 0xAC820000 ; 800D0EE0
--- asm/overlays/s06a/s06a_pato_lmp_800D9374.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s06a_pato_lmp_800D9374
-s06a_pato_lmp_800D9374:
-	dw 0x03E00008 ; 800D9374
-	dw 0x00000000 ; 800D9378
