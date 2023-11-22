918dfac370a7ba2cbe3f8516722eac885a08d9ae
nocato <nocato@protonmail.com>
2023-11-22 23:32:47 +0100

    Deal with some duplicates in s12c

--- asm/overlays/s12c/s12c_800C3FEC.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s12c_800C3FEC
-s12c_800C3FEC:
-	dw 0x03E00008 ; 800C3FEC
-	dw 0x00000000 ; 800C3FF0
--- asm/overlays/s12c/s12c_800D44BC.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s12c_800D44BC
-s12c_800D44BC:
-	dw 0x3C02800E ; 800D44BC
-	dw 0x8C42A438 ; 800D44C0
-	dw 0x03E00008 ; 800D44C4
-	dw 0x00000000 ; 800D44C8
