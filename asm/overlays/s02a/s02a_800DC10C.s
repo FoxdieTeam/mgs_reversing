43b6de17ea36ecb92566985195e867d61c6918d5
nocato <nocato@protonmail.com>
2023-11-22 16:33:41 +0100

    Find unknown.c, unknown2.c in s02a

--- asm/overlays/s02a/s02a_800DC10C.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s02a_800DC10C
-s02a_800DC10C:
-	dw 0x3C02800E ; 800DC10C
-	dw 0x8C423700 ; 800DC110
-	dw 0x03E00008 ; 800DC114
-	dw 0x00000000 ; 800DC118
