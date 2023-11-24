f43ca1c5af32a162ac7c79eaa631c0f16b94c3ab
nocato <nocato@protonmail.com>
2023-11-24 20:48:03 +0100

    Smart split s04a

---
+++ asm/overlays/s04a/s04a_800C9254.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s04a_800C9254
+s04a_800C9254:
+	dw 0x03E00008 ; 800C9254
+	dw 0x00000000 ; 800C9258
---
+++ asm/overlays/s04a/s04a_800D7E2C.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s04a_800D7E2C
+s04a_800D7E2C:
+	dw 0x03E00008 ; 800D7E2C
+	dw 0x00000000 ; 800D7E30
