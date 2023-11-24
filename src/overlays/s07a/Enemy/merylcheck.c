0afb2e42cd1fda73d9c059b681b87a2d579c9f50
nocato <nocato@protonmail.com>
2023-11-24 17:53:22 +0100

    Deal with some trivial meryl_unk duplicates

--- src/overlays/s07a/Enemy/merylcheck.c
+++ src/overlays/s07a/Enemy/merylcheck.c
@@ -1,9 +1,23 @@
+#include "../../s00a/Enemy/enemy.h"
+
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D66B0.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D66F4.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6738.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D677C.s")
-#pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6808.s")
-#pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6824.s")
+
+// Identical to s00a_command_800C513C
+int s07a_meryl_unk_800D6808( WatcherWork* work ) {
+    int check;
+    check = work->target->field_6_flags & 0x20;
+    return check > 0;
+}
+
+// Identical to s00a_command_800C5158
+int s07a_meryl_unk_800D6824( SVECTOR* svec )
+{
+    return abs( svec->vx ) + abs( svec->vy ) + abs( svec->vz ) ;
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6860.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6A50.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800D6B30.s")
