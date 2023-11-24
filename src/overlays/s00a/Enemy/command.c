47ac5809e44f127d36eba0af8675cda1c49a2763
nocato <nocato@protonmail.com>
2023-11-24 19:37:22 +0100

    Inline command BSS into command.c

--- src/overlays/s00a/Enemy/command.c
+++ src/overlays/s00a/Enemy/command.c
@@ -4,26 +4,57 @@
 #include "Game/linkvarbuf.h"
 #include "Game/map.h"
 
-extern ENEMY_COMMAND     EnemyCommand_800E0D98;
-extern int               GM_GameFlag_800E0F64;
-extern GM_Camera         s00a_dword_800E0CB0;
-extern TOPCOMMAND_STRUCT TOPCOMMAND_800E0F20;
-extern TOPCOMMAND_STRUCT s00a_dword_800E0F28;
-extern int               COM_ALERT_DECREMENT_800E0F60;
-extern int               COM_EYE_LENGTH_800E0D8C;
-extern int               COM_SHOOTRANGE_800E0D88;
-extern unsigned int      COM_GameStatus_800E0F3C;
-extern int               COM_PlayerAddress_800E0D90;
-extern int               COM_PlayerAddressOne_800E0F40[8];
-extern SVECTOR           COM_PlayerPositionOne_800E0D48[8];
-extern int               COM_PlayerMapOne_800E0F70[8];
-extern SVECTOR           COM_PlayerPosition_800E0F30;
-extern int               COM_PlayerMap_800E0F1C;
-extern int               COM_PlayerOnZone_800E0D40;
-extern int               COM_NOISEMODE_DIS_800E0F38;
-extern SVECTOR           s00a_dword_800E0D38;
-extern int               s00a_dword_800E0CA0;
-extern int               s00a_dword_800E0D2C;
+int SECTION("overlay.bss") s00a_dword_800E0C9C;
+
+int SECTION("overlay.bss") s00a_dword_800E0CA0;
+int SECTION("overlay.bss") s00a_dword_800E0CA4;
+int SECTION("overlay.bss") s00a_dword_800E0CA8;
+int SECTION("overlay.bss") s00a_dword_800E0CAC;
+
+GM_Camera SECTION("overlay.bss") s00a_dword_800E0CB0;
+
+int SECTION("overlay.bss") s00a_dword_800E0D2C;
+int SECTION("overlay.bss") s00a_dword_800E0D30;
+int SECTION("overlay.bss") s00a_dword_800E0D34;
+
+SVECTOR SECTION("overlay.bss") s00a_dword_800E0D38;
+
+int SECTION("overlay.bss") COM_PlayerOnZone_800E0D40;
+
+int SECTION("overlay.bss") COM_NoiseMinDisID_800E0D44;
+
+SVECTOR SECTION("overlay.bss") COM_PlayerPositionOne_800E0D48[8];
+
+int SECTION("overlay.bss") COM_SHOOTRANGE_800E0D88;
+int SECTION("overlay.bss") COM_EYE_LENGTH_800E0D8C;
+int SECTION("overlay.bss") COM_PlayerAddress_800E0D90;
+unsigned int SECTION("overlay.bss") ENE_SPECIAL_FLAG_800E0D94;
+
+ENEMY_COMMAND SECTION("overlay.bss")  EnemyCommand_800E0D98;
+
+int SECTION("overlay.bss") COM_PlayerMap_800E0F1C;
+
+TOPCOMMAND_STRUCT SECTION("overlay.bss") TOPCOMMAND_800E0F20;
+
+TOPCOMMAND_STRUCT SECTION("overlay.bss") s00a_dword_800E0F28;
+
+SVECTOR SECTION("overlay.bss")  COM_PlayerPosition_800E0F30;
+
+int SECTION("overlay.bss") COM_NOISEMODE_DIS_800E0F38;
+
+unsigned int SECTION("overlay.bss") COM_GameStatus_800E0F3C;
+
+int SECTION("overlay.bss") COM_PlayerAddressOne_800E0F40[8];
+
+int SECTION("overlay.bss") COM_ALERT_DECREMENT_800E0F60;
+
+int SECTION("overlay.bss") GM_GameFlag_800E0F64;
+
+int SECTION("overlay.bss") COM_VibTime_800E0F68;
+
+int SECTION("overlay.bss") s00a_dword_800E0F6C;
+
+int SECTION("overlay.bss") COM_PlayerMapOne_800E0F70[8];
 
 extern int       GV_Time_800AB330;
 extern SVECTOR   DG_ZeroVector_800AB39C;
@@ -57,10 +88,6 @@ int s00a_dword_800C35E4[] = {
     0x00001644,
     0x000016A8
 };
-
-extern int    COM_NoiseMinDisID_800E0D44;
-extern int    s00a_dword_800E0D30;
-extern int    COM_VibTime_800E0F68;
 
 const char aCresetposd_800E07FC[] = " c_reset_pos = %d \n";
 const char aKottida_800E0810[] = "kottida !!\n";
