ab2456f193483c2a32c9d516093270dabd10bc3b
nocato <nocato@protonmail.com>
2023-11-24 17:53:22 +0100

    Deal with some trivial meryl_unk duplicates

--- src/overlays/s07a/Enemy/merylenemy.c
+++ src/overlays/s07a/Enemy/merylenemy.c
@@ -1,4 +1,17 @@
-#pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800DA948.s")
+#include "../../s00a/Enemy/enemy.h"
+
+// Identical to s00a_command_800C9878
+void s07a_meryl_unk_800DA948( WatcherWork* work )
+{
+    VISION  *vision = &work->vision;
+    SVECTOR *svec   = &work->control.field_3C;
+
+    svec->vx = vision->facedir;
+    svec->vy = vision->length;
+    svec->vz = vision->field_B8E * 2;
+    svec->pad = 0;
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800DA974.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800DAA00.s")
 #pragma INCLUDE_ASM("asm/overlays/s07a/s07a_meryl_unk_800DAA60.s")
