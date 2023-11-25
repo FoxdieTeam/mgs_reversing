3a05da201f9972a68c5c7e4449baa3ecd4f82200
nocato <nocato@protonmail.com>
2023-11-25 17:52:59 +0100

    Deal with some duplicates in s02a

--- asm/overlays/s02a/s02a_cinema_800DFF48.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s02a_cinema_800DFF48
-s02a_cinema_800DFF48:
-	dw 0xAC800028 ; 800DFF48
-	dw 0x03E00008 ; 800DFF4C
-	dw 0x00001021 ; 800DFF50
--- asm/overlays/s02a/s02a_wt_area_800DD554.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s02a_wt_area_800DD554
-s02a_wt_area_800DD554:
-	dw 0x03E00008 ; 800DD554
-	dw 0x00000000 ; 800DD558
