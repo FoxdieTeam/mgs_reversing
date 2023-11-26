c939b180376e21b382ea498473795506e875877f
nocato <nocato@protonmail.com>
2023-11-26 19:49:08 +0100

    Raw import s16a

---
+++ asm/overlays/s16a/s16a_800C58E4.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s16a_800C58E4
+s16a_800C58E4:
+	dw 0xA48503F4 ; 800C58E4
+	dw 0xA48603F6 ; 800C58E8
+	dw 0x03E00008 ; 800C58EC
+	dw 0xA48703F8 ; 800C58F0
