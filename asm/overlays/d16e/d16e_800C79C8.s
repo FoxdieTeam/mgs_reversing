ea261888b25a9ec9d1541684aec758339a12e283
nocato <nocato@protonmail.com>
2023-11-27 08:38:57 +0100

    Smart split d16e

---
+++ asm/overlays/d16e/d16e_800C79C8.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d16e_800C79C8
+d16e_800C79C8:
+	dw 0x03E00008 ; 800C79C8
+	dw 0x00000000 ; 800C79CC
---
+++ asm/overlays/d16e/d16e_800CF4F8.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d16e_800CF4F8
+d16e_800CF4F8:
+	dw 0x3C02800D ; 800CF4F8
+	dw 0x8C4235D8 ; 800CF4FC
+	dw 0x03E00008 ; 800CF500
+	dw 0x00000000 ; 800CF504
