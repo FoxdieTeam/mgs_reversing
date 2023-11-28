df07b8b4f9fb6b9698800ea32416397006ec96c5
nocato <nocato@protonmail.com>
2023-11-28 18:02:13 +0100

    Deal with some duplicates in s20a

--- asm/overlays/s20a/s20a_800C7AF8.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s20a_800C7AF8
-s20a_800C7AF8:
-	dw 0x3C02800D ; 800C7AF8
-	dw 0x8C42A94C ; 800C7AFC
-	dw 0x03E00008 ; 800C7B00
-	dw 0x00000000 ; 800C7B04
--- asm/overlays/s20a/s20a_800C9330.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s20a_800C9330
-s20a_800C9330:
-	dw 0x03E00008 ; 800C9330
-	dw 0x00000000 ; 800C9334
