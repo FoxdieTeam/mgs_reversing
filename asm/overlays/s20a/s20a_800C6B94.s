1dd47d760625bbceb8af739e50140b458e2f4f02
nocato <nocato@protonmail.com>
2023-11-28 17:58:57 +0100

    Smart split s20a

---
+++ asm/overlays/s20a/s20a_800C6B94.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s20a_800C6B94
+s20a_800C6B94:
+	dw 0x8FB00028 ; 800C6B94
+	dw 0x03E00008 ; 800C6B98
+	dw 0x27BD0030 ; 800C6B9C
---
+++ asm/overlays/s20a/s20a_800C6BA0.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s20a_800C6BA0
+s20a_800C6BA0:
+	dw 0x03E00008 ; 800C6BA0
+	dw 0x00000000 ; 800C6BA4
