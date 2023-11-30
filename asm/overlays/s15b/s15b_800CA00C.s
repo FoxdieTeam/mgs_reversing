ffa75b36b24594520d39c63c0c2b81e082e44295
nocato <nocato@protonmail.com>
2023-11-30 21:50:21 +0100

    Deal with some duplicates in s15b

--- asm/overlays/s15b/s15b_800CA00C.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s15b_800CA00C
-s15b_800CA00C:
-	dw 0x3C02800D ; 800CA00C
-	dw 0x8C42C8E0 ; 800CA010
-	dw 0x03E00008 ; 800CA014
-	dw 0x00000000 ; 800CA018
