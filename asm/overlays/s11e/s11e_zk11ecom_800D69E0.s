d0272ecc2ed5f7528921b429fd4f0be8d5f71332
nocato <nocato@protonmail.com>
2023-12-01 19:42:24 +0100

    Batch renames

---
+++ asm/overlays/s11e/s11e_zk11ecom_800D69E0.s
@@ -0,0 +1,11 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11e_zk11ecom_800D69E0
+s11e_zk11ecom_800D69E0:
+	dw 0x8FB30034 ; 800D69E0
+	dw 0x8FB20030 ; 800D69E4
+	dw 0x8FB1002C ; 800D69E8
+	dw 0x8FB00028 ; 800D69EC
+	dw 0x03E00008 ; 800D69F0
+	dw 0x27BD0040 ; 800D69F4
---
+++ asm/overlays/s11e/s11e_zk11ecom_800D9A84.s
@@ -0,0 +1,8 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11e_zk11ecom_800D9A84
+s11e_zk11ecom_800D9A84:
+	dw 0x3C02800E ; 800D9A84
+	dw 0x03E00008 ; 800D9A88
+	dw 0xAC44F3AC ; 800D9A8C
