1eeeafa081337fa5381f9284aa999f7f65eb2601
Jayveer <jayveerk@gmail.com>
2023-12-02 09:17:55 +0000

    renames

---
+++ src/overlays/s11e/Enemy/zako11e.c
@@ -0,0 +1,20 @@
+#include "zako11e.h"
+
+void s11e_zako11e_800D34C8( void )
+{
+}
+
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D34D0.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D354C.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3684.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3800.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3934.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3990.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3BD8.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3C84.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3CA4.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3D40.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3D98.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3DF0.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D3EC8.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zako11e_800D42E0.s")
