0ec5aa3d8320425fde2bced816709e43bafbfc99
nocato <nocato@protonmail.com>
2023-12-02 02:32:20 +0100

    Deal with some duplicates in s07c

--- asm/overlays/s07c/s07c_800D0AEC.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s07c_800D0AEC
-s07c_800D0AEC:
-	dw 0x3C02800D ; 800D0AEC
-	dw 0x8C425308 ; 800D0AF0
-	dw 0x03E00008 ; 800D0AF4
-	dw 0x00000000 ; 800D0AF8
---
+++ src/overlays/s07c/overlay3.c
@@ -0,0 +1,47 @@
+#include "linker.h"
+
+int s07c_dword_800C351C = 0x00000000;
+int s07c_dword_800C3520 = 0x00000000;
+int s07c_dword_800C3524 = 0x00000000;
+int s07c_dword_800C3528 = 0x006BD182;
+
+
+const char s07c_aBintadisd_800D5134[] = " binta dis=%d\n";
+const char s07c_dword_800D5144[] = {0xa5, 0xb9, 0xa5, 0xcd};
+const char s07c_dword_800D5148[] = {0xa1, 0xbc, 0xa5, 0xaf};
+const char s07c_dword_800D514C[] = {0x0, 0x0, 0x0, 0x0};
+const char s07c_aRunmove_800D5150[] = "run_move";
+const char s07c_aMitumetevoiced_800D515C[] = " mitumete voice = %d \n";
+const char s07c_aStopdisd_800D5174[] = " stop dis=%d\n";
+const int s07c_dword_800D5184 = 0x800CE6E0;
+const int s07c_dword_800D5188 = 0x800CE6F0;
+const int s07c_dword_800D518C = 0x800CE700;
+const int s07c_dword_800D5190 = 0x800CE710;
+const int s07c_dword_800D5194 = 0x800CE720;
+const char s07c_dword_800D5198[] = {0x0, 0x0, 0x0, 0x0};
+const int s07c_dword_800D519C = 0x800CE78C;
+const int s07c_dword_800D51A0 = 0x800CE79C;
+const int s07c_dword_800D51A4 = 0x800CE7C4;
+const int s07c_dword_800D51A8 = 0x800CE7C4;
+const int s07c_dword_800D51AC = 0x800CE77C;
+const int s07c_dword_800D51B0 = 0x800CE7AC;
+const int s07c_dword_800D51B4 = 0x800CE7BC;
+const char s07c_aBintadisd_800D51B8[] = " binta dis=%d\n";
+const char s07c_aStopdisd_800D51C8[] = " stop dis=%d\n";
+const int s07c_dword_800D51D8 = 0x800CFFB8;
+const int s07c_dword_800D51DC = 0x800CFFC8;
+const int s07c_dword_800D51E0 = 0x800D0000;
+const int s07c_dword_800D51E4 = 0x800D0000;
+const int s07c_dword_800D51E8 = 0x800CFFA8;
+const int s07c_dword_800D51EC = 0x800CFFD8;
+const int s07c_dword_800D51F0 = 0x800CFFE8;
+const int s07c_dword_800D51F4 = 0x800CFFF8;
+const char s07c_dword_800D51F8[] = {'b', 'l', 'u', 'r'};
+const char s07c_dword_800D51FC[] = {'p', 'u', 'r', 'e'};
+const char s07c_dword_800D5200[] = {'.', 'c', 0x0, 'e'};
+const char s07c_dword_800D5204[] = {'g', 'a', 's', '_'};
+const char s07c_dword_800D5208[] = {'e', 'f', 'c', 't'};
+const char s07c_dword_800D520C[] = {'.', 'c', 0x0, ' '};
+const char s07c_dword_800D5210[] = {'m', 'i', 'r', 'r'};
+const char s07c_dword_800D5214[] = {'o', 'r', '.', 'c'};
+const char s07c_dword_800D5218[] = {0x0, ' ', 'N', 'e'};
