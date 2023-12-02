ec4b62ef712e4548b18ec97a672dcdd67a1a98ab
nocato <nocato@protonmail.com>
2023-12-02 02:32:20 +0100

    Deal with some duplicates in s07c

--- asm/overlays/s07c/s07c_800D3BDC.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s07c_800D3BDC
-s07c_800D3BDC:
-	dw 0xAC800028 ; 800D3BDC
-	dw 0x03E00008 ; 800D3BE0
-	dw 0x00001021 ; 800D3BE4
--- asm/overlays/s07c/s07c_800D4518.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s07c_800D4518
-s07c_800D4518:
-	dw 0x03E00008 ; 800D4518
-	dw 0x00000000 ; 800D451C
-	dw 0x00000000 ; 800D4520
-	dw 0x00000000 ; 800D4524
