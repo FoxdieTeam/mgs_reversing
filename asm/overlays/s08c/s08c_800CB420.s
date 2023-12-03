1f6f1d7c6cfd09e5a874ec16220e8cf90677292e
nocato <nocato@protonmail.com>
2023-12-03 02:29:59 +0100

    Smart split s08c

---
+++ asm/overlays/s08c/s08c_800CB420.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s08c_800CB420
+s08c_800CB420:
+	dw 0x03E00008 ; 800CB420
+	dw 0x00000000 ; 800CB424
---
+++ asm/overlays/s08c/s08c_800CFF8C.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s08c_800CFF8C
+s08c_800CFF8C:
+	dw 0x03E00008 ; 800CFF8C
+	dw 0x00000000 ; 800CFF90
