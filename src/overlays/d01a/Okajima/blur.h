48400839ae9afb3bd41f46d0243790bc02691bbb
August <vlad.socolov@mail.ru>
2023-12-04 00:22:40 +0300

    Replaced hashes and harcoded addresses in d01a (except CHARA_UNKNOWN_000E)

---
+++ src/overlays/d01a/Okajima/blur.h
@@ -0,0 +1,6 @@
+#ifndef _BLOOD_BL_H_
+#define _BLOOD_BL_H_
+
+GV_ACT * NewBlur_800CD530(int name, int where, int argc, char **argv);
+
+#endif // _BLOOD_BL_H_
--- src/overlays/d11c/Enemy/demokage.h
+++ src/overlays/d11c/Enemy/demokage.h
@@ -1,6 +1,8 @@
 #ifndef _DEMOKAGE_H_
 #define _DEMOKAGE_H_
 
+#include "Game/object.h"
+
 GV_ACT * NewDemoKage_800C48A4(OBJECT *parent, SVECTOR arg1, int *arg2, int *arg3, char r, char g, char b, int unused);
 
 #endif // _DEMOKAGE_H_
