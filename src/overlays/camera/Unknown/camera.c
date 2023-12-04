e0275c5b7e3882e1164e01b84bf84bf38fcb6312
nocato <nocato@protonmail.com>
2023-12-04 23:24:20 +0100

    Two duplicates in camera.c

--- src/overlays/camera/Unknown/camera.c
+++ src/overlays/camera/Unknown/camera.c
@@ -116,9 +116,45 @@ void camera_800C3A7C(unsigned long *runlevel, RECT *pRect)
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C4D20.s")
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C4D70.s")
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C5308.s")
-#pragma INCLUDE_ASM("asm/overlays/camera/camera_800C5388.s")
+
+void move_coord_800C5388(int *arr, int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++, arr += 2)
+    {
+        arr[0] += arr[1];
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C53B8.s")
-#pragma INCLUDE_ASM("asm/overlays/camera/camera_800C5440.s")
+
+// Copy of menu_radio_do_file_mode_helper4_helper_8004A54C
+void camera_800C5440(MenuPrim *pGlue, RadioFileModeStruElem *pElem)
+{
+    LINE_F2           *pPrim;
+    RadioFileModeUnk1 *pUnk;
+
+    pUnk = pElem->field_C_unk1;
+    if (pElem->field_0 == 1)
+    {
+        move_coord_800C5388(&pUnk->field_4, 4);
+    }
+
+    if (pUnk->field_4 != pUnk->field_14 || pUnk->field_C != pUnk->field_1C)
+    {
+        _NEW_PRIM(pPrim, pGlue);
+
+        LSTORE(pUnk->field_24, &pPrim->r0);
+        pPrim->x0 = pUnk->field_4 >> 16;
+        pPrim->y0 = pUnk->field_C >> 16;
+        pPrim->x1 = pUnk->field_14 >> 16;
+        pPrim->y1 = pUnk->field_1C >> 16; // pUnk->field_1C / 65536 wouldn't match
+        setLineF2(pPrim);
+        addPrim(pGlue->mPrimBuf.mOt, pPrim);
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C553C.s")
 #pragma INCLUDE_ASM("asm/overlays/camera/camera_800C5684.s")
 
