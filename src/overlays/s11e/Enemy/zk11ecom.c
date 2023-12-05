d77a220322612895c3a0ed98244ed1115a3a6882
Jayveer <jayveerk@gmail.com>
2023-12-05 10:30:54 +0000

    split asm files

--- src/overlays/s11e/Enemy/zk11ecom.c
+++ src/overlays/s11e/Enemy/zk11ecom.c
@@ -9,15 +9,13 @@ extern TOPCOMMAND_STRUCT s11e_dword_800DF3A8;
 
 
 //#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D9A84.s")
-void s11e_zk11ecom_800D9A84( int alert )
+void ZAKO11E_SetTopCommAL_800D9A84( int alert )
 {
     s11e_dword_800DF3A8.alert = alert;
 }
 
-
-
 //#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D9A90.s")
-void s11e_zk11ecom_800D9A90( int mode )
+void ZAKO11E_SetTopCommMD_800D9A90( int mode )
 {
     s11e_dword_800DF3A8.mode = mode;
 }
@@ -47,6 +45,27 @@ void s11e_zk11ecom_800D9A90( int mode )
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DA690.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DA784.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DA7F8.s")
+
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DA85C.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DABF4.s")
+#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DAC5C.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DACA0.s")
+
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800DAF38.s")
+/*
+extern void s11e_zk11ecom_800DABF4();
+extern void s11e_zk11ecom_800DAC5C();
+extern const char s11e_aZkecomc_800DED84[];
+
+GV_ACT *s11e_zk11ecom_800DAF38(int name, int where, int argc, char **argv)
+{
+    CommanderWork *work ;
+
+    work = (ZakoCommanderWork *)GV_NewActor_800150E4( 4, sizeof( ZakoCommanderWork ) ) ;
+    if ( work != NULL ) {
+        GV_SetNamedActor_8001514C( &( work->actor ), ( TActorFunction )s11e_zk11ecom_800DABF4, ( TActorFunction )s11e_zk11ecom_800DAC5C, s11e_aZkecomc_800DED84 );
+        s11e_zk11ecom_800DACA0( work, name, where );
+    }
+    return &work->actor;
+}
+*/
