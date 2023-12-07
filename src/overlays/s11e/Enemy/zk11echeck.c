07e5e7464c5fb3d4f899eb939ea02f128d16c528
Jayveer <jayveerk@gmail.com>
2023-12-07 10:29:50 +0000

    zako check funcs

--- src/overlays/s11e/Enemy/zk11echeck.c
+++ src/overlays/s11e/Enemy/zk11echeck.c
@@ -10,6 +10,16 @@ extern void s11e_zk11ecom_800D5EEC( WatcherWork *work, int time );
 
 extern SVECTOR DG_ZeroVector_800AB39C;
 
+extern SVECTOR ZAKO_TARGET_SIZE_800C38CC;
+extern SVECTOR ZAKO_TARGET_FORCE_800C38D4;
+extern SVECTOR ZAKO_ATTACK_SIZE_800C38DC;
+extern SVECTOR ZAKO_ATTACK_FORCE_800C38E4;
+extern SVECTOR ZAKO_TOUCH_SIZE_800C38EC;
+extern SVECTOR ZAKO_TOUCH_FORCE_800C38F4;
+
+extern int ZAKO_EYE_LENGTH_800C3904;
+extern SVECTOR ZAKO_NO_POINT_800C38FC;
+
 int s11e_zk11ecom_800D435C( WatcherWork *work )
 {
     TARGET *target;
@@ -131,9 +141,85 @@ int s11e_zk11ecom_800D46A0( WatcherWork * work )
     return 0;
 }
 
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D4700.s")
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D47D4.s")
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D4828.s")
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D4700.s")
+void s11e_zk11ecom_800D4700( ZakoWork* work )
+{
+    int addr;
+    CONTROL *ctrl;
+
+    ctrl = &work->control;
+    ctrl->field_0_mov.vx = 30000;
+    ctrl->field_0_mov.vy = 30000;
+    ctrl->field_0_mov.vz = 30000;
+    
+    work->param_item = 1;
+    work->think3 = 1;
+    work->visible = 0;
+    work->think1 = 0;
+    work->think2 = 0;
+    work->think4 = 0;
+    work->count3 = 0;
+
+    work->next_node   = work->field_9E8 - 1;
+    work->target_addr = work->start_addr;
+    work->target_pos  = work->start_pos;
+    
+    work->vision.field_B8E = 512;
+    work->vision.length    = ZAKO_EYE_LENGTH_800C3904;
+    work->alert_level      = 0;
+    work->vision.facedir   = 0;
+    work->pad.sound        = 0;
+    work->pad.mode         = 1;
+    work->field_C4C        = 0;
+    
+    work->target_map = work->start_map;    
+    work->field_C1C  = work->start_pos;
+
+    addr = work->start_addr;
+    work->field_C10 = addr;
+    work->field_BF0 = addr;
+}
+
+
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D47D4.s")
+int s11e_zk11ecom_800D47D4( int dir, int dist )
+{
+    int diff ;
+    if ( dist < 0 || dir < 0 ) return 0 ;
+
+    diff = GV_DiffDirS_8001704C( dir, dist ) ;
+
+    if ( diff + 0x380 > 0x700u ) return 0 ;
+    if ( diff >= 0 ) return 1 ;
+
+    return 2 ;
+}
+
+extern OBJECT *GM_PlayerBody_800ABA20;
+
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D4828.s")
+void s11e_zk11ecom_800D4828( ZakoWork* work )
+{
+    int trans;
+    int near;
+
+    trans = ( ( GM_PlayerBody_800ABA20->objs->objs[6].world.t[1] - work->body.objs->objs[6].world.t[1] ) );
+    trans = ratan2( work->sn_dis, trans  )  & 0xFFF;
+
+    near = GV_NearExp8_800263E4( work->field_734, trans - 0x400 );
+    work->field_734 = near;
+    work->field_754 = near;
+
+    if ( near < 0 )
+    {
+        work->field_75C = near * 3 ;
+    }
+    else
+    {
+        work->field_75C = ( near * 3 ) / 2 ;
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D48B0.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D49C0.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D4A28.s")
