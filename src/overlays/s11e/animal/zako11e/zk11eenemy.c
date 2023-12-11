750c67e66c3aecb8db76216bfa6f1556046d0e0a
Jayveer <jayveerk@gmail.com>
2023-12-11 09:08:31 +0000

    zako enemy funcs

--- src/overlays/s11e/animal/zako11e/zk11eenemy.c
+++ src/overlays/s11e/animal/zako11e/zk11eenemy.c
@@ -1,5 +1,22 @@
 #include "../../../s00a/Enemy/enemy.h"
 
+extern ZAKO_COMMAND ZakoCommand_800DF280;
+
+extern int      GV_Time_800AB330;
+extern SVECTOR  DG_ZeroVector_800AB39C;
+extern int      GM_ClaymoreMap_800AB9DC;
+extern SVECTOR  GM_NoisePosition_800AB9F8;
+extern int      GM_PlayerMap_800ABA0C;
+extern SVECTOR  GM_PlayerPosition_800ABA10;
+extern int      GM_NoisePower_800ABA24;
+extern int      GM_NoiseLength_800ABA30;
+extern int      GM_PlayerStatus_800ABA50;
+extern CONTROL *GM_WhereList_800B56D0[94];
+
+extern int AsiatoCheck_800D16C0( HZD_HDL*, SVECTOR* );
+extern int SearchNearAsiato_800D13B0( HZD_HDL*, SVECTOR*, short, short, short );
+
+
 //#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D80E0.s")
 void s11e_zk11ecom_800D80E0( ZakoWork* work )
 {
@@ -120,9 +137,84 @@ void s11e_zk11ecom_800D81F0( ZakoWork* work )
     work->field_BA2 |= 1;
 }
 
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8370.s")
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8498.s")
-#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8544.s")
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8370.s")
+void s11e_zk11ecom_800D8370( ZakoWork *work )
+{
+
+    if ( work->field_BA3 & 2 && work->vision.field_B92 == 2 && ( ( GM_PlayerStatus_800ABA50 & 0x1000 ) == 0x1000 ) )
+    {
+        if ( ( GV_DiffVec3_80016E84( &work->field_BA4, &GM_PlayerPosition_800ABA10 ) > 50 ) || ( work->field_BAC != GM_WhereList_800B56D0[0]->field_8_rot.vy ) )
+        {
+            if ( ZakoCommand_800DF280.mode != TOP_COMM_ALERT )
+            {
+                work->field_BA4 = GM_PlayerPosition_800ABA10;
+                work->field_BAC = GM_WhereList_800B56D0[0]->field_8_rot.vy;
+                work->field_BA2 |= 0x2;
+            }
+            else
+            {
+                return;
+            }
+        }
+        else
+        {
+            if ( GV_DiffVec3_80016E84( &work->control.field_0_mov, &GM_PlayerPosition_800ABA10 ) < 1500 )
+            {
+                work->field_BA2 |= 0x40;
+            }
+        }
+        work->vision.field_B92 = 0;
+        work->field_BA2 |= 0x80;
+    }
+}
+
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8498.s")
+void s11e_zk11ecom_800D8498( ZakoWork* work )
+{
+    HZD_HDL *hzd;
+    CONTROL *ctrl;
+    VISION  *vision;
+
+    if ( !( work->field_BA3 & 16 ) )
+    {
+        return;
+    }
+
+    ctrl = &work->control;
+    if ( work->think2 == 5 )
+    {
+        return;
+    }
+
+    hzd = work->control.field_2C_map->field_8_hzd;
+    vision = &work->vision;
+    if ( !( AsiatoCheck_800D16C0( hzd, &ctrl->field_0_mov ) ) )
+    {
+        return;
+    }
+
+    if ( SearchNearAsiato_800D13B0( hzd, &ctrl->field_0_mov, vision->facedir, vision->field_B8E, vision->length ) < 0 )
+    {
+        return;
+    }
+
+    work->field_BA2 |= 16;
+}
+
+//#pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8544.s")
+void s11e_zk11ecom_800D8544( ZakoWork* work )
+{
+    work->field_BA2 = 0;
+    s11e_zk11ecom_800D81F0( work );
+
+    if ( ZakoCommand_800DF280.mode != TOP_COMM_ALERT )
+    {
+        s11e_zk11ecom_800D810C( work );
+        s11e_zk11ecom_800D8370( work );
+        s11e_zk11ecom_800D8498( work );
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8598.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8668.s")
 #pragma INCLUDE_ASM("asm/overlays/s11e/s11e_zk11ecom_800D8830.s")
