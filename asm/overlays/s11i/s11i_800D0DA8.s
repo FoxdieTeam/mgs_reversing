4cc8b0fe6eea7f49a9096b5596eeae0295d82f93
nocato <nocato@protonmail.com>
2024-01-01 21:00:13 +0100

    Batch renames

--- asm/overlays/s11i/s11i_800D0DA8.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s11i_800D0DA8
-s11i_800D0DA8:
-	dw 0x3C02800D ; 800D0DA8
-	dw 0x84425C0A ; 800D0DAC
-	dw 0x03E00008 ; 800D0DB0
-	dw 0x0002102A ; 800D0DB4
