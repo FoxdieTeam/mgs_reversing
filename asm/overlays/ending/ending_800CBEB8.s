f80838dec85e7f801d87193589b44b7570cd28f1
nocato <nocato@protonmail.com>
2024-01-07 23:30:57 +0100

    Deal with PSYQ functions in "ending" overlay

--- asm/overlays/ending/ending_800CBEB8.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef ending_800CBEB8
-ending_800CBEB8:
-	dw 0x8FBF0010 ; 800CBEB8
-	dw 0x27BD0018 ; 800CBEBC
-	dw 0x03E00008 ; 800CBEC0
-	dw 0x00000000 ; 800CBEC4
