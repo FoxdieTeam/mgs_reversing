a14768237ee99db4e15a11511401aba53176ca43
nocato <nocato@protonmail.com>
2024-01-07 21:26:15 +0100

    Deal with some duplicates in "ending"

--- asm/overlays/ending/ending_800C6D28.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef ending_800C6D28
-ending_800C6D28:
-	dw 0xA4850000 ; 800C6D28
-	dw 0xA4860002 ; 800C6D2C
-	dw 0x03E00008 ; 800C6D30
-	dw 0xA4870004 ; 800C6D34
---
+++ src/overlays/ending/overlay3.c
@@ -0,0 +1,9 @@
+#include "libgcl/libgcl.h"
+
+const char ending_aDmastatuserrorx_800CCD2C[] = "DMA STATUS ERROR %x\n";
+const char ending_dword_800CCD44[] = {0x0, 0x0, 0x0, 0x0};
+const char ending_dword_800CCD48[] = {0x0, 0x0, 0x0, 0x0};
+const char ending_aMdecrestbadoptiond_800CCD4C[] = "MDEC_rest:bad option(%d)\n";
+const char ending_aMdecinsync_800CCD68[] = "MDEC_in_sync";
+const char ending_aMdecoutsync_800CCD78[] = "MDEC_out_sync";
+const char ending_aStimeout_800CCD88[] = "%s timeout:\n";
