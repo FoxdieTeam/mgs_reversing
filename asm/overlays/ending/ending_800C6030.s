3f6963099bb08d385ccd680563728bce022d958d
nocato <nocato@protonmail.com>
2024-01-07 21:26:15 +0100

    Deal with some duplicates in "ending"

--- asm/overlays/ending/ending_800C6030.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef ending_800C6030
-ending_800C6030:
-	dw 0xA4850000 ; 800C6030
-	dw 0xA4860002 ; 800C6034
-	dw 0x03E00008 ; 800C6038
-	dw 0xA4870004 ; 800C603C
