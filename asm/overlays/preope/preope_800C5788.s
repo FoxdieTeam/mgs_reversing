e5fb1207cde3c00e6fe15f17c31568f88bec6515
nocato <nocato@protonmail.com>
2024-01-08 11:06:51 +0100

    Batch renames

--- asm/overlays/preope/preope_800C5788.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef preope_800C5788
-preope_800C5788:
-	dw 0x8C820028 ; 800C5788
-	dw 0x03E00008 ; 800C578C
-	dw 0x00000000 ; 800C5790
--- asm/overlays/preope/preope_800C790C.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef preope_800C790C
-preope_800C790C:
-	dw 0x8C820028 ; 800C790C
-	dw 0x03E00008 ; 800C7910
-	dw 0x00000000 ; 800C7914
