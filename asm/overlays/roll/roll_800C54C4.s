35c128c5d05bbeb2d98731b609493fed258579f7
nocato <nocato@protonmail.com>
2024-01-28 23:53:29 +0100

    Batch renames in roll

--- asm/overlays/roll/roll_800C54C4.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef roll_800C54C4
-roll_800C54C4:
-	dw 0x8FB10014 ; 800C54C4
-	dw 0x8FB00010 ; 800C54C8
-	dw 0x03E00008 ; 800C54CC
-	dw 0x27BD0020 ; 800C54D0
