b9424ebd8de66c50b4f217f498aa2f8d054d2de1
nocato <nocato@protonmail.com>
2024-01-28 23:25:13 +0100

    Deal with some duplicates in roll

--- asm/overlays/roll/roll_800C8D7C.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef roll_800C8D7C
-roll_800C8D7C:
-	dw 0x03E00008 ; 800C8D7C
-	dw 0x00000000 ; 800C8D80
--- asm/overlays/roll/roll_800C9304.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef roll_800C9304
-roll_800C9304:
-	dw 0x3C04800D ; 800C9304
-	dw 0x0C022EE8 ; 800C9308
-	dw 0x24849B0C ; 800C930C
