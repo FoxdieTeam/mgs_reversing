19b5610d04bed4b76809f16749f37eb50f032977
Kieron <kieron.josephs00@gmail.com>
2024-01-30 20:11:21 +0000

    Add wall_spa.c for s08c/s08cr

--- src/overlays/s08cr/overlay2.c
+++ src/overlays/s08cr/overlay2.c
@@ -1,9 +1,6 @@
 #include "linker.h"
 
-const char s08c_dword_800D16EC[] = {0xa4, 0xcf, 0xa4, 0xb8};
-const char s08c_dword_800D16F0[] = {0xa4, 0xe1, 0x0, 0x0};
-const char s08c_dword_800D16F4[] = {0xa4, 0xe4, 0xa4, 0xe1};
-const char s08c_dword_800D16F8[] = {0x0, 0x0, 0x0, 0x0};
-const char s08c_dword_800D16FC[] = {'w', 'a', 'l', 'l'};
-const char s08c_dword_800D1700[] = {'_', 's', 'p', 'a'};
-const char s08c_dword_800D1704[] = {'.', 'c', 0x0, ' '};
+const char s08c_dword_800D1C04[] = "はじめ";
+const char s08c_dword_800D1C0C[] = "やめ";
+const char s08c_aPlasma_800D1C14[] = "plasma";
+const char s08c_dword_800D1C1C[] = "plasma.c";
