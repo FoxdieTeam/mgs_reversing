9e923fc9c47f5bd1d24464a10800076f3aab81c0
Kieron <kieron.josephs00@gmail.com>
2024-01-31 19:56:34 +0000

    Add sub_room.c for opening

--- src/overlays/opening/overlay2.c
+++ src/overlays/opening/overlay2.c
@@ -1,5 +1,3 @@
 #include "libgcl/libgcl.h"
 
-const char opening_dword_800CD600[] = {'s', 'u', 'b', '_'};
-const char opening_dword_800CD604[] = {'e', 'f', 'c', 't'};
-const char opening_dword_800CD608[] = {'.', 'c', 0x0, '\n'};
+const char opening_dword_800CD600[] = "sub_efct.c";
