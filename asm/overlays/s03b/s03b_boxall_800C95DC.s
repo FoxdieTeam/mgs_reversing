3342c8f6da414618b454ea6c939cab87dfa02ccd
nocato <nocato@protonmail.com>
2024-02-03 15:28:03 +0100

    Batch renames in s03b

---
+++ asm/overlays/s03b/s03b_boxall_800C95DC.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s03b_boxall_800C95DC
+s03b_boxall_800C95DC:
+	dw 0x3C02800C ; 800C95DC
+	dw 0x8C423374 ; 800C95E0
+	dw 0x03E00008 ; 800C95E4
+	dw 0x00000000 ; 800C95E8
---
+++ asm/overlays/s03b/s03b_info_800CA314.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s03b_info_800CA314
+s03b_info_800CA314:
+	dw 0x03E00008 ; 800CA314
+	dw 0x00000000 ; 800CA318
