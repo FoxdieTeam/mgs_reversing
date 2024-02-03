e7f3daf0235fd6bad875bc9a372860478819f846
nocato <nocato@protonmail.com>
2024-02-03 20:41:26 +0100

    Smart split s11g

---
+++ asm/overlays/s11g/s11g_800C8B74.s
@@ -0,0 +1,7 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11g_800C8B74
+s11g_800C8B74:
+	dw 0x03E00008 ; 800C8B74
+	dw 0x00000000 ; 800C8B78
---
+++ asm/overlays/s11g/s11g_800D45A4.s
@@ -0,0 +1,12 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11g_800D45A4
+s11g_800D45A4:
+	dw 0x8FB40080 ; 800D45A4
+	dw 0x8FB3007C ; 800D45A8
+	dw 0x8FB20078 ; 800D45AC
+	dw 0x8FB10074 ; 800D45B0
+	dw 0x8FB00070 ; 800D45B4
+	dw 0x03E00008 ; 800D45B8
+	dw 0x27BD0088 ; 800D45BC
