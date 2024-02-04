3f423357ad7d9b57539b7447edf3a4fbef3cde18
Kieron <kieron.josephs00@gmail.com>
2024-02-04 01:36:39 +0000

    Add bed.c, boxall.c & info.c for s03b

---
+++ src/overlays/s03b/chara/torture/info.c
@@ -0,0 +1,202 @@
+#include "libdg/libdg.h"
+#include "libgv/libgv.h"
+
+typedef struct _InfoWork
+{
+    GV_ACT   actor;
+    int      f20;
+    int      f24;
+    int      f28;
+    int      f2C;
+    int      f30;
+    int      f34;
+    int      f38;
+    DG_TEX  *tex[2];
+    POLY_FT4 poly[2][2];
+} InfoWork;
+
+int info_alive = 0;
+int info_unused = 0;
+
+extern int GV_Clock_800AB920;
+
+#define EXEC_LEVEL 7
+
+void InfoAct_800CA114(InfoWork *work)
+{
+    int       f24;
+    char     *ot;
+    POLY_FT4 *poly1;
+    POLY_FT4 *poly2;
+    int       w, h;
+
+    if (info_alive == 0)
+    {
+        GV_DestroyActor_800151C8(&work->actor);
+        return;
+    }
+
+    f24 = work->f24++;
+
+    ot = DG_ChanlOTag(1);
+
+    poly1 = &work->poly[GV_Clock_800AB920][0];
+    poly2 = &work->poly[GV_Clock_800AB920][1];
+
+    switch (work->f20)
+    {
+    case 0:
+        w = f24 * work->f34;
+        if (work->tex[0]->field_A_width < w)
+        {
+            w = work->tex[0]->field_A_width;
+            work->f20 = 1;
+            work->f24 = 0;
+        }
+
+        work->f28 = w;
+        break;
+
+    case 1:
+        w = f24 * work->f38;
+        if (work->tex[1]->field_A_width < w)
+        {
+            w = work->tex[1]->field_A_width;
+        }
+
+        work->f2C = w;
+
+        h = f24 * work->f38;
+        if (work->tex[1]->field_B_height < h)
+        {
+            h = work->tex[1]->field_B_height;
+        }
+
+        work->f30 = h;
+
+        if (h == work->tex[1]->field_B_height)
+        {
+            if (w == work->tex[1]->field_A_width)
+            {
+                work->f20++;
+            }
+        }
+        break;
+
+    default:
+        work->f24--;
+        break;
+    }
+
+    poly1->x1 = poly1->x3 = work->f28 + 188;
+    poly2->x1 = poly2->x3 = work->f2C + 188;
+    poly2->y2 = poly2->y3 = work->f30 + 8;
+
+    addPrim(ot, poly2);
+    addPrim(ot, poly1);
+}
+
+void InfoDie_800CA314(InfoWork *work)
+{
+}
+
+int InfoGetResources_800CA31C(InfoWork *work, unsigned short name1, unsigned short name2, int *abe)
+{
+    DG_TEX  **texlist;
+    POLY_FT4 *poly;
+    int       i;
+    DG_TEX   *tex;
+    int       x, y, w, h;
+
+    texlist = work->tex;
+    texlist[0] = DG_GetTexture_8001D830(name1);
+    texlist[1] = DG_GetTexture_8001D830(name2);
+
+    if (texlist[0] == NULL || texlist[1] == NULL)
+    {
+        return -1;
+    }
+
+    poly = (POLY_FT4 *)work->poly;
+    for (i = 0; i < 4; i++, poly++)
+    {
+        setPolyFT4(poly);
+        setRGB0(poly, 128, 128, 128);
+
+        if (i & 1)
+        {
+            tex = texlist[1];
+        }
+        else
+        {
+            tex = texlist[0];
+        }
+
+        x = tex->field_8_offx;
+        w = tex->field_A_width;
+        y = tex->field_9_offy;
+        h = tex->field_B_height;
+        setUVWH(poly, x, y, w, h);
+
+        poly->tpage = tex->field_4_tPage;
+        poly->clut = tex->field_6_clut;
+
+        if (abe[i & 1] != 0)
+        {
+            SetSemiTrans(poly, 1);
+        }
+
+        switch (i & 1)
+        {
+        case 0:
+            poly->x0 = poly->x2 = 188;
+            poly->y0 = poly->y1 = 9;
+            poly->x1 = poly->x3 = 188;
+            poly->y2 = poly->y3 = tex->field_B_height + 9;
+            break;
+
+        case 1:
+            poly->x0 = poly->x2 = 188;
+            poly->y0 = poly->y1 = 8;
+            poly->x1 = poly->x3 = 188;
+            poly->y2 = poly->y3 = 8;
+            break;
+        }
+    }
+
+    work->f34 = 8;
+    work->f38 = 8;
+    work->f20 = 0;
+    work->f24 = 0;
+    work->f30 = 0;
+    work->f2C = 0;
+    work->f28 = 0;
+
+    info_alive = 1;
+    return 0;
+}
+
+GV_ACT * NewInfo_800CA534(unsigned short name1, unsigned short name2, int *abe)
+{
+    InfoWork *work;
+
+    work = (InfoWork *)GV_NewActor_800150E4(EXEC_LEVEL, sizeof(InfoWork));
+    if (work != NULL)
+    {
+        GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)InfoAct_800CA114, (TActorFunction)InfoDie_800CA314, "info.c");
+
+        if (InfoGetResources_800CA31C(work, name1, name2, abe) >= 0)
+        {
+            return &work->actor;
+        }
+
+        GV_DestroyActor_800151C8(&work->actor);
+    }
+
+    return NULL;
+}
+
+void InfoKill_800CA5D0(void)
+{
+    info_alive = 0;
+}
