eef9373912ce71e7ad1e27ed5c49dbf486d78877
nocato <nocato@protonmail.com>
2024-02-12 21:38:54 +0100

    Deal with more duplicates in s11c

---
+++ src/overlays/s11c/overlay3.c
@@ -0,0 +1,21 @@
+#include "linker.h"
+
+int SECTION("overlay.bss") s11c_dword_800D2C74;
+int SECTION("overlay.bss") s11c_dword_800D2C78;
+int SECTION("overlay.bss") s11c_dword_800D2C7C;
+int SECTION("overlay.bss") s11c_dword_800D2C80;
+int SECTION("overlay.bss") s11c_dword_800D2C84;
+int SECTION("overlay.bss") s11c_dword_800D2C88;
+int SECTION("overlay.bss") s11c_dword_800D2C8C;
+int SECTION("overlay.bss") s11c_dword_800D2C90;
+int SECTION("overlay.bss") s11c_dword_800D2C94;
+int SECTION("overlay.bss") s11c_dword_800D2C98;
+int SECTION("overlay.bss") s11c_dword_800D2C9C;
+int SECTION("overlay.bss") s11c_dword_800D2CA0;
+int SECTION("overlay.bss") s11c_dword_800D2CA4;
+int SECTION("overlay.bss") s11c_dword_800D2CA8;
+int SECTION("overlay.bss") s11c_dword_800D2CAC;
+int SECTION("overlay.bss") s11c_dword_800D2CB0;
+int SECTION("overlay.bss") s11c_dword_800D2CB4;
+int SECTION("overlay.bss") s11c_dword_800D2CB8;
+int SECTION("overlay.bss") s11c_dword_800D2CBC;
