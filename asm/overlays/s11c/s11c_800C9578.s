d4d2bfb8052800c0cd2a8bcdb95a540c9338c2a4
nocato <nocato@protonmail.com>
2024-02-12 22:05:41 +0100

    Batch renames

--- asm/overlays/s11c/s11c_800C9578.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s11c_800C9578
-s11c_800C9578:
-	dw 0x03E00008 ; 800C9578
-	dw 0x00000000 ; 800C957C
--- asm/overlays/s11c/s11c_800CBA74.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s11c_800CBA74
-s11c_800CBA74:
-	dw 0x03E00008 ; 800CBA74
-	dw 0x00000000 ; 800CBA78
