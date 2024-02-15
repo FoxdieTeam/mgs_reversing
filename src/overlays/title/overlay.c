42330a8a1e4d1c1edf5f1873812749d229329a49
Kieron <kieron.josephs00@gmail.com>
2024-02-15 22:05:04 +0000

    Three open.c matches

--- src/overlays/title/overlay.c
+++ src/overlays/title/overlay.c
@@ -44,78 +44,102 @@ int title_dword_800C32AC = 0x00000000;
 
 int fonttext_dword_800C32B0 = 0x00000000;
 
-int title_dword_800C32B4 = 0x00000001;
+int open_800C32B4[1] = {0x00000001}; // { int, DVECTOR, int }[24]
 int title_dword_800C32B8 = 0x00BE00A0;
 int title_dword_800C32BC = 0x00000000;
+
 int title_dword_800C32C0 = 0x00000001;
 int title_dword_800C32C4 = 0x00BE00A0;
 int title_dword_800C32C8 = 0x00000000;
+
 int title_dword_800C32CC = 0x00000001;
 int title_dword_800C32D0 = 0x00D200A0;
 int title_dword_800C32D4 = 0x00000000;
+
 int title_dword_800C32D8 = 0x00000001;
 int title_dword_800C32DC = 0x00BE00A0;
 int title_dword_800C32E0 = 0x00000000;
+
 int title_dword_800C32E4 = 0x00000001;
 int title_dword_800C32E8 = 0x00C400A2;
 int title_dword_800C32EC = 0x00000000;
+
 int title_dword_800C32F0 = 0x00000001;
 int title_dword_800C32F4 = 0x00BE00A0;
 int title_dword_800C32F8 = 0x00000000;
+
 int title_dword_800C32FC = 0x00000000;
 int title_dword_800C3300 = 0x00BA0058;
 int title_dword_800C3304 = 0x00000000;
+
 int title_dword_800C3308 = 0x00000000;
 int title_dword_800C330C = 0x00BA006A;
 int title_dword_800C3310 = 0x00000000;
+
 int title_dword_800C3314 = 0x00000000;
 int title_dword_800C3318 = 0x00BA0064;
 int title_dword_800C331C = 0x00000000;
+
 int title_dword_800C3320 = 0x00000000;
 int title_dword_800C3324 = 0x00BA003C;
 int title_dword_800C3328 = 0x00000000;
+
 int title_dword_800C332C = 0x00000000;
 int title_dword_800C3330 = 0x00B800C8;
 int title_dword_800C3334 = 0x00000000;
+
 int title_dword_800C3338 = 0x00000000;
 int title_dword_800C333C = 0x00BA00D6;
 int title_dword_800C3340 = 0x00000000;
+
 int title_dword_800C3344 = 0x00000000;
 int title_dword_800C3348 = 0x00BA00D6;
 int title_dword_800C334C = 0x00000000;
+
 int title_dword_800C3350 = 0x00000000;
 int title_dword_800C3354 = 0x00BA005E;
 int title_dword_800C3358 = 0x00000000;
+
 int title_dword_800C335C = 0x00000001;
 int title_dword_800C3360 = 0x00C400A2;
 int title_dword_800C3364 = 0x00000000;
+
 int title_dword_800C3368 = 0x00000001;
 int title_dword_800C336C = 0x00C400A2;
 int title_dword_800C3370 = 0x00000000;
+
 int title_dword_800C3374 = 0x00000001;
 int title_dword_800C3378 = 0x00C400A2;
 int title_dword_800C337C = 0x00000000;
+
 int title_dword_800C3380 = 0x00000001;
 int title_dword_800C3384 = 0x00C400A2;
 int title_dword_800C3388 = 0x00000000;
+
 int title_dword_800C338C = 0x00000000;
 int title_dword_800C3390 = 0x00BA0058;
 int title_dword_800C3394 = 0x00000000;
+
 int title_dword_800C3398 = 0x00000000;
 int title_dword_800C339C = 0x00500036;
 int title_dword_800C33A0 = 0x00000000;
+
 int title_dword_800C33A4 = 0x00000000;
 int title_dword_800C33A8 = 0x00500036;
 int title_dword_800C33AC = 0x00000000;
+
 int title_dword_800C33B0 = 0x00000000;
 int title_dword_800C33B4 = 0x00B40036;
 int title_dword_800C33B8 = 0x00000000;
+
 int title_dword_800C33BC = 0x00000000;
 int title_dword_800C33C0 = 0x00820060;
 int title_dword_800C33C4 = 0x00000000;
+
 int title_dword_800C33C8 = 0x00000000;
 int title_dword_800C33CC = 0x008200C8;
 int title_dword_800C33D0 = 0x00000000;
+
 int title_dword_800C33D4 = 0xFFFFFFFF;
 int title_dword_800C33D8 = 0x007773DF;
 int title_dword_800C33DC = 0x0080801F;
@@ -127,6 +151,7 @@ int title_dword_800C33F0 = 0x008EA370;
 int title_dword_800C33F4 = 0x0091A2BD;
 int title_dword_800C33F8 = 0x007B83AB;
 int title_dword_800C33FC = 0x00808080;
+
 int title_dword_800C3400 = 0x000100FF;
 int title_dword_800C3404 = 0xFF000100;
 int title_dword_800C3408 = 0x000100FF;
