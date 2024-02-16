0640d296f7c82c00106821349528d49aafd52d9b
Kieron <kieron.josephs00@gmail.com>
2024-02-16 14:20:13 +0000

    Three matches for open.c

--- asm/overlays/title/title_open_800C5750.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef title_open_800C5750
-title_open_800C5750:
-	dw 0x8FB10014 ; 800C5750
-	dw 0x8FB00010 ; 800C5754
-	dw 0x03E00008 ; 800C5758
-	dw 0x27BD0020 ; 800C575C
