834e80c6d4b74e736e0dc331dc7e50b68dd7ad67
nocato <nocato@protonmail.com>
2024-03-02 15:48:25 +0100

    Match 5 functions in pre_met1.c

---
+++ src/overlays/preope/Onoda/preope/pre_met2.c
@@ -0,0 +1,30 @@
+// Please finish matching pre_met1 before
+// matching functions from pre_met2,
+// it looks like pre_met2 is identical to pre_met1
+// except for different strings.
+
+const int preope_dword_800C9298 = 0x800C7EB0;
+const int preope_dword_800C929C = 0x800C7EEC;
+const int preope_dword_800C92A0 = 0x800C8010;
+const int preope_dword_800C92A4 = 0x800C82A0;
+const int preope_dword_800C92A8 = 0x800C8068;
+const int preope_dword_800C92AC = 0x800C8180;
+const char preope_aD_800C92B0[] = "%2d";
+const char preope_a_800C92B4[] = "/";
+const char preope_aD_800C92B8[] = "%d";
+const char preope_aPreupl_800C92BC[] = "pre_up2_l";
+const char preope_aPreupr_800C92C8[] = "pre_up_r";
+const char preope_aPredownl_800C92D4[] = "pre_down_l";
+const char preope_aPredownr_800C92E0[] = "pre_down_r";
+const char preope_aPremetl_800C92EC[] = "pre_met2_l";
+const char preope_aPremetr_800C92F8[] = "pre_met2_r";
+const char preope_aCurlu_800C9304[] = "cur_lu";
+const char preope_aCurru_800C930C[] = "cur_ru";
+const char preope_aCurld_800C9314[] = "cur_ld";
+const char preope_aCurrd_800C931C[] = "cur_rd";
+const char preope_aCuru_800C9324[] = "cur_u";
+const char preope_aCurd_800C932C[] = "cur_d";
+const char preope_aCurl_800C9334[] = "cur_l";
+const char preope_aCurr_800C933C[] = "cur_r";
+const char preope_aCurc_800C9344[] = "cur_c";
+const char preope_aPremet2c_800C934C[] = "pre_met2.c";
