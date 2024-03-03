b4efb79666b850e8c9317d5bc39607edc6538ef5
Kieron <kieron.josephs00@gmail.com>
2024-03-03 18:14:41 +0000

    Add unknown files for s03b

--- asm/overlays/s03b/s03b_boxall_800C95FC.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s03b_boxall_800C95FC
-s03b_boxall_800C95FC:
-	dw 0x3C02800C ; 800C95FC
-	dw 0x9042337D ; 800C9600
-	dw 0x03E00008 ; 800C9604
-	dw 0x00000000 ; 800C9608
