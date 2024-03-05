bef490bd7be67a4c5f388220f894412b36bed7db
nocato <nocato@protonmail.com>
2024-03-05 15:04:32 +0100

    Smart split s11d

---
+++ asm/overlays/s11d/s11d_800CD0B4.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11d_800CD0B4
+s11d_800CD0B4:
+	dw 0x3C02800C ; 800CD0B4
+	dw 0x90423335 ; 800CD0B8
+	dw 0x03E00008 ; 800CD0BC
+	dw 0x00000000 ; 800CD0C0
