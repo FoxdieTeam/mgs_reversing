fa99240dcacab9e538a40f37c83896426077c433
nocato <nocato@protonmail.com>
2024-03-05 15:32:53 +0100

    Deal with some duplicates in s11d

--- asm/overlays/s11d/s11d_800D02BC.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s11d_800D02BC
-s11d_800D02BC:
-	dw 0x03E00008 ; 800D02BC
-	dw 0x00000000 ; 800D02C0
--- asm/overlays/s11d/s11d_800D0CB4.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s11d_800D0CB4
-s11d_800D0CB4:
-	dw 0x3C02800D ; 800D0CB4
-	dw 0x8C421FB8 ; 800D0CB8
-	dw 0x03E00008 ; 800D0CBC
-	dw 0x00000000 ; 800D0CC0
