cbf0039fc1bc2151b7636c708d979bfad06231e5
nocato <nocato@protonmail.com>
2024-03-10 18:18:24 +0100

    Finish crow.c

--- src/overlays/s15c/Okajima/valcan/crow.c
+++ src/overlays/s15c/Okajima/valcan/crow.c
@@ -2,6 +2,7 @@
 #include "Game/game.h"
 #include "Game/object.h"
 #include "Game/target.h"
+#include "Game/linkvarbuf.h"
 
 typedef struct _CrowEntry
 {
@@ -13,14 +14,15 @@ typedef struct _CrowEntry
     SVECTOR        rots[14];
     MATRIX         light[2];
     SVECTOR        f384;
-    char           pad1[0x10];
+    SVECTOR        f38C;
+    SVECTOR        f394;
     SVECTOR        f39C;
     SVECTOR        f3A4;
     int            f3AC;
-    char           pad2[0x4];
+    int            f3B0;
     int            f3B4;
     int            action_flag;
-    char           pad3[0x4];
+    int            f3BC;
     int            f3C0;
     int            f3C4;
     int            f3C8;
@@ -48,6 +50,7 @@ typedef struct _CrowWork
 
 extern SVECTOR DG_ZeroVector_800AB39C;
 extern int     GM_GameStatus_800AB3CC;
+extern int     GV_Time_800AB330;
 
 #define EXEC_LEVEL 6
 
@@ -228,8 +231,364 @@ void Crow_800DDCD0(CrowEntry *entry)
     }
 }
 
-#pragma INCLUDE_ASM("asm/overlays/s15c/s15c_crow_800DDD08.s")
-void s15c_crow_800DDD08(CrowWork *work); // CrowAct
+void CrowAct_800DDD08(CrowWork *work)
+{
+    SVECTOR    svec1;
+    SVECTOR    svec2;
+    int        time1, time2;
+    CrowEntry *entry;
+    int        rand2;
+    int        rand1;
+    int        rand3;
+    int        i;
+
+    GM_CurrentMap_800AB9B0 = work->map;
+
+    Crow_800DDB50(work);
+
+    svec1.vx = 0;
+    svec1.vz = 0;
+
+    svec2 = DG_ZeroVector_800AB39C;
+
+    entry = work->entries;
+
+    if (work->f68 == 0)
+    {
+        work->f68 = 1;
+
+        GM_ActControl_80025A7C(&entry->control);
+        GM_ActObject2_80034B88(&entry->body);
+
+        if (GM_CurrentItemId == ITEM_THERM_G)
+        {
+            DG_AmbientObjs(entry->body.objs);
+        }
+        else
+        {
+            DG_UnAmbientObjs(entry->body.objs);
+        }
+
+        DG_GetLightMatrix2_8001A5D8(&entry->control.field_0_mov, entry->light);
+    }
+
+    time1 = GV_Time_800AB330 % work->n_entries;
+    time2 = (GV_Time_800AB330 + work->n_entries / 2) % work->n_entries;
+
+    for (i = 0; i < work->n_entries; i++, entry++)
+    {
+        if (!entry->body.objs[0].bound_mode && i != time1 && i != time2)
+        {
+            continue;
+        }
+
+        entry->f3B0--;
+        entry->f3B4++;
+
+        if (entry->f3C8 == 0)
+        {
+            if (entry->f3C0 == 0)
+            {
+                GM_Target_SetVector_8002D500(entry->target, &entry->control.field_0_mov);
+                if (entry->target->field_6_flags & TARGET_POWER)
+                {
+                    GM_ConfigControlHazard_8002622C(&entry->control, 50, 50, 50);
+                    work->f28--;
+
+                    entry->f39C = entry->target->field_2C_vec;
+                    entry->f39C.vx = GV_RandS_800170BC(64);
+                    entry->f39C.vy = GV_RandU_80017090(32) + 32;
+                    entry->f39C.vz = GV_RandU_80017090(64);
+
+                    entry->f3C0 = 1;
+                    entry->f3C4 = 3;
+                }
+            }
+        }
+        else
+        {
+            entry->target->field_6_flags |= ~TARGET_POWER;
+        }
+
+        switch (entry->f3C4)
+        {
+        case 0:
+            entry->f3B0 = GV_RandU_80017090(256) + 30;
+            Crow_800DD7D8(&entry->control.field_0_mov, &work->f2C, &entry->control.field_4C_turn);
+
+        case 1:
+            entry->f3C4 = 2;
+            entry->body.m_ctrl = &entry->m_ctrl;
+
+            rand1 = GV_RandU_80017090(8);
+            if (rand1 == 0)
+            {
+                entry->f3BC = 7;
+            }
+            else if (rand1 >= 0)
+            {
+                if (rand1 >= 4)
+                {
+                    entry->f3BC = 6;
+                }
+                else
+                {
+                    entry->f3BC = 5;
+                }
+            }
+            else
+            {
+                entry->f3BC = 6;
+            }
+
+            svec2.vz = GV_RandU_80017090(4096) + 2000;
+
+            DG_SetPos2_8001BC8C(&entry->control.field_0_mov, &entry->control.field_4C_turn);
+            DG_PutVector_8001BE48(&svec2, &entry->f394, 1);
+
+            if (Crow_800DD968(&entry->f394, &work->f44, &work->f4C) == -1)
+            {
+                svec2.vz = 1000;
+                DG_SetPos2_8001BC8C(&entry->control.field_0_mov, &entry->control.field_4C_turn);
+                DG_PutVector_8001BE48(&svec2, &entry->f394, 1);
+            }
+
+            entry->f394.vy = entry->control.field_0_mov.vy + GV_RandS_800170BC(2048);
+            if (entry->f394.vy < work->f34.vy || work->f3C.vy < entry->f394.vy)
+            {
+                entry->f394.vy = work->f2C.vy;
+            }
+            svec1.vy = entry->control.field_4C_turn.vy;
+
+            if (entry->f3AC == 0)
+            {
+                svec1.vy += 1024;
+            }
+            else
+            {
+                svec1.vy -= 1024;
+            }
+
+            svec2.vz = GV_RandU_80017090(2048) + 1000;
+            DG_SetPos2_8001BC8C(&entry->f394, &svec1);
+            DG_PutVector_8001BE48(&svec2, &entry->f38C, 1);
+
+            if (Crow_800DD968(&entry->f38C, &work->f44, &work->f4C) == -1)
+            {
+                svec2.vz = 1000;
+                DG_SetPos2_8001BC8C(&entry->f394, &svec1);
+                DG_PutVector_8001BE48(&svec2, &entry->f38C, 1);
+            }
+            else if (GV_DiffVec3_80016E84(&entry->control.field_0_mov, &work->f2C) < 3500)
+            {
+                entry->f3AC = 1 - entry->f3AC;
+            }
+
+            entry->f39C.vz = GV_RandU_80017090(64) + 100;
+
+        case 2:
+            if (GV_DiffVec3_80016E84(&entry->control.field_0_mov, &entry->f38C) < 1000)
+            {
+                entry->f3C4 = 1;
+            }
+            entry->f384.vx = (entry->f394.vx * 7 + entry->f38C.vx) >> 3;
+            entry->f384.vy = (entry->f394.vy * 7 + entry->f38C.vy) >> 3;
+            entry->f384.vz = (entry->f394.vz * 7 + entry->f38C.vz) >> 3;
+            entry->f394 = entry->f384;
+            Crow_800DDAD4(entry);
+            Crow_800DD8A8(entry, entry->f3BC, 2);
+            Crow_800DDCD0(entry);
+            break;
+
+        case 3:
+            entry->f3C4 = 4;
+            GM_ConfigControlHazard_8002622C(&entry->control, 50, 50, 50);
+            GM_SeSet2_80032968(0, 0x3F, 184);
+            work->f60 = 0;
+            work->f64 = 0;
+            break;
+
+        case 4:
+            GV_AddVec3_80016D00(&entry->control.field_0_mov, &entry->f39C, &entry->control.field_0_mov);
+
+            entry->control.field_4C_turn.vx += 164;
+            entry->control.field_4C_turn.vy += 284;
+            entry->control.field_4C_turn.vz += 394;
+
+            entry->f39C.vx = entry->f39C.vx * 15 / 16;
+            entry->f39C.vy -= 3;
+            entry->f39C.vz = entry->f39C.vz * 15 / 16;
+
+            Crow_800DD8A8(entry, 4, 4);
+
+            if (work->f60-- < 0)
+            {
+                work->f60 = GV_RandU_80017090(4) + 4;
+                rand3 = GV_RandU_80017090(4);
+                if (work->f58 != rand3)
+                {
+                    work->f58 = rand3;
+                }
+                else if (++work->f58 >= 4)
+                {
+                    work->f58 = 0;
+                }
+                GM_SeSet2_80032968(GV_RandS_800170BC(32), 63, work->f58 + 181);
+            }
+
+            if (work->f64-- < 0)
+            {
+                work->f64 = GV_RandU_80017090(4) + 4;
+                rand3 = GV_RandU_80017090(4);
+                if (work->f5C != rand3)
+                {
+                    work->f5C = rand3;
+                }
+                else if (++work->f5C >= 4)
+                {
+                    work->f5C = 0;
+                }
+                GM_SeSet2_80032968(0, 63, work->f5C + 185);
+            }
+
+            if (entry->control.field_57 || entry->control.field_0_mov.vy < -20000)
+            {
+                entry->f3C4 = 5;
+            }
+
+            break;
+
+        case 5:
+            entry->f3B0 = 60;
+            entry->f3C4 = 6;
+            if (work->f54 != -1)
+            {
+                GCL_ExecProc_8001FF2C(work->f54, NULL);
+            }
+
+        case 6:
+            if (entry->f3B0 % 4 < 2)
+            {
+                DG_InvisibleObjs(entry->body.objs);
+            }
+            else
+            {
+                DG_VisibleObjs(entry->body.objs);
+            }
+
+            if (entry->f3B0 < 0)
+            {
+                entry->f3C4 = 7;
+            }
+            break;
+
+        case 7:
+            GM_ConfigControlHazard_8002622C(&entry->control, -1, -2, -1);
+            DG_InvisibleObjs(entry->body.objs);
+            entry->f3C4 = 8;
+            break;
+
+        case 8:
+            entry->f3C4 = 9;
+            break;
+
+        case 10:
+            entry->f3C4 = 0xB;
+            GM_ConfigControlHazard_8002622C(&entry->control, 50, 50, 50);
+            entry->f39C.vy = GV_RandU_80017090(64) + 4;
+            work->f60 = 0;
+            work->f64 = 0;
+
+        case 11:
+            GV_AddVec3_80016D00(&entry->control.field_0_mov, &entry->f39C, &entry->control.field_0_mov);
+
+            entry->control.field_4C_turn.vx += 164;
+            entry->control.field_4C_turn.vy += 284;
+            entry->control.field_4C_turn.vz += 394;
+
+            entry->f39C.vx = entry->f39C.vx * 15 / 16;
+            entry->f39C.vy -= 3;
+            entry->f39C.vz = entry->f39C.vz * 15 / 16;
+
+            Crow_800DD8A8(entry, 4, 11);
+
+            if (work->f60-- < 0)
+            {
+                work->f60 = GV_RandU_80017090(8) + 10;
+                rand2 = GV_RandU_80017090(4);
+
+                if (work->f58 != rand2)
+                {
+                    work->f58 = rand2;
+                }
+                else if (++work->f58 >= 4)
+                {
+                    work->f58 = 0;
+                }
+
+                GM_SeSet2_80032968(GV_RandS_800170BC(32), 63, work->f58 + 181);
+            }
+
+            if (work->f64-- < 0)
+            {
+                work->f64 = GV_RandU_80017090(4) + 4;
+                rand2 = GV_RandU_80017090(4);
+
+                if (work->f5C != rand2)
+                {
+                    work->f5C = rand2;
+                }
+                else if (++work->f5C >= 4)
+                {
+                    work->f5C = 0;
+                }
+
+                GM_SeSet2_80032968(GV_RandS_800170BC(32), 63, work->f5C + 185);
+            }
+
+            if (entry->control.field_57 != 0 || entry->control.field_0_mov.vy < -20000)
+            {
+                entry->control.field_57 = 0;
+
+                GM_ConfigControlHazard_8002622C(&entry->control, -1, -2, -1);
+
+                entry->f3C4 = 12;
+                entry->f3B0 = GV_RandU_80017090(128) + 60;
+
+                entry->control.field_4C_turn.vx = 0;
+                entry->control.field_4C_turn.vz = 0;
+            }
+            break;
+
+        case 12:
+            Crow_800DD8A8(entry, 9, 12);
+            if (entry->f3B0 < 0 && !(GM_GameStatus_800AB3CC & GAME_FLAG_BIT_02))
+            {
+                entry->f3C8 = 0;
+                entry->f3C4 = 0;
+                entry->f39C = DG_ZeroVector_800AB39C;
+            }
+            break;
+        }
+
+        if (entry->f3C4 != 9)
+        {
+            GM_ActControl_80025A7C(&entry->control);
+            GM_ActObject2_80034B88(&entry->body);
+
+            if (GM_CurrentItemId == ITEM_THERM_G)
+            {
+                DG_AmbientObjs(entry->body.objs);
+            }
+            else
+            {
+                DG_UnAmbientObjs(entry->body.objs);
+            }
+
+            DG_GetLightMatrix2_8001A5D8(&entry->control.field_0_mov, entry->light);
+        }
+    }
+}
 
 int Crow_800DE890(CrowWork *work, int name, int map)
 {
@@ -263,10 +622,10 @@ int Crow_800DE890(CrowWork *work, int name, int map)
 
 int Crow_800DE93C(CrowWork *work, int name, int map)
 {
-    int        opt;
-    int        i;
-    CONTROL   *control;
-    OBJECT    *body;
+    int      opt;
+    int      i;
+    CONTROL *control;
+    OBJECT  *body;
 
     opt = GCL_GetOption_80020968('i');
     if (opt != NULL)
@@ -317,7 +676,8 @@ int Crow_800DE93C(CrowWork *work, int name, int map)
 
         DG_VisibleObjs(work->entries[i].body.objs);
 
-        GM_ConfigMotionControl_80034F08(body, &work->entries[i].m_ctrl, GV_StrCode_80016CCC("crow"), work->entries[i].anims, NULL, control, work->entries[i].rots);
+        GM_ConfigMotionControl_80034F08(body, &work->entries[i].m_ctrl, GV_StrCode_80016CCC("crow"),
+                                        work->entries[i].anims, NULL, control, work->entries[i].rots);
         GM_ConfigObjectAction_80034CD4(body, 0, 0, 0);
 
         work->entries[i].f3B4 = GV_RandU_80017090(4096);
@@ -355,7 +715,7 @@ void CrowDie_800DEC78(CrowWork *work)
     }
 }
 
-GV_ACT * NewCrow_800DED08(int name, int where)
+GV_ACT *NewCrow_800DED08(int name, int where)
 {
     CrowWork *work;
     int       opt;
@@ -381,7 +741,8 @@ GV_ACT * NewCrow_800DED08(int name, int where)
         work->n_entries = n_entries;
         work->f28 = n_entries;
 
-        GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)s15c_crow_800DDD08, (TActorFunction)CrowDie_800DEC78, "crow.c");
+        GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)CrowAct_800DDD08, (TActorFunction)CrowDie_800DEC78,
+                                  "crow.c");
 
         if (Crow_800DE93C(work, name, where) < 0 || Crow_800DE890(work, name, where) < 0)
         {
