8ea2fb2fed5163e7392106e9ec1710451af43607
nocato <nocato@protonmail.com>
2024-03-16 21:22:43 +0100

    Deal with some duplicates in "demosel"

--- asm/overlays/demosel/demosel_800C6B74.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef demosel_800C6B74
-demosel_800C6B74:
-	dw 0x3C03800A ; 800C6B74
-	dw 0x24020002 ; 800C6B78
-	dw 0x03E00008 ; 800C6B7C
-	dw 0xAC62D45C ; 800C6B80
---
+++ src/overlays/demosel/Onoda/demosel/demosel.c
@@ -0,0 +1,159 @@
+// CAUTION: most of the functions here are (modified?) duplicates
+// of change.c (but no "change.c" string), pre_met1.c/open.c functions
+
+
+int demosel_dword_800C3218 = 0x00000001;
+int demosel_dword_800C321C = 0x00BE00A0;
+int demosel_dword_800C3220 = 0x00006739;
+int demosel_dword_800C3224 = 0x00000001;
+int demosel_dword_800C3228 = 0x00BE00A0;
+int demosel_dword_800C322C = 0x00006739;
+int demosel_dword_800C3230 = 0x00000001;
+int demosel_dword_800C3234 = 0x00D200A0;
+int demosel_dword_800C3238 = 0x00006739;
+int demosel_dword_800C323C = 0x00000001;
+int demosel_dword_800C3240 = 0x00BE00A0;
+int demosel_dword_800C3244 = 0x00006739;
+int demosel_dword_800C3248 = 0x00000001;
+int demosel_dword_800C324C = 0x00BE00A0;
+int demosel_dword_800C3250 = 0x00006739;
+int demosel_dword_800C3254 = 0x00000001;
+int demosel_dword_800C3258 = 0x00BE00A0;
+int demosel_dword_800C325C = 0x00006739;
+int demosel_dword_800C3260 = 0x00000001;
+int demosel_dword_800C3264 = 0x00C300A0;
+int demosel_dword_800C3268 = 0x00006739;
+int demosel_dword_800C326C = 0x00000001;
+int demosel_dword_800C3270 = 0x00C300A0;
+int demosel_dword_800C3274 = 0x00006739;
+int demosel_dword_800C3278 = 0x00000001;
+int demosel_dword_800C327C = 0x00C300A0;
+int demosel_dword_800C3280 = 0x00006739;
+int demosel_dword_800C3284 = 0x00000001;
+int demosel_dword_800C3288 = 0x00C300A0;
+int demosel_dword_800C328C = 0x00006739;
+int demosel_dword_800C3290 = 0x000100FF;
+int demosel_dword_800C3294 = 0xFF000100;
+
+
+const int demosel_dword_800C6D38 = 0x800C3C0C;
+const int demosel_dword_800C6D3C = 0x800C3C0C;
+const int demosel_dword_800C6D40 = 0x800C3C0C;
+const int demosel_dword_800C6D44 = 0x800C3C18;
+const int demosel_dword_800C6D48 = 0x800C3C18;
+const int demosel_dword_800C6D4C = 0x800C3C34;
+const int demosel_dword_800C6D50 = 0x800C3C34;
+const int demosel_dword_800C6D54 = 0x800C3C34;
+const int demosel_dword_800C6D58 = 0x800C3C34;
+const int demosel_dword_800C6D5C = 0x800C3C40;
+const int demosel_dword_800C6D60 = 0x800C3C4C;
+const char demosel_aCdcaseopen_800C6D64[] = "CD CASE OPEN!!\n";
+const char demosel_aCdnormalspeedsetfailed_800C6D74[] = "CD NORMAL SPEED SET FAILED!!\n";
+const char demosel_aCdnormalspeedsetsuccess_800C6D94[] = "CD NORMAL SPEED SET SUCCESS!!\n";
+const char demosel_a_800C6DB4[] = "???????\n";
+const char demosel_aThisisnotpsdisc_800C6DC0[] = "THIS IS NOT PS DISC!!\n";
+const char demosel_aCdstopfailed_800C6DD8[] = "CD STOP FAILED!!\n";
+const char demosel_aCdstopsuccess_800C6DEC[] = "CD STOP SUCCESS!!\n";
+const char demosel_aCdcaseclose_800C6E00[] = "CD CASE CLOSE!!\n";
+const char demosel_aStartbuttonpush_800C6E14[] = "START BUTTON PUSH!!\n";
+const char demosel_aCdspinstart_800C6E2C[] = "CD SPIN START!!\n";
+const char demosel_aCdnotspin_800C6E40[] = "CD NOT SPIN!!\n";
+const char demosel_aCdtocreadnotfinish_800C6E50[] = "CD TOC READ NOT FINISH!!\n";
+const char demosel_aCdtocreadsuccess_800C6E6C[] = "CD TOC READ SUCCESS!!\n";
+const char demosel_aCdspeedinitfailed_800C6E84[] = "CD SPEED INIT FAILED!!\n";
+const char demosel_aCdspeedinitsuccess_800C6E9C[] = "CD SPEED INIT SUCCESS!!\n";
+const char demosel_aCdcheckfailed_800C6EB8[] = "CD CHECK FAILED!!\n";
+const char demosel_aCdcheckok_800C6ECC[] = "CD CHECK OK!!\n";
+const char demosel_aThisisnotpsdisc_800C6EDC[] = "THIS IS NOT PS DISC!!!\n";
+const char demosel_aCdcheckerror_800C6EF4[] = "CD CHECK ERROR!!\n";
+const char demosel_aThisispsdisc_800C6F08[] = "THIS IS PS DISC!!\n";
+const char demosel_aThisisdisc_800C6F1C[] = "THIS IS DISC 1!!\n";
+const char demosel_aThisisnotdisc_800C6F30[] = "THIS IS NOT DISC 1!!!\n";
+const char demosel_aOkok_800C6F48[] = "OK! OK!\n";
+const char demosel_aNotoknotok_800C6F54[] = "NOT OK! NOT OK!\n";
+const int demosel_dword_800C6F68 = 0x800C3CC0;
+const int demosel_dword_800C6F6C = 0x800C3D60;
+const int demosel_dword_800C6F70 = 0x800C3E04;
+const int demosel_dword_800C6F74 = 0x800C3E34;
+const int demosel_dword_800C6F78 = 0x800C3E8C;
+const int demosel_dword_800C6F7C = 0x800C3ECC;
+const int demosel_dword_800C6F80 = 0x800C3F1C;
+const int demosel_dword_800C6F84 = 0x800C3F68;
+const int demosel_dword_800C6F88 = 0x800C3FD0;
+const int demosel_dword_800C6F8C = 0x800C4198;
+const int demosel_dword_800C6F90 = 0x800C41CC;
+const char demosel_aFlagddd_800C6F94[] = "flag %d %d %d\n";
+const char demosel_dword_800C6FA4[] = {0x0, 0x0, 0x0, 0x0};
+const int demosel_dword_800C6FA8 = 0x800C48D8;
+const int demosel_dword_800C6FAC = 0x800C4BDC;
+const int demosel_dword_800C6FB0 = 0x800C4DEC;
+const int demosel_dword_800C6FB4 = 0x800C5164;
+const int demosel_dword_800C6FB8 = 0x800C5324;
+const int demosel_dword_800C6FBC = 0x800C54E8;
+const int demosel_dword_800C6FC0 = 0x800C56C0;
+const int demosel_dword_800C6FC4 = 0x800C56E0;
+const int demosel_dword_800C6FC8 = 0x800C56E0;
+const int demosel_dword_800C6FCC = 0x800C56E0;
+const int demosel_dword_800C6FD0 = 0x800C56E0;
+const int demosel_dword_800C6FD4 = 0x800C5778;
+const int demosel_dword_800C6FD8 = 0x800C490C;
+const int demosel_dword_800C6FDC = 0x800C4994;
+const int demosel_dword_800C6FE0 = 0x800C4A2C;
+const int demosel_dword_800C6FE4 = 0x800C4AD8;
+const int demosel_dword_800C6FE8 = 0x800C4B20;
+const int demosel_dword_800C6FEC = 0x800C4B68;
+const int demosel_dword_800C6FF0 = 0x800C4C1C;
+const int demosel_dword_800C6FF4 = 0x800C57AC;
+const int demosel_dword_800C6FF8 = 0x800C4C60;
+const int demosel_dword_800C6FFC = 0x800C4CB0;
+const int demosel_dword_800C7000 = 0x800C4D00;
+const int demosel_dword_800C7004 = 0x800C4D50;
+const int demosel_dword_800C7008 = 0x800C4E2C;
+const int demosel_dword_800C700C = 0x800C4E70;
+const int demosel_dword_800C7010 = 0x800C4EB4;
+const int demosel_dword_800C7014 = 0x800C4F04;
+const int demosel_dword_800C7018 = 0x800C4F50;
+const char demosel_dword_800C701C[] = {0x0, 0x0, 0x0, 0x0};
+const int demosel_dword_800C7020 = 0x800C5528;
+const int demosel_dword_800C7024 = 0x800C5564;
+const int demosel_dword_800C7028 = 0x800C55A4;
+const int demosel_dword_800C702C = 0x800C55E4;
+const int demosel_dword_800C7030 = 0x800C5624;
+const int demosel_dword_800C7034 = 0x800C5624;
+const char demosel_aDemobackl_800C7038[] = "demo_back_l";
+const char demosel_aDemobackr_800C7044[] = "demo_back_r";
+const char demosel_aDemodemo_800C7050[] = "demo_demo";
+const char demosel_aDemora_800C705C[] = "demo_ra";
+const char demosel_aDemorb_800C7064[] = "demo_rb";
+const char demosel_aDemorollc_800C706C[] = "demo_roll_c";
+const char demosel_aDemorolld_800C7078[] = "demo_roll_d";
+const char demosel_aDemoexit_800C7084[] = "demo_exit";
+const char demosel_aCurlu_800C7090[] = "cur_lu";
+const char demosel_aCurru_800C7098[] = "cur_ru";
+const char demosel_aCurld_800C70A0[] = "cur_ld";
+const char demosel_aCurrd_800C70A8[] = "cur_rd";
+const char demosel_aCuru_800C70B0[] = "cur_u";
+const char demosel_aCurd_800C70B8[] = "cur_d";
+const char demosel_aCurl_800C70C0[] = "cur_l";
+const char demosel_aCurr_800C70C8[] = "cur_r";
+const char demosel_aCurc_800C70D0[] = "cur_c";
+const char demosel_aDemorankd_800C70D8[] = "demorank = %d\n";
+const char demosel_aDemoselc_800C70E8[] = "demosel.c";
+
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C35FC.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C373C.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C3880.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C3AE4.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C3B34.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C3C74.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C4214.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C434C.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C436C.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C438C.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C43D8.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C46BC.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C4880.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C5884.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C58F4.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C5A78.s")
+#pragma INCLUDE_ASM("asm/overlays/demosel/demosel_800C61B0.s")
