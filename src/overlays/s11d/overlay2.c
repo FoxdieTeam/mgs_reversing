959f8909b1c1f58c2fb8a2db0f3e5065f5b91a5b
nocato <nocato@protonmail.com>
2024-03-17 02:28:17 +0100

    Match pipe.c in s11d

--- src/overlays/s11d/overlay2.c
+++ src/overlays/s11d/overlay2.c
@@ -64,10 +64,6 @@ int s11d_dword_800C334C = 0x000002FF;
 int s11d_dword_800C3350 = 0x00000000;
 int s11d_dword_800C3354 = 0x00000000;
 int s11d_dword_800C3358 = 0x00000000;
-int s11d_dword_800C335C = 0x00000000;
-int s11d_dword_800C3360 = 0x0000027F;
-int s11d_dword_800C3364 = 0x044104AF;
-int s11d_dword_800C3368 = 0x00000000;
 
 
 const char s11d_aTosi_800D1D5C[] = "to_s11i";
@@ -136,10 +132,6 @@ const char s11d_aSned_800D1E9C[] = "sne_11d2";
 const char s11d_dword_800D1EA8[] = {'l', 'a', 'n', 'd'};
 const char s11d_dword_800D1EAC[] = {'i', 'n', 'g', '.'};
 const char s11d_dword_800D1EB0[] = {'c', 0x0, 'c', 0x94};
-const char s11d_dword_800D1EB4[] = {'i', 'n', 'f', 'o'};
-const char s11d_dword_800D1EB8[] = {'.', 'c', 0x0, 0x8};
-const char s11d_dword_800D1EBC[] = {'p', 'i', 'p', 'e'};
-const char s11d_dword_800D1EC0[] = {'.', 'c', 0x0, '<'};
 
 
 int SECTION("overlay.bss") s11d_dword_800D1F78;
