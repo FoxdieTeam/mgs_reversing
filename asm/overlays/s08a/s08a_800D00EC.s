6eb78a2f6394e42c15986cec0ea05b7c63f1388a
nocato <nocato@protonmail.com>
2024-03-25 18:55:57 +0100

    Batch renames in s08a

--- asm/overlays/s08a/s08a_800D00EC.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s08a_800D00EC
-s08a_800D00EC:
-	dw 0x03E00008 ; 800D00EC
-	dw 0x00000000 ; 800D00F0
--- asm/overlays/s08a/s08a_800D4BF4.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s08a_800D4BF4
-s08a_800D4BF4:
-	dw 0x03E00008 ; 800D4BF4
-	dw 0x00000000 ; 800D4BF8
