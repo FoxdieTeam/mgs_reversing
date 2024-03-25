22799aba5e150b35a1ea8bc13bdd6ad6b2b67dd6
nocato <nocato@protonmail.com>
2024-03-25 18:20:33 +0100

    Deal with some duplicates in s08a

--- asm/overlays/s08a/s08a_800C902C.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s08a_800C902C
-s08a_800C902C:
-	dw 0x03E00008 ; 800C902C
-	dw 0x00001021 ; 800C9030
--- asm/overlays/s08a/s08a_800D6944.s
+++
@@ -1,8 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s08a_800D6944
-s08a_800D6944:
-	dw 0xAC800028 ; 800D6944
-	dw 0x03E00008 ; 800D6948
-	dw 0x00001021 ; 800D694C
