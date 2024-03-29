950bd11579d5d5ff79044926d9c79a4cecd7f4e9
nocato <nocato@protonmail.com>
2024-03-29 11:34:16 +0100

    Raw import d18a

---
+++ asm/overlays/d18a/d18a_800D9608.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d18a_800D9608
+d18a_800D9608:
+	dw 0x8FBF0010 ; 800D9608
+	dw 0x27BD0018 ; 800D960C
+	dw 0x03E00008 ; 800D9610
+	dw 0x00000000 ; 800D9614
---
+++ asm/overlays/d18a/d18a_800D984C.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef d18a_800D984C
+d18a_800D984C:
+	dw 0x8FBF0018 ; 800D984C
+	dw 0x27BD0020 ; 800D9850
+	dw 0x03E00008 ; 800D9854
+	dw 0x00000000 ; 800D9858
