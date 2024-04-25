9a3f1994638dbd6356606e7a5937c208fb2b4cdd
nocato <nocato@protonmail.com>
2024-04-25 11:54:50 +0200

    Start blastoff.c in s11g

---
+++ asm/overlays/s11g/s11g_hind_800D5E44.s
@@ -0,0 +1,9 @@
+	opt	c+, at+, e+, n-
+	section overlay.text
+
+	xdef s11g_hind_800D5E44
+s11g_hind_800D5E44:
+	dw 0x2402001E ; 800D5E44
+	dw 0xAC82095C ; 800D5E48
+	dw 0x03E00008 ; 800D5E4C
+	dw 0xAC850960 ; 800D5E50
