e233d34c0031da9fb4883110dc816cbf98b64ba4
Kieron <kieron.josephs00@gmail.com>
2024-06-06 16:27:39 +0100

    Add 4 actors for s08a

---
+++ src/overlays/s08a/Okajima/death_sp.c
@@ -0,0 +1,214 @@
+#include "libgv/libgv.h"
+#include "Game/game.h"
+
+typedef struct _DeathSpWork
+{
+    GV_ACT actor;
+    int    map;
+    int    name;
+    int    f28[4][8];
+    int    fA8[4];
+    int    fB8[4];
+    int    fC8[4];
+    int    fD8[4];
+    int    fE8[4];
+    int    s_time;
+    int    time;
+} DeathSpWork;
+
+extern int     GM_CurrentMap_800AB9B0;
+extern OBJECT *GM_PlayerBody_800ABA20;
+
+void AN_Smoke_800CE164(SVECTOR *pos, SVECTOR *speed, int index, int script);
+
+GV_ACT * NewPlasma_800CD30C(SVECTOR *arg0, SVECTOR *arg1, int arg2, int arg3);
+GV_ACT * NewSpark2_800CA714(MATRIX *world);
+
+#define EXEC_LEVEL 4
+
+void DeathSp_800CFDBC(SVECTOR *out, int index)
+{
+    OBJECT *body;
+
+    body = GM_PlayerBody_800ABA20;
+    out->vx = body->objs->objs[index].world.t[0];
+    out->vy = body->objs->objs[index].world.t[1];
+    out->vz = body->objs->objs[index].world.t[2];
+}
+
+void DeathSpAct_800CFE1C(DeathSpWork *work)
+{
+    SVECTOR pos0;
+    SVECTOR pos1;
+    MATRIX  world;
+    SVECTOR rot;
+    SVECTOR pos;
+    SVECTOR speed;
+    VECTOR  scale;
+    int     i;
+    int     index;
+
+    GM_CurrentMap_800AB9B0 = work->map;
+
+    for (i = 0; i < 2; i++)
+    {
+        if (GV_RandU_80017090(2) == 0)
+        {
+            work->fC8[i] = work->fD8[i];
+            work->fA8[i] += work->fB8[i];
+
+            if (work->fA8[i] >= 8)
+            {
+                work->fA8[i] = 6;
+                work->fB8[i] = -work->fB8[i];
+            }
+            else if (work->fA8[i] < 0)
+            {
+                work->fA8[i] = 1;
+                work->fB8[i] = -work->fB8[i];
+            }
+
+            work->fD8[i] = work->fA8[i];
+        }
+
+        DeathSp_800CFDBC(&pos0, work->f28[i][work->fC8[i]]);
+        DeathSp_800CFDBC(&pos1, work->f28[i][work->fD8[i]]);
+
+        NewPlasma_800CD30C(&pos0, &pos1, work->fE8[i], GV_RandU_80017090(64) + 64);
+        work->fE8[i] += 32;
+    }
+
+    if (GV_RandU_80017090(8) == 0)
+    {
+        rot.vx = GV_RandU_80017090(4096);
+        rot.vy = GV_RandU_80017090(4096);
+        rot.vz = 0;
+
+        RotMatrix_gte(&rot, &world);
+
+        index = GV_RandU_80017090(16);
+        world.t[0] = GM_PlayerBody_800ABA20->objs->objs[index].world.t[0];
+        world.t[1] = GM_PlayerBody_800ABA20->objs->objs[index].world.t[1];
+        world.t[2] = GM_PlayerBody_800ABA20->objs->objs[index].world.t[2];
+
+        scale.vx = scale.vy = scale.vz = GV_RandU_80017090(512) + 1024;
+        ScaleMatrix(&world, &scale);
+
+        NewSpark2_800CA714(&world);
+    }
+
+    index = GV_RandU_80017090(16);
+    pos.vx = GM_PlayerBody_800ABA20->objs->objs[index].world.t[0];
+    pos.vy = GM_PlayerBody_800ABA20->objs->objs[index].world.t[1];
+    pos.vz = GM_PlayerBody_800ABA20->objs->objs[index].world.t[2];
+
+    speed.vx = 0;
+    speed.vy = 10;
+    speed.vz = 0;
+
+    AN_Smoke_800CE164(&pos, &speed, 1, 0);
+
+    if (--work->s_time < 0)
+    {
+        work->s_time = GV_RandU_80017090(4) + 5;
+        GM_SeSet2_80032968(0, 63, 180);
+    }
+
+    if (work->time > 0 && --work->time == 0)
+    {
+        GV_DestroyActor_800151C8(&work->actor);
+    }
+}
+
+void DeathSpDie_800D00EC(DeathSpWork *work)
+{
+}
+
+int DeathSpGetResources_800D00F4(DeathSpWork *work, int name, int map)
+{
+    char *opt;
+    int   i;
+
+    work->name = name;
+    work->map = map;
+
+    GM_CurrentMap_800AB9B0 = map;
+
+    work->s_time = 0;
+
+    opt = GCL_GetOption_80020968('t');
+    if (opt != NULL)
+    {
+        work->time = GCL_StrToInt_800209E8(opt);
+    }
+    else
+    {
+        work->time = -1;
+    }
+
+    for (i = 0; i < 4; i++)
+    {
+        work->fA8[i] = 0;
+        work->fB8[i] = 1;
+        work->fC8[i] = 0;
+        work->fD8[i] = 1;
+        work->fE8[i] = GV_RandU_80017090(4096);
+    }
+
+    work->f28[0][0] = 12;
+    work->f28[0][1] = 11;
+    work->f28[0][2] = 10;
+    work->f28[0][3] = 0;
+    work->f28[0][4] = 1;
+    work->f28[0][5] = 2;
+    work->f28[0][6] = 3;
+    work->f28[0][7] = 4;
+
+    work->f28[1][0] = 15;
+    work->f28[1][1] = 14;
+    work->f28[1][2] = 13;
+    work->f28[1][3] = 0;
+    work->f28[1][4] = 1;
+    work->f28[1][5] = 7;
+    work->f28[1][6] = 8;
+    work->f28[1][7] = 9;
+
+    work->f28[2][0] = 12;
+    work->f28[2][1] = 10;
+    work->f28[2][2] = 0;
+    work->f28[2][3] = 1;
+    work->f28[2][4] = 6;
+    work->f28[2][5] = 1;
+    work->f28[2][6] = 0;
+    work->f28[2][7] = 15;
+
+    work->f28[3][0] = 15;
+    work->f28[3][1] = 13;
+    work->f28[3][2] = 0;
+    work->f28[3][3] = 1;
+    work->f28[3][4] = 2;
+    work->f28[3][5] = 7;
+    work->f28[3][6] = 2;
+    work->f28[3][7] = 7;
+
+    return 0;
+}
+
+GV_ACT * NewDeathSp_800D025C(int name, int where)
+{
+    DeathSpWork *work;
+
+    work = (DeathSpWork *)GV_NewActor_800150E4(EXEC_LEVEL, sizeof(DeathSpWork));
+    if (work != NULL)
+    {
+        GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)DeathSpAct_800CFE1C, (TActorFunction)DeathSpDie_800D00EC, "death_sp.c");
+
+        if (DeathSpGetResources_800D00F4(work, name, where) < 0)
+        {
+            GV_DestroyActor_800151C8(&work->actor);
+            return NULL;
+        }
+    }
+
+    return &work->actor;
+}
---
+++ src/overlays/s08a/Takabe/glass.c
@@ -0,0 +1,344 @@
+#include "libgv/libgv.h"
+#include "libdg/libdg.h"
+#include "Game/game.h"
+#include "Takabe/thing.h"
+
+typedef struct _GlassWork
+{
+    GV_ACT   actor;
+    MATRIX   world;
+    DG_PRIM *prim;
+    DG_TEX  *tex;
+    SVECTOR  verts[8];
+    TARGET  *target;
+    SVECTOR  pos;
+    SVECTOR  dir;
+    SVECTOR  size;
+    short    fA4; // front?
+    short    fA6; // back?
+    int      n_prims;
+    int      map;
+    int      fB0;
+    short    has_bounds;
+    HZD_HDL *hzd;
+    HZD_SEG  bounds[2];
+    int      proc;
+} GlassWork;
+
+extern SVECTOR DG_ZeroVector_800AB39C;
+extern int     GM_CurrentMap_800AB9B0;
+
+void s16b_800C4874(int n_segs, HZD_SEG *segs, int n_flrs, HZD_FLR *flrs);
+void s16b_800C49AC(HZD_SEG *seg);
+
+GV_ACT *NewGsplash_800D4000(MATRIX *pos, SVECTOR *arg1, short arg2, short n_prims);
+
+#define EXEC_LEVEL 5
+
+static inline void GlassInitPack(POLY_FT4 *poly, DG_TEX *tex, int size, int which)
+{
+    int x, y, w, h;
+
+    setPolyFT4(poly);
+    setSemiTrans(poly, 1);
+    setRGB0(poly, 128, 128, 128);
+
+    x = tex->field_8_offx;
+    w = tex->field_A_width + 1;
+    poly->u0 = poly->u2 = x + (w * size) / 3;
+    poly->u1 = poly->u3 = x + (w * (size + 1)) / 3 - 1;
+
+    y = tex->field_9_offy;
+    h = tex->field_B_height + 1;
+
+    if (which == 0)
+    {
+        poly->v0 = poly->v1 = y;
+        poly->v2 = poly->v3 = y + h / 2 - 1;
+    }
+    else if (which == 1)
+    {
+        poly->v0 = poly->v1 = y + h / 2;
+        poly->v2 = poly->v3 = y + h - 1;
+    }
+
+    poly->tpage = tex->field_4_tPage;
+    poly->clut = tex->field_6_clut;
+}
+
+void GlassInitPacks_800D2E88(POLY_FT4 *poly, DG_TEX *tex, int size)
+{
+    GlassInitPack(&poly[0], tex, size, 0);
+    GlassInitPack(&poly[1], tex, size, 1);
+}
+
+void GlassAct_800D302C(GlassWork *work)
+{
+    MATRIX  world;
+    SVECTOR sp30;
+    TARGET *target;
+    int     check;
+    int     size;
+
+    GM_CurrentMap_800AB9B0 = work->map;
+
+    target = work->target;
+    if (target->damaged & TARGET_POWER)
+    {
+        target->damaged &= ~TARGET_POWER;
+
+        check = 0;
+        switch (target->a_mode)
+        {
+        case 1:
+            work->fB0--;
+            check = 1;
+            break;
+
+        case 2:
+            work->fB0 = 0;
+            check = 1;
+            break;
+        }
+
+        if (check != 0)
+        {
+            if (work->fB0 <= 0)
+            {
+                GM_SeSet_80032858(&work->pos, 63);
+
+                DG_TransposeMatrix_8001EAD8(&work->world, &world);
+                gte_ApplyMatrixSV(&world, &target->field_2C_vec, &sp30);
+
+                if (sp30.vz > 0)
+                {
+                    NewGsplash_800D4000(&work->world, &work->size, work->fA4, work->n_prims);
+                }
+                else
+                {
+                    world = work->world;
+                    RotMatrixY(2048, &world);
+                    NewGsplash_800D4000(&world, &work->size, work->fA6, work->n_prims);
+                }
+
+                if (work->proc != 0)
+                {
+                    GCL_ExecProc_8001FF2C(work->proc, NULL);
+                }
+
+                s16b_800C4874(2, work->bounds, 0, NULL);
+
+                GV_DestroyActor_800151C8(&work->actor);
+                return;
+            }
+
+            GM_SeSet_80032858(&work->pos, 62);
+
+            size = 3 - work->fB0;
+            GlassInitPacks_800D2E88(&work->prim->packs[0]->poly_ft4, work->tex, size);
+            GlassInitPacks_800D2E88(&work->prim->packs[1]->poly_ft4, work->tex, size);
+        }
+    }
+
+    GM_MoveTarget_8002D500(target, &work->pos);
+    GM_PushTarget_8002DA14(target);
+    target->damaged &= ~TARGET_PUSH;
+}
+
+void GlassDie_800D3270(GlassWork *work)
+{
+    DG_PRIM* prim;
+
+    if (work->has_bounds != 0)
+    {
+        HZD_DequeueDynamicSegment2_8006FE44(work->hzd, &work->bounds[0]);
+        HZD_DequeueDynamicSegment2_8006FE44(work->hzd, &work->bounds[1]);
+    }
+
+    GM_FreeTarget_8002D4B0(work->target);
+
+    prim = work->prim;
+    if (prim != NULL)
+    {
+        DG_DequeuePrim_800182E0(prim);
+        DG_FreePrim_8001BC04(prim);
+    }
+}
+
+void GlassCreateTarget_800D32E4(GlassWork *work, SVECTOR *size)
+{
+    TARGET *target;
+
+    target = GM_AllocTarget_8002D400();
+    work->target = target;
+
+    GM_SetTarget_8002DC74(target, ( TARGET_SEEK | TARGET_POWER ), NO_SIDE, size);
+    GM_Target_8002DCCC(target, 1, -1, 0, 0, &DG_ZeroVector_800AB39C);
+}
+
+static inline void GlassClampSize(SVECTOR *size)
+{
+    size->vx = abs(size->vx);
+    size->vy = abs(size->vy);
+    size->vz = abs(size->vz);
+
+    if (size->vx < 150)
+    {
+        size->vx = 150;
+    }
+
+    if (size->vy < 150)
+    {
+        size->vy = 150;
+    }
+
+    if (size->vz < 150)
+    {
+        size->vz = 150;
+    }
+}
+
+int GlassGetResources_800D335C(GlassWork *work, int name, int map)
+{
+    SVECTOR  size;
+    SVECTOR  max;
+    SVECTOR  min;
+    int      w;
+    int      hzd_flags;
+    SVECTOR *verts;
+    DG_PRIM *prim;
+    DG_TEX  *tex;
+    int      x, y;
+    HZD_SEG *maxb;
+    HZD_SEG *minb;
+
+    GM_CurrentMap_800AB9B0 = map;
+
+    THING_Gcl_GetSVector('p', &work->pos);
+    THING_Gcl_GetSVector('d', &work->dir);
+    THING_Gcl_GetSVector('s', &size);
+
+    work->size = size;
+
+    work->fA4 = -THING_Gcl_GetIntDefault('f', work->pos.vy);
+    work->fA6 = -THING_Gcl_GetIntDefault('b', -work->fA4);
+    work->n_prims = THING_Gcl_GetIntDefault('n', 64);
+    work->proc = THING_Gcl_GetInt('e');
+
+    w = THING_Gcl_GetIntDefault('w', 125);
+    hzd_flags = THING_Gcl_GetIntDefault('z', 180);
+    hzd_flags &= ~0x4;
+
+    verts = work->verts;
+
+    prim = DG_GetPrim(0x12, 2, 0, verts, NULL);
+    work->prim = prim;
+    if (prim == NULL)
+    {
+        return -1;
+    }
+
+    tex = DG_GetTexture_8001D830(GV_StrCode_80016CCC("glass"));
+    work->tex = tex;
+    if (tex == NULL)
+    {
+        return -1;
+    }
+
+    RotMatrix(&work->dir, &work->world);
+    work->world.t[0] = work->pos.vx;
+    work->world.t[1] = work->pos.vy;
+    work->world.t[2] = work->pos.vz;
+
+    DG_SetPos_8001BC44(&work->world);
+    DG_PutPrim_8001BE00(&prim->world);
+
+    GlassInitPacks_800D2E88(&work->prim->packs[0]->poly_ft4, tex, 0);
+    GlassInitPacks_800D2E88(&work->prim->packs[1]->poly_ft4, tex, 0);
+
+    x = work->size.vx;
+    y = work->size.vy;
+
+    verts[0].vx = -x;
+    verts[0].vy = y;
+    verts[0].vz = 0;
+
+    verts[1].vx = x;
+    verts[1].vy = y;
+    verts[1].vz = 0;
+
+    verts[2].vx = -x;
+    verts[2].vy = 0;
+    verts[2].vz = 0;
+
+    verts[3].vx = x;
+    verts[3].vy = 0;
+    verts[3].vz = 0;
+
+    verts[4].vx = -x;
+    verts[4].vy = 0;
+    verts[4].vz = 0;
+
+    verts[5].vx = x;
+    verts[5].vy = 0;
+    verts[5].vz = 0;
+
+    verts[6].vx = -x;
+    verts[6].vy = -y;
+    verts[6].vz = 0;
+
+    verts[7].vx = x;
+    verts[7].vy = -y;
+    verts[7].vz = 0;
+
+    DG_RotVector_8001BE98(&work->size, &size, 1);
+    GlassClampSize(&size);
+    GlassCreateTarget_800D32E4(work, &size);
+
+    maxb = &work->bounds[0];
+    max = work->verts[1];
+    min = work->verts[6];
+    max.vz += w;
+    min.vz += w;
+
+    s16b_800C45C4(maxb, &work->world, &max, &min);
+    s16b_800C49AC(maxb);
+
+    minb = &work->bounds[1];
+    max = work->verts[1];
+    min = work->verts[6];
+    max.vz -= w;
+    min.vz -= w;
+
+    s16b_800C45C4(minb, &work->world, &max, &min);
+    s16b_800C49AC(minb);
+
+    work->hzd = Map_FromId_800314C0(map)->field_8_hzd;
+    HZD_QueueDynamicSegment2_8006FDDC(work->hzd, maxb, hzd_flags);
+    HZD_QueueDynamicSegment2_8006FDDC(work->hzd, minb, hzd_flags);
+    work->has_bounds = 1;
+
+    return 0;
+}
+
+GV_ACT * NewGlass_800D37A4(int name, int where)
+{
+    GlassWork *work;
+
+    work = (GlassWork *)GV_NewActor_800150E4(EXEC_LEVEL, sizeof(GlassWork));
+    if (work != NULL)
+    {
+        GV_SetNamedActor_8001514C(&work->actor, (TActorFunction)GlassAct_800D302C, (TActorFunction)GlassDie_800D3270, "glass.c");
+
+        if (GlassGetResources_800D335C(work, name, where) < 0)
+        {
+            GV_DestroyActor_800151C8(&work->actor);
+            return NULL;
+        }
+
+        work->fB0 = 3;
+        work->map = where;
+    }
+
+    return &work->actor;
+}
