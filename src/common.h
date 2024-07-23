7b24b8b8210ea57f5812536a04640d75456dd04e
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-07-23 04:28:07 -0400

    Rewrite with more relevant info

--- src/common.h
+++ src/common.h
@@ -13,9 +13,13 @@
 // These macros were taken from "GTE Advanced Topics" (slide 18),
 // originally presented at the March 1996 PlayStation Developer's Conference.
 //
-// "spadstk.h" from the SDK's texture address modulation sample
-// (psx/sample/graphics/texaddr/wave) defines slightly modified versions
-// of SetSpadStack/ResetSpadStack that add/sub 24 from $sp instead of 4.
+// According to stack.txt (in Japanese) from the seminar sample code,
+// compiler optimizations should be set to -O1 or higher, otherwise
+// the $fp register may be used, potentially resulting in a crash.
+//
+// In addition, function calls with 5 or more arguments will use the stack
+// for passing the 5th argument onwards. The callee will receive garbage data
+// if these are set before the stack switch.
 
 // clang-format off
 /* scratch pad address 0x1f800000 - 0x1f800400 */
@@ -29,13 +33,13 @@
 }
 
 #define ResetSpadStack() { \
-    __asm__ volatile ("addiu $29,$29,4" :::"$29","memory"); \
-    __asm__ volatile ("lw $29,0($29)"   :::"$29","memory"); \
+    __asm__ volatile ("addiu $29,$29,4":::"$29","memory"); \
+    __asm__ volatile ("lw $29,0($29)"  :::"$29","memory"); \
 }
 
 #define GetStackAddr(addr) { \
-    __asm__ volatile ("move $8,%0"  ::"r"(addr):"$8","memory"); \
-    __asm__ volatile ("sw $29,0($8)"::         :"$8","memory"); \
+    __asm__ volatile ("move $8,%0"     ::"r"(addr):"$8","memory"); \
+    __asm__ volatile ("sw $29,0($8)"   ::         :"$8","memory"); \
 }
 // clang-format on
 
