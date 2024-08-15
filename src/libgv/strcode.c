ea6e41b85b3c3d13b000e3363f3db0565c0b2e40
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-08-15 12:58:12 -0400

    More detailed GV_StrCode documentation

--- src/libgv/strcode.c
+++ src/libgv/strcode.c
@@ -1,10 +1,28 @@
 #include <SYS/TYPES.H>
 
 /**
- * @brief Generate a simple hash code (16-bit) from a string.
+ *  @brief Generate a simple hash from a string.
  *
- * @param string input string
- * @return int hash value
+ *  Given that the digest is only 16 bits wide, this algorithm is highly
+ *  prone to collisions. Check for collisions after precalculating a set
+ *  of hashes intended for use at runtime. (e.g. stage data files)
+ *
+ *  @li Passing an empty string will return 0.
+ *  @li Passing a single-char string will return the value of the char.
+ *  @li Mutations of strings differing in only the last character will
+ *      result in the lowest byte of the hash mirroring that difference.
+ *
+ *  Input/Output Examples:
+ *
+ *  @li GV_StrCode("cb_box11") returns @c 0xADDB
+ *  @li GV_StrCode("cb_box12") returns @c 0xADDC
+ *  @li GV_StrCode("op_kcej_l") returns @c 0x4349
+ *  @li GV_StrCode("op_kcej_r") returns @c 0x434F
+ *
+ *  @param      string      NULL-terminated string.
+ *
+ *  @retval     0           if @p string is empty.
+ *  @retval     non-zero    16-bit hash of @p string.
  */
 int GV_StrCode_80016CCC(const char *string)
 {
