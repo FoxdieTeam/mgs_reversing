0fe0d98e6a100aca40402714c486c30abd06ef59
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-08-22 01:09:55 -0400

    Rename Stream/Jimaku Work structs

--- src/data/bss.c
+++ src/data/bss.c
@@ -172,8 +172,8 @@ DG_OBJS *BSS        StageObjs_800B7890[32]; // 0x80 (128) bytes
 MAP BSS      gMapRecs_800B7910[16]; // 0x140 (320) bytes
 DG_LitVertex BSS    DG_LitVertices_800B7A50[84]; // 0x7E0 (2016) bytes
 HOMING BSS   gHomingTargets_800B8230[HOMING_TARGET_ARRAY_LENGTH];
-Actor_strctrl BSS   strctrl_800B82B0; // 0x40 (64) bytes
-Actor_JimCtrl BSS   jimCtrlActor_800B82F0; // 0x104C (4172) bytes
+StreamCtrlWork BSS  strctrl_800B82B0; // 0x40 (64) bytes
+JimakuCtrlWork BSS  jimCtrlActor_800B82F0; // 0x104C (4172) bytes
 array_800B933C_child BSS array_800B933C[array_800B933C_SIZE]; // 0x1C (28) bytes
 int BSS             dword_800B9358; // 0x4 (4) bytes
 
