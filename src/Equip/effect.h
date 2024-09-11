277867d47d35269c1b6e03d516cd862f77f594ed
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-09-10 22:45:50 -0400

    Style cleanup in Equip/effect.c

--- src/Equip/effect.h
+++ src/Equip/effect.h
@@ -4,9 +4,9 @@
 #include "libdg/libdg.h"
 
 void EQ_ChangeTexture_80060CE4(const char *itemName1, const char *itemName2);
-void EQ_InvisibleHead_80060D5C(OBJECT *pObj, short *pnPacks, short *pRaise);
-void EQ_VisibleHead_80060DF0(OBJECT *pObj, short *pnPacks, short *pRaise);
+void EQ_InvisibleHead_80060D5C(OBJECT *object, short *n_packs, short *raise);
+void EQ_VisibleHead_80060DF0(OBJECT *object, short *n_packs, short *raise);
 int  EQ_VisibleUnit2_80060F20(short *, char *);
-void EQ_InvisibleUnit2_80060E68(DG_OBJS *pObjs, unsigned int color, int arg2);
+void EQ_InvisibleUnit2_80060E68(DG_OBJS *objs, unsigned int color, int arg2);
 
 #endif // _EFFECT_H_
