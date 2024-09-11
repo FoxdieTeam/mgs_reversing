2425fd4dcfc6731c5447a372e895d0e47d306a94
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-09-10 22:47:05 -0400

    Style cleanup in Game/control.c

--- src/Game/control.h
+++ src/Game/control.h
@@ -110,7 +110,7 @@ int  GM_InitControl(CONTROL *control, int scriptData, int scriptBinds);
 void GM_ActControl(CONTROL *control);
 void GM_FreeControl(CONTROL *control);
 void GM_ConfigControlVector(CONTROL *control, SVECTOR *pos, SVECTOR *rot);
-void GM_ConfigControlMatrix(CONTROL *control, MATRIX *pMatrix);
+void GM_ConfigControlMatrix(CONTROL *control, MATRIX *matrix);
 void GM_ConfigControlString(CONTROL *control, char *param_pos, char *param_dir);
 void GM_ConfigControlHazard(CONTROL *control, short height, short f36, short f38);
 void GM_ConfigControlAttribute(CONTROL *control, int radar_atr);
