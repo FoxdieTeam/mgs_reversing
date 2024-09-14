b5b8be8be269b35aa9f633453cc9370cc21e527f
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2024-09-13 22:05:25 -0400

    Move format definitions

---
+++ src/include/fmt_mot.h
@@ -0,0 +1,21 @@
+#ifndef _FMT_MOT_H_
+#define _FMT_MOT_H_
+
+#include <sys/types.h>
+
+typedef unsigned short MOTION_ARCHIVE;
+typedef unsigned short MOTION_TABLE;
+
+typedef struct MOTION_SEGMENT
+{
+    SVECTOR         field_0;
+    SVECTOR         field_8;
+    int             field_10;
+    unsigned short *field_14;
+    short           field_18;
+    short           field_1A;
+    unsigned char   field_1C;
+    char            field_1D[7];
+} MOTION_SEGMENT;
+
+#endif // _FMT_MOT_H_
