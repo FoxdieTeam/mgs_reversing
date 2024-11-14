7fdfca1edca06549cf6eb09dd160f033bdfa1e6d
Voxel <darrensweeneydev@gmail.com>
2024-11-14 01:22:23 +0100

    Reverse Snake18_800CB030

--- src/overlays/d18a/snake18.c
+++ src/overlays/d18a/snake18.c
@@ -37,6 +37,7 @@ extern int d18a_dword_800DAEF0;
 
 extern HZD_FLR *d18a_dword_800DAF00[2];
 extern HZD_FLR *d18a_dword_800DAF10;
+extern int GM_PlayerStatus_800ABA50;
 
 void Snake18_800CABEC(Snake18Work *arg0) 
 {
@@ -240,4 +241,68 @@ int Snake18_800CAF20(int arg0)
     }
     
     return 1;
+}
+
+typedef struct 
+{
+    char pad1[0x6C];
+    short unk6C;
+    char pad2[0x98 - 0x6C - (sizeof(short))];
+    short unk98;
+    char pad3[0x8FE - 0x98 - (sizeof(short))];
+    short unk8FE;
+    char pad4[0x902 - 0x8FE - (sizeof(short))];
+    short unk902;
+} Arg0;
+
+void Snake18_800CB030(Arg0* arg0) 
+{
+    SVECTOR vec;
+    SVECTOR sp18;
+    short temp_s1;
+    int temp_v0;
+    int var_v1;
+    HZD_FLR* temp_a1;   // @todo(Voxel): Var re-names etc.
+
+    temp_s1 = arg0->unk8FE;
+    if (temp_s1 != 2) 
+    {
+        arg0->unk902 = 0;
+        return;
+    }
+    
+    vec.vy = 0;
+    vec.vx = 0;
+    vec.vz = 0x2EE;
+    DG_PutVector(&vec, &vec, 1);
+    
+    temp_a1 = d18a_dword_800DAF00[0];
+    
+    if ((temp_a1 == NULL) || (temp_a1->b1.h == temp_s1)) 
+    {
+        arg0->unk902 = 0;
+        if (!(GM_PlayerStatus_800ABA50 & 1)) 
+        {
+            arg0->unk6C = 0;
+        }
+    } 
+    else 
+    {
+        temp_v0 = HZD_SlopeFloorLevel(&vec, (SVECTOR*)temp_a1) - arg0->unk98;   // @todo(Voxel): Cleanup
+        sp18.vx = temp_v0;
+        sp18.vz = SquareRoot0(0x89544 - (temp_v0 * temp_v0));
+        var_v1 = -GV_VecDir2(&sp18);
+        
+        if (var_v1 < -0x800) 
+        {
+            var_v1 += 0x1000;
+        }
+        
+        arg0->unk902 = var_v1;
+        
+        if (!(GM_PlayerStatus_800ABA50 & 1)) 
+        {
+            arg0->unk6C = var_v1;
+        }
+    }
 }
