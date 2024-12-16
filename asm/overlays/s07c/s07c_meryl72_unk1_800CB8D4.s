1c3862d88568d323ba552d7b62c5d7139f75aefe
Kieron <kieron.josephs00@gmail.com>
2024-12-16 21:02:55 +0000

    Match ml72act.c

--- asm/overlays/s07c/s07c_meryl72_unk1_800CB8D4.s
+++
@@ -1,7 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s07c_meryl72_unk1_800CB8D4
-s07c_meryl72_unk1_800CB8D4:
-	dw 0x03E00008 ; 800CB8D4
-	dw 0x00000000 ; 800CB8D8
--- src/overlays/s07cr/overlay4.c
+++ src/overlays/s07cr/overlay4.c
@@ -1,8 +1,6 @@
 #include "common.h"
 #include "Game/camera.h"
 
-const char s07c_dword_800D48A0[] = "noise dis  =%d,\n";
-
 int SECTION("overlay.bss") s07c_dword_800D5274;
 GM_Camera SECTION("overlay.bss") meryl72_camera_800D5278;
 int SECTION("overlay.bss") s07c_dword_800D52F4;
