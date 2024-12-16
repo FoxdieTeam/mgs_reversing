abb37680d1c01de3d029424d6c0f166fa217b678
Voxel <darrensweeneydev@gmail.com>
2024-12-16 22:42:17 +0100

    Update comment with decomp links. WIP.

--- src/overlays/d18a/snake18.c
+++ src/overlays/d18a/snake18.c
@@ -390,12 +390,19 @@ short Snake18_800CB280(Snake18Work* work, int arg1)
 }
 
 // Todo:        d18a_snake18_800CB2EC   https://decomp.me/scratch/UVAkx (M2C_ERROR)
-//      Done:        d18a_snake18_800CB34C   https://decomp.me/scratch/9w98i
-//      Done:        d18a_snake18_800CB34C   https://decomp.me/scratch/Yq7dc
+//      Done:       d18a_snake18_800CB34C   https://decomp.me/scratch/9w98i
+//      Done:       d18a_snake18_800CB34C   https://decomp.me/scratch/Yq7dc
 // Todo:        d18a_snake18_800CB378   https://decomp.me/scratch/T45AR (M2C_ERROR)
 // Todo:        d18a_snake18_800CB3E8   https://decomp.me/scratch/VuutT 
 // Todo:        d18a_snake18_800CB42C   https://decomp.me/scratch/Menij
 // Todo:        d18a_snake18_800CB470   https://decomp.me/scratch/YLL1m
 // Todo:        d18a_snake18_800CB514   https://decomp.me/scratch/jVQmU 
-//      Done:   d18a_snake18_800CB59C        https://decomp.me/scratch/LA1cM
+//      Done:       d18a_snake18_800CB59C   https://decomp.me/scratch/LA1cM
 // Todo:        d18a_snake18_800CB60C   https://decomp.me/scratch/D94DB
+// Todo:        d18a_snake18_800CB710   https://decomp.me/scratch/7C9mW
+// Todo:        d18a_snake18_800CB760   https://decomp.me/scratch/6jT89
+//      Done:        d18a_snake18_800CB7BC   https://decomp.me/scratch/cNTUX
+// Todo:        d18a_snake18_800CB7D0   https://decomp.me/scratch/EkLlJ
+// Todo:        d18a_snake18_800CB838   https://decomp.me/scratch/awpwA
+// Todo:        d18a_snake18_800CB888   https://decomp.me/scratch/H2TFc
+// Todo:        d18a_snake18_800CB908   https://decomp.me/scratch/Eed1g
