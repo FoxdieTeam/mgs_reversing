3f050241a10ff2d7c6444a4af3b5d350017a0e08
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-01-02 13:17:01 -0500

    Create libfs/file.cnf

--- src/libfs/fscd.c
+++ src/libfs/fscd.c
@@ -6,17 +6,7 @@
 
 extern int FS_DiskNum_800ACBF0;
 
-/* #include "file.cnf" */
-FS_FILE_INFO fs_file_info[] = {
-    { "STAGE.DIR",  0 },    // 0
-    { "RADIO.DAT",  0 },    // 1
-    { "FACE.DAT",   0 },    // 2
-    { "ZMOVIE.STR", 0 },    // 3
-    { "VOX.DAT",    0 },    // 4
-    { "DEMO.DAT",   0 },    // 5
-    { "BRF.DAT",    0 },    // 6
-    { NULL, 0 }
-};
+#include "file.cnf"     // defines fs_file_info
 
 int FS_ResetCdFilePosition(void *pHeap)
 {
