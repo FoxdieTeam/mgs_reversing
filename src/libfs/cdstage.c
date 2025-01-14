0bd5cf1d748b4c9a1731ae57fed653741bee31bb
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-01-14 15:45:20 -0500

    Document libfs/cdstage.c

--- src/libfs/cdstage.c
+++ src/libfs/cdstage.c
@@ -11,21 +11,30 @@
 
 /*---------------------------------------------------------------------------*/
 
-#define FS_DIRNAME_MAX 8
+#define FS_DIRNAME_MAX  8
+#define FS_STAGEDIR_MAX 170     // ((FS_SECTOR_SIZE - 4) / sizeof(FS_DIR_ENTRY))
 
 typedef struct _FS_DIR_ENTRY {
-    char    name[FS_DIRNAME_MAX];
-    int     offset;     // in number of sectors
+    char    name[FS_DIRNAME_MAX];   // ASCII stage name
+    int     offset;                 // in number of sectors
 } FS_DIR_ENTRY;
 
-typedef struct _FS_FILE_TABLE {
-    int             pos;        // LBA position of STAGE.DIR
-    int             size;       // size of the directory table
-    int             count;      // number of stage directories
-    FS_DIR_ENTRY   *dirs;       // in-memory copy of the dir table
-} FS_FILE_TABLE;
+typedef struct _FS_DIR_INFO {
+    int             pos;            // LBA position of stage archive
+    int             table_size;     // size of the directory table
+    int             n_stages;       // number of stage directories
+    FS_DIR_ENTRY   *table_buf;      // in-memory copy of the dir table
+} FS_DIR_INFO;
 
-STATIC FS_FILE_TABLE fs_file_table = {};
+STATIC FS_DIR_INFO fs_dir_info = {};
+
+// NOTE: This code registers a single DIR-format archive and keeps it for
+// the full duration of the game's runtime. This is only cleared at shutdown,
+// since there are no deinitialization routines for LibFS's position tables.
+//
+// Theoretically, if there were more than one DIR-format archives present on
+// the CD-ROM, the game could dynamically select which to register at startup,
+// however, the program is currently hard-coded to load only "STAGE.DIR".
 
 /*---------------------------------------------------------------------------*/
 
@@ -41,17 +50,29 @@ STATIC int FS_CdStageReadCallback(CDBIOS_TASK *task)
         task->size = rounded;
         task->remaining = rounded - 512;
 
-        fs_file_table.size = size;
+        fs_dir_info.table_size = size;
     }
-
     return 1;
 }
 
+/**
+ * @brief       Registers the DIR archive to read from.
+ *
+ * @note        Because the dir entry table is expected to fit within
+ *              the first 2048 bytes (1 sector) of the file, archives
+ *              can contain a maximum of 170 stage entries.
+ *
+ *              Therefore, the the maximum size of the entry table is
+ *              2040 bytes (sizeof(FS_DIR_ENTRY) * FS_STAGEDIR_MAX).
+ *
+ * @param[out]  buffer      temporary work buffer
+ * @param[in]   sector      starting LBA position of the DIR archive
+ */
 void FS_CdStageFileInit(void *buffer, int sector)
 {
-    int size;
+    int table_size;
 
-    fs_file_table.pos = sector;
+    fs_dir_info.pos = sector;
     CDBIOS_ReadRequest(buffer, sector, FS_SECTOR_SIZE, &FS_CdStageReadCallback);
 
     while (CDBIOS_ReadSync() > 0)
@@ -59,31 +80,44 @@ void FS_CdStageFileInit(void *buffer, int sector)
         mts_wait_vbl(1);
     }
 
-    size = fs_file_table.size;
+    table_size = fs_dir_info.table_size;
 
-    if (!fs_file_table.dirs)
+    if (fs_dir_info.table_buf == NULL)
     {
-        fs_file_table.dirs = GV_AllocResidentMemory(size);
+        fs_dir_info.table_buf = GV_AllocResidentMemory(table_size);
     }
 
-    printf("%X %X %d\n", (unsigned int)buffer + 4, (unsigned int)fs_file_table.dirs, size);
-    GV_CopyMemory((char *)buffer + 4, fs_file_table.dirs, size);
+    printf("%X %X %d\n",
+        (unsigned int)buffer + 4,               // src address
+        (unsigned int)fs_dir_info.table_buf,    // dest address
+        table_size);                            // size
 
-    fs_file_table.count = size / sizeof(FS_DIR_ENTRY);
+    GV_CopyMemory((char *)buffer + 4, fs_dir_info.table_buf, table_size);
+
+    fs_dir_info.n_stages = table_size / sizeof(FS_DIR_ENTRY);
 }
 
+/**
+ * Gets the starting offset (logical block address) of a requested
+ * stage block within the registered DIR archive.
+ *
+ * @param       dirname     name of the requested stage
+ *
+ * @return      LBA position of the requested stage block.
+ *              Returns -1 if the stage is not found.
+ */
 int FS_CdGetStageFileTop(char *dirname)
 {
     FS_DIR_ENTRY *dir;
     int count;
 
-    dir = fs_file_table.dirs;
+    dir = fs_dir_info.table_buf;
 
-    for (count = fs_file_table.count; count > 0; count--)
+    for (count = fs_dir_info.n_stages; count > 0; count--)
     {
         if (strncmp(dir->name, dirname, FS_DIRNAME_MAX) == 0)
         {
-            return dir->offset + fs_file_table.pos;
+            return dir->offset + fs_dir_info.pos;
         }
         dir++;
     }
