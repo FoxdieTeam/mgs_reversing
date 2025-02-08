8eecaaf66b789a322895ebb0398784e7100fcb98
Kieron <kieron.josephs00@gmail.com>
2025-02-08 16:51:09 +0000

    Add action.c & wolf2.c for s12a

--- asm/overlays/s12a/s12a_wolf2_800D3D44.s
+++
@@ -1,9 +0,0 @@
-	opt	c+, at+, e+, n-
-	section overlay.text
-
-	xdef s12a_wolf2_800D3D44
-s12a_wolf2_800D3D44:
-	dw 0x8FB10014 ; 800D3D44
-	dw 0x8FB00010 ; 800D3D48
-	dw 0x03E00008 ; 800D3D4C
-	dw 0x27BD0020 ; 800D3D50
