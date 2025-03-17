b557b4b8a016a252fc3c613243ea698914d38c66
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-03-16 21:06:30 -0400

    Rename psxsdk.h to psxdefs.h

--- src/include/psxdefs.h
+++ src/include/psxdefs.h
@@ -1,11 +1,11 @@
 /**
  * Supplemental definitions for PSX SDK headers
  */
-#ifndef _PSXSDK_H_
-#define _PSXSDK_H_
+#ifndef _PSXDEFS_H_
+#define _PSXDEFS_H_
 
 // Due to none of the OpenEvent callbacks using the correct prototype,
 // we now have to cast them to avoid warnings.
 typedef long (*openevent_cb_t)();
 
-#endif // _PSXSDK_H_
+#endif // _PSXDEFS_H_
