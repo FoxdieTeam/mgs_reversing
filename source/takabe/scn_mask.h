a84c35f6393382fec6fa252cfeb3c7e7a1e7ff10
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-29 02:54:28 -0400

    Cleanup takabe/scn_mask.c

--- source/takabe/scn_mask.h
+++ source/takabe/scn_mask.h
@@ -1,8 +1,9 @@
 #ifndef _SCN_MASK_H_
 #define _SCN_MASK_H_
 
-// Thermal goggles or Night vision goggles screen effect
+#define SCREEN_MASK_NV_MODE     0
+#define SCREEN_MASK_IR_MODE     1
 
-void *NewNightVisionScreen(int type);
+void *NewNightVisionScreen(int mode);
 
 #endif // _SCN_MASK_H_
