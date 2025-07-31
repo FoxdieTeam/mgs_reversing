d525dccc752b3bff9de948109bdb4946a3f274cd
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-31 15:01:54 -0400

    misc. cleanups for thing/*

--- source/thing/sight.h
+++ source/thing/sight.h
@@ -5,12 +5,14 @@ void *NewSight(int hashedFileName0, int hashedFileName1,
                short *itemEquippedIndicator, short itemId,
                short *xyOffsetBuffer);
 
-void *sight_init_80071DC8(int hashedFileName, short *xyOffsetBuffer);
+void *NewSight_80071DC8(int hashedFileName, short *xyOffsetBuffer);
 
 void *NewSightFast(int hashedFileName0, int hashedFileName1,
                    short *itemEquippedIndicator, short itemId,
                    short *xyOffsetBuffer);
 
-void *sight_init_80071F98(int hashedFileName, short *xyOffsetBuffer);
+void *NewSight_80071F98(int hashedFileName, short *xyOffsetBuffer);
+
+void sub_80072074(void);
 
 #endif // _SIGHT_H_
