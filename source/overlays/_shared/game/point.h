78e6d5a62e9f6d33c6bf3b1dc3412f288d2818c2
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-31 15:11:02 -0400

    Cleanup game/point.c

--- source/overlays/_shared/game/point.h
+++ source/overlays/_shared/game/point.h
@@ -1,6 +1,6 @@
 #ifndef _POINT_H_
 #define _POINT_H_
 
-void *NewPoint_800C5AB4(int name, int where, int argc, char **argv);
+void *NewPoint(int name, int where, int argc, char **argv);
 
 #endif // _POINT_H_
