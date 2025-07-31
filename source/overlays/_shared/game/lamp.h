a36e9fbc564da182814bac6183bf7e6a879703ec
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-31 15:10:27 -0400

    Cleanup game/lamp.c

--- source/overlays/_shared/game/lamp.h
+++ source/overlays/_shared/game/lamp.h
@@ -1,6 +1,6 @@
 #ifndef _LAMP_H_
 #define _LAMP_H_
 
-void *NewLamp_800C3B34(int name, int where, int argc, char **argv);
+void *NewLamp(int name, int where, int argc, char **argv);
 
 #endif // _LAMP_H_
