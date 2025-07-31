ab0d045b89e5c62f648597bcc33b4a942d80e606
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-07-31 15:11:18 -0400

    Cleanup game/select.c

--- source/overlays/_shared/game/select.h
+++ source/overlays/_shared/game/select.h
@@ -1,6 +1,6 @@
 #ifndef _SELECT_H_
 #define _SELECT_H_
 
-void *NewSelect_800C3434(int name, int where, int argc, char **argv);
+void *NewSelect(int name, int where, int argc, char **argv);
 
 #endif // _SELECT_H_
