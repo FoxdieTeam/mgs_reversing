da1421c6c79f2fb88701e3ebf24dd9953a1927dd
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-01 04:36:42 -0400

    Unsuffix BSS for memcard.c

--- source/data/bss.c
+++ source/data/bss.c
@@ -160,20 +160,20 @@ unsigned int BSS    fs_stream_task_state; // 0x4 (4) bytes
 gap                                     gap_800B52C4[0x4]; // 4 bytes
 
 /* memcard/memcard.obj */
-long BSS            gHardware_end_io_800B52C8; // 0x4 (4) bytes
-long BSS            gHardware_end_write_800B52CC; // 0x4 (4) bytes
-long BSS            gHardware_timeout_800B52D0; // 0x4 (4) bytes
-long BSS            gHardware_new_device_800B52D4; // 0x4 (4) bytes
-long BSS            gSoftware_end_io_800B52D8; // 0x4 (4) bytes
-long BSS            gSoftware_end_write_800B52DC; // 0x4 (4) bytes
-long BSS            gSoftware_timeout_800B52E0; // 0x4 (4) bytes
-long BSS            gSoftware_new_device_800B52E4; // 0x4 (4) bytes
-TMemCardFunc BSS    gHwCard_do_op_800B52E8; // 0x4 (4) bytes
-TMemCardFunc BSS    gSwCard_do_op_800B52EC; // 0x4 (4) bytes
-volatile int BSS    gSwCardLastOp_800B52F0; // 0x4 (4) bytes
-volatile int BSS    gHwCardLastOp_800B52F4; // 0x4 (4) bytes
-MEM_CARD BSS        gMemCards_800B52F8[2]; // 0x350 (848) bytes
-volatile long BSS   gMemCard_io_size_800B5648; // 0x4 (4) bytes
+long BSS            gHardware_end_io; // 0x4 (4) bytes
+long BSS            gHardware_end_write; // 0x4 (4) bytes
+long BSS            gHardware_timeout; // 0x4 (4) bytes
+long BSS            gHardware_new_device; // 0x4 (4) bytes
+long BSS            gSoftware_end_io; // 0x4 (4) bytes
+long BSS            gSoftware_end_write; // 0x4 (4) bytes
+long BSS            gSoftware_timeout; // 0x4 (4) bytes
+long BSS            gSoftware_new_device; // 0x4 (4) bytes
+TMemCardFunc BSS    gHwCard_do_op; // 0x4 (4) bytes
+TMemCardFunc BSS    gSwCard_do_op; // 0x4 (4) bytes
+volatile int BSS    gSwCardLastOp; // 0x4 (4) bytes
+volatile int BSS    gHwCardLastOp; // 0x4 (4) bytes
+MEM_CARD BSS        gMemCards[2]; // 0x350 (848) bytes
+volatile long BSS   gMemCard_io_size; // 0x4 (4) bytes
 
 gap                                     gap_800B564C[0x4]; // 4 bytes
 
