b8d035f21522f428328e18d148310b8866de7c9d
Kieron <kieron.josephs00@gmail.com>
2025-08-06 18:39:56 +0100

    Begin demo.c cleanup

--- source/kojo/demothrd.h
+++ source/kojo/demothrd.h
@@ -7,184 +7,79 @@
 #include "libgv/libgv.h"
 #include "game/game.h"
 
-typedef struct dmo_model_0x14
-{
-    int field_0_type;
-    int field_4_flags;
-    int field_8;
-    int field_C_hashCode;
-    int field_10;
-} dmo_model_0x14;
+#include "fmt_dmo.h"
 
-typedef struct Dmo_Map8
+typedef struct DEMO_M1E1
 {
-    int field_0;
-    int field_8;
-} Dmo_Map8;
-
-typedef struct demothrd_0x1C
-{
-    int field_0_magic;
-    int field_4_chunk_size;
-    int field_8_movie_frames;
-    int field_C_num_maps;
-    int field_10_num_models;
-    Dmo_Map8* field_14_pMaps;
-    dmo_model_0x14 *field_18_pModels;
-} demothrd_0x1C;
-
-
-typedef struct dmo_data_0x34
-{
-    int field_0;
-    int field_4_type;
-    short field_8_x;
-    short field_A_y;
-    short field_C_z;
-    short field_E_x;
-    short field_10_y;
-    short field_12_z;
-    short field_14;
-    short field_16;
-    int field_18;
-    short field_1C;
-    short field_1E;
-    int field_20;
-    char field_24;
-    char field_25;
-    char field_26;
-    char field_27;
-    short field_28;
-    short field_2A;
-    int field_2C;
-    short field_30;
-    short field_32;
-} dmo_data_0x34;
-
-typedef struct dmo_6
-{
-    short field_0;
-    short field_2;
-    short field_4;
-} dmo_6;
-
-typedef struct dmo_data_0x18
-{
-    int field_0_type;
-    short field_4;
-    short field_6_rot_x;
-    short rot_y;
-    short field_A_rot_z;
-    short field_C_pos_x;
-    short field_E_pos_y;
-    short field_10_pos_z;
-    short field_12_total;
-    dmo_6 *field_14_pEndData;
-} dmo_data_0x18;
-
-typedef struct dmo_data_0x28
-{
-    int field_0;
-    int field_4;
-    short field_8_xpos;
-    short field_A_ypos;
-    short field_C_zpos;
-    short field_E_x;
-    short field_10_y;
-    short field_12_z;
-    short field_14_z;
-    short field_16;
-    short field_18_count;
-    short field_1A;
-    dmo_data_0x34 *field_1C_dmo_data_offset;
-    short field_20_count;
-    short field_22;
-    dmo_data_0x18 *field_24_pDmoEnd;
-} dmo_data_0x28;
-
-typedef struct dmo_m1e1_data
-{
-    OBJECT  field_0[2][3];
+    OBJECT  object[2][3];
     int     field_558_idx[2];
     int     field_560;
     SVECTOR field_564[2];
-} dmo_m1e1_data;
+} DEMO_M1E1;
 
-typedef struct dmo_hind
+typedef struct DEMO_HIND
 {
     int field_0;
     int field_4;
     int field_8;
     int field_C;
-} dmo_hind;
+} DEMO_HIND;
 
-
-typedef struct dmo_model_0x1A4
+typedef struct DEMO_MODEL
 {
-    CONTROL        field_0_ctrl;
-    OBJECT         field_7C_obj;
-    MATRIX         field_160_mtx[2];
-    dmo_m1e1_data *field_1A0_pM1OrHind; // union, can be dmo_hind also, depending on the current stage
-} dmo_model_0x1A4;
+    CONTROL    control;
+    OBJECT     object;
+    MATRIX     light[2];
+    DEMO_M1E1 *extra; // union, can be DEMO_HIND also, depending on the current stage
+} DEMO_MODEL;
 
-typedef struct dmo_chain_0x10
+typedef struct ACTNODE
 {
-    int field_0_type;
-    int field_4_type;
-    int field_8_xy;
-    int field_C_zpad;
-} dmo_chain_0x10;
-
-typedef struct DemothrdWork_0x78_Chain
-{
-    struct DemothrdWork_0x78_Chain *field_0_pPrev;
-    struct DemothrdWork_0x78_Chain *field_4_pNext;
-    char field_8_fileNameBuffer[4]; // union, also an int
-    GV_ACT *field_C_actor1;
-    GV_ACT *field_10_actor2;
-    dmo_chain_0x10 field_14[3];
-    int field_44;
-    int field_48;
-    int field_4C;
-    short field_50;
-    short field_52;
-    short field_54;
-    short field_56;
-    MATRIX field_58;
-} DemothrdWork_0x78_Chain;
-
-STATIC_ASSERT(sizeof(DemothrdWork_0x78_Chain) == 0x78,
-             "sizeof(DemothrdWork_0x78_Chain) is wrong!");
+    struct ACTNODE *prev;
+    struct ACTNODE *next;
+    int             used;
+    GV_ACT         *actor1;
+    GV_ACT         *actor2;
+    DMO_CHA         chara;
+    int             field_48;
+    int             field_4C;
+    short           field_50;
+    short           field_52;
+    short           field_54;
+    short           field_56;
+    MATRIX          field_58;
+} ACTNODE;
 
 typedef struct DemothrdWork
 {
-    GV_ACT             actor;
-    int                field_20_flag;
-    int                field_24_ticks;
-    int                field_28_map;
-    int                field_2C_timer_ticks;
-    demothrd_0x1C     *field_30_dmo_header;
-    dmo_model_0x1A4   *field_34_pModels;
-    DemothrdWork_0x78_Chain field_38;
-    int                field_B0;
-    int                field_B4;
-    int                field_B8;
-    int                field_BC;
-    demothrd_0x1C     *field_C0_pHeader;
-    CONTROL            field_C4_ctrl;
-    OBJECT             field_140_obj;
-    MATRIX             field_224_light_mtx[2];
-    int                field_264;
-    int                field_268;
-    int                field_26C;
-    DG_CHANLFUNC       field_270_pOldRenderFn;
-    int                field_274_old_game_state_flags;
-    GM_CAMERA          field_278;
-    int                field_2F4_old_equipped_item;
-    int                field_2F8_old_equipped_weapon;
+    GV_ACT       actor;
+    int          flag;
+    int          start_time;
+    int          map;
+    int          frame;
+    DMO_DEF     *header;
+    DEMO_MODEL  *models;
+    ACTNODE      chain;
+    int          field_B0; /* unused */
+    int          field_B4; /* unused */
+    int          field_B8; /* unused */
+    int          field_BC; /* unused */
+    DMO_DEF     *stream;
+    CONTROL      control;
+    OBJECT       object;
+    MATRIX       light[2];
+    int          field_264; /* unused */
+    int          field_268; /* unused */
+    int          field_26C; /* unused */
+    DG_CHANLFUNC old_screen;
+    int          old_game_status;
+    GM_CAMERA    old_camera;
+    int          old_item;
+    int          old_weapon;
 } DemothrdWork;
 
-typedef struct dmo_data_0x36
+/* This should be replaced by loads of individual structs */
+typedef struct DMO_DATA_0x36
 {
     int field_0;
     int field_4_type;
@@ -675,13 +570,9 @@ typedef struct dmo_data_0x36
             int   field_20;
         } variant_0x4A;
     } data;
-} dmo_data_0x36;
+} DMO_DATA_0x36;
 
-int  DM_ThreadStream_80079460(int flag, int unused);
-int  DM_ThreadFile_800794E4(int param_1, int param_2);
-void demothrd_cd_act_80079664(DemothrdWork *work);
-void demothrd_cd_stream_die_800797CC(DemothrdWork *work);
-int  DestroyDemo_8007A66C(DemothrdWork *work);
-void demothrd_remove_via_id_8007CD60(DemothrdWork *pThis, int id_to_remove);
+int DM_ThreadStream_80079460(int flag, int unused);
+int DM_ThreadFile_800794E4(int param_1, int param_2);
 
 #endif // _DEMOTHRD_H_
