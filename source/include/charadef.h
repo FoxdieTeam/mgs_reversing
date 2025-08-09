6cc04e05c110a82a6c83c0bf24da0044580df886
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-08-09 01:28:59 -0400

    Update charadef.h
    NEWCHARA is now defined with an empty parameter list like the comment above says. The old definition with args is left commented-out below.

--- source/include/charadef.h
+++ source/include/charadef.h
@@ -10,11 +10,10 @@
 // XXX NOTE: The empty parameter list is widely regarded as one of old C's
 // XXX anti-features. C++ and C23 have changed its meaning to (void).
 
-//typedef void *(*NEWCHARA)();
-typedef void *(*NEWCHARA)(int name, int where, int argc, char **argv);
+typedef void *(*NEWCHARA)();
+//typedef void *(*NEWCHARA)(int name, int where, int argc, char **argv);
 
-typedef struct CHARA
-{
+typedef struct {
     unsigned short  class_id;   // strcode of chara's GCL name
     NEWCHARA        func;       // pointer to actor creation function
 } CHARA;
