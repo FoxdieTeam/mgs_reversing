2040b10f901303bcb324a301502b847a5a5ceee7
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-09-19 16:41:06 -0400

    Cleanup chara/snake/breath.c

--- source/overlays/_shared/chara/snake/breath.h
+++ source/overlays/_shared/chara/snake/breath.h
@@ -1,6 +1,6 @@
 #ifndef __MGS_CHARA_BREATH_H__
 #define __MGS_CHARA_BREATH_H__
 
-void *NewBreath_800C3A1C(int name, int where, int argc, char **argv);
+void *NewBreath(int name, int where, int argc, char **argv);
 
 #endif // __MGS_CHARA_BREATH_H__
