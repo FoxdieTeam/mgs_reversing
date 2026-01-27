c2b30854046b3e708e11df6edf7dddce810c159f
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-01-27 13:52:36 -0500

    add change.c chara name, rename func/macros

--- source/overlays/_shared/onoda/change/change.h
+++ source/overlays/_shared/onoda/change/change.h
@@ -1,6 +1,6 @@
 #ifndef __MGS_ONODA_CHANGE_H__
 #define __MGS_ONODA_CHANGE_H__
 
-void *NewChange( int name, int where, int argc, char **argv );
+void *NewCdChange( int name, int where, int argc, char **argv );
 
 #endif // __MGS_ONODA_CHANGE_H__
