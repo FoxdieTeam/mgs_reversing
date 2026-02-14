01d34b5dd3e4fb9bedff5e99f4b2f6e47bfbc4fc
junioratcheeseyfries2 <jjwilliamsjordan@gmail.com>
2026-02-14 14:57:21 -0600

    fix: remove INCLUDE_ASM macros for implemented functions

--- source/overlays/camera/unknown/camera.c
+++ source/overlays/camera/unknown/camera.c
@@ -893,7 +893,35 @@ void camera_800C85D8(void)
     setClut(sprt, 976, 511);
 }
 
-#pragma INCLUDE_ASM("asm/overlays/camera/camera_800C864C.s")
+void camera_800C864C(int *arg0)
+{
+    int var_v0;
+    int *var_v1;
+
+    var_v0 = 3;
+    var_v1 = (int *)((char *)arg0 + 0xC);
+    do {
+        *(int *)((char *)var_v1 + 0x654) = 0;
+        var_v0--;
+        var_v1--;
+    } while (var_v0 >= 0);
+
+    var_v0 = 8;
+    var_v1 = (int *)((char *)arg0 + 0x20);
+    do {
+        *(int *)((char *)var_v1 + 0x664) = 0;
+        var_v0--;
+        var_v1--;
+    } while (var_v0 >= 0);
+
+    var_v0 = 0x19;
+    arg0 = (int *)((char *)arg0 + 0x64);
+    do {
+        *(int *)((char *)arg0 + 0x688) = 0;
+        var_v0--;
+        arg0--;
+    } while (var_v0 >= 0);
+}
 void camera_800C869C(int *arg0)
 {
     int var_v0;
