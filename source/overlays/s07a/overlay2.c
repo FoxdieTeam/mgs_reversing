1dd03dfd36d8bf5e1e1d3cf4fdeea4686934cf56
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-04-07 15:46:23 -0400

    Update overlay2.c

--- source/overlays/s07a/overlay2.c
+++ source/overlays/s07a/overlay2.c
@@ -77,10 +77,8 @@ const char s07a_aErrerrerrsettimeover_800E2E40[] = "Err Err Err  Set time Over\n
 const char s07a_aErrerrerrsetdirover_800E2E5C[] = "Err Err Err  Set Dir Over\n";
 const char s07a_aErrerrerrsoundbuffover_800E2E78[] = "Err Err Err Sound Buff Over !!\n";
 
-// "メイン"
-const char s07a_dword_800E2E98[] = {0xa5, 0xe1, 0xa5, 0xa4, 0xa5, 0xf3, 0x0, 0x0};
-// "女子トイレ"
-const char s07a_dword_800E2EA0[] = {0xbd, 0xf7, 0xbb, 0xd2, 0xa5, 0xc8, 0xa5, 0xa4, 0xa5, 0xec, 0x0, 0x0};
+const char s07a_dword_800E2E98[] = "メイン";
+const char s07a_dword_800E2EA0[] = "女子トイレ";
 
 const char s07a_aErrnotenoughwork_800E2EAC[] = "Err not enough work !!\n";
 const char s07a_aMerylcactionpointerr_800E2EC4[] = "meryl.c : action point Err\n";
