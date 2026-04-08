363994a996bc1e800f71c831aba14ebc01e59bed
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-04-08 00:36:57 -0400

    Update emitter.c

--- source/thing/emitter.c
+++ source/thing/emitter.c
@@ -31,7 +31,7 @@ static void ShadePacks( POLY_FT4 *packs, int n_packs, DG_TEX *unused, char shade
     while ( --n_packs >= 0 ) {
         setPolyFT4( packs );
         setSemiTrans( packs, 1 );
-        setRGB0(packs, shade, shade, shade);
+        setRGB0( packs, shade, shade, shade );
         DG_SetPacketTexture( packs, tex ) ;
         packs->tpage = tex->tpage;
         packs->clut = tex->clut;
