95eef097df428339278ba07177a8a437ac327e3d
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-04-15 04:35:45 -0400

    Fix missing semicolon

--- source/takabe/dymc_flr.h
+++ source/takabe/dymc_flr.h
@@ -1,6 +1,6 @@
 #ifndef __MGS_TAKABE_DYMC_FLR_H__
 #define __MGS_TAKABE_DYMC_FLR_H__
 
-void *NewDynamicFloorSet(int name, int where, int argc, char **argv)
+void *NewDynamicFloorSet(int name, int where, int argc, char **argv);
 
 #endif // __MGS_TAKABE_DYMC_FLR_H__
