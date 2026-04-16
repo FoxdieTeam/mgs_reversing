9817d71a5e5f05187f827a37d847f4826a66f602
Kieron <kieron.josephs00@gmail.com>
2026-04-16 21:58:28 +0100

    Matches for jdrumsmk.c & jdrumfir.c

--- source/overlays/s19b/takabe/jeep/jdrum.c
+++ source/overlays/s19b/takabe/jeep/jdrum.c
@@ -23,36 +23,30 @@ typedef struct _Work
 } Work;
 
 #define EXEC_LEVEL  5
-#define BODY_MODEL  GV_StrCode(s19b_aBoa_800DDC58)
-#define BODY_MODEL2 GV_StrCode(s19b_aBob_800DDC50)
+#define BODY_MODEL  GV_StrCode("19b_o4a")
+#define BODY_MODEL2 GV_StrCode("19b_o4b")
 #define BODY_FLAG   ( DG_FLAG_TEXT | DG_FLAG_TRANS | DG_FLAG_SHADE | DG_FLAG_GBOUND | DG_FLAG_ONEPIECE )
 #define TARGET_FLAG ( TARGET_POWER | TARGET_SEEK )
 
 extern BLAST_DATA blast_data_8009F4B8[8];
 
-extern SVECTOR s19b_dword_800C3494;
-extern VECTOR  s19b_dword_800DDC40;
+SVECTOR s19b_dword_800C3494 = {435, 375, 185};
+
 extern SVECTOR s19b_dword_800DE670;
 extern SVECTOR s19b_dword_800DE690;
 
-extern const char s19b_aBob_800DDC50[];    // = "19b_o4b"
-extern const char s19b_aBoa_800DDC58[];    // = "19b_o4a"
-extern const char s19b_aJdrumc_800DDC60[]; // = "jdrum.c"
-
 void *NewJeepBlast(MATRIX *world, BLAST_DATA *blast);
-void *s19b_jdrumsmk_800C9BB8(MATRIX *world, int, int);
-void *s19b_jdrumfir_800CA344(MATRIX *world, int, int);
+void *NewJeepDrumSmoke(MATRIX *world, int, int);
+void *NewJeepDrumFire(MATRIX *world, int, int);
 
 void s19b_jdrum_800C90BC(Work *work)
 {
-    VECTOR   scale;
+    VECTOR   scale = {2048, 2048, 2048};
     MATRIX   world;
     CONTROL *control;
     int      changed;
     TARGET  *target;
     OBJECT  *body;
-
-    scale = s19b_dword_800DDC40;
 
     work->field_1F4 = FALSE;
 
@@ -126,10 +120,10 @@ void s19b_jdrum_800C90BC(Work *work)
     {
         world = work->body.objs->world;
         NewJeepBlast(&work->body.objs->world, blast_data_8009F4B8);
-        s19b_jdrumsmk_800C9BB8(&work->body.objs->world, 0, 150);
+        NewJeepDrumSmoke(&work->body.objs->world, 0, 150);
 
         world.t[1] -= work->height;
-        s19b_jdrumfir_800CA344(&world, 0, 30);
+        NewJeepDrumFire(&world, 0, 30);
     }
 
     GM_MoveTarget(target, &work->control.mov);
@@ -214,7 +208,7 @@ void *NewJeepDrum(int name, int where)
     work = GV_NewActor(EXEC_LEVEL, sizeof(Work));
     if (work != NULL)
     {
-        GV_SetNamedActor(work, s19b_jdrum_800C90BC, s19b_jdrum_800C9374, s19b_aJdrumc_800DDC60);
+        GV_SetNamedActor(work, s19b_jdrum_800C90BC, s19b_jdrum_800C9374, "jdrum.c");
 
         if (s19b_jdrum_800C9438(work, name, where) < 0)
         {
--- source/overlays/s19b/takabe/jeep/jdrumsmk.c
+++ source/overlays/s19b/takabe/jeep/jdrumsmk.c
@@ -1,6 +1,252 @@
-#include "common.h"
+#include "strcode.h"
+#include "game/game.h"
+#include "takabe/prim.h"
 
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jdrumsmk_800C99A0.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jdrumsmk_800C9AEC.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jdrumsmk_800C9B28.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jdrumsmk_800C9BB8.s")
+typedef struct _JEEP_SYSTEM
+{
+    char     pad1[0x4];
+    CONTROL *control;
+    char     pad2[0x10];
+    SVECTOR  pos;
+    char     pad3[0x40];
+    OBJECT  *body;
+    char     pad4[0x1C];
+    MATRIX   world;
+} JEEP_SYSTEM;
+
+extern JEEP_SYSTEM s19b_dword_800DE658; // Takabe_JeepSystem
+
+#define EXEC_LEVEL 5
+
+typedef struct _Work
+{
+    GV_ACT   actor;
+    int      map;
+    DG_PRIM *prim;
+    DG_TEX  *tex;
+    MATRIX   world;
+    MATRIX  *root;
+    int      unk50;
+    int      unk54;
+    int      unk58;
+    int      unk5C;
+    int      unk60;
+    int      unk64;
+    SVECTOR  vertices[16];
+    SVECTOR  speeds[16];
+    DVECTOR  flags[16];
+} Work;
+
+void s19b_jdrumfir_800C9D18(Work *work);
+void s19b_jdrumfir_800C9DDC(Work *work, int);
+
+void s19b_jdrumsmk_800C99A0(Work *work)
+{
+    GM_CurrentMap = work->map;
+
+    work->prim->world.t[0] += s19b_dword_800DE658.pos.vx;
+    work->prim->world.t[1] += s19b_dword_800DE658.pos.vy;
+    work->prim->world.t[2] += s19b_dword_800DE658.pos.vz;
+
+    if (work->prim->world.t[2] > 32000)
+    {
+        GV_DestroyActor(work);
+        return;
+    }
+
+    if (work->unk50 & 1)
+    {
+        work->world = *work->root;
+    }
+
+    if (work->unk54 > 0)
+    {
+        s19b_jdrumfir_800C9DDC(work, 1);
+        work->unk54--;
+        work->unk5C++;
+        return;
+    }
+
+    s19b_jdrumfir_800C9DDC(work, 1);
+
+    if (work->unk58 == 0)
+    {
+        GV_DestroyActor(work);
+        return;
+    }
+
+    work->unk58--;
+    work->unk54--;
+    work->unk5C++;
+}
+
+void s19b_jdrumsmk_800C9AEC(Work *work)
+{
+    GM_FreePrim(work->prim);
+}
+
+int s19b_jdrumsmk_800C9B28(Work *work, MATRIX *world, int arg2, int arg3)
+{
+    work->map = GM_CurrentMap;
+
+    work->root = world;
+    work->world = *world;
+    work->unk50 = arg2;
+    work->unk54 = arg3;
+
+    s19b_jdrumfir_800C9D18(work);
+
+    work->unk58 = 80;
+    work->unk5C = 20;
+    return 0;
+}
+
+void *NewJeepDrumSmoke(MATRIX *world, int arg1, int arg2)
+{
+    Work *work;
+
+    work = GV_NewActor(EXEC_LEVEL, sizeof(Work));
+    if (work != NULL)
+    {
+        GV_SetNamedActor(work, s19b_jdrumsmk_800C99A0, s19b_jdrumsmk_800C9AEC, "jdrumsmk.c");
+
+        if (s19b_jdrumsmk_800C9B28(work, world, arg1, arg2) < 0)
+        {
+            GV_DestroyActor(work);
+            return NULL;
+        }
+    }
+
+    return work;
+}
+
+void s19b_jdrumfir_800C9C5C(POLY_FT4 *packs, DG_TEX *tex, int n_packs)
+{
+    int x, y, w, h;
+
+    while (--n_packs != -1)
+    {
+        setPolyFT4(packs);
+        setSemiTrans(packs, 1);
+
+        x = tex->off_x;
+        w = tex->w;
+        packs->u0 = packs->u2 = x;
+        packs->u1 = packs->u3 = x + (w + 1) / 4 - 1;
+
+        y = tex->off_y;
+        h = tex->h;
+        packs->v0 = packs->v1 = y;
+        packs->v2 = packs->v3 = y + (h + 1) / 4 - 1;
+
+        packs->tpage = tex->tpage;
+        packs->clut = tex->clut;
+
+        packs->tpage = (packs->tpage & ~0x60) | (2 << 5);
+
+        setRGB0(packs, 0, 0, 0);
+        packs++;
+    }
+}
+
+void s19b_jdrumfir_800C9D18(Work *work)
+{
+    DG_TEX  *tex;
+    DG_PRIM *prim;
+
+    work->tex = tex = DG_GetTexture(PCX_SMOKE);
+    work->prim = prim = Takabe_MakeIndividualRect3DPrim(16, work->vertices);
+
+    s19b_jdrumfir_800C9C5C(prim->packs[0], tex, 16);
+    s19b_jdrumfir_800C9C5C(prim->packs[1], tex, 16);
+
+    prim->world = DG_ZeroMatrix;
+    prim->raise = 250;
+}
+
+void s19b_jdrumfir_800C9DDC(Work *work, int arg1)
+{
+    DVECTOR  *flag;
+    SVECTOR  *vert;
+    POLY_FT4 *packs;
+    SVECTOR  *speed;
+    int       i;
+    int       xs, ys;
+    int       x, y, w, h;
+    int       shade;
+
+    flag = work->flags;
+    vert = work->vertices;
+    packs = work->prim->packs[GV_Clock];
+    speed = work->speeds;
+
+    if (arg1 != 0)
+    {
+        if (work->unk60 < 0)
+        {
+            work->flags[work->unk64].vx = MIN(work->unk5C, work->unk58);
+            work->flags[work->unk64].vy = GV_RandU(16);
+
+            work->vertices[work->unk64].vx = work->world.t[0];
+            work->vertices[work->unk64].vy = work->world.t[1];
+            work->vertices[work->unk64].vz = work->world.t[2];
+            work->vertices[work->unk64].pad = 375;
+
+            work->speeds[work->unk64].vx = GV_RandS(4);
+            work->speeds[work->unk64].vy = GV_RandS(4) + 15;
+            work->speeds[work->unk64].vz = GV_RandS(4);
+
+            work->unk60 = 12;
+            work->unk64 = (work->unk64 + 1) & 15;
+        }
+        else
+        {
+            work->unk60--;
+        }
+    }
+
+    for (i = 16; i > 0; i--)
+    {
+        if (flag->vx != 0)
+        {
+            vert->pad += 5;
+
+            vert->vx += speed->vx;
+            vert->vy += speed->vy;
+            vert->vz += speed->vz;
+
+            xs = flag->vy & 7;
+            ys = flag->vy / 8;
+
+
+            x = work->tex->off_x;
+            w = work->tex->w + 1;
+            packs->u0 = packs->u2 = x + (w * xs) / 8;
+            packs->u1 = packs->u3 = x + (w * (xs + 1)) / 8 - 1;
+
+            y = work->tex->off_y;
+            h = work->tex->h + 1;
+            packs->v0 = packs->v1 = y + (h * ys) / 4;
+            packs->v2 = packs->v3 = y + (h * (ys + 1)) / 4 - 1;
+
+            if (++flag->vy >= 30)
+            {
+                flag->vy = 0;
+            }
+
+            shade = MIN(flag->vx * 2, 255);
+            setRGB0(packs, shade, shade, shade);
+            flag->vx--;
+        }
+        else
+        {
+            vert->pad = 0;
+            setRGB0(packs, 0, 0, 0);
+        }
+
+        vert++;
+        speed++;
+        flag++;
+        packs++;
+    }
+}
