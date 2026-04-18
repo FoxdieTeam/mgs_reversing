9d6092928950bb8ecf5af71df13a07efd13b5d56
Kieron <kieron.josephs00@gmail.com>
2026-04-18 23:16:14 +0100

    Finish jeep_emy.c & jspark.c

--- source/overlays/s19b/takabe/jeep_emy/jeep_emy.c
+++ source/overlays/s19b/takabe/jeep_emy/jeep_emy.c
@@ -19,7 +19,10 @@ typedef struct _JEEP_SYSTEM
     MATRIX   world;
 } JEEP_SYSTEM;
 
-extern JEEP_SYSTEM s19b_dword_800DE658; // Takabe_JeepSystem
+extern JEEP_SYSTEM s19b_dword_800DE658; /* Takabe_JeepSystem */
+
+struct _Work;
+typedef void (*JBULLET_FN)(struct _Work *, int);
 
 typedef struct _Work
 {
@@ -38,26 +41,47 @@ typedef struct _Work
     TARGET        *target1;
     TARGET        *target2;
     HOMING        *hom;
-    char           pad1[0xC];
+    int            field_8D4;
+    char           pad1[0x8];
     int            field_8E0;
     SVECTOR        field_8E4;
     SVECTOR        field_8EC;
     SVECTOR        field_8F4;
     int            field_8FC;
     SVECTOR        field_900[4];
-    void          *field_920;
+    JBULLET_FN     field_920;
     char           pad2[0x4];
     int            field_928;
-    char           pad3[0x8];
+    char           pad3[0x4];
+    int            field_930;
     int            field_934;
-    char           pad4[0x4];
+    int            field_938;
     int            field_93C;
-    char           pad5[0x4];
+    int            field_940;
     int            field_944;
     int            field_948;
     int            field_94C;
-    char           pad6[0x44];
+    int            field_950;
+    int            field_954;
+    char           pad4[0xC];
+    int            field_964;
+    int            field_968;
+    char           pad5[0x28];
 } Work;
+
+/* unused */
+u_short s19b_dword_800C32BC[] = {0x3201, 0x0143};
+
+static short s19b_dword_800C32C0[] =
+{
+    19, 10, 22, 9, 8, 27, 3, 7, 38, 24, 23, 17, 18, 43, 22
+};
+
+static SVECTOR s19b_dword_800C32E0 = {0, 0, 100};
+static SVECTOR s19b_dword_800C32E8 = {-1024, 0, 0};
+static SVECTOR s19b_dword_800C32F0 = {200, 500, 200};
+static SVECTOR s19b_dword_800C32F8 = {0, 0, 0};
+static SVECTOR s19b_dword_800C3300 = {5, -500, 80};
 
 extern GM_CAMERA GM_Camera;
 
@@ -66,13 +90,25 @@ extern SVECTOR s19b_dword_800DE690;
 
 void s01a_800E2364(MATRIX *mtx, SVECTOR *in, VECTOR *out);
 void *NewJeepBlood(MATRIX *world, int count, MATRIX *root);
+void *NewJeepBullet2(MATRIX *world, int side, int mode, int mode2);
+void Voicesys_800CE2D0(void);
+void AN_Unknown_800CA1EC(MATRIX *, int);
 
-void s19b_jbullet_800C5560(void);
-void s19b_jbullet_800C5654(void);
-void s19b_jbullet_800C5714(void);
-void s19b_jbullet_800C58CC(void);
-void s19b_jbullet_800C598C(void);
-void s19b_jbullet_800C5F7C(void *);
+/* BUG: bad declaration, should have int arg1 */
+void s19b_jblood_800C8128(MATRIX *world);
+void s19b_jblood_800C7FB8(MATRIX *world);
+
+void s19b_jbullet_800C5560(Work *work, int);
+void s19b_jbullet_800C5654(Work *work, int);
+void s19b_jbullet_800C5714(Work *work, int);
+void s19b_jbullet_800C57E8(Work *work, int arg1);
+void s19b_jbullet_800C58CC(Work *work, int);
+void s19b_jbullet_800C598C(Work *work, int);
+void s19b_jbullet_800C5C50(Work *work, int);
+void s19b_jbullet_800C5D18(Work *work, int);
+void s19b_jbullet_800C5E40(Work *work, int);
+void s19b_jbullet_800C5F7C(Work *work);
+void s19b_jbullet_800C66BC(Work *work);
 
 #define EXEC_LEVEL 5
 
@@ -290,20 +326,6 @@ void *NewJeepEnemy(int name, int where)
     return work;
 }
 
-/* TODO: Fix */
-int s19b_dword_800C32BC = 0x01433201;
-int s19b_dword_800C32C0 = 0x000A0013;
-int s19b_dword_800C32C4 = 0x00090016;
-int s19b_dword_800C32C8 = 0x001B0008;
-int s19b_dword_800C32CC = 0x00070003;
-int s19b_dword_800C32D0 = 0x00180026;
-int s19b_dword_800C32D4 = 0x00110017;
-int s19b_dword_800C32D8 = 0x002B0012;
-int s19b_dword_800C32DC = 0x00000016;
-
-static SVECTOR s19b_dword_800C32E0 = {0, 0, 100};
-static SVECTOR s19b_dword_800C32E8 = {-1024, 0, 0};
-
 void s19b_jbullet_800C4F54(Work *work, int unit, int count)
 {
     MATRIX  matrix;
@@ -419,9 +441,6 @@ int s19b_jbullet_800C5258(Work *work)
     return s19b_jbullet_800C5010(work) != 0;
 }
 
-static SVECTOR s19b_dword_800C32F0 = {200, 500, 200};
-static SVECTOR s19b_dword_800C32F8 = {0, 0, 0};
-
 void s19b_jbullet_800C5278(Work *work)
 {
     TARGET *trg;
@@ -479,28 +498,462 @@ int s19b_jbullet_800C5348(Work *work)
     return 0;
 }
 
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C53C0.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C543C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C54D0.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5560.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5654.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5714.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C57E8.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C58CC.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C598C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5C50.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5D18.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5E40.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5E60.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5F2C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5F7C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C5FB0.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C63DC.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C6404.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C6428.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C645C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C649C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C6524.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C65A8.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C666C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jbullet_800C66BC.s")
+void s19b_jbullet_800C53C0(Work *work)
+{
+    int height;
+    int ang;
+
+    height = GM_PlayerBody->objs->objs[6].world.t[1] - work->body.objs->objs[6].world.t[1];
+    ang = (ratan2(work->field_968, height) & 0xFFF) - 1024;
+
+    work->adjust[2].vx = ang;
+    work->adjust[6].vx = ang;
+
+    if (ang < 0)
+    {
+        work->adjust[7].vx = ang * 3;
+    }
+    else
+    {
+        work->adjust[7].vx = (ang * 3) / 2;
+    }
+}
+
+void s19b_jbullet_800C543C(Work *work)
+{
+    MATRIX  world;
+    MATRIX *head;
+
+    head = &work->body.objs->objs[4].world;
+
+    DG_SetPos(head);
+    DG_MovePos(&s19b_dword_800C3300);
+    ReadRotMatrix(&world);
+    RotMatrixY(GV_RandU(16), &world);
+
+    NewJeepBullet2(&world, ENEMY_SIDE, 1, 0);
+    GM_SeSet(&work->control.mov, SE_GUNCAM_SHOT);
+
+    s19b_jblood_800C8128(head);
+    s19b_jblood_800C7FB8(&world);
+}
+
+void s19b_jbullet_800C54D0(Work *work, int arg1)
+{
+    CONTROL *control;
+
+    control = &work->control;
+
+    if (arg1 == 0)
+    {
+        work->field_930 = 0;
+        GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[0], 0, 4);
+    }
+
+    if (!s19b_jbullet_800C5258(work) && !s19b_jbullet_800C5348(work))
+    {
+        control->turn.vy = work->field_940;
+        work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+    }
+}
+
+void s19b_jbullet_800C5560(Work *work, int arg1)
+{
+    CONTROL *control;
+
+    control = &work->control;
+
+    if (arg1 == 0)
+    {
+        work->field_930 = 4;
+        GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[4], 0, 4);
+        AN_Unknown_800CA1EC(&work->body.objs->objs[6].world, 0);
+
+        GM_SeSet2(0, 63, SE_ALERT_SIREN);
+
+        if (s19b_dword_800DE658.field_7C == 0)
+        {
+            Voicesys_800CE2D0();
+        }
+
+        s19b_dword_800DE658.field_7C = 1;
+    }
+
+    work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+
+    if (!s19b_jbullet_800C5258(work))
+    {
+        control->turn.vy = work->field_940;
+
+        if (work->body.is_end)
+        {
+            work->field_920 = s19b_jbullet_800C54D0;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+    }
+}
+
+void s19b_jbullet_800C5654(Work *work, int arg1)
+{
+    CONTROL *control;
+    int      turn;
+
+    control = &work->control;
+    turn = work->field_940;
+
+    if (arg1 == 0)
+    {
+        work->field_930 = 1;
+        GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[1], 0, 4);
+    }
+
+    work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+
+    if (!s19b_jbullet_800C5258(work))
+    {
+        if (!(work->field_93C & 4))
+        {
+            if (!s19b_jbullet_800C5348(work))
+            {
+                work->field_920 = s19b_jbullet_800C54D0;
+                work->field_928 = 0;
+                work->control.turn.vz = 0;
+                work->control.turn.vx = 0;
+            }
+        }
+        else
+        {
+            control->turn.vy = turn;
+        }
+    }
+}
+
+void s19b_jbullet_800C5714(Work *work, int arg1)
+{
+    int temp_s2;
+
+    temp_s2 = work->field_93C;
+
+    if (arg1 == 0)
+    {
+        work->field_930 = 2;
+        GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[2], 0, 4);
+        GM_ConfigMotionAdjust(&work->body, work->adjust);
+    }
+
+    s19b_jbullet_800C53C0(work);
+    work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+
+    if (!s19b_jbullet_800C5258(work))
+    {
+        if (!(temp_s2 & 1))
+        {
+            work->field_920 = s19b_jbullet_800C54D0;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+        else if (temp_s2 & 2)
+        {
+            work->field_920 = s19b_jbullet_800C57E8;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+        else
+        {
+            work->control.turn.vy = work->field_964;
+        }
+    }
+}
+
+void s19b_jbullet_800C57E8(Work *work, int arg1)
+{
+    if (arg1 == 0)
+    {
+        work->field_930 = 2;
+        GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[2], 0, 4);
+        GM_ConfigMotionAdjust(&work->body, work->adjust);
+    }
+
+    if ((arg1 == 5) || (arg1 == 8) || (arg1 == 11))
+    {
+        s19b_jbullet_800C543C(work);
+    }
+
+    work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+
+    if (!s19b_jbullet_800C5258(work))
+    {
+        s19b_jbullet_800C53C0(work);
+        work->control.turn.vy = work->field_964;
+
+        if (arg1 > 18)
+        {
+            work->field_920 = s19b_jbullet_800C5714;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+    }
+}
+
+void s19b_jbullet_800C58CC(Work *work, int arg1)
+{
+    if (arg1 > 8)
+    {
+        work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+    }
+
+    if (!s19b_jbullet_800C5258(work))
+    {
+        if (arg1 == 0)
+        {
+            work->field_930 = 5;
+            GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[5], 0, 4);
+            GM_SeSet(&work->control.mov, SE_ENEMY_DAMAGE);
+            s19b_jbullet_800C4F54(work, 5, 1);
+        }
+
+        if (work->body.is_end)
+        {
+            work->field_920 = s19b_jbullet_800C54D0;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+    }
+}
+
+void s19b_jbullet_800C598C(Work *work, int arg1)
+{
+    work->control.step_size = 500;
+    work->control.step = work->target1->scale;
+
+    if (arg1 == 0)
+    {
+        switch (work->field_934)
+        {
+        case 0:
+            work->field_930 = 6;
+            GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[6], 0, 4);
+
+            if (work->target1->life <= 0)
+            {
+                s19b_jbullet_800C4F54(work, 6, 1);
+            }
+            else
+            {
+                s19b_jbullet_800C4F54(work, 5, 0);
+            }
+
+            work->field_938 = 46;
+            break;
+        case 2:
+            work->field_930 = 7;
+            GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[7], 0, 4);
+            s19b_jbullet_800C4F54(work, 5, 0);
+            work->field_938 = 14;
+            break;
+        case 1:
+            work->field_930 = 8;
+            GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[8], 0, 4);
+            s19b_jbullet_800C4F54(work, 5, 0);
+            work->field_938 = 22;
+            break;
+        }
+
+        GM_SeSet(&work->control.mov, SE_ENEMY_DAMAGE);
+        GM_TotalEnemiesKilled++;
+    }
+
+    switch (work->field_934)
+    {
+    case 0:
+        if (arg1 == 24)
+        {
+            GM_SeSet(&work->control.mov, SE_ENEMY_COLLAPSE);
+        }
+        break;
+    case 2:
+        if (arg1 == 17)
+        {
+            work->field_8E0 = 0;
+        }
+        break;
+    case 1:
+        if (arg1 == 37)
+        {
+            work->field_8E0 = 0;
+        }
+        break;
+    }
+
+    if ((arg1 > 16) && (work->control.level_flag != 0))
+    {
+        work->control.step = DG_ZeroVector;
+    }
+
+    if (arg1 == work->field_938)
+    {
+        GM_SeSet(&work->control.mov, SE_HIT_FLOOR);
+    }
+
+    if (work->body.is_end)
+    {
+        work->control.step_size = -1;
+        work->target1->scale = DG_ZeroVector;
+
+        if (work->target1->life <= 0)
+        {
+            work->field_920 = s19b_jbullet_800C5D18;
+        }
+        else
+        {
+            work->field_920 = s19b_jbullet_800C5C50;
+        }
+
+        work->field_928 = 0;
+        work->control.turn.vz = 0;
+        work->control.turn.vx = 0;
+    }
+}
+
+void s19b_jbullet_800C5C50(Work *work, int arg1)
+{
+    work->target1->class |= ( TARGET_POWER | TARGET_SEEK );
+
+    if (s19b_jbullet_800C5258(work) == 0)
+    {
+        if (arg1 == 0)
+        {
+            switch (work->field_934)
+            {
+            case 2:
+                work->field_930 = 9;
+                GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[9], 0, 4);
+                break;
+            case 1:
+                work->field_930 = 10;
+                GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[10], 0, 4);
+                break;
+            }
+        }
+
+        if (work->body.is_end)
+        {
+            work->field_920 = s19b_jbullet_800C54D0;
+            work->field_928 = 0;
+            work->control.turn.vz = 0;
+            work->control.turn.vx = 0;
+        }
+    }
+}
+
+void s19b_jbullet_800C5D18(Work *work, int arg1)
+{
+    work->hom->flag = 0;
+
+    if (arg1 == 0)
+    {
+        if (work->field_934 < 2)
+        {
+            if (work->field_934 == 0)
+            {
+                work->field_930 = 13;
+                GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[13], 0, 4);
+
+            }
+            else
+            {
+                work->field_930 = 11;
+                GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[11], 0, 4);
+            }
+        }
+        else
+        {
+            work->field_930 = 12;
+            GM_ConfigObjectAction(&work->body, s19b_dword_800C32C0[12], 0, 4);
+        }
+
+        DG_InvisibleObjs(work->weapon.objs);
+        GM_ConfigControlAttribute(&work->control, 0);
+        work->target1->class = TARGET_AVAIL;
+    }
+
+    if (arg1 & 0x2)
+    {
+        DG_InvisibleObjs(work->body.objs);
+    }
+    else
+    {
+        DG_VisibleObjs(work->body.objs);
+    }
+
+    if (arg1 > 32)
+    {
+        work->field_920 = s19b_jbullet_800C5E40;
+        work->field_928 = 0;
+        work->control.turn.vz = 0;
+        work->control.turn.vx = 0;
+    }
+}
+
+void s19b_jbullet_800C5E40(Work *work, int arg1)
+{
+    GV_DestroyActor(work);
+}
+
+void s19b_jbullet_800C5E60(Work *work)
+{
+    CONTROL   *control;
+    OBJECT    *body;
+    JBULLET_FN callback;
+    int        count;
+
+    control = &work->control;
+    body = &work->body;
+
+    work->target1->class = TARGET_AVAIL;
+
+    callback = work->field_920;
+    count = work->field_928++;
+
+    if (callback == NULL)
+    {
+        s19b_jbullet_800C5278(work);
+        callback = s19b_jbullet_800C54D0;
+        work->field_920 = callback;
+    }
+
+    callback(work, count);
+    control->height = body->height;
+
+    if ((work->field_8D4 < 0) && (control->level_flag != 0))
+    {
+        work->field_8D4 = 0;
+    }
+
+    work->field_8D4 -= 16;
+    control->step.vy = work->field_8D4;
+}
+
+void s19b_jbullet_800C5F2C(Work *work)
+{
+    SVECTOR diff;
+
+    GV_SubVec3(&GM_PlayerPosition, &work->control.mov, &diff);
+    diff.vy = 0;
+
+    work->field_964 = GV_VecDir2(&diff);
+    work->field_968 = GV_VecLen3(&diff);
+}
+
+void s19b_jbullet_800C5F7C(Work *work)
+{
+    s19b_jbullet_800C5F2C(work);
+    s19b_jbullet_800C66BC(work);
+    s19b_jbullet_800C5E60(work);
+}
