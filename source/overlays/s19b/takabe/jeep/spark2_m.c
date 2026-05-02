51825d14e8f3728b98cf338edf7f5ed7e66dd2f1
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-05-02 08:51:19 +0200

    Match s19b_spark2_m_800DA46C

--- source/overlays/s19b/takabe/jeep/spark2_m.c
+++ source/overlays/s19b/takabe/jeep/spark2_m.c
@@ -6,7 +6,27 @@ typedef struct _Spark2MWork
     GV_ACT   actor;
     int      map;
     DG_PRIM *prim;
+    MATRIX   world;
+    SVECTOR  vecs[16];
+    SVECTOR  verts[32];
+    char     pad2[0x28];
+    int      f1F0;
 } Spark2MWork;
+
+typedef struct _JEEP_SYSTEM_S
+{
+    char     pad1[0x4];
+    CONTROL *control;
+    char     pad2[0x10];
+    SVECTOR  pos;
+} JEEP_SYSTEM_S;
+
+extern JEEP_SYSTEM_S Takabe_JeepSystem;
+
+extern void s19b_spark2_m_800DA19C(SVECTOR *, SVECTOR *, int);
+extern void s19b_spark2_m_800DA314(void *, SVECTOR *, int);
+extern void s19b_spark2_m_800DA3EC(LINE_F2 *, int, int);
+extern void s19b_spark2_m_800DA41C(LINE_F2 *, int, int);
 
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800D8724.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800D87A4.s")
@@ -55,7 +75,40 @@ typedef struct _Spark2MWork
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800DA314.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800DA3EC.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800DA41C.s")
-#pragma INCLUDE_ASM("asm/overlays/s19b/s19b_spark2_m_800DA46C.s")
+void s19b_spark2_m_800DA46C(Spark2MWork *work)
+{
+    int      time;
+    DG_PRIM *prim;
+    int      shade;
+
+    GM_CurrentMap = work->map;
+
+    time = --work->f1F0;
+    if (time <= 0)
+    {
+        GV_DestroyActor(&work->actor);
+        return;
+    }
+
+    s19b_spark2_m_800DA314(work->vecs, work->verts, 16);
+
+    prim = work->prim;
+
+    shade = (time * 16) + 50;
+    if (shade > 255)
+    {
+        shade = 255;
+    }
+
+    s19b_spark2_m_800DA41C((LINE_F2 *)prim->packs[GV_Clock], 16, shade);
+
+    work->world.t[0] += Takabe_JeepSystem.pos.vx;
+    work->world.t[1] += Takabe_JeepSystem.pos.vy;
+    work->world.t[2] += Takabe_JeepSystem.pos.vz;
+
+    DG_SetPos(&work->world);
+    DG_PutPrim(&work->prim->world);
+}
 void s19b_spark2_m_800DA55C(Spark2MWork *work)
 {
     GM_FreePrim(work->prim);
