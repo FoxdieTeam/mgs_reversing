15f06f860c9904aa297500b3f4fe5d27e1b7715d
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-05-04 20:45:50 +0200

    Match s11d_hind_800C97F0

--- source/overlays/s11d/chara/hind/hind.c
+++ source/overlays/s11d/chara/hind/hind.c
@@ -7,11 +7,24 @@ typedef struct _HindWork
 {
     GV_ACT  actor;       // 0x00
     CONTROL control;     // 0x20
+    char    pad_978[0x978 - 0x20 - sizeof(CONTROL)];
+    short   f978;        // 0x978
+    short   f97A;        // 0x97A
 } HindWork;
 
 #pragma INCLUDE_ASM("asm/overlays/s11d/s11d_hind_800C976C.s")
 #pragma INCLUDE_ASM("asm/overlays/s11d/s11d_hind_800C97B8.s")
-#pragma INCLUDE_ASM("asm/overlays/s11d/s11d_hind_800C97F0.s")
+void s11d_hind_800C97F0(HindWork *work)
+{
+    if (work->f978 > 0)
+    {
+        work->f978--;
+        if (work->f978 == 0)
+        {
+            GM_SeSetMode(&work->control.mov, work->f97A, GM_SEMODE_BOMB);
+        }
+    }
+}
 
 void s11d_hind_800C9838(HindWork *work)
 {
