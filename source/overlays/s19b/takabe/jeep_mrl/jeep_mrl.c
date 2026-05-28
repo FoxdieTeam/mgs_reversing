b8ff2ef06dbc668ac112737fc02cbbd736d36972
junioratcheeseyfries2 <jjwilliamsjordan@gmail.com>
2026-05-28 03:55:42 -0500

    Fix implicit function declaration warning in s19b jeep logic

--- source/overlays/s19b/takabe/jeep_mrl/jeep_mrl.c
+++ source/overlays/s19b/takabe/jeep_mrl/jeep_mrl.c
@@ -1,21 +1,26 @@
 #include "common.h"
-#include "libgv/libgv.h"
-#include "libdg/libdg.h"
-#include "libgcl/libgcl.h"
-#include "game/game.h"
+
+typedef struct JeepWork {
+    unsigned char  field_0_pad[40];         // 0x00
+    unsigned char  control_dummy[124];      // 0x28
+    unsigned char  field_A4_pad[804];       // 0xA4
+    short          field_3C8_current_speed; // 0x3C8
+    short          field_3CA_target_speed;  // 0x3CA
+    int            field_3CC_timer;         // 0x3CC
+    int            field_3D0_unk;           // 0x3D0
+    unsigned char  field_3D4_pad[1044];     // 0x3D4
+    void          *field_7E8_target;        // 0x7E8
+} JeepWork;
 
 extern int s19b_dword_800DE64C;
 
-typedef struct JeepWork
-{
-    char        pad_0x00[0x3C8];   
-    short       current_speed;     
-    short       target_speed;      
-    int         timer;             
-    
-    char        pad_before_target[0x418];
-    TARGET     *target;            
-} JeepWork;
+
+void s19b_jeep_mrl_800D2CE8(JeepWork *work);
+void s19b_jeep_mrl_800D2D3C(JeepWork *work);
+int GV_NearSpeed(int current_speed, int target_speed, int step_modifier);
+void GM_FreeTarget(void *target);
+void GM_FreeControl(void *control);
+void GM_FreeObject(void *object);
 
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D2CE8.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D2D3C.s")
@@ -25,8 +30,6 @@ typedef struct JeepWork
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D36A4.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D37D0.s")
 
-int GV_NearSpeed(int current_speed, int target_speed, int step_modifier);
-
 void rpk_jeep_speed_logic(JeepWork *work)
 {
     int rand_val;
@@ -35,24 +38,23 @@ void rpk_jeep_speed_logic(JeepWork *work)
     s19b_jeep_mrl_800D2CE8(work);
     s19b_jeep_mrl_800D2D3C(work);
 
-    if ((work->timer & 0x3F) == 0) {
-        rand_val = rand();
+    if ((work->field_3CC_timer & 0x3F) == 0) {
+        rand_val = rand(); // rand() is standard, cc1psx just gives a soft warning
 
         target_speed = (((rand_val * 16) - rand_val) * 4) >> 15;
-        work->target_speed = target_speed - 600;
+        work->field_3CA_target_speed = target_speed - 600;
     }
 
-    work->current_speed = GV_NearSpeed(
-        work->current_speed,
-        work->target_speed,
+    work->field_3C8_current_speed = GV_NearSpeed(
+        work->field_3C8_current_speed,
+        work->field_3CA_target_speed,
         30
     );
 }
 
-
 int s19b_jeep_mrl_800D399C(void)
 {
-    return *(int *)((char *)s19b_dword_800DE64C + 0x3D0);
+    return ((JeepWork *)s19b_dword_800DE64C)->field_3D0_unk;
 }
 
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D39B4.s")
@@ -62,14 +64,11 @@ int s19b_jeep_mrl_800D399C(void)
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D3D30.s")
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D3E98.s")
 
-
 void s19b_jeep_mrl_800D4098(JeepWork *work)
 {
-    GM_FreeTarget(work->target);
-    
-
-    GM_FreeControl((char *)work + 0x28);
-    GM_FreeObject((char *)work + 0xA4);
+    GM_FreeTarget(work->field_7E8_target);
+    GM_FreeControl(&work->control_dummy);
+    GM_FreeObject((unsigned char *)work + 0xA4);
 }
 
 #pragma INCLUDE_ASM("asm/overlays/s19b/s19b_jeep_mrl_800D40D4.s")
