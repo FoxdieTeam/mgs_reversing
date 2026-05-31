c23170cb8e5f12c3b5b032b73fdecb2bf96a0e7c
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-01 00:42:34 +0200

    Match s08b_p_sphere_800D8F20 and s08b_p_sphere_800D9574

--- source/overlays/s08b/okajima/p_sphere.c
+++ source/overlays/s08b/okajima/p_sphere.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "game/game.h"
 
 int s08b_dword_800C3614 = 0x00000000;
 int s08b_dword_800C3618 = 0x00000000;
@@ -7,9 +8,29 @@ int s08b_dword_800C361C = 0x00000000;
 const char s08b_aNjaball_800E40B8[] = "nja_ball";
 const char s08b_aBallhlf_800E40C4[] = "ballhlf";
 
-#pragma INCLUDE_ASM("asm/overlays/s08b/s08b_p_sphere_800D8F20.s")
+void s08b_p_sphere_800D8F20(void *unused, MATRIX **m, int scale)
+{
+    VECTOR v;
+
+    (void)unused;
+    v.vx = scale;
+    v.vy = scale;
+    v.vz = scale;
+    ScaleMatrix(*m, &v);
+}
 #pragma INCLUDE_ASM("asm/overlays/s08b/s08b_p_sphere_800D8F50.s")
-#pragma INCLUDE_ASM("asm/overlays/s08b/s08b_p_sphere_800D9574.s")
+void s08b_p_sphere_800D9574(void *work)
+{
+    int off;
+    int i = 0;
+
+    for (off = 0x20; i < 2; off += 0xE4)
+    {
+        GM_FreeObject((OBJECT *)((char *)work + off));
+        i++;
+    }
+    s08b_dword_800C361C = 0;
+}
 #pragma INCLUDE_ASM("asm/overlays/s08b/s08b_p_sphere_800D95CC.s")
 
 const char s08b_aPspherec_800E40CC[] = "p_sphere.c";
