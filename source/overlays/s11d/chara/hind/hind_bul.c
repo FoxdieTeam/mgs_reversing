c1011d89272dca23eab7dddf2d2d805b0251dcbf
Otacon <mak1jk@proton.me>
2026-06-08 13:10:02 +0200

    s11d_hind_bul_800CB794, s11d_hind_bul_800CB938 (#2290)
    * decomp: hind_bul matches
    
    ---------
    
    Co-authored-by: otacon <mak1jk@oroton.me>

--- source/overlays/s11d/chara/hind/hind_bul.c
+++ source/overlays/s11d/chara/hind/hind_bul.c
@@ -2,19 +2,48 @@
 #include "libgv/libgv.h"
 #include "game/game.h"
 #include "game/control.h"
+#include "okajima/spark.h"
 
 typedef struct _HindBulWork
 {
     GV_ACT  actor;       // 0x00
     CONTROL control;     // 0x20
-    char    pad_a0[0xE8 - 0x20 - sizeof(CONTROL)];
+    TARGET  target;      // 0x9C
+    int     field_E4;
     int     field_E8;
-    char    pad_ec[0xF8 - 0xE8 - sizeof(int)];
+    int     field_EC;
+    SVECTOR field_F0;
     DG_PRIM *prim;
     char    pad_fc[0x17C - 0xF8 - sizeof(DG_PRIM *)];
 } HindBulWork;
 
-#pragma INCLUDE_ASM("asm/overlays/s11d/s11d_hind_bul_800CB794.s")
+int s11d_hind_bul_800CB794(HindBulWork *work)
+{
+    SVECTOR pos;
+    SVECTOR out;
+    SVECTOR diff;
+    int     hit;
+
+    if (work->field_E8 == 0)
+    {
+        return 0;
+    }
+
+    pos = work->control.mov;
+    GV_SubVec3(&work->field_F0, &pos, &diff);
+    GV_LenVec3(&diff, &out, GV_VecLen3(&diff), 2000);
+    GV_AddVec3(&pos, &out, &out);
+
+    hit = GM_Target_8002E1B8(&pos, &out, work->field_EC, &out, ENEMY_SIDE);
+    if (hit)
+    {
+        GM_MoveTarget(&work->target, &out);
+        GM_PowerTarget(&work->target);
+    }
+
+    return hit;
+}
+
 extern SVECTOR DG_ZeroVector;
 
 void s11d_hind_bul_800CB888(HindBulWork *work)
@@ -34,7 +63,48 @@ void s11d_hind_bul_800CB888(HindBulWork *work)
     work->prim->world.t[1] = work->control.mov.vy;
     work->prim->world.t[2] = work->control.mov.vz;
 }
-#pragma INCLUDE_ASM("asm/overlays/s11d/s11d_hind_bul_800CB938.s")
+void s11d_hind_bul_800CB938(HindBulWork *work)
+{
+    MATRIX  mat;
+    SVECTOR rot;
+    VECTOR  scale;
+
+    GM_ActControl(&work->control);
+
+    work->field_E4 -= 2000;
+    work->control.mov.vy = (unsigned short)work->control.mov.vy + (unsigned short)work->control.step.vy;
+
+    s11d_hind_bul_800CB888(work);
+
+    if (s11d_hind_bul_800CB794(work) != 0)
+    {
+        GV_DestroyActor(work);
+        return;
+    }
+
+    if (work->field_E4 <= 0)
+    {
+        rot.vx = -1024;
+        rot.vz = 0;
+        rot.vy = 0;
+        RotMatrixYXZ(&rot, &mat);
+
+        mat.t[0] = work->field_F0.vx;
+        mat.t[1] = work->field_F0.vy;
+        mat.t[2] = work->field_F0.vz;
+
+        GM_SeSet(&work->field_F0, 0xB5);
+
+        scale.vz = 0x2000;
+        scale.vy = 0x2000;
+        scale.vx = 0x2000;
+        ScaleMatrix(&mat, &scale);
+        NewSpark(&mat, 1);
+
+        GV_DestroyActor(work);
+    }
+}
+
 void s11d_hind_bul_800CBA14(HindBulWork *work)
 {
     GM_FreeControl(&work->control);
