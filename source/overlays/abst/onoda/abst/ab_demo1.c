bd2a7166fb75b8ff9deed197d102f5efb91a0370
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-13 21:59:57 +0200

    Match abst_800C9C30

--- source/overlays/abst/onoda/abst/ab_demo1.c
+++ source/overlays/abst/onoda/abst/ab_demo1.c
@@ -81,6 +81,20 @@ extern Unknown2 abst_dword_800C37B8[];
 extern AbstRes abst_dword_800C3820[];
 extern const char abst_dword_800CE7E4[];
 extern const char abst_dword_800CE800[];
+extern const char abst_dword_800CE818[];
+extern const char abst_dword_800CE830[];
+extern const char abst_dword_800CE83C[];
+extern const char abst_dword_800CE848[];
+extern const char abst_dword_800CE854[];
+extern const char abst_dword_800CE860[];
+extern const char abst_dword_800CE868[];
+extern const char abst_dword_800CE870[];
+extern const char abst_dword_800CE878[];
+extern const char abst_dword_800CE880[];
+extern const char abst_dword_800CE888[];
+extern const char abst_dword_800CE890[];
+extern const char abst_dword_800CE898[];
+extern const char abst_dword_800CE8A0[];
 
 void abst_800C7AB0(Work *work, int index)
 {
@@ -364,8 +378,252 @@ void abst_800C9AAC(void *work, int name, POLY_FT4 *poly, int x0, int y0, int x1,
     }
 }
 
-#pragma INCLUDE_ASM("asm/overlays/abst/abst_800C9C30.s")
-int abst_800C9C30(Work *work, int where);
+int abst_800C9C30(Work *work, int where)
+{
+    char     *opt;
+    POLY_FT4 *poly;
+    DG_PRIM  *prim;
+    int       i;
+
+    GM_CurrentMap = where;
+
+    work->font_x = 704;
+    work->font_y = 256;
+    work->clut_x = 704;
+    work->clut_y = 276;
+
+    prim = DG_MakePrim(DG_PRIM_SORTONLY | DG_PRIM_POLY_FT4, 6, 0, 0, 0);
+    if (prim)
+    {
+        DG_QueuePrim(prim);
+        DG_GroupPrim(prim, GM_CurrentMap);
+    }
+    work->prim1 = prim;
+
+    prim = DG_MakePrim(DG_PRIM_SORTONLY | DG_PRIM_POLY_FT4, 9, 0, 0, 0);
+    if (prim)
+    {
+        DG_QueuePrim(prim);
+        DG_GroupPrim(prim, GM_CurrentMap);
+    }
+    work->prim2 = prim;
+
+    poly = work->polys1;
+    i = 0;
+
+    opt = GCL_GetOption('l');
+    if (opt)
+    {
+        abst_800C9954(work, GCL_StrToInt(opt));
+        abst_800C9AAC(work, GCL_StrToInt(opt), poly, -160, -82, 0, 82, 0, 0);
+    }
+    else
+    {
+        printf((char *)abst_dword_800CE818);
+    }
+
+    poly++;
+    work->attrs1[i] = 0;
+    i++;
+
+    opt = GCL_GetOption('r');
+    if (opt)
+    {
+        abst_800C9954(work, GCL_StrToInt(opt));
+        abst_800C9AAC(work, GCL_StrToInt(opt), poly, 0, -82, 160, 82, 0, 0);
+    }
+    else
+    {
+        printf((char *)abst_dword_800CE818);
+    }
+
+    poly++;
+    work->attrs1[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE830), poly, -160, -112, 0, -82, 0, 0);
+
+    poly++;
+    work->attrs1[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE83C), poly, 0, -112, 160, -82, 0, 0);
+
+    poly++;
+    work->attrs1[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE848), poly, -160, 82, 0, 112, 1, 0);
+
+    poly++;
+    work->attrs1[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE854), poly, 0, 82, 160, 112, 1, 0);
+
+    work->attrs1[i] = 0;
+
+    poly = work->polys2;
+    i = 0;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE860), poly, -33, 81, -25, 89, 0, 0);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE868), poly, 25, 81, 33, 89, 0, 0);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE870), poly, -33, 97, -25, 105, 0, 0);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE878), poly, 25, 97, 33, 105, 0, 0);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE880), poly, -25, 81, 25, 89, 0, 2);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE888), poly, -25, 97, 25, 105, 0, 2);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE890), poly, -33, 89, -25, 97, 0, 1);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE898), poly, 25, 89, 33, 97, 0, 1);
+    setRGB0(poly, 255, 255, 255);
+
+    poly++;
+    work->attrs2[i] = 0;
+    i++;
+
+    abst_800C9AAC(work, GV_StrCode((char *)abst_dword_800CE8A0), poly, -25, 89, 25, 97, 0, 3);
+    setRGB0(poly, 255, 255, 255);
+
+    work->attrs2[i] = 0;
+
+    if (GCL_GetOption('e'))
+    {
+        work->end_proc = GCL_StrToInt(GCL_NextStr());
+    }
+    else
+    {
+        work->end_proc = -1;
+    }
+
+    if (GCL_GetOption('d'))
+    {
+        work->field_24 = GCL_StrToInt(GCL_NextStr());
+    }
+    else
+    {
+        work->field_24 = -1;
+    }
+
+    if (GCL_GetOption('i'))
+    {
+        work->field_2C8 = GCL_StrToInt(GCL_NextStr());
+    }
+    else
+    {
+        work->field_2C8 = 12;
+    }
+
+    for (i = 0; i < work->field_2C8 + 1; i++)
+    {
+        work->field_520[i].string = GCL_GetString(GCL_NextStr());
+        work->field_520[i].num = 0;
+        abst_800C7AB0(work, i);
+    }
+
+    for (i = 0; i < work->field_2C8 + 1; i++)
+    {
+        abst_800C7C0C(work, i);
+    }
+
+    work->pad = &GV_PadData[2];
+    work->field_1D60 = 0;
+    work->field_1D68 = 0;
+    work->field_1D6C = 0;
+
+    for (i = 0; i < 1; i++)
+    {
+        abst_800C85FC(work, i, 0x6739);
+    }
+
+    for (i = 1; i < work->field_2C8 + 1; i++)
+    {
+        abst_800C85FC(work, i, 0);
+    }
+
+    work->primbuf.buf[0] = work->field_1DA4;
+    work->primbuf.buf[1] = work->field_3DA4;
+    sub_80048124();
+
+    work->field_5DA8 = 0;
+    work->field_1D68 = 1;
+
+    for (i = 0; i < 6; i++)
+    {
+        work->attrs1[i] = 0x100;
+    }
+
+    work->field_1D60 = 0;
+
+    {
+        POLY_FT4 *p = work->polys1;
+        setRGB0(&p[2], 86, 137, 116);
+        setRGB0(&p[3], 86, 137, 116);
+        setRGB0(&p[4], 86, 137, 116);
+        setRGB0(&p[5], 86, 137, 116);
+    }
+
+    for (i = 0; i < 1; i++)
+    {
+        abst_800C85FC(work, i, 0);
+    }
+
+    for (i = 0; i < 9; i++)
+    {
+        work->attrs2[i] = 0x200;
+    }
+
+    abst_800C877C(work, 41, 87, 78, 12, 255, 1);
+
+    poly = work->polys2;
+    for (i = 0; i < 9; i++)
+    {
+        setRGB0(poly, 0, 0, 0);
+        poly++;
+    }
+
+    return 0;
+}
 
 void *NewAbstractDemo1(int name, int where)
 {
