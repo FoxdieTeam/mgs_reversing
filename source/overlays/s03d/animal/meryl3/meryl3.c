2e5e5ef44c2fc2c76468178910ebfd63dc08664c
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-14 18:04:34 +0200

    s03d/meryl3: name the TARGET field 'target'

--- source/overlays/s03d/animal/meryl3/meryl3.c
+++ source/overlays/s03d/animal/meryl3/meryl3.c
@@ -9,7 +9,7 @@ typedef struct _Work
     char            pad_180[0x7DC - 0x180];
     OBJECT          field_7DC;      /* 0x7DC */
     char            pad_8C0[0x8CC - 0x8C0];
-    TARGET         *field_8CC;      /* 0x8CC */
+    TARGET         *target;      /* 0x8CC */
     char            pad_8D0[0x9B0 - 0x8D0];
     void           *field_9B0;      /* 0x9B0 */
     void           *field_9B4;      /* 0x9B4 */
@@ -23,7 +23,7 @@ extern int s03d_dword_800C3968;
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800CB640.s")
 void Meryl3_800CB834(Work *work)
 {
-    TARGET *t = work->field_8CC;
+    TARGET *t = work->target;
 
     GM_SetTarget(t, 0x14, 1, (SVECTOR *)&s03d_dword_800C3960);
     GM_SetPowerTarget(t, 1, -1, 0xEA60, 0xEA60, (SVECTOR *)&s03d_dword_800C3968);
@@ -36,7 +36,7 @@ void Meryl3_800CB894(Work *work)
     GM_FreeControl(&work->control);
     GM_FreeObject(&work->field_9C);
     GM_FreeObject(&work->field_7DC);
-    GM_FreeTarget(work->field_8CC);
+    GM_FreeTarget(work->target);
 }
 
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800CB8F0.s")
