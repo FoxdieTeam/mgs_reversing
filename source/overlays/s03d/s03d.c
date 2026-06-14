f734a76f72b22bce00acf67a6978c98baedf8da8
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-14 18:55:34 +0200

    Match s03d_800D3390 and s03d_800D34AC

--- source/overlays/s03d/s03d.c
+++ source/overlays/s03d/s03d.c
@@ -3,19 +3,26 @@
 
 typedef struct _Work
 {
-    char            pad_0[0xAC8];
+    char            pad_0[0xB6];
+    short           field_B6;       /* 0x0B6 */
+    char            pad_B8[0xAC8 - 0xB8];
     short           field_AC8;      /* 0xAC8 */
     short           field_ACA;      /* 0xACA */
     short           field_ACC;      /* 0xACC */
     char            pad_ACE[0xAD0 - 0xACE];
     int             field_AD0;      /* 0xAD0 */
-    char            pad_AD4[0xADC - 0xAD4];
+    char            pad_AD4[0xAD8 - 0xAD4];
+    int             field_AD8;      /* 0xAD8 */
     int             field_ADC;      /* 0xADC */
-    char            pad_AE0[0xB48 - 0xAE0];
+    char            pad_AE0[0xAE6 - 0xAE0];
+    short           field_AE6;      /* 0xAE6 */
+    char            pad_AE8[0xB48 - 0xAE8];
     SVECTOR         field_B48;      /* 0xB48 */
     char            pad_B50[0xB5C - 0xB50];
     int             field_B5C;      /* 0xB5C */
     int             field_B60;      /* 0xB60 */
+    char            pad_B64[0xB94 - 0xB64];
+    unsigned short  field_B94;      /* 0xB94 */
 } Work;
 
 extern int s03d_dword_800DC2FC;
@@ -54,9 +61,37 @@ void s03d_800D2D68(Work *work)
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D31F4.s")
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D327C.s")
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D3310.s")
-#pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D3390.s")
+int s03d_800D3390(Work *work)
+{
+    work->field_AD8 |= 0x20000;
+    if (work->field_AD0 == 0)
+    {
+        work->field_AD8 |= 0x80000;
+    }
+    else if (work->field_AD0 == 8)
+    {
+        return 1;
+    }
+    work->field_AE6 = work->field_B94;
+    work->field_AD0++;
+    return 0;
+}
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D33E8.s")
-#pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D34AC.s")
+int s03d_800D34AC(Work *work)
+{
+    work->field_AD8 |= 0x10000;
+    if (work->field_AD0 == 0)
+    {
+        work->field_AD8 |= 0x200000;
+    }
+    else if (work->field_B6 != 0)
+    {
+        return 1;
+    }
+    work->field_AE6 = work->field_B94;
+    work->field_AD0++;
+    return 0;
+}
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D3508.s")
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D3570.s")
 #pragma INCLUDE_ASM("asm/overlays/s03d/s03d_800D367C.s")
