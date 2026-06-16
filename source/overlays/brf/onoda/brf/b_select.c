1a5eddd8788393bec72ac4db47fb7a16f87b9b71
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-16 03:23:39 +0200

    brf: match brf_800C8128

--- source/overlays/brf/onoda/brf/b_select.c
+++ source/overlays/brf/onoda/brf/b_select.c
@@ -477,7 +477,11 @@ void brf_800C80E8(Work *work)
     brf_800C7FF0(work);
 }
 #pragma INCLUDE_ASM("asm/overlays/brf/brf_800C8108.s")
-#pragma INCLUDE_ASM("asm/overlays/brf/brf_800C8128.s")
+void brf_800C8128(Work *work)
+{
+    brf_800C7F20(work);
+    brf_800C7F9C(work);
+}
 #pragma INCLUDE_ASM("asm/overlays/brf/brf_800C8154.s")
 #pragma INCLUDE_ASM("asm/overlays/brf/brf_800C8180.s")
 #pragma INCLUDE_ASM("asm/overlays/brf/brf_800C81AC.s")
