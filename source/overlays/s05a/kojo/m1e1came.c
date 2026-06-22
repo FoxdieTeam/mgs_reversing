9451ba9ae071fe809019d0cec72e8ddb9dee2296
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 08:43:28 +0200

    Match s05a_800E1A68

--- source/overlays/s05a/kojo/m1e1came.c
+++ source/overlays/s05a/kojo/m1e1came.c
@@ -1,8 +1,17 @@
 #include "game/game.h"
 
+extern GM_CameraSystemWork GM_Camera;
+extern int s05a_dword_800C38CC;
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1134.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1448.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1A68.s")
+
+void s05a_800E1A68(void)
+{
+    GM_Camera.flag &= ~2;
+    GM_SetCameraCallbackFunc(1, NULL);
+    s05a_dword_800C38CC = 0;
+}
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1AA8.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1C14.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800E1D7C.s")
