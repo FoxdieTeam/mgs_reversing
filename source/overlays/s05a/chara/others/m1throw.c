0a05b6edfc84f59f91b9c53e9421dcc1b4811e4a
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 08:42:31 +0200

    Match s05a_800D4654

--- source/overlays/s05a/chara/others/m1throw.c
+++ source/overlays/s05a/chara/others/m1throw.c
@@ -1,6 +1,19 @@
 #include "game/game.h"
 
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D4654.s")
+void s05a_800D4654(MOTION_SEGMENT *dst, MOTION_SEGMENT *src, int mask)
+{
+    int i;
+
+    for (i = 0; i < 16; i++, dst++, src++)
+    {
+        if (mask & 1)
+        {
+            dst->base = src->base;
+        }
+        mask >>= 1;
+    }
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D46A4.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D4968.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D4998.s")
