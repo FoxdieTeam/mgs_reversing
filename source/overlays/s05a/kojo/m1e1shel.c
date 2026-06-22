4b16d40febe2d075dd6b09dd418620ad5e122cf7
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 11:43:32 +0200

    Match s05a_800DBED0

--- source/overlays/s05a/kojo/m1e1shel.c
+++ source/overlays/s05a/kojo/m1e1shel.c
@@ -14,6 +14,17 @@ typedef struct _Casing
     DG_PRIM *field_24;  /* 0x24 */
 } Casing;
 
+typedef struct _Smoke
+{
+    GV_ACT  actor;     /* 0x00 */
+    int     field_20;  /* 0x20 */
+    SVECTOR field_24;  /* 0x24 */
+    SVECTOR field_2C;  /* 0x2C */
+    int     field_34;  /* 0x34 */
+} Smoke;
+
+extern void *s05a_800DBF58(int arg0, SVECTOR *arg1, SVECTOR *arg2);
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DAE58.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB278.s")
 
@@ -37,7 +48,27 @@ void s05a_800DBC44(Casing *work)
 
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBC80.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBD14.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBED0.s")
+
+void s05a_800DBED0(Smoke *work)
+{
+    int n;
+
+    GM_CurrentMap = work->field_20;
+    n = work->field_34 + 1;
+    work->field_34 = n;
+    if (n == 1)
+    {
+        s05a_800DBF58(1, &work->field_24, &work->field_2C);
+    }
+    else if (n == 2)
+    {
+        sub_8007913C();
+    }
+    else if (n >= 30)
+    {
+        GV_DestroyActor(work);
+    }
+}
 
 void s05a_800DBF50(void *work)
 {
