f42c0f906b155c7dd9b50dc30bcb16e7e1b56d0b
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 08:42:44 +0200

    Match s05a_800D4968

--- source/overlays/s05a/chara/others/m1throw.c
+++ source/overlays/s05a/chara/others/m1throw.c
@@ -1,5 +1,12 @@
 #include "game/game.h"
 
+typedef struct _Work
+{
+    GV_ACT  actor;   /* 0x000 */
+    CONTROL control;  /* 0x020 */
+    OBJECT  body;     /* 0x09C */
+} Work;
+
 void s05a_800D4654(MOTION_SEGMENT *dst, MOTION_SEGMENT *src, int mask)
 {
     int i;
@@ -15,6 +22,12 @@ void s05a_800D4654(MOTION_SEGMENT *dst, MOTION_SEGMENT *src, int mask)
 }
 
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D46A4.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D4968.s")
+
+void s05a_800D4968(Work *work)
+{
+    GM_FreeControl(&work->control);
+    GM_FreeObject(&work->body);
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D4998.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800D49F8.s")
