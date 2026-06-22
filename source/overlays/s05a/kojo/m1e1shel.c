c986215bc7c50ab32cc185915f76b581ff616c48
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 08:43:46 +0200

    Match s05a_800DB654

--- source/overlays/s05a/kojo/m1e1shel.c
+++ source/overlays/s05a/kojo/m1e1shel.c
@@ -1,8 +1,21 @@
 #include "game/game.h"
 
+typedef struct _Work
+{
+    GV_ACT  actor;   /* 0x000 */
+    CONTROL control;  /* 0x020 */
+    OBJECT  body;     /* 0x09C */
+} Work;
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DAE58.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB278.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB654.s")
+
+void s05a_800DB654(Work *work)
+{
+    GM_FreeControl(&work->control);
+    GM_FreeObject(&work->body);
+}
+
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB684.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBA94.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBC44.s")
