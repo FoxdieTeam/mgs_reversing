b39f6a3099d570c9ddb3c1d9c1e026dec2bcba42
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-22 23:50:55 +0200

    Match s05a_800DBA94

--- source/overlays/s05a/kojo/m1e1shel.c
+++ source/overlays/s05a/kojo/m1e1shel.c
@@ -12,6 +12,11 @@ typedef struct _Casing
     GV_ACT   actor;     /* 0x00 */
     int      field_20;  /* 0x20 */
     DG_PRIM *field_24;  /* 0x24 */
+    char     pad_28[0x68 - 0x28];
+    int      field_68;  /* 0x68 */
+    int      field_6C;  /* 0x6C */
+    int      field_70;  /* 0x70 */
+    int      field_74;  /* 0x74 */
 } Casing;
 
 typedef struct _Smoke
@@ -34,7 +39,35 @@ void s05a_800DB654(Work *work)
     GM_FreeObject(&work->body);
 }
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB684.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBA94.s")
+
+void s05a_800DBA94(Casing *work)
+{
+    GM_CurrentMap = work->field_20;
+    work->field_68++;
+    work->field_6C -= 25;
+    if (work->field_6C < 0)
+    {
+        work->field_6C = 0;
+    }
+    work->field_70 -= 22;
+    if (work->field_70 < 0)
+    {
+        work->field_70 = 0;
+    }
+    work->field_74 -= 19;
+    if (work->field_74 < 0)
+    {
+        work->field_74 = 0;
+    }
+    setRGB0((POLY_FT4 *)work->field_24->packs[0], work->field_6C, work->field_70, work->field_74);
+    setRGB0((POLY_FT4 *)work->field_24->packs[0] + 1, work->field_6C, work->field_70, work->field_74);
+    setRGB0((POLY_FT4 *)work->field_24->packs[1], work->field_6C, work->field_70, work->field_74);
+    setRGB0((POLY_FT4 *)work->field_24->packs[1] + 1, work->field_6C, work->field_70, work->field_74);
+    if (work->field_68 >= 11)
+    {
+        GV_DestroyActor(work);
+    }
+}
 
 void s05a_800DBC44(Casing *work)
 {
