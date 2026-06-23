5909a277b65d765bc2837343a707fcd10cf1ef76
Matteo Piazzolla <matteo.piazzolla@gmail.com>
2026-06-23 09:26:06 +0200

    Match s05a_800DBD14

--- source/overlays/s05a/kojo/m1e1shel.c
+++ source/overlays/s05a/kojo/m1e1shel.c
@@ -26,9 +26,14 @@ typedef struct _Smoke
     SVECTOR field_24;  /* 0x24 */
     SVECTOR field_2C;  /* 0x2C */
     int     field_34;  /* 0x34 */
+    TARGET  target;    /* 0x38 */
 } Smoke;
 
+extern const char s05a_dword_800E34D4[]; /* = "m1e1shel.c" */
+
 extern void *s05a_800DBF58(int arg0, SVECTOR *arg1, SVECTOR *arg2);
+void s05a_800DBED0(Smoke *work);
+void s05a_800DBF50(void *work);
 
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DAE58.s")
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DB278.s")
@@ -80,7 +85,43 @@ void s05a_800DBC44(Casing *work)
 }
 
 #pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBC80.s")
-#pragma INCLUDE_ASM("asm/overlays/s05a/s05a_800DBD14.s")
+
+void *s05a_800DBD14(SVECTOR *arg0, SVECTOR *arg1)
+{
+    Smoke *work;
+    SVECTOR force;
+    SVECTOR size;
+
+    memset(&force, 0, 8);
+    work = GV_NewActor(GV_ACTOR_USER, sizeof(Smoke));
+    if (work == NULL)
+    {
+        return NULL;
+    }
+
+    GV_SetNamedActor(work, s05a_800DBED0, s05a_800DBF50, s05a_dword_800E34D4);
+    work->field_34 = 0;
+    work->field_20 = GM_CurrentMap;
+    work->field_24 = *arg0;
+    work->field_2C = *arg1;
+
+    setVector(&size, 0x3E8, 0x3E8, 0x3E8);
+    force.vx = 0x64;
+    GM_SetTarget(&work->target, 4, 0, &size);
+    GM_SetPowerTarget(&work->target, 7, 2, 5, 2, &force);
+    GM_MoveTarget(&work->target, &work->field_24);
+    GM_PowerTarget(&work->target);
+
+    setVector(&size, 0x7D0, 0x7D0, 0x7D0);
+    force.vx = 0x32;
+    GM_SetTarget(&work->target, 4, 0, &size);
+    GM_SetPowerTarget(&work->target, 7, 2, 1, 3, &force);
+    GM_MoveTarget(&work->target, &work->field_24);
+    GM_PowerTarget(&work->target);
+
+    sub_800790E8();
+    return work;
+}
 
 void s05a_800DBED0(Smoke *work)
 {
