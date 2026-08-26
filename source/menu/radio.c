84486650dbc76d6385fea32f815eae02b2e2818a
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-08-26 00:49:36 -0400

    Restore OG symbols DG_BackGroundNormal/Black

--- source/menu/radio.c
+++ source/menu/radio.c
@@ -1510,7 +1510,7 @@ STATIC void menu_radio_update_80042198(MenuWork *work, u_long *ot)
                 MENU_JimakuClear();
                 GV_PauseLevel |= GV_PAUSE_STOP;
                 DG_StopMainChanlSystem();
-                DG_ClearBackGroundColor();
+                DG_BackGroundBlack();
                 GV_SetPacketTempMemory();
                 menu_radio_update_helper5_80042160(work);
                 gCodecFadingStep = -32;
@@ -1623,7 +1623,7 @@ STATIC void menu_radio_update_80042198(MenuWork *work, u_long *ot)
             }
             DG_ChangeReso(0);
             DG_UnDrawFrameCount = 3;
-            DG_RestoreBackGroundColor();
+            DG_BackGroundNormal();
             DG_FrameRate = dword_800ABB1C;
             printf("EXIT MUSENKI\n");
             return;
