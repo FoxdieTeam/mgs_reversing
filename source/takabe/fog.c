2a109973225e9d0223d3d2b1970fe11054fb45f1
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-09-10 23:55:01 -0400

    Cleanup takabe/fog.c

--- source/takabe/fog.c
+++ source/takabe/fog.c
@@ -4,6 +4,18 @@
 #include "game/game.h"
 #include "takabe/thing.h"
 
+extern GM_CameraSystemWork GM_Camera;
+
+extern void s12c_800D497C(int, int);
+extern void s12c_800D4AB4(int);
+
+extern void FogSortChanl_800D4E98(DG_CHANL *chanl, int idx);
+extern void FogBoundChanl_800D5500(DG_CHANL *chanl, int idx);
+extern void FogTransChanl_800D63B0(DG_CHANL *chanl, int idx);
+extern void FogShadeChanl_800D6A04(DG_CHANL *chanl, int idx);
+
+/*---------------------------------------------------------------------------*/
+
 typedef struct _Work
 {
     GV_ACT       actor;
@@ -18,24 +30,16 @@ typedef struct _Work
     int          f40;
 } Work;
 
+/*---------------------------------------------------------------------------*/
+
 int SECTION(".bss") s12c_800DA428;
 int SECTION(".bss") s12c_800DA42C;
 int SECTION(".bss") s12c_800DA430;
 int SECTION(".bss") s12c_800DA434;
 
-extern GM_CameraSystemWork GM_Camera;
+/*---------------------------------------------------------------------------*/
 
-void s12c_800D497C(int, int);
-void s12c_800D4AB4(int);
-
-void FogSortChanl_800D4E98(DG_CHANL *chanl, int idx);
-void FogBoundChanl_800D5500(DG_CHANL *chanl, int idx);
-void FogTransChanl_800D63B0(DG_CHANL *chanl, int idx);
-void FogShadeChanl_800D6A04(DG_CHANL *chanl, int idx);
-
-#define EXEC_LEVEL GV_ACTOR_PREV2
-
-void FogAct_800D4074(Work *work)
+static void Act(Work *work)
 {
     int scale;
 
@@ -46,7 +50,7 @@ void FogAct_800D4074(Work *work)
     s12c_800D4AB4(scale);
 }
 
-void FogDie_800D40E0(Work *work)
+static void Die(Work *work)
 {
     DG_SetChanlSystemUnits(DG_BOUND_CHANL, work->old_bound);
     DG_SetChanlSystemUnits(DG_TRANS_CHANL, work->old_trans);
@@ -54,7 +58,7 @@ void FogDie_800D40E0(Work *work)
     DG_SetChanlSystemUnits(DG_SORT_CHANL,  work->old_sort);
 }
 
-int FogGetResources_800D4130(Work *work, int name, int map)
+static int GetResources(Work *work, int name, int map)
 {
     int temp_s1;
     int temp_s2;
@@ -81,16 +85,18 @@ int FogGetResources_800D4130(Work *work, int name, int map)
     return 0;
 }
 
-void *NewFog(int arg0, int arg1)
+/*---------------------------------------------------------------------------*/
+
+void *NewFog(int name, int map)
 {
     Work *work;
 
-    work = GV_NewActor(EXEC_LEVEL, sizeof(Work));
+    work = GV_NewActor(GV_ACTOR_PREV2, sizeof(Work));
     if (work != NULL)
     {
-        GV_SetNamedActor(&work->actor, FogAct_800D4074, FogDie_800D40E0, "fog.c");
+        GV_SetNamedActor(&work->actor, Act, Die, "fog.c");
 
-        if (FogGetResources_800D4130(work, arg0, arg1) < 0)
+        if (GetResources(work, name, map) < 0)
         {
             GV_DestroyActor(&work->actor);
             return NULL;
