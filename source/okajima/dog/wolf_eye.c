5b59cc8ca88dbfb8fa7e590f2a4ec452a84daf5c
Kieron <11843278+KieronJ@users.noreply.github.com>
2026-09-15 22:48:09 +0100

    Tidy wolf_eye.c

--- source/okajima/dog/wolf_eye.c
+++ source/okajima/dog/wolf_eye.c
@@ -1,20 +1,18 @@
-#include "mgstype.h"
-#include "libgv/libgv.h"
-#include "libdg/libdg.h"
 #include "game/game.h"
 
-typedef struct _Work
-{
+/*---------------------------------------------------------------------------*/
+
+typedef struct _Work {
     GV_ACT   actor;
     int      map;
-    SVECTOR  verts_right[4];
-    SVECTOR  verts_left[4];
-    char     pad[0x3E];
-    DG_PRIM *prim_right;
-    DG_PRIM *prim_left;
-    int      timer;
+    SVECTOR  verts_r[ 4 ];
+    SVECTOR  verts_l[ 4 ];
+    char     unused[ 62 ];
+    DG_PRIM *prim_r;
+    DG_PRIM *prim_l;
+    int      time;
     MATRIX  *root;
-    int     *visible;
+    int     *enable;
 } Work;
 
 SVECTOR SECTION(".bss") s12c_800DA418;
@@ -31,137 +29,118 @@ void WolfEye_800D3554()
 {
 }
 
-void WolfEye_800D355C(POLY_FT4 *packs, int n_packs, DG_TEX *tex)
+/*---------------------------------------------------------------------------*/
+
+static void InitPacks( POLY_FT4 *packs, int n_packs, DG_TEX *tex )
 {
-    while (--n_packs >= 0)
+    while ( --n_packs >= 0 )
     {
-        setPolyFT4(packs);
-        setSemiTrans(packs, 1);
-        setRGB0(packs, 255, 0, 0);
-        DG_SetPacketTexture4(packs, tex);
-        packs->tpage |= 0x20;
+        setPolyFT4( packs );
+        setSemiTrans( packs, 1 );
+        setRGB0( packs, 255, 0, 0 );
+        DG_SetPacketTexture4( packs, tex );
+        packs->tpage |= ( 1 << 5 );
         packs++;
     }
 }
 
-void WolfEyeAct_800D35EC(Work *work)
+static void Act( Work *work )
 {
-    GM_CurrentMap = work->map;
+    GM_SetCurrentMap( work->map );
 
-    if (--work->timer < 0)
+    if ( --work->time < 0 )
     {
-        work->timer = GV_RandU(128) + 10;
+        work->time = GV_RandU( 128 ) + 10;
     }
 
-    if (*work->visible == 0 || work->timer < 5)
+    if ( *work->enable == 0 || work->time < 5 )
     {
-        DG_InvisiblePrim(work->prim_right);
-        DG_InvisiblePrim(work->prim_left);
+        DG_InvisiblePrim( work->prim_r );
+        DG_InvisiblePrim( work->prim_l );
     }
     else
     {
-        DG_VisiblePrim(work->prim_right);
-        DG_VisiblePrim(work->prim_left);
+        DG_VisiblePrim( work->prim_r );
+        DG_VisiblePrim( work->prim_l );
     }
 }
 
-void WolfEyeDie_800D36C0(Work *work)
+static void Die( Work *work )
 {
-    GM_FreePrim(work->prim_right);
-    GM_FreePrim(work->prim_left);
+    GM_FreePrim( work->prim_r );
+    GM_FreePrim( work->prim_l );
 }
 
-int WolfEyeGetResources_800D3728(Work *work, MATRIX *root, int *visible)
+static int GetResources( Work *work, MATRIX *root, int *enable )
 {
-    DG_TEX  *tex;
+    DG_TEX *tex;
     DG_PRIM *prim;
 
     work->root = root;
     work->map = GM_CurrentMap;
 
-    tex = DG_GetTexture(GV_StrCode("wolf_eye_r"));
-
-    prim = GM_MakePrim(DG_PRIM_ONESIDE | DG_PRIM_POLY_FT4, 1, work->verts_right, NULL);
-    work->prim_right = prim;
-    if (prim == NULL)
-    {
-        return -1;
-    }
-
-    prim->raise = 300;
+    tex = DG_GetTexture( GV_StrCode( "wolf_eye_r" ) );
+    work->prim_r = prim = GM_MakePrim( DG_PRIM_ONESIDE | DG_PRIM_POLY_FT4, 1, work->verts_r, NULL );
+    if ( prim == NULL ) return -1;
+    DG_RaisePrim( prim, 300 );
     prim->root = root;
 
-    WolfEye_800D355C(prim->packs[0], 1, tex);
-    WolfEye_800D355C(prim->packs[1], 1, tex);
+    InitPacks( prim->packs[ 0 ], 1, tex );
+    InitPacks( prim->packs[ 1 ], 1, tex );
 
-    tex = DG_GetTexture(GV_StrCode("wolf_eye_l"));
-
-    prim = GM_MakePrim(DG_PRIM_ONESIDE | DG_PRIM_POLY_FT4, 1, work->verts_left, NULL);
-    work->prim_left = prim;
-    if (prim == NULL)
-    {
-        return -1;
-    }
-
-    prim->raise = 300;
+    tex = DG_GetTexture( GV_StrCode( "wolf_eye_l" ) );
+    work->prim_l = prim = GM_MakePrim( DG_PRIM_ONESIDE | DG_PRIM_POLY_FT4, 1, work->verts_l, NULL );
+    if ( prim == NULL ) return -1;
+    DG_RaisePrim( prim, 300 );
     prim->root = root;
 
-    WolfEye_800D355C(prim->packs[0], 1, tex);
-    WolfEye_800D355C(prim->packs[1], 1, tex);
+    InitPacks( prim->packs[ 0 ], 1, tex );
+    InitPacks( prim->packs[ 1 ], 1, tex );
 
-    work->verts_right[0].vx = -55;
-    work->verts_right[0].vy = 50;
-    work->verts_right[0].vz = 160;
+    work->verts_r[ 0 ].vx = -55;
+    work->verts_r[ 0 ].vy = 50;
+    work->verts_r[ 0 ].vz = 160;
+    work->verts_r[ 1 ].vx = -30;
+    work->verts_r[ 1 ].vy = 45;
+    work->verts_r[ 1 ].vz = 170;
+    work->verts_r[ 2 ].vx = -55;
+    work->verts_r[ 2 ].vy = 10;
+    work->verts_r[ 2 ].vz = 160;
+    work->verts_r[ 3 ].vx = -30;
+    work->verts_r[ 3 ].vy = 5;
+    work->verts_r[ 3 ].vz = 170;
 
-    work->verts_right[1].vx = -30;
-    work->verts_right[1].vy = 45;
-    work->verts_right[1].vz = 170;
+    work->verts_l[ 0 ].vx = 30;
+    work->verts_l[ 0 ].vy = 45;
+    work->verts_l[ 0 ].vz = 170;
+    work->verts_l[ 1 ].vx = 55;
+    work->verts_l[ 1 ].vy = 50;
+    work->verts_l[ 1 ].vz = 160;
+    work->verts_l[ 2 ].vx = 30;
+    work->verts_l[ 2 ].vy = 5;
+    work->verts_l[ 2 ].vz = 170;
+    work->verts_l[ 3 ].vx = 55;
+    work->verts_l[ 3 ].vy = 10;
+    work->verts_l[ 3 ].vz = 160;
 
-    work->verts_right[2].vx = -55;
-    work->verts_right[2].vy = 10;
-    work->verts_right[2].vz = 160;
-
-    work->verts_right[3].vx = -30;
-    work->verts_right[3].vy = 5;
-    work->verts_right[3].vz = 170;
-
-    work->verts_left[0].vx = 30;
-    work->verts_left[0].vy = 45;
-    work->verts_left[0].vz = 170;
-
-    work->verts_left[1].vx = 55;
-    work->verts_left[1].vy = 50;
-    work->verts_left[1].vz = 160;
-
-    work->verts_left[2].vx = 30;
-    work->verts_left[2].vy = 5;
-    work->verts_left[2].vz = 170;
-
-    work->verts_left[3].vx = 55;
-    work->verts_left[3].vy = 10;
-    work->verts_left[3].vz = 160;
-
-    work->visible = visible;
-    work->timer = 0;
-
+    work->enable = enable;
+    work->time = 0;
     return 0;
 }
 
-void *NewWolfEye_800D3930(MATRIX *root, int *visible)
+void *NewWolfEye( MATRIX *root, int *enable )
 {
     Work *work;
 
-    work = GV_NewActor(GV_ACTOR_USER, sizeof(Work));
-    if (work != NULL)
+    work = GV_NewActor( GV_ACTOR_USER, sizeof(Work) );
+    if ( work != NULL )
     {
-        GV_SetNamedActor(&work->actor, WolfEyeAct_800D35EC, WolfEyeDie_800D36C0, "wolf_eye.c");
-
-        if (WolfEyeGetResources_800D3728(work, root, visible) < 0)
+        GV_SetNamedActor( work, Act, Die, "wolf_eye.c" );
+        if ( GetResources( work, root, enable ) < 0 )
         {
-            GV_DestroyActor(&work->actor);
+            GV_DestroyActor( work );
             return NULL;
         }
     }
-
     return (void *)work;
 }
