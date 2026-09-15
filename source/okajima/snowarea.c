d811f6f06c267dcfe5ca714b2449f0841eaf2c59
Wanted <4wantedthing@hotmail.com>
2026-09-15 18:21:54 +0200

    continue s09a

---
+++ source/okajima/snowarea.c
@@ -0,0 +1,16 @@
+
+
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2BA8.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2C38.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2CE8.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2DC4.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2E8C.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D2FDC.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D3074.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D30C8.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D31C4.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D322C.s")
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D3378.s")
+
+// https://decomp.me/scratch/vcWI4
+#pragma INCLUDE_ASM("asm/overlays/s09a/s09a_800D347C.s")
