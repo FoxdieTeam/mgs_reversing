13bff6c6300a8cf60c08c651f907fd3c787775f9
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-09-23 00:32:29 -0400

    Cleanup anime effect funcs/data

--- source/anime/effect/rcm2.c
+++ source/anime/effect/rcm2.c
@@ -1,17 +1,14 @@
 #include "mgstype.h"
-#include "libdg/libdg.h"
+#include "game/game.h"
 #include "anime/animconv/anime.h"
 #include "strcode.h"
 
 /*---------------------------------------------------------------------------*/
 
 static const char anim_data_800D13CC[] = {
-    0x00,0x23,0x02,0x00, 0x07,0x00,0x1a,0x02,
-    0x00,0x01,0x08,0x6a, 0x6a,0x6a,0x0c,0x00,
-    0x0a,0x08,0xf6,0xf6, 0xf6,0x02,0x00,0x01,
-    0x0d,0x0f,0x08,0x6a, 0x6a,0x6a,0x0c,0x00,
-    0x05,0x08,0xec,0xec, 0xec,0x02,0x00,0x01,
-    0x0d,0x0f
+    0x00,0x23,0x02,0x00, 0x07,0x00,0x1a,0x02, 0x00,0x01,0x08,0x6a, 0x6a,0x6a,0x0c,0x00,
+    0x0a,0x08,0xf6,0xf6, 0xf6,0x02,0x00,0x01, 0x0d,0x0f,0x08,0x6a, 0x6a,0x6a,0x0c,0x00,
+    0x05,0x08,0xec,0xec, 0xec,0x02,0x00,0x01, 0x0d,0x0f
 };
 
 static ANIMATION anm_800C32F0 = {
@@ -47,7 +44,7 @@ static ANIMATION anm_800C330C = {
 
 void s08c_800C4194(MATRIX *pos)
 {
-    PRESCRIPT prescript;
+    PRESCRIPT pre;
     MATRIX    mat;
     SVECTOR   svec;
     SVECTOR  *pos2, *speed;
@@ -56,7 +53,7 @@ void s08c_800C4194(MATRIX *pos)
     svec.vy = 18;
     svec.vz = 130;
 
-    speed = &prescript.speed;
+    speed = &pre.speed;
     speed->vx = 0;
     speed->vy = 0;
     speed->vz = 0;
@@ -64,21 +61,21 @@ void s08c_800C4194(MATRIX *pos)
     DG_MovePos(&svec);
     ReadRotMatrix(&mat);
 
-    pos2 = &prescript.pos;
+    pos2 = &pre.pos;
     pos2->vx = mat.t[0];
     pos2->vy = mat.t[1];
     pos2->vz = mat.t[2];
 
-    prescript.scr_num = 0;
-    prescript.s_anim = 0;
+    pre.scr_num = 0;
+    pre.s_anim = 0;
 
-    anm_800C32F0.pre_script = &prescript;
+    anm_800C32F0.pre_script = &pre;
     NewAnime(NULL, NULL, &anm_800C32F0);
 }
 
 void s08c_800C4220(MATRIX *pos)
 {
-    PRESCRIPT prescript;
+    PRESCRIPT pre;
     MATRIX    mat;
     SVECTOR   svec;
     SVECTOR  *pos2, *speed;
@@ -87,7 +84,7 @@ void s08c_800C4220(MATRIX *pos)
     svec.vy = 18;
     svec.vz = 130;
 
-    speed = &prescript.speed;
+    speed = &pre.speed;
     speed->vx = 0;
     speed->vy = 0;
     speed->vz = 0;
@@ -95,41 +92,41 @@ void s08c_800C4220(MATRIX *pos)
     DG_MovePos(&svec);
     ReadRotMatrix(&mat);
 
-    pos2 = &prescript.pos;
+    pos2 = &pre.pos;
     pos2->vx = mat.t[0];
     pos2->vy = mat.t[1];
     pos2->vz = mat.t[2];
 
-    prescript.scr_num = 1;
-    prescript.s_anim = 0;
+    pre.scr_num = 1;
+    pre.s_anim = 0;
 
-    anm_800C32F0.pre_script = &prescript;
+    anm_800C32F0.pre_script = &pre;
     NewAnime(NULL, NULL, &anm_800C32F0);
 }
 
 void s08c_800C42B0(SVECTOR *pos)
 {
-    PRESCRIPT prescript;
+    PRESCRIPT pre;
     MATRIX    mat;
     SVECTOR   svec;
     SVECTOR  *speed;
 
-    prescript.pos.vx = pos->vx;
-    prescript.pos.vy = pos->vy;
-    prescript.pos.vz = pos->vz;
+    pre.pos.vx = pos->vx;
+    pre.pos.vy = pos->vy;
+    pre.pos.vz = pos->vz;
 
     svec.vx = 0;
     svec.vy = 18;
     svec.vz = 130;
 
-    speed = &prescript.speed;
+    speed = &pre.speed;
     speed->vx = 0;
     speed->vy = 0;
     speed->vz = 0;
 
-    prescript.scr_num = 0;
-    prescript.s_anim = 0;
+    pre.scr_num = 0;
+    pre.s_anim = 0;
 
-    anm_800C330C.pre_script = &prescript;
+    anm_800C330C.pre_script = &pre;
     NewAnime(NULL, NULL, &anm_800C330C);
 }
